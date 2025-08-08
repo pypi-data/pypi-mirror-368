07409f962ccb2e772065e93d039a55d78acf5a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:46 +0000

    Add package odoo14-addon-ssi-odoo-implementation/14.0.4.10.0/odoo14_addon_ssi_odoo_implementation-14.0.4.10.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-odoo-implementation/odoo14_addon_ssi_odoo_implementation-14.0.4.10.0-py3-none-any.whl/odoo/addons/ssi_odoo_implementation/models/__init__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    odoo_version,
+    odoo_feature_category,
+    odoo_feature,
+    odoo_environment,
+    odoo_implementation,
+    odoo_implementation_update,
+    odoo_deployment,
+    odoo_deployment_new_feature,
+    odoo_feature_implementation,
+    odoo_feature_issue,
+    odoo_use_case,
+    odoo_module,
+    odoo_website_theme,
+    odoo_additional_functionality,
+    odoo_feature_additional_functionality,
+)
