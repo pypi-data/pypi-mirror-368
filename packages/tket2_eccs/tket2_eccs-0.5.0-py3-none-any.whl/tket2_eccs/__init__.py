0c0ea5d3cfaf87597791b6f51eedc9309a73a855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:55 +0000

    Add package tket2_eccs/0.5.0/tket2_eccs-0.5.0-py3-none-any.whl

---
+++ packages/tket2_eccs/tket2_eccs-0.5.0-py3-none-any.whl/tket2_eccs/__init__.py
@@ -0,0 +1,16 @@
+"""`tket2-eccs` is a Python package containing compiler rewrite sets used for `tket2`'s optimisation passes.
+
+| ⚠️ WARNING                  |
+|:----------------------------|
+| This package has been renamed to `tket-eccs`. Please update your dependencies accordingly |
+
+This package is intended to be used as an internal dependency for `tket2`.
+"""
+
+# This is updated by our release-please workflow, triggered by this
+# annotation: x-release-please-version
+__version__ = "0.5.0"
+
+raise RuntimeError(
+    "The `tket2-eccs` package has been renamed to `tket-eccs`. Please update your dependencies accordingly."
+)
