6e039bc08562a0a7105ed9c3d72f79af1972ea1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 15:04:58 +0000

    Add package highlander-enum/0.9.1/highlander_enum-0.9.1-py3-none-any.whl

---
+++ packages/highlander-enum/highlander_enum-0.9.1-py3-none-any.whl/highlander/__init__.py
@@ -0,0 +1,6 @@
+from .enums import ExFlag, OptionsFlag
+from .type import ConflictResolution, EnumPlusType
+
+RHS, LHS, STRICT = ConflictResolution
+
+__all__ = ["LHS", "RHS", "STRICT", "EnumPlusType", "ExFlag", "OptionsFlag"]
