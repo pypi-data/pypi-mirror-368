3a2eeebd36a723fdc0fa706f39daba7e1de46e71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 15:05:15 +0000

    Add package midas-civil/0.2.2/midas_civil-0.2.2.tar.gz

---
+++ packages/midas-civil/midas_civil-0.2.2.tar.gz/midas_civil-0.2.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+with open('README.md','r') as f:
+    description = f.read()
+
+
+setup(name='midas_civil',
+    version='0.2.2',
+    description='Python library for MIDAS Civil NX',
+    author='Sumit Shekhar',
+    author_email='sumit.midasit@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'polars',
+        'xlsxwriter',
+        'requests'
+    ],          
+    long_description= description,
+    long_description_content_type='text/markdown'
+    )
