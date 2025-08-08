e52b8cd78390e447bd70ee6db09cc85a5cc82810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:20 +0000

    Add package aircraftdetective/0.0.1/aircraftdetective-0.0.1.tar.gz

---
+++ packages/aircraftdetective/aircraftdetective-0.0.1.tar.gz/aircraftdetective-0.0.1/README.md
@@ -0,0 +1,3 @@
+# AircraftDetective
+
+A Python package for estimating the efficiency of commercial aircraft.
---
+++ packages/aircraftdetective/aircraftdetective-0.0.1.tar.gz/aircraftdetective-0.0.1/src/aircraftdetective/__init__.py
@@ -0,0 +1,10 @@
+# https://pint.readthedocs.io/en/stable/getting/pint-in-your-projects.html#having-a-shared-registry
+from pint import UnitRegistry
+ureg = UnitRegistry()
+import pint_pandas
+
+__all__ = (
+    "__version__",
+)
+
+__version__ = "0.0.1"
