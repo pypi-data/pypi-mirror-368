67d7479f6c9704228727eb9c59f3cba81940b600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 17:04:10 +0000

    Add package pyobjcryst/2025.1.0/pyobjcryst-2025.1.0-cp311-cp311-macosx_10_13_x86_64.whl

---
+++ packages/pyobjcryst/pyobjcryst-2025.1.0-cp311-cp311-macosx_10_13_x86_64.whl/pyobjcryst/lsq.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# pyobjcryst
+#
+# File coded by:    Vincent Favre-Nicolin
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+"""Python wrapping of LSQNumObj.h.
+
+See the online ObjCryst++ documentation (https://objcryst.readthedocs.io).
+
+Changes from ObjCryst::LSQNumObj::
+        In development !
+"""
+
+__all__ = ["LSQ"]
+
+from pyobjcryst._pyobjcryst import LSQ
---
+++ packages/pyobjcryst/pyobjcryst-2025.1.0-cp311-cp311-macosx_10_13_x86_64.whl/pyobjcryst/zscatterer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# pyobjcryst        by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2009 The Trustees of Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Chris Farrow
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE_DANSE.txt for license information.
+#
+##############################################################################
+"""Python wrapping of Zscatterer.
+
+See the online ObjCryst++ documentation (https://objcryst.readthedocs.io).
+
+Changes from ObjCryst::ZAtom
+- XMLOutput and Input are not wrapped.
+
+Changes from ObjCryst++
+- XMLOutput and Input are not wrapped.
+"""
+
+__all__ = [
+    "ZScatterer",
+    "ZAtom",
+    "ZPolyhedron",
+    "RegularPolyhedraType",
+    "GlobalScatteringPower",
+]
+
+from urllib.request import urlopen
+
+from pyobjcryst._pyobjcryst import (
+    GlobalScatteringPower,
+    RegularPolyhedraType,
+    ZAtom,
+    ZPolyhedron,
+)
+from pyobjcryst._pyobjcryst import ZScatterer as ZScatterer_orig
+
+
+class ZScatterer(ZScatterer_orig):
+
+    def ImportFenskeHallZMatrix(self, src, named=False):
+        """Import atoms from a Fenske-Hall z-matrix :param src: either a
+        python filed (opened in 'rb' mode), or.
+
+            a filename, or an url ("http://...") to a text file with the z-matrix
+        :param named: if True, allows to read a named Z-matrix - the formatting
+            is similar to a Fenske-Hall z-matrix but only relies on spaces between the
+            different fields instead of a strict number of characters.
+        """
+        if isinstance(src, str):
+            if len(src) > 4:
+                if src[:4].lower() == "http":
+                    return super().ImportFenskeHallZMatrix(urlopen(src), named)
+            with open(src, "rb") as fhz:  # Make sure file object is closed
+                super().ImportFenskeHallZMatrix(fhz, named)
+        else:
+            super().ImportFenskeHallZMatrix(src, named)
