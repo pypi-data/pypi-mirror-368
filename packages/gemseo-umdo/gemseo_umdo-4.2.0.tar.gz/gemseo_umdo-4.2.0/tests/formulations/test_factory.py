f8e9661bfd118b61cf76d55c6a5ec56d2a78fabd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:25 +0000

    Add package gemseo-umdo/4.2.0/gemseo_umdo-4.2.0.tar.gz

---
+++ packages/gemseo-umdo/gemseo_umdo-4.2.0.tar.gz/gemseo_umdo-4.2.0/tests/formulations/test_factory.py
@@ -0,0 +1,42 @@
+# Copyright 2021 IRT Saint Exupéry, https://www.irt-saintexupery.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+from __future__ import annotations
+
+import pytest
+from gemseo.formulations.factory import MDOFormulationFactory
+
+from gemseo_umdo.formulations.factory import UMDOFormulationsFactory
+
+
+@pytest.fixture
+def factory() -> UMDOFormulationsFactory:
+    """The factory of BaseUMDOFormulation."""
+    return UMDOFormulationsFactory()
+
+
+def test_inheritance(factory):
+    """Check that the factory is also a MDOFormulationsFactory."""
+    assert isinstance(factory, MDOFormulationFactory)
+
+
+def test_mdo_formulation(factory):
+    """Check that UMDOFormulationsFactory does not contain MDO formulations."""
+    mdo_formulations = MDOFormulationFactory().class_names
+    assert not set(mdo_formulations).intersection(set(factory.class_names))
+
+
+def test_u_formulation(factory):
+    """Check the method is_available of the factory."""
+    assert factory.is_available("Sampling")
