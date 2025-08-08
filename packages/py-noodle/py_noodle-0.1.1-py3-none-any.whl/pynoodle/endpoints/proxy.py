6653ec979a6f0b7d1107fd66884ba23863dc6b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:05:04 +0000

    Add package py-noodle/0.1.1/py_noodle-0.1.1-py3-none-any.whl

---
+++ packages/py-noodle/py_noodle-0.1.1-py3-none-any.whl/pynoodle/endpoints/proxy.py
@@ -0,0 +1,92 @@
+import asyncio
+import logging
+import c_two as cc
+from typing import Literal
+from fastapi import APIRouter, HTTPException, Body, Response
+
+from ..noodle import noodle
+from ..scene.lock import RWLock
+from ..schemas.lock import LockInfo
+
+router = APIRouter()
+logger = logging.getLogger(__name__)
+
+@router.get('/', response_model=LockInfo)
+async def activate_node(node_key: str, icrm_tag: str, lock_type: Literal['r', 'w'], timeout: float | None = None, retry_interval: float = 1.0):
+    """
+    Activates a node in the Noodle system.
+    """
+    try:
+        # Try to get node information
+        node_info = noodle.get_node_info(node_key)
+        if not node_info:
+            raise HTTPException(status_code=404, detail=f'Node {node_key} not found')
+        
+        # Validate the ICRM tag
+        node_icrm_tag = noodle.scenario.get_icrm_tag(node_info.scenario_node_name)
+        if node_icrm_tag != icrm_tag:
+            raise HTTPException(status_code=404, detail=f'ICRM tag "{icrm_tag}" not match node "{node_key}", expected "{node_icrm_tag}"')
+
+        # Get the node (mock the icrm class not provided)
+        node = noodle.get_node(None, node_key, 'p' + lock_type, timeout, retry_interval)
+
+        # Acquire the lock for the node asynchronously
+        lock = node._lock
+        await lock.async_acquire()
+        
+        # Launch the node CRM server at process level
+        node.launch_crm_server()
+        server_address = noodle.node_server_address(node_key, lock.id, 'p')
+
+        # Spin up the CRM server, asynchronously wait for it to be ready
+        count = 0
+        while cc.rpc.Client.ping(server_address, 0.1) is False:
+            if (timeout is not None) and count >= timeout * 10:
+                raise TimeoutError(f'CRM server "{node._node_key}" did not start in time')
+            await asyncio.sleep(0.1)
+            count += 1
+        
+        return LockInfo(lock_id=lock.id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Error activating node {node_key}: {e}')
+
+@router.post('/')
+async def proxy_node(node_key: str, lock_id: str, timeout: float | None = None, body: bytes=Body(..., description='C-Two Event Message in Bytes')):
+    """
+    Proxies a C-Two event message to the specified node CRM server in process level.
+    """
+    try:
+        # Check if the lock exists
+        if not RWLock.has_lock(lock_id):
+            raise HTTPException(status_code=404, detail=f'Lock {lock_id} not found for node {node_key}')
+        
+        server_address = noodle.node_server_address(node_key, lock_id, 'p')
+        
+        # Relay the message to the node's CRM server asynchronously
+        timeout = timeout if timeout is not None else -1.0
+        res = await cc.rpc.routing(server_address, body, timeout)
+        return Response(res, media_type='application/octet-stream')
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Error proxying node {node_key}: {e}')
+
+@router.delete('/')
+def deactivate_node(node_key: str, lock_id: str):
+    """
+    Deactivates a node in the Noodle system.
+    """
+    try:
+        # Check if the lock exists
+        if not RWLock.has_lock(lock_id):
+            raise HTTPException(status_code=404, detail=f'Lock {lock_id} not found for node {node_key}')
+        
+        server_address = noodle.node_server_address(node_key, lock_id, 'p')
+        
+        # Shutdown the node's CRM server
+        cc.rpc.Client.shutdown(server_address, -1.0)
+        
+        # Remove the local lock
+        RWLock.remove_lock(lock_id)
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Error deactivating node {node_key}: {e}')
---
+++ packages/py-noodle/py_noodle-0.1.1-py3-none-any.whl/pynoodle/scenario.py
@@ -0,0 +1,161 @@
+import yaml
+import logging
+import threading
+from pathlib import Path
+from fastapi import APIRouter
+from dataclasses import dataclass
+from typing import TypeVar, Type, Callable
+
+from .config import settings
+from .schemas.scenario import ScenarioConfiguration
+
+T = TypeVar('T')
+logger = logging.getLogger(__name__)
+
+@dataclass
+class RawScenarioNode:
+    CRM: Type[T] = None
+    ICRM: Type[T] = None
+    MOUNT: Callable[[str], None] = lambda x: None
+    UNMOUNT: Callable[[str], None] = lambda x: None
+    PARAM_CONVERTER: Callable[[str, dict | None], dict | None] = lambda x, y: y
+    ENDPOINT: APIRouter | None = None
+    
+    def __post_init__(self):
+        if not self.ICRM:
+            raise ImportError(f'ICRM class not found in module {self.module}')
+        if not self.CRM:
+            self.CRM = self.ICRM
+
+@dataclass
+class ScenarioNode:
+    name: str
+    module: str
+    dependencies: list['ScenarioNode']
+    
+    _lock: threading.Lock = threading.Lock()
+    
+    _crm_class: Type[T] = None
+    _icrm_class: Type[T] = None
+    _endpoint: APIRouter = None
+    _mount: Callable[[str], None] = None
+    _unmount: Callable[[str], None] = None
+    _params_converter: Callable[[str, dict | None], dict | None] = None
+    
+    def _load_from_module(self):
+        m = __import__(self.module, fromlist=[''])
+        raw: RawScenarioNode = getattr(m, 'RAW', None)
+        if not raw:
+            raise ImportError(f'RawScenarioNode class not found in module {self.module}')
+        
+        self._mount = raw.MOUNT
+        self._crm_class = raw.CRM
+        self._icrm_class = raw.ICRM
+        self._unmount = raw.UNMOUNT
+        self._endpoint = raw.ENDPOINT
+        self._params_converter = raw.PARAM_CONVERTER
+    
+    @property
+    def crm_class(self) -> Type[T]:
+        with self._lock:
+            if self._crm_class is None:
+                self._load_from_module()
+            return self._crm_class
+    
+    @property
+    def icrm_class(self) -> Type[T]:
+        with self._lock:
+            if self._icrm_class is None:
+                self._load_from_module()
+            return self._icrm_class
+    
+    @property
+    def mount(self) -> Callable[[str], None]:
+        with self._lock:
+            if self._mount is None:
+                self._load_from_module()
+            return self._mount
+    
+    @property
+    def unmount(self) -> Callable[[str], None]:
+        with self._lock:
+            if self._unmount is None:
+                self._load_from_module()
+            return self._unmount
+        
+    @property
+    def params_converter(self) -> Callable[[str, dict | None], dict | None]:
+        with self._lock:
+            if self._params_converter is None:
+                self._load_from_module()
+            return self._params_converter
+
+    @property
+    def endpoint(self) -> APIRouter | None:
+        with self._lock:
+            if self._endpoint is None:
+                self._load_from_module()
+            return self._endpoint
+    
+    @property
+    def icrm_tag(self) -> str:
+        cls = self.icrm_class
+        
+        name = cls.__name__
+        version = cls.__version__
+        namespace = cls.__namespace__
+        
+        if not namespace or not name or not version:
+            raise ValueError(f'ICRM class {cls.__name__} is missing namespace, name, or version attributes.')
+        
+        return f'{namespace}/{name}/{version}'
+
+class Scenario:
+    def __init__(self):
+        # Read configuration
+        configuration_path = Path(settings.NOODLE_CONFIG_PATH)
+        if not configuration_path.is_absolute():
+            configuration_path = Path.cwd() / configuration_path
+        with open(configuration_path, 'r') as f:
+            config_data = yaml.safe_load(f)
+            
+        # Parse scenario graph
+        config = ScenarioConfiguration(**config_data)
+        
+        self.graph: dict[str, ScenarioNode] = {}
+        
+        # - Firstly, create all nodes
+        module_prefix = f'{config.module_prefix}.' if config.module_prefix else ''
+        module_postfix = f'{config.module_postfix}' if config.module_postfix else ''
+        for node_description in config.scenario_nodes:
+            node = ScenarioNode(
+                name=node_description.name,
+                module=module_prefix + node_description.name + module_postfix,
+                dependencies=[]
+            )
+            self.graph[node_description.name] = node
+
+        # - Secondly, resolve dependencies
+        for node_description in config.scenario_nodes:
+            node = self.graph[node_description.name]
+            if node_description.dependencies:
+                node.dependencies = [
+                    self.graph[dep_name]
+                    for dep_name in node_description.dependencies
+                ]
+    
+    def __getitem__(self, scenario_node_name: str) -> ScenarioNode | None:
+        if scenario_node_name not in self.graph:
+            return None
+        return self.graph[scenario_node_name]
+    
+    def __iter__(self):
+        return iter(self.graph.values())
+
+    def get_icrm_tag(self, scenario_node_name: str) -> str | None:
+        node = self.graph.get(scenario_node_name)
+        if not node:
+            return None
+        return node.icrm_tag
+
+scenario_graph = Scenario()
