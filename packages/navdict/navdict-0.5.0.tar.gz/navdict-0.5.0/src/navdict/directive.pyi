e203af818d2bd6b0cb35d2494ba71f451969ca1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 17:04:20 +0000

    Add package navdict/0.5.0/navdict-0.5.0.tar.gz

---
+++ packages/navdict/navdict-0.5.0.tar.gz/navdict-0.5.0/src/navdict/directive.pyi
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from importlib.metadata import EntryPoint
+from typing import Callable
+
+@dataclass
+class Directive:
+    ep: EntryPoint
+
+    @property
+    def name(self) -> str: ...
+    @property
+    def func(self) -> Callable: ...
+
+def is_directive(value: str) -> bool: ...
+def get_directive_plugin(name: str) -> Directive | None: ...
+def unravel_directive(value: str) -> tuple[str, str]: ...
