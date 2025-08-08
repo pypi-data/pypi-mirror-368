2a57fb8d8631800217cef5dc9c6113da84d707b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:23 +0000

    Add package bgp-data-interface/0.8.1/bgp_data_interface-0.8.1-py3-none-any.whl

---
+++ packages/bgp-data-interface/bgp_data_interface-0.8.1-py3-none-any.whl/bgp_data_interface/s3_bucket/S3Bucket.py
@@ -0,0 +1,123 @@
+from bgp_data_interface.utils import location
+import boto3
+from botocore.exceptions import ClientError
+from datetime import datetime
+from io import StringIO
+import os
+import pandas as pd
+
+
+OUTPUT_DIR = 'outputs'
+DEFAULT_SITE = location.BBO
+DEFAULT_SOURCE = 'openmeteo'
+DEFAULT_TYPE = 'forecast'
+
+
+class S3Bucket:
+
+    # bucket: Bucket
+
+
+    def __init__(self, access_key: str, secret_key: str, bucket_name: str) -> None:
+        session = boto3.Session(
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key
+        )
+        s3 = session.resource('s3')
+        self.bucket = s3.Bucket(bucket_name)
+
+
+
+    def retrieve(self, params: dict) -> pd.DataFrame:
+        site = params.get('location', DEFAULT_SITE)
+        type = params.get('type', DEFAULT_TYPE)
+        source = params.get('source', DEFAULT_SOURCE)
+
+        today = pd.Timestamp.now().strftime("%Y-%m-%d")
+        start_date = datetime.strptime(params.get('start_date', today), "%Y-%m-%d")
+        end_date = datetime.strptime(params.get('end_date', today), "%Y-%m-%d")
+
+        self._download_files(site, type, start_date, end_date, source)
+        df = self._combine_files(site, type, start_date, end_date, source)
+    
+        return df
+
+
+
+    def _download_files(self,
+            location: str,
+            type: str,
+            start_date: datetime,
+            end_date: datetime,
+            source: str) -> None:
+        
+        for date in pd.date_range(start_date, end_date):
+            folder = f"{location}/{type}/{date.year}/"
+            filename = date.strftime("%Y%m%d") + f"_{source}.csv"
+
+            os.makedirs(os.path.dirname(f"{OUTPUT_DIR}/{folder}"), exist_ok=True)
+            s3_file_key = folder + filename
+            local_file_path = f"{OUTPUT_DIR}/{s3_file_key}"
+
+            if not os.path.exists(local_file_path):
+                self._download_one_file(s3_file_key, local_file_path)
+
+
+    def _download_one_file(self, s3_key: str, file_path: str) -> None:
+        try:
+            self.bucket.download_file(s3_key, file_path)
+        except ClientError as ce:
+            status_code = ce.response['Error']['Code']
+            print(status_code, end=' ', flush=True)
+
+    def _combine_files(self,
+            location: str,
+            type: str,
+            start_date: datetime,
+            end_date: datetime,
+            source: str) -> pd.DataFrame:
+
+        file_list = []
+        for date in pd.date_range(start_date, end_date):
+            folder = f"{OUTPUT_DIR}/{location}/{type}/{date.year}/"
+            filename = date.strftime("%Y%m%d") + f"_{source}.csv"
+
+            file_path = folder + filename
+            if not os.path.exists(file_path):
+                continue
+
+            file_list.append(file_path)
+
+        if not file_list:
+            return pd.DataFrame()
+
+        df = pd.concat([pd.read_csv(file) for file in file_list], ignore_index=True)
+        df['date_time'] = pd.to_datetime(df['date_time'])
+
+        return df
+
+
+
+
+
+    def store(self, data: pd.DataFrame, key: str) -> None:
+        csv_buffer = StringIO()
+        data.to_csv(csv_buffer, index=False)
+
+        self.bucket.put_object(Key=key, Body=csv_buffer.getvalue())
+
+
+
+
+    def object_exists(self, key: str) -> bool:
+        try:
+            self.bucket.Object(key).load()
+            return True
+        except ClientError as e:
+            return False
+
+
+
+
+    def delete_object(self, key: str) -> None:
+        self.bucket.Object(key).delete()
