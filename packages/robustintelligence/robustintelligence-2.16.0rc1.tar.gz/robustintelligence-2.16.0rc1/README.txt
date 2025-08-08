5fe61978f04a52069e88ca89b5fd4a5454647b4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:39 +0000

    Add package robustintelligence/2.16.0rc1/robustintelligence-2.16.0rc1.tar.gz

---
+++ packages/robustintelligence/robustintelligence-2.16.0rc1.tar.gz/robustintelligence-2.16.0rc1/README.txt
@@ -0,0 +1,1 @@
+This is the Robust Intelligence REST API SDK. It provides tools and utilities to interact with the Robust Intelligence platform.
---
+++ packages/robustintelligence/robustintelligence-2.16.0rc1.tar.gz/robustintelligence-2.16.0rc1/ri/apiclient/api/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+# import apis into api package
+from ri.apiclient.api.agent_manager_api import AgentManagerApi
+from ri.apiclient.api.customer_managed_key_service_api import CustomerManagedKeyServiceApi
+from ri.apiclient.api.data_collector_api import DataCollectorApi
+from ri.apiclient.api.detection_api import DetectionApi
+from ri.apiclient.api.feature_flag_api import FeatureFlagApi
+from ri.apiclient.api.file_scanning_api import FileScanningApi
+from ri.apiclient.api.file_upload_api import FileUploadApi
+from ri.apiclient.api.firewall_service_api import FirewallServiceApi
+from ri.apiclient.api.generative_validation_api import GenerativeValidationApi
+from ri.apiclient.api.image_registry_api import ImageRegistryApi
+from ri.apiclient.api.integration_service_api import IntegrationServiceApi
+from ri.apiclient.api.job_reader_api import JobReaderApi
+from ri.apiclient.api.model_card_service_api import ModelCardServiceApi
+from ri.apiclient.api.model_testing_api import ModelTestingApi
+from ri.apiclient.api.monitor_service_api import MonitorServiceApi
+from ri.apiclient.api.notification_setting_api import NotificationSettingApi
+from ri.apiclient.api.project_service_api import ProjectServiceApi
+from ri.apiclient.api.rime_info_api import RIMEInfoApi
+from ri.apiclient.api.registry_service_api import RegistryServiceApi
+from ri.apiclient.api.results_reader_api import ResultsReaderApi
+from ri.apiclient.api.schedule_service_api import ScheduleServiceApi
+from ri.apiclient.api.security_db_api import SecurityDBApi
+from ri.apiclient.api.user_api import UserApi
+from ri.apiclient.api.workspace_service_api import WorkspaceServiceApi
