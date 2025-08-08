67955b4077883615d293607e7f42364af3ec0c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:20 +0000

    Add package facu-weather-flow/0.1.2/facu_weather_flow-0.1.2.tar.gz

---
+++ packages/facu-weather-flow/facu_weather_flow-0.1.2.tar.gz/facu_weather_flow-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='facu-weather-flow',
+    version='0.1.2',
+    description='ETL para extraer, transformar y cargar datos del clima en S3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Walter Facundo Vega',
+    author_email='facundo.vega1234@gmail.com',
+    url='https://github.com/facuvegaingenieer/weather-flow.git',
+    packages=find_packages(include=["facu_weather_flow", "facu_weather_flow.*"]),
+    install_requires=[
+        'pandas',
+        'boto3',
+        'pyarrow',
+        'python-dotenv',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'weather-flow = facu_weather_flow.main:main',
+        ],
+    },
+    python_requires='>=3.8',
+)
