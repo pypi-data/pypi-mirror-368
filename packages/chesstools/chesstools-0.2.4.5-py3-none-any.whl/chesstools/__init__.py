960d45d1f2ae10f0bd40a159a6701f605627ebbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:05 +0000

    Add package chesstools/0.2.4.5/chesstools-0.2.4.5-py3-none-any.whl

---
+++ packages/chesstools/chesstools-0.2.4.5-py3-none-any.whl/chesstools/__init__.py
@@ -0,0 +1,24 @@
+__version__ = "0.2.4.5"
+
+try:
+    import pyjion
+    if input("you have pyjion. it's probs not faster. use it anyway? [N/y] ").lower() == "y":
+        pyjion.enable()
+        print("pyjioning!!!")
+    else:
+        print("no pyjion speedup (will probs be faster anyway)")
+except:
+    try:
+        import psyco
+        psyco.full()
+        print("initializing psyco - wow, you must have python <2.7! how?")
+    except:
+        print("pyjion (py3.10+) or psyco (py<2.7) _may_ help your bot run faster, but you should really just use pypy")
+
+COLORS = {'white':'black','black':'white'}
+
+from . import board, move, list, timer, piece, ai, book
+from .board import Board
+from .move import Move
+from .list import List
+from .timer import Timer, TimeLockTimer
