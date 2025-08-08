8a6363a859acb6531f636cdc0ddaffa7f27eaf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 15:04:35 +0000

    Add package otlmow-model/2.16.1.3/otlmow_model-2.16.1.3-py3-none-any.whl

---
+++ packages/otlmow-model/otlmow_model-2.16.1.3-py3-none-any.whl/otlmow_model/OtlmowModel/Datatypes/KlPLCModelnaam.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstField import KeuzelijstField
+from otlmow_model.OtlmowModel.BaseClasses.KeuzelijstWaarde import KeuzelijstWaarde
+
+
+# Generated with OTLEnumerationCreator. To modify: extend, do not edit
+class KlPLCModelnaam(KeuzelijstField):
+    """De modelnaam van de PLC."""
+    naam = 'KlPLCModelnaam'
+    label = 'PLC model'
+    objectUri = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#KlPLCModelnaam'
+    definition = 'De modelnaam van de PLC.'
+    status = 'ingebruik'
+    codelist = 'https://wegenenverkeer.data.vlaanderen.be/id/conceptscheme/KlPLCModelnaam'
+    options = {
+        'axc-f-2152': KeuzelijstWaarde(invulwaarde='axc-f-2152',
+                                       label='AXC-F-2152',
+                                       status='ingebruik',
+                                       definitie='AXC-F-2152',
+                                       objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlPLCModelnaam/axc-f-2152'),
+        'axc-f-3152': KeuzelijstWaarde(invulwaarde='axc-f-3152',
+                                       label='AXC-F-3152',
+                                       status='ingebruik',
+                                       definitie='AXC-F-3152',
+                                       objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlPLCModelnaam/axc-f-3152'),
+        'dmu703': KeuzelijstWaarde(invulwaarde='dmu703',
+                                   label='DMU703',
+                                   status='ingebruik',
+                                   definitie='DMU703',
+                                   objectUri='https://wegenenverkeer.data.vlaanderen.be/id/concept/KlPLCModelnaam/dmu703')
+    }
+
+    @classmethod
+    def create_dummy_data(cls):
+        return cls.create_dummy_data_keuzelijst(cls.options)
+
