b421876820e7456971cf96a4996b3732461914c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 17:04:22 +0000

    Add package imgui-builder/0.1.2/imgui_builder-0.1.2-py3-none-any.whl

---
+++ packages/imgui-builder/imgui_builder-0.1.2-py3-none-any.whl/imgui_builder/constructors/__init__.py
@@ -0,0 +1,3 @@
+from ._VertDivision import _VertDivision
+from ._HorDivision import _HorDivision
+from ._Table import Table
---
+++ packages/imgui-builder/imgui_builder-0.1.2-py3-none-any.whl/imgui_builder/constructors/__init__.pyi
@@ -0,0 +1,3 @@
+from ._HorDivision import _HorDivision as _HorDivision
+from ._Table import Table as Table
+from ._VertDivision import _VertDivision as _VertDivision
