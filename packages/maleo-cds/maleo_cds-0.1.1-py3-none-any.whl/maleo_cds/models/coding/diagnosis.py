86203cd27d5e8a0d161f3272f7b573434d7092d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:07 +0000

    Add package maleo-cds/0.1.1/maleo_cds-0.1.1-py3-none-any.whl

---
+++ packages/maleo-cds/maleo_cds-0.1.1-py3-none-any.whl/maleo_cds/models/coding/diagnosis.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text
+from maleo_soma.models.table import DataTable
+from maleo_cds.db import MaleoCDSBase
+
+
+class DiagnosesMixin:
+    organization_id = Column(name="organization_id", type_=Integer)
+    user_id = Column(name="user_id", type_=Integer, nullable=False)
+    code = Column(name="code", type_=String(20), unique=True, nullable=False)
+    description = Column(name="description", type_=Text)
+    reasoning = Column(name="reasoning", type_=Text)
+
+
+class DiagnosesTable(DiagnosesMixin, DataTable, MaleoCDSBase):
+    __tablename__ = "diagnoses"
---
+++ packages/maleo-cds/maleo_cds-0.1.1-py3-none-any.whl/maleo_cds/types/data/coding/procedure.py
@@ -0,0 +1,9 @@
+from typing import List, Optional
+from maleo_cds.schemas.data.coding.procedure import ProcedureDataSchema
+
+
+# Exapanded procedures
+ExpandedProcedures = ProcedureDataSchema
+OptionalExpandedProcedures = Optional[ExpandedProcedures]
+ListOfExpandedProcedures = List[ExpandedProcedures]
+OptionalListOfExpandedProcedures = Optional[List[ExpandedProcedures]]
