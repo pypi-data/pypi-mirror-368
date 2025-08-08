f08493ce9af3037006704c23c18650fa6895893d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:43 +0000

    Add package rpa-temu/1.0.3/rpa_temu-1.0.3.tar.gz

---
+++ packages/rpa-temu/rpa_temu-1.0.3.tar.gz/rpa_temu-1.0.3/rpa_temu/api/shop/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+from .AmountApi import AmountApi
+from .BillDetailApi import BillDetailApi
+from .DeliveryLabelApi import DeliveryLabelApi
+from .RefundLabelApi import RefundLabelApi
+from .ShopApi import ShopApi
