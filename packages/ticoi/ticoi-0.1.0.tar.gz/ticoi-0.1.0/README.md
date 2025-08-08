f3dfca04e0eb16e79fd2b51d99753df2f6d4c583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:07 +0000

    Add package ticoi/0.1.0/ticoi-0.1.0.tar.gz

---
+++ packages/ticoi/ticoi-0.1.0.tar.gz/ticoi-0.1.0/README.md
@@ -0,0 +1,111 @@
+# TICOI
+
+[![Language](https://img.shields.io/badge/python-3.10%2B-blue.svg?style=flat-square)](https://www.python.org/)
+[![Python test](https://github.com/ticoi/ticoi/actions/workflows/python-app.yml/badge.svg?branch=main)](https://github.com/ticoi/ticoi/actions/workflows/python-app.yml)
+[![License](https://img.shields.io/badge/license-GPLv3+-blue.svg?style=flat-square)](https://github.com/ticoi/ticoi/blob/main/LICENSE)
+[![Hatch project](https://img.shields.io/badge/%F0%9F%A5%9A-Hatch-4051b5.svg)](https://github.com/pypa/hatch)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+
+TICOI is a tool to postprocess surface velocity time series estimated from remote sensing (e.g., ice flow, landslides).
+The method is based on the temporal closure principle. It fuses velocity measurements which are multi-temporal (with
+different temporal baselines) and multi-sensor (from different satellite images),
+and may have been computed by different processing chains. It takes as input NetCDF files containing the image-pair
+velocities, that you may have generated yourself, or natively supports data from the [NASA ITS_LIVE project](https://its-live.jpl.nasa.gov/) or from
+[Millan et al. (2022)](https://www.theia-land.fr/en/blog/product/glacier-surface-flow-velocity/).
+
+The package is based on the methodological developments published in:
+
+- Charrier, L., Dehecq, A., Guo, L., Brun, F., Millan, R., Lioret, N., ... & Halas, P. (2025). TICOI: an operational
+  Python package to generate regular glacier velocity time series. EGUsphere, 2025, 1-40.
+
+- Charrier, L., Yan, Y., Koeniguer, E. C., Leinss, S., & Trouvé, E. (2021). Extraction of velocity time series with an
+  optimal temporal sampling from displacement observation networks. IEEE Transactions on Geoscience and Remote Sensing,
+  60, 1-10.
+
+The main principle of TICOI relies on the temporal closure of the displacement measurement network.
+Measured displacements with different temporal baselines are expressed as linear combinations of estimated
+displacement (see the Figure below).
+The aim is to take advantage of different types of information (displacement measured using different temporal
+baselines,
+on images from different types of satellite) to extract glacier velocity time series, with a given temporal sampling.
+This enable the
+harmonization of various datasets, and the creation of standardized sub-annual velocity products.
+
+<p align="center">
+  <img src="examples/image/Temporal_closure.png" alt="Temporal_closure" width="800"/>
+</p>
+
+## INSTALLATION
+
+### With `mamba`
+
+Clone the git repo and create a `mamba` environment (see how to install `mamba` in
+the [mamba documentation](https://mamba.readthedocs.io/en/latest/)):
+
+```bash
+mamba env create -f environment.yml  # Add '-n custom_name' if you want.
+mamba install -c conda-forge ticoi
+```
+
+### With `pip`
+
+```bash
+pip install ticoi
+```
+
+## TUTORIALS
+
+### Basic examples
+
+**- notebook**
+
+* [How to process one pixel of a NetCDF file](examples/basic/notebook/pixel_demo_local_ncdata.ipynb)
+* [How to process one pixel of ITS_LIVE data, stored on a cloud](examples/basic/notebook/pixel_demo_its_live_on_cloud.ipynb)
+
+**- python_script**
+
+* [How to process one cube](examples/basic/python_script/cube_ticoi_demo.py)
+* [How to process one pixel](examples/basic/python_script/pixel_ticoi_demo.py)
+
+### Advanced examples
+
+* [How to process one ITS_LIVE cube directly from the cloud](/examples/advanced/cube_ticoi_demo_its_live.py)
+* [How to format several geotiff files into a netCDF file](examples/advanced/cube_prep_from_geotiff.py)
+* [How to apply GLAFT on TICOI results](examples/advanced/glaft_for_ticoi_results.py)
+* [How to compute statistics in static areas](examples/advanced/stats_in_static_areas.py)
+
+## TO USE YOUR OWN DATASET
+
+### You have geotiff files
+
+You need to convert them into netcdf, by
+modifying [this script](examples/advanced/cube_prep_from_geotiff.py).
+
+### You have netcdf files
+
+If it is [ITS_LIVE data]((https://its-live.jpl.nasa.gov/)), or [Millan et al., 2022](https://www.theia-land.fr/en/blog/product/glacier-surface-flow-velocity/), you can directly use them!
+If not, you have to create your own dataloader, within which the dimension should be ("mid_date", "y", "x"), and the
+variables should be "vx", "vy", and should contain the projection information in the ds.proj4 attribute. You can add in this [file](src/ticoi/cube_data_classxr.py).
+
+## HYPERPARAMETERS AND OUTPUTS
+
+* to understand the output of pixel_demo please
+  check [README_output](README_output.md)
+* to understand the parameters you can change, please
+  check [README_possible_parameters](README_possible_parameters.md)
+
+## TO CONTRIBUTE
+
+We welcome any contribution to this package! See guidelines [here](CONTRIBUTING.md).
+
+[packaging guide]: https://packaging.python.org
+
+[distribution tutorial]: https://packaging.python.org/tutorials/packaging-projects/
+
+[src]: https://github.com/pypa/sampleproject
+
+[rst]: http://docutils.sourceforge.net/rst.html
+
+[md]: https://tools.ietf.org/html/rfc7764#section-3.5 "CommonMark variant"
+
+[md use]: https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
---
+++ packages/ticoi/ticoi-0.1.0.tar.gz/ticoi-0.1.0/tests/test_inversion_functions.py
@@ -0,0 +1,194 @@
+import numpy as np
+import pytest
+
+from ticoi.core import mu_regularisation
+from ticoi.inversion_functions import (
+    construction_a_lf,
+    construction_dates_range_np,
+    inversion_one_component,
+)
+
+
+class Test_inversion:
+    @pytest.fixture(autouse=True)
+    def setup_method(self):
+        # This method will run before each test
+        self.dates = np.array(
+            [
+                ["2013-03-14", "2013-03-30"],
+                ["2013-03-14", "2013-03-30"],
+                ["2013-03-14", "2013-04-15"],
+                ["2013-03-30", "2013-04-15"],
+                ["2013-03-30", "2013-04-15"],
+                ["2013-03-14", "2013-08-13"],
+                ["2013-03-14", "2013-10-16"],
+                ["2013-06-19", "2013-07-13"],
+                ["2013-03-14", "2013-10-24"],
+                ["2013-03-14", "2013-11-01"],
+            ]
+        ).astype("datetime64[D]")
+
+        self.dates_range = np.array(
+            [
+                "2013-03-14",
+                "2013-03-30",
+                "2013-04-15",
+                "2013-06-19",
+                "2013-07-13",
+                "2013-08-13",
+                "2013-10-16",
+                "2013-10-24",
+                "2013-11-01",
+            ]
+        ).astype("datetime64[D]")
+
+        self.A = np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0],
+                [1, 1, 1, 1, 1, 1, 1, 0],
+                [1, 1, 1, 1, 1, 1, 1, 1],
+            ]
+        )
+
+        self.data = np.array(
+            [
+                [-0.69107729, -8.73340321],
+                [2.40452456, -13.41930866],
+                [-3.96273065, -9.17936611],
+                [3.73120785, -14.85955429],
+                [-2.19656491, -9.20514107],
+                [10.38781738, -28.12755966],
+                [3.23966694, -17.77642059],
+                [368.12982178, -118.80034637],
+                [1.13138795, -11.47720432],
+                [2.95655584, -19.49642754],
+            ]
+        )
+
+        self.mu1accelnotnull = np.array(
+            [
+                [-0.0625, 0.0625, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, -0.0625, 0.01538462, 0.0, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, -0.01538462, 0.04166667, 0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, -0.04166667, 0.03225806, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, -0.03225806, 0.015625, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, -0.015625, 0.125, 0.0],
+                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.125, 0.125],
+            ]
+        ).astype("float32")
+
+    def test_construct_dates_range(self):
+        """Test construction of Dates_range for a small subset of values"""
+        expected_dates_range = self.dates_range
+        result = construction_dates_range_np(self.dates)
+        np.testing.assert_array_equal(result, expected_dates_range)
+
+    def test_construction_a_lf(self):
+        """Test construction of A for a small subset of values"""
+
+        expected = self.A
+        actual = construction_a_lf(self.dates, self.dates_range)
+        np.testing.assert_array_equal(actual, expected, err_msg="Construction A LP does not give the correct result")
+
+    @pytest.mark.parametrize(
+        "regu, expected",
+        [
+            (
+                1,
+                np.array(
+                    [
+                        [-0.0625, 0.0625, 0, 0, 0, 0, 0, 0],
+                        [0, -0.0625, 0.01538462, 0, 0, 0, 0, 0],
+                        [0, 0, -0.01538462, 0.04166667, 0, 0, 0, 0],
+                        [0, 0, 0, -0.04166667, 0.03225806, 0, 0, 0],
+                        [0, 0, 0, 0, -0.03225806, 0.015625, 0, 0],
+                        [0, 0, 0, 0, 0, -0.015625, 0.125, 0],
+                        [0, 0, 0, 0, 0, 0, -0.125, 0.125],
+                    ]
+                ).astype("float32"),
+            ),  # Shortened for brevity
+            (
+                2,
+                np.array(
+                    [
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0625, -0.125, 0.01538462, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0625, -0.03076923, 0.04166667, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.01538462, -0.08333333, 0.03225806, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.04166667, -0.06451613, 0.015625, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.03225806, -0.03125, 0.125, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.015625, -0.25, 0.125],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                    ]
+                ).astype("float32"),
+            ),
+            (
+                "1accelnotnull",
+                np.array(
+                    [
+                        [-0.0625, 0.0625, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, -0.0625, 0.01538462, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, -0.01538462, 0.04166667, 0.0, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, -0.04166667, 0.03225806, 0.0, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, -0.03225806, 0.015625, 0.0, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, -0.015625, 0.125, 0.0],
+                        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.125, 0.125],
+                    ]
+                ).astype("float32"),
+            ),
+        ],
+    )
+    def test_mu_regularization(self, regu, expected):
+        """Test construction of mu for a three different regularization"""
+        actual = mu_regularisation(regu, self.A, self.dates_range)
+        np.testing.assert_allclose(
+            actual,
+            expected,
+            rtol=1e-6,
+            atol=1,
+            err_msg=f"mu_regularisation does not give the correct result for regu={regu}",
+        )
+
+    @pytest.mark.parametrize(
+        "solver, expected, ini",
+        [
+            (
+                "LSMR",
+                np.array(
+                    [-7.578118, -8.461816, 108.818729, -118.183104, -2.871212, 10.768036, 5.558181, -7.348526]
+                ).astype("float64"),
+                None,
+            ),
+            (
+                "LS",
+                np.array(
+                    [-7.5791097, -8.460544, 113.54745, -118.18973, -7.585056, 10.759924, 5.560593, -7.3482523]
+                ).astype("float64"),
+                None,
+            ),
+            # (
+            #     "LSMR_ini",
+            #     np.array(
+            #         [[  -7.576902,   -8.461586,  106.760633, -118.179535,   -0.81826 ,10.772076,    5.557501,   -7.34847 ]]
+            #     ).astype("float64"),
+            #     np.array([-7, -8.0, 100.0, -110.0, -7.0, 10.0, 5.0, -10.0]).astype("float64"),
+            # ),
+        ],
+    )
+    def test_inversion_one_component(self, solver, expected, ini):
+        actual = inversion_one_component(
+            self.A, self.dates_range, 1, self.data, solver=solver, Weight=1, mu=self.mu1accelnotnull, ini=ini
+        )[0]
+        np.testing.assert_allclose(
+            actual,
+            expected,
+            rtol=1e-6,
+            atol=1,
+        )
