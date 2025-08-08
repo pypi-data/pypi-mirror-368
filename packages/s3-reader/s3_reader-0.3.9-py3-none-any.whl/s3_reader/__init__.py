c66f14d008e602bbab7b35fb72068be1a68e78ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:15 +0000

    Add package s3-reader/0.3.9/s3_reader-0.3.9-py3-none-any.whl

---
+++ packages/s3-reader/s3_reader-0.3.9-py3-none-any.whl/s3_reader/__init__.py
@@ -0,0 +1,12 @@
+from .file import File
+
+__all__ = ['File', '__version__']
+
+
+def __getattr__(name: str) -> str:
+    if name == '__version__':
+        from .version import __version__
+
+        return __version__
+    msg = f'module {__name__} has no attribute {name}'
+    raise AttributeError(msg)
