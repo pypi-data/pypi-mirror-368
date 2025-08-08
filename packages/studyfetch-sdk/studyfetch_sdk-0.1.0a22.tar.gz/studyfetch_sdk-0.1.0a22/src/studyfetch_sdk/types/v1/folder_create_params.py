8510b90dfe963982469ddb3fa0045c36333ce62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:24 +0000

    Add package studyfetch-sdk/0.1.0a22/studyfetch_sdk-0.1.0a22.tar.gz

---
+++ packages/studyfetch-sdk/studyfetch_sdk-0.1.0a22.tar.gz/studyfetch_sdk-0.1.0a22/src/studyfetch_sdk/types/v1/folder_create_params.py
@@ -0,0 +1,17 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing_extensions import Required, Annotated, TypedDict
+
+from ..._utils import PropertyInfo
+
+__all__ = ["FolderCreateParams"]
+
+
+class FolderCreateParams(TypedDict, total=False):
+    name: Required[str]
+    """Folder name"""
+
+    parent_id: Annotated[str, PropertyInfo(alias="parentId")]
+    """Parent folder ID (optional)"""
