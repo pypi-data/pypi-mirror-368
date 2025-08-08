d2db5ae3f87136ce46ed06101362be86a52a19ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 15:05:05 +0000

    Add package libdebug/0.9.0/libdebug-0.9.0.tar.gz

---
+++ packages/libdebug/libdebug-0.9.0.tar.gz/libdebug-0.9.0/README.md
@@ -0,0 +1,227 @@
+![logo](https://github.com/libdebug/libdebug/blob/dev/media/libdebug_header.png?raw=true)
+# libdebug [![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.13151549.svg)](https://doi.org/10.5281/zenodo.13151549)
+
+libdebug is an open source Python library for programmatic debugging of userland binary executables. 
+
+libdebug provides a comprehensive set of building blocks designed to facilitate the development of debugging tools for different purposes, including reverse engineering and exploitation. **Build Your Own Debugger!**
+
+With libdebug you have full control of your debugged executable. With it you can:
+- Access process memory and registers 
+- Control the execution flow of the process
+- Handle and hijack syscalls
+- Catch and hijack signals
+- Interact with stdin, stdout, and stderr of the debugged process
+- Debug multithreaded and multiprocess applications with ease
+- Seamlessly switch to GDB for interactive analysis
+- Debug on Linux systems based on AMD64, AArch64, and i386
+
+When running the same executable multiple times, choosing efficient implementations can make the difference. For this reason, libdebug prioritizes performance.
+
+## Project Links
+Homepage: https://libdebug.org  \
+Documentation: https://docs.libdebug.org
+
+## Installation
+```bash
+python3 -m pip install libdebug
+```
+
+### Install from another branch
+If you want to stay up to date with the most cutting-edge features (and you don't mind being on an unstable branch) you can install from a different branch (e.g., `dev`).
+
+```bash
+python3 -m pip install git+https://github.com/libdebug/libdebug.git@dev
+```
+
+Please visit the [build guide](https://docs.libdebug.org/development/building_libdebug/) for more information on how to manually build libdebug from source.
+
+## Your first script
+
+Now that you have libdebug installed, you can start using it in your scripts. Here is a simple example of how to use libdebug to debug a binary:
+
+```python
+
+from libdebug import debugger
+
+def my_callback(thread, bp) -> None:
+	# This callback will be called when the breakpoint is hit
+	print(f"RDX is {hex(thread.regs.rdx)}")
+	print(f"This is the {bp.hit_count} time the breakpoint was hit")
+
+d = debugger("./test")
+
+# Start the process
+# By default, the process will stop at the entry point
+io = d.run()
+
+my_breakpoint = d.breakpoint("function", hardware=True, file="binary")
+my_callback_breakpoint = d.breakpoint("function2", callback=my_callback, file="binary")
+
+# Continue the execution
+d.cont()
+
+# Interact with the process
+io.sendline(b"Hello world!")
+io.recvuntil(b"libdebug is like sushi")
+
+# Print RAX. This will execute as soon as the process is stopped
+print(f"RAX is {hex(d.regs.rax)}")
+
+# Write to memory
+d.memory[0x10ad, 8, "binary"] = b"Hello!\x00\x00"
+
+# Continue the execution
+d.cont()
+```
+
+The above script will run the binary `test` in the working directory and set two breakpoints: one at the function `function` and another at `function2`. 
+
+The first breakpoint has no callback, so it will just stop the execution and wait for your script to interact with the process. When the process stops at this breakpoint, you can read and write memory, access registers, and so on. In the example, we print the value of the RAX register and write a string to memory. Then, we continue the execution of the process.
+
+The second breakpoint has a callback that will be called when the breakpoint is hit. Inside a callback, you can interact with the process, read and write memory, access registers, and so on. At the end of the callback, libdebug will automatically continue the execution of the process.
+
+There is so much more that can be done with libdebug. Please read the [documentation](https://docs.libdebug.org/) to find out more.
+
+## The cool stuff
+
+libdebug offers many advanced features. Take a look at this script doing magic with signals:
+
+```python
+from libdebug import debugger, libcontext
+
+libcontext.terminal = ['tmux', 'splitw', '-h']
+
+# Define signal catchers
+def catcher_SIGUSR1(thread, catcher) -> None:
+    thread.signal = 0x0
+    print(f"SIGUSR1: Signal number {catcher}")
+
+def catcher_SIGINT(thread, catcher) -> None:
+    print(f"SIGINT: Signal number {catcher}")
+
+def catcher_SIGPIPE(thread, catcher) -> None:
+    print(f"SIGPIPE: Signal number {catcher}")
+
+def handler_geteuid(thread, handler) -> None:
+	thread.regs.rax = 0x0
+
+# Initialize the debugger
+d = debugger('/path/to/executable', continue_to_binary_entrypoint=False, aslr=False)
+
+# Start the process
+io = d.run()
+
+# Register signal catchers
+catcher1 = d.catch_signal("SIGUSR1", callback=catcher_SIGUSR1)
+catcher2 = d.catch_signal("SIGINT", callback=catcher_SIGINT)
+catcher3 = d.catch_signal("SIGPIPE", callback=catcher_SIGPIPE)
+
+# Register signal hijackings
+d.hijack_signal("SIGQUIT", "SIGTERM")
+d.hijack_signal("SIGINT", "SIGPIPE", recursive=True)
+
+# Define which signals to block
+d.signals_to_block = ["SIGPOLL", "SIGIO", "SIGALRM"]
+
+# Register a syscall handler
+d.handle_syscall("geteuid", on_exit=handler_geteuid)
+
+# Register a breakpoint
+bp = d.breakpoint("function", hardware=True, file="binary")
+
+# Continue execution
+d.cont()
+
+# Interact with the process
+io.sendlineafter(b"libdebug is like provola", b"Hello world!")
+
+# Wait for the process to stop
+d.wait()
+
+# Disable the catchers after execution
+catcher1.disable()
+catcher2.disable()
+catcher3.disable()
+
+# Register a new breakpoint
+bp = d.breakpoint(0xdeadc0de, hardware=True)
+
+d.cont()
+d.wait()
+
+d.gdb()
+```
+
+## Auto Interrupt on Command
+libdebug also allows you to make all commands execute as soon as possible, without having to wait for a stopping event. To enable this mode, you can use the `auto_interrupt_on_command=True` 
+
+```python
+from libdebug import debugger
+
+d = debugger("/path/to/executable", auto_interrupt_on_command=True)
+
+io = d.run()
+
+bp = d.breakpoint("function", file="binary")
+
+d.cont()
+
+# Read shortly after the cont is issued
+# The process is forcibly stopped to read the register
+value = d.regs.rax
+print(f"RAX is {hex(value)}")
+
+system_offset = d.symbols.filter("system")[0].start
+libc_base = d.maps.filter("libc")[0].base
+
+system_address = libc_base + system_offset
+
+d.memory[0x12ebe, 8, "libc"] = int.to_bytes(system_address, 8, "little")
+
+d.cont()
+d.wait()
+
+# Here we should be at the breakpoint
+
+# This value is read while the process is stopped at the breakpoint
+ip_value = d.regs.rip
+
+print(f"RIP is {hex(ip_value)}")
+
+d.kill()
+```
+
+## Attribution
+We've published a poster on libdebug. If you use libdebug in your research, you can cite the associated poster paper:
+```bibtex
+@inproceedings{10.1145/3658644.3691391,
+	author = {Digregorio, Gabriele and Bertolini, Roberto Alessandro and Panebianco, Francesco and Polino, Mario},
+	title = {Poster: libdebug, Build Your Own Debugger for a Better (Hello) World},
+	year = {2024},
+	isbn = {9798400706363},
+	publisher = {Association for Computing Machinery},
+	address = {New York, NY, USA},
+	url = {https://doi.org/10.1145/3658644.3691391},
+	doi = {10.1145/3658644.3691391},
+	booktitle = {Proceedings of the 2024 on ACM SIGSAC Conference on Computer and Communications Security},
+	pages = {4976–4978},
+	numpages = {3},
+	keywords = {debugging, reverse engineering, software security},
+	location = {Salt Lake City, UT, USA},
+	series = {CCS '24}
+}
+```
+
+If you intend to use libdebug in your projects, you can also cite the software using the following bibtex:
+```bibtex
+@software{libdebug_2024,
+	title = {libdebug: {Build} {Your} {Own} {Debugger}},
+	copyright = {MIT Licence},
+	url = {https://libdebug.org},
+	publisher = {libdebug.org},
+	author = {Digregorio, Gabriele and Bertolini, Roberto Alessandro and Panebianco, Francesco and Polino, Mario},
+	year = {2024},
+	doi = {10.5281/zenodo.13151549},
+}
+```
+
---
+++ packages/libdebug/libdebug-0.9.0.tar.gz/libdebug-0.9.0/libdebug/architectures/i386/i386_ptrace_register_holder.py
@@ -0,0 +1,283 @@
+#
+# This file is part of libdebug Python library (https://github.com/libdebug/libdebug).
+# Copyright (c) 2024-2025 Roberto Alessandro Bertolini, Gabriele Digregorio. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+from __future__ import annotations
+
+from ctypes import c_longdouble
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from libdebug.architectures.amd64.amd64_ptrace_register_holder import (
+    _get_property_8h,
+    _get_property_8l,
+    _get_property_16,
+    _get_property_32,
+    _get_property_fp_mmx,
+    _get_property_fp_xmm0,
+    _get_property_fp_ymm0,
+    _get_property_fp_zmm0,
+)
+from libdebug.architectures.i386.i386_registers import I386Registers
+from libdebug.ptrace.ptrace_register_holder import PtraceRegisterHolder
+
+if TYPE_CHECKING:
+    from libdebug.state.thread_context import ThreadContext
+
+I386_GP_REGS = ["a", "b", "c", "d"]
+
+I386_BASE_REGS = ["bp", "sp", "si", "di"]
+
+I386_REGS = [
+    "eax",
+    "ebx",
+    "ecx",
+    "edx",
+    "esi",
+    "edi",
+    "ebp",
+    "esp",
+    "eip",
+]
+
+I386_SPECIAL_REGS = [
+    "eflags",
+    "cs",
+    "ss",
+    "ds",
+    "es",
+    "fs",
+    "gs",
+]
+
+
+# We cannot recycle this function from the amd64 version because the c_longdouble returns a 12-byte array
+# instead of a 16-byte array, which the nanobind binding does not expect.
+# We need to reimplement it here.
+def _get_property_fp_st(name: str, index: int) -> property:
+    # We should be able to expose the long double member from nanobind directly
+    # But their support for long double does not actually allow for value comparison or manipulation
+    # So, ctypes it is
+    def getter(self: I386Registers) -> float:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        return c_longdouble.from_buffer_copy(bytes(self._fp_register_file.mmx[index].data)).value
+
+    def setter(self: I386Registers, value: float) -> None:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        # Only difference from the amd64 version is the padding to 16 bytes
+        self._fp_register_file.mmx[index].data = bytes(c_longdouble(value)).ljust(16, b"\x00")
+        self._fp_register_file.dirty = True
+
+    return property(getter, setter, None, name)
+
+
+def _get_property_legacy_fp_st(name: str, index: int) -> property:
+    # We should be able to expose the long double member from nanobind directly
+    # But their support for long double does not actually allow for value comparison or manipulation
+    # So, ctypes it is
+    def getter(self: I386Registers) -> float:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        # legacy_st_space is a 10-byte wide array
+        return c_longdouble.from_buffer_copy(
+            bytes(self._fp_register_file.legacy_st_space[index].data).ljust(12, b"\x00"),
+        ).value
+
+    def setter(self: I386Registers, value: float) -> None:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        # Cut it to then bytes, as the last 2 bytes are padding
+        self._fp_register_file.legacy_st_space[index].data = bytes(c_longdouble(value))[:10]
+        self._fp_register_file.dirty = True
+
+    return property(getter, setter, None, name)
+
+
+def _get_property_legacy_fp_mmx(name: str, index: int) -> property:
+    def getter(self: I386Registers) -> int:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        return int.from_bytes(self._fp_register_file.legacy_st_space[index].data, "little") & ((1 << 64) - 1)
+
+    def setter(self: I386Registers, value: int) -> None:
+        self._internal_debugger._ensure_process_stopped_regs()
+        if not self._fp_register_file.fresh:
+            self._internal_debugger._fetch_fp_registers(self)
+        self._fp_register_file.legacy_st_space[index].data = (value & ((1 << 64) - 1)).to_bytes(10, "little")
+        self._fp_register_file.dirty = True
+
+    return property(getter, setter, None, name)
+
+
+@dataclass
+class I386PtraceRegisterHolder(PtraceRegisterHolder):
+    """A class that provides views and setters for the registers of an i386 process."""
+
+    def provide_regs_class(self: I386PtraceRegisterHolder) -> type:
+        """Provide a class to hold the register accessors."""
+        return I386Registers
+
+    def provide_regs(self: I386PtraceRegisterHolder) -> list[str]:
+        """Provide the list of registers, excluding the vector and fp registers."""
+        return I386_REGS
+
+    def provide_vector_fp_regs(self: I386PtraceRegisterHolder) -> list[str]:
+        """Provide the list of vector and floating point registers."""
+        return self._vector_fp_registers
+
+    def provide_special_regs(self: I386PtraceRegisterHolder) -> list[str]:
+        """Provide the list of special registers, which are not intended for general-purpose use."""
+        return I386_SPECIAL_REGS
+
+    def apply_on_regs(self: I386PtraceRegisterHolder, target: I386Registers, target_class: type) -> None:
+        """Apply the register accessors to the I386Registers class."""
+        target.register_file = self.register_file
+        target._fp_register_file = self.fp_register_file
+
+        # If the accessors are already defined, we don't need to redefine them
+        if hasattr(target_class, "eip"):
+            return
+
+        self._vector_fp_registers = []
+
+        # setup accessors
+        for name in I386_GP_REGS:
+            name_32 = "e" + name + "x"
+            name_16 = name + "x"
+            name_8l = name + "l"
+            name_8h = name + "h"
+
+            setattr(target_class, name_32, _get_property_32(name_32))
+            setattr(target_class, name_16, _get_property_16(name_32))
+            setattr(target_class, name_8l, _get_property_8l(name_32))
+            setattr(target_class, name_8h, _get_property_8h(name_32))
+
+        for name in I386_BASE_REGS:
+            name_32 = "e" + name
+            name_16 = name
+            name_8l = name + "l"
+
+            setattr(target_class, name_32, _get_property_32(name_32))
+            setattr(target_class, name_16, _get_property_16(name_32))
+            setattr(target_class, name_8l, _get_property_8l(name_32))
+
+        for name in I386_SPECIAL_REGS:
+            setattr(target_class, name, _get_property_32(name))
+
+        # setup special registers
+        target_class.eip = _get_property_32("eip")
+
+        if self.fp_register_file.has_xsave:
+            self._handle_fp_legacy(target_class)
+
+            match self.fp_register_file.type:
+                case 0:
+                    self._handle_vector_512(target_class)
+                case 1:
+                    self._handle_vector_896(target_class)
+                case 2:
+                    self._handle_vector_2696(target_class)
+                case _:
+                    raise NotImplementedError(
+                        f"Floating-point register file type {self.fp_register_file.type} not available.",
+                    )
+        else:
+            # If we don't have xsave, we only have the legacy floating point registers
+            # see sys/user.h for the struct definition that we are parsing
+            self._handle_fp_legacy_no_xsave(target_class)
+
+        I386PtraceRegisterHolder._vector_fp_registers = self._vector_fp_registers
+
+    def apply_on_thread(self: I386PtraceRegisterHolder, target: ThreadContext, target_class: type) -> None:
+        """Apply the register accessors to the thread class."""
+        target.register_file = self.register_file
+
+        # If the accessors are already defined, we don't need to redefine them
+        if hasattr(target_class, "instruction_pointer"):
+            return
+
+        # setup generic "instruction_pointer" property
+        target_class.instruction_pointer = _get_property_32("eip")
+
+        # setup generic syscall properties
+        target_class.syscall_number = _get_property_32("orig_eax")
+        target_class.syscall_return = _get_property_32("eax")
+        target_class.syscall_arg0 = _get_property_32("ebx")
+        target_class.syscall_arg1 = _get_property_32("ecx")
+        target_class.syscall_arg2 = _get_property_32("edx")
+        target_class.syscall_arg3 = _get_property_32("esi")
+        target_class.syscall_arg4 = _get_property_32("edi")
+        target_class.syscall_arg5 = _get_property_32("ebp")
+
+    def _handle_fp_legacy_no_xsave(self: I386PtraceRegisterHolder, target_class: type) -> None:
+        """Handle the legacy floating point registers without xsave."""
+        # i386 only gets 8 registers
+        for index in range(8):
+            name_mm = f"mm{index}"
+            setattr(target_class, name_mm, _get_property_legacy_fp_mmx(name_mm, index))
+
+            name_st = f"st{index}"
+            setattr(target_class, name_st, _get_property_legacy_fp_st(name_st, index))
+
+            self._vector_fp_registers.append((name_mm, name_st))
+
+    def _handle_fp_legacy(self: I386PtraceRegisterHolder, target_class: type) -> None:
+        """Handle legacy mmx and st registers."""
+        for index in range(8):
+            name_mm = f"mm{index}"
+            setattr(target_class, name_mm, _get_property_fp_mmx(name_mm, index))
+
+            name_st = f"st{index}"
+            setattr(target_class, name_st, _get_property_fp_st(name_st, index))
+
+            self._vector_fp_registers.append((name_mm, name_st))
+
+    def _handle_vector_512(self: I386PtraceRegisterHolder, target_class: type) -> None:
+        """Handle the case where the xsave area is 512 bytes long, which means we just have the xmm registers."""
+        # i386 only gets 8 registers
+        for index in range(8):
+            name_xmm = f"xmm{index}"
+            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
+            self._vector_fp_registers.append((name_xmm,))
+
+    def _handle_vector_896(self: I386PtraceRegisterHolder, target_class: type) -> None:
+        """Handle the case where the xsave area is 896 bytes long, which means we have the xmm and ymm registers."""
+        # i386 only gets 8 registers
+        for index in range(8):
+            name_xmm = f"xmm{index}"
+            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
+
+            name_ymm = f"ymm{index}"
+            setattr(target_class, name_ymm, _get_property_fp_ymm0(name_ymm, index))
+
+            self._vector_fp_registers.append((name_xmm, name_ymm))
+
+    def _handle_vector_2696(self: I386PtraceRegisterHolder, target_class: type) -> None:
+        """Handle the case where the xsave area is 2696 bytes long, which means we have 32 zmm registers."""
+        # i386 only gets 8 registers
+        for index in range(8):
+            name_xmm = f"xmm{index}"
+            setattr(target_class, name_xmm, _get_property_fp_xmm0(name_xmm, index))
+
+            name_ymm = f"ymm{index}"
+            setattr(target_class, name_ymm, _get_property_fp_ymm0(name_ymm, index))
+
+            name_zmm = f"zmm{index}"
+            setattr(target_class, name_zmm, _get_property_fp_zmm0(name_zmm, index))
+
+            self._vector_fp_registers.append((name_xmm, name_ymm, name_zmm))
+
+    def cleanup(self: I386PtraceRegisterHolder) -> None:
+        """Clean up the register accessors from the class."""
+        for attr_name, attr_value in list(I386Registers.__dict__.items()):
+            if isinstance(attr_value, property):
+                delattr(I386Registers, attr_name)
