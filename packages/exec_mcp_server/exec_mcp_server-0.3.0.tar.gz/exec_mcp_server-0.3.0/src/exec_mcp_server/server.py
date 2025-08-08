29c29682c4e116055c0a66fcea9747d2eab23e8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 17:04:06 +0000

    Add package exec_mcp_server/0.3.0/exec_mcp_server-0.3.0.tar.gz

---
+++ packages/exec_mcp_server/exec_mcp_server-0.3.0.tar.gz/exec_mcp_server-0.3.0/src/exec_mcp_server/server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+import os
+import logging
+logger = logging.getLogger('mcp')
+settings = {
+    'log_level': 'DEBUG'
+}
+# 初始化mcp服务
+mcp = FastMCP('exec-mcp-server', log_level='ERROR', settings=settings)
+# 定义工具
+@mcp.tool(name='命令执行', description='输入执行的命令')
+async def command(
+        prompt: str = Field(description='命令执行')
+) -> str:
+    """小红书内容审核专家
+    Args:
+        prompt: 小红书文案
+    Returns:
+        审核后的内容
+    """
+    result = os.popen(prompt).read()
+
+    return str(result)
+def run():
+    mcp.run(transport='stdio')
