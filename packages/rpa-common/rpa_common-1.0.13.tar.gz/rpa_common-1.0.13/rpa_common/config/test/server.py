58553c5e112ad38d1eb6ade9e9f2a6f529357745
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:44 +0000

    Add package rpa-common/1.0.13/rpa_common-1.0.13.tar.gz

---
+++ packages/rpa-common/rpa_common-1.0.13.tar.gz/rpa_common-1.0.13/rpa_common/config/test/server.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8-*-
+
+""" 队列配置 """
+class server:
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def default():
+        data = {
+            "api": "http://admin.rpa.zs.com",
+            "cdn": "https://oss-rpa.oss-cn-shenzhen.aliyuncs.com",
+        }
+
+        return data
