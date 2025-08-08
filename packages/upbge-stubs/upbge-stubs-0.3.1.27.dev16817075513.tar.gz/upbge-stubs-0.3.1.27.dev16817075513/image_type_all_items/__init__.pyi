b3bca09d3dbe6e410bc9ccc72d64212f0b757f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:39 +0000

    Add package upbge-stubs/0.3.1.27.dev16817075513/upbge-stubs-0.3.1.27.dev16817075513.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev16817075513.tar.gz/upbge-stubs-0.3.1.27.dev16817075513/image_type_all_items/__init__.pyi
@@ -0,0 +1,86 @@
+"""
+
+
+Image Type All Items
+^^^^^^^^^^^^^^^^^^^^
+
+:BMP:             
+  BMP.
+
+  Output image in bitmap format.
+
+:IRIS:            
+  Iris.
+
+  Output image in SGI IRIS format.
+
+:PNG:             
+  PNG.
+
+  Output image in PNG format.
+
+:JPEG:            
+  JPEG.
+
+  Output image in JPEG format.
+
+:JPEG2000:        
+  JPEG 2000.
+
+  Output image in JPEG 2000 format.
+
+:TARGA:           
+  Targa.
+
+  Output image in Targa format.
+
+:TARGA_RAW:       
+  Targa Raw.
+
+  Output image in uncompressed Targa format.
+
+.. _rna-enum-image-type-all-items:
+
+======================================================================
+
+:CINEON:          
+  Cineon.
+
+  Output image in Cineon format.
+
+:DPX:             
+  DPX.
+
+  Output image in DPX format.
+
+:OPEN_EXR:        
+  OpenEXR.
+
+  Output image in OpenEXR format.
+
+:HDR:             
+  Radiance HDR.
+
+  Output image in Radiance HDR format.
+
+:TIFF:            
+  TIFF.
+
+  Output image in TIFF format.
+
+:WEBP:            
+  WebP.
+
+  Output image in WebP format.
+
+:OPEN_EXR_MULTILAYER:
+  OpenEXR MultiLayer.
+
+  Output image in multilayer OpenEXR format.
+
+:FFMPEG:          
+  FFmpeg Video.
+
+"""
+
+import typing
