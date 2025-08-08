2c87509a7c5210956449af24957c9f12058d7a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:06:07 +0000

    Add package nominal-api/0.816.0/nominal_api-0.816.0.tar.gz

---
+++ packages/nominal-api/nominal_api-0.816.0.tar.gz/nominal_api-0.816.0/nominal_api/module_internal/__init__.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+from .._impl import (
+    module_internal_BatchGetResolvedModuleDefinitionsRequest as BatchGetResolvedModuleDefinitionsRequest,
+    module_internal_BatchGetResolvedModuleDefinitionsResponse as BatchGetResolvedModuleDefinitionsResponse,
+    module_internal_InternalModuleService as InternalModuleService,
+    module_internal_ModuleApplicationReference as ModuleApplicationReference,
+    module_internal_ModuleComputeDefinition as ModuleComputeDefinition,
+    module_internal_ResolvedModuleVersionDefinition as ResolvedModuleVersionDefinition,
+)
+
+__all__ = [
+    'BatchGetResolvedModuleDefinitionsRequest',
+    'BatchGetResolvedModuleDefinitionsResponse',
+    'ModuleApplicationReference',
+    'ModuleComputeDefinition',
+    'ResolvedModuleVersionDefinition',
+    'InternalModuleService',
+]
+
