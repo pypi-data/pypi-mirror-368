4d037faa6269231badc1b36130d99c541afe6045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:38 +0000

    Add package djgentelella/0.5.0/djgentelella-0.5.0-py3-none-any.whl

---
+++ packages/djgentelella/djgentelella-0.5.0-py3-none-any.whl/djgentelella/admin.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+
+from djgentelella.firmador_digital.models import UserSignatureConfig
+from djgentelella.models import MenuItem, Help, GentelellaSettings, Notification, \
+    ChunkedUpload, Trash
+from djgentelella.models import PermissionsCategoryManagement
+from djgentelella.utils import clean_cache
+
+
+class MenuAdmin(admin.ModelAdmin):
+    filter_horizontal = ['permission']
+
+class GentelellaSettingsAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value']
+    search_fields = ['key']
+    list_editable = ['value']
+    actions = ['clean_settings_cache']
+
+    def clean_settings_cache(self, request, queryset):
+        clean_cache(queryset.values_list('key', flat=True))
+
+    clean_settings_cache.short_description = "Clean settings cache"
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['description', 'state', 'message_type']
+    list_editable = ['state']
+
+class ChunkedUploadAdmin(admin.ModelAdmin):
+    list_display = ('upload_id', 'filename', 'status', 'created_on')
+    search_fields = ('filename', 'filename')
+    list_filter = ('status',)
+
+class UserSignatureConfigAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'config')
+
+class TrashAdmin(admin.ModelAdmin):
+    list_display = ("id", "deleted_by", "content_type", "object_id", "object_repr", "created_at")
+    ordering = ("-created_at",)
+    search_fields = ("id",)
+
+admin.site.register(UserSignatureConfig, UserSignatureConfigAdmin)
+admin.site.register(ChunkedUpload, ChunkedUploadAdmin)
+admin.site.register(MenuItem, MenuAdmin)
+admin.site.register(Help)
+admin.site.register(PermissionsCategoryManagement)
+admin.site.register(GentelellaSettings, GentelellaSettingsAdmin)
+admin.site.register(Notification, NotificationAdmin)
+admin.site.register(Trash, TrashAdmin)
