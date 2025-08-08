d9391b0560e0b2be000072d71a5d96914721b5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:55 +0000

    Add package tket2-exts/0.10.0/tket2_exts-0.10.0-py3-none-any.whl

---
+++ packages/tket2-exts/tket2_exts-0.10.0-py3-none-any.whl/tket2_exts/__init__.py
@@ -0,0 +1,15 @@
+"""HUGR extension definitions for tket2 circuits.
+
+| ⚠️ WARNING                  |
+|:----------------------------|
+| This package has been renamed to `tket-exts`. Please update your dependencies accordingly |
+
+"""
+
+# This is updated by our release-please workflow, triggered by this
+# annotation: x-release-please-version
+__version__ = "0.10.0"
+
+raise RuntimeError(
+    "The `tket2-exts` package has been renamed to `tket-exts`. Please update your dependencies accordingly."
+)
