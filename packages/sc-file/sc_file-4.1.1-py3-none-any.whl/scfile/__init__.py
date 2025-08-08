15776edff4e76eee326ac5eb321f573f1549c5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 17:04:26 +0000

    Add package sc-file/4.1.1/sc_file-4.1.1-py3-none-any.whl

---
+++ packages/sc-file/sc_file-4.1.1-py3-none-any.whl/scfile/__init__.py
@@ -0,0 +1,14 @@
+from . import cli, convert, core, exceptions, formats, structures, consts, enums
+from .core.context.options import UserOptions
+
+__all__ = (
+    "cli",
+    "convert",
+    "core",
+    "exceptions",
+    "formats",
+    "structures",
+    "consts",
+    "enums",
+    "UserOptions",
+)
