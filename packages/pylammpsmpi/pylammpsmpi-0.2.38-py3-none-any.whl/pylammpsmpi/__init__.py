57c15d39488532742fd3eec1665d301bfae06797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 15:04:52 +0000

    Add package pylammpsmpi/0.2.38/pylammpsmpi-0.2.38-py3-none-any.whl

---
+++ packages/pylammpsmpi/pylammpsmpi-0.2.38-py3-none-any.whl/pylammpsmpi/__init__.py
@@ -0,0 +1,16 @@
+from pylammpsmpi.wrapper.base import LammpsBase
+from pylammpsmpi.wrapper.concurrent import LammpsConcurrent
+from pylammpsmpi.wrapper.extended import LammpsLibrary
+
+from ._version import get_versions
+
+__all__ = ["LammpsLibrary", "LammpsConcurrent", "LammpsBase"]
+__version__ = get_versions()["version"]
+
+
+try:
+    from pylammpsmpi.wrapper.ase import LammpsASELibrary
+
+    __all__ += ["LammpsASELibrary"]
+except ImportError:
+    pass
