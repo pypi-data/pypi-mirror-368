71664d780daafcf791b05d8c52edf0b177221c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 16:04:43 +0000

    Add package atk-common/3.9.0/atk_common-3.9.0.tar.gz

---
+++ packages/atk-common/atk_common-3.9.0.tar.gz/atk_common-3.9.0/src/atk_common/classes/http_response_handler.py
@@ -0,0 +1,42 @@
+import json
+from flask import Response
+from http import HTTPStatus
+from atk_common.enums.api_error_type_enum import ApiErrorType
+from atk_common.utils.http_utils import is_http_status_internal, is_http_status_ok
+from atk_common.utils.internal_response_utils import is_response_http
+from atk_common.interfaces import IErrorHandler
+from atk_common.interfaces import IHttpResponseHandler
+
+class HttpResponseHandler(IHttpResponseHandler):
+    def __init__(self, error_handler: IErrorHandler):
+        self.error_handler = error_handler
+
+    def _convert_response_data(self, data):
+        if isinstance(data, dict):
+            return json.dumps(data)
+        elif isinstance(data, list):
+            return json.dumps(data)
+        elif isinstance(data, str):
+            json_data = json.loads(data)
+            return json.dumps(json_data)
+        else:
+            return data
+
+    # If response['status'] == 0 (OK, http status = 200): create Response and return response['responseMsg']   
+    # If http status == 500: 
+    #   If response['status'] == 1 (HTTP): resend received error entity
+    #   If response['status'] == 2 (INTERNAL): create new error entity and return as response
+    # If http status other value: create new error entity and return as response
+    def http_response(self, method, response):
+        if is_http_status_ok(response['statusCode']):
+            return Response(
+                response=self._convert_response_data(response['responseMsg']),
+                status=HTTPStatus.OK,
+                mimetype=response['contentType'],
+                headers=response['httpHeaders']
+            )
+        elif is_http_status_internal(response['statusCode']):
+            if is_response_http(response):
+                return self.error_handler.resend_error_entity(response['responseMsg'])
+            return self.error_handler.get_error_entity(response['responseMsg'], method, ApiErrorType.INTERNAL, response['statusCode'])
+        return self.error_handler.get_error_entity(response['responseMsg'], method, ApiErrorType.CONNECTION, response['statusCode'])
