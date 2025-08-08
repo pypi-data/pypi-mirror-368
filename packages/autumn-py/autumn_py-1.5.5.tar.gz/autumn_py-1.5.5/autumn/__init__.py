876e730cbaabd5b94dda06dbc15e8177d0568de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-08 14:05:33 +0000

    Add package autumn-py/1.5.5/autumn_py-1.5.5.tar.gz

---
+++ packages/autumn-py/autumn_py-1.5.5.tar.gz/autumn_py-1.5.5/autumn/__init__.py
@@ -0,0 +1,18 @@
+from .client import *
+from .error import *
+from .models.meta import *
+from .models.balance import *
+from .models.customers import *
+from .models.features import *
+from .models.products import *
+from .aio.client import AsyncClient as Autumn
+
+__title__ = "autumn"
+__version__ = "1.5.2"
+__license__ = "MIT"
+__author__ = "justanotherbyte"
+__copyright__ = "Copyright 2025 justanotherbyte"
+
+
+BASE_URL = "https://api.useautumn.com"
+VERSION = "v1"
