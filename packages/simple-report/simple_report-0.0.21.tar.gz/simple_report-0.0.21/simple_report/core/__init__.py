2c6ce70ce11e75a38ac60b17c927a789fffda261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:01 +0000

    Add package simple-report/0.0.21/simple_report-0.0.21.tar.gz

---
+++ packages/simple-report/simple_report-0.0.21.tar.gz/simple_report-0.0.21/simple_report/core/__init__.py
@@ -0,0 +1,27 @@
+from .components.base import BaseElement
+from .components.text import H1, H2, H3, List, P
+from .components.plot import Plot
+from .components.table import Table
+from .components.row import Row
+from .components.collapse import Collapse
+from .components.tabs import Tabs
+from .components.modal import Modal
+from .components.code import Code
+from .report import HtmlReport
+
+__all__ = [
+    "BaseElement",
+    "H1",
+    "H2",
+    "H3",
+    "List",
+    "P",
+    "Plot",
+    "Table",
+    "Row",
+    "Collapse",
+    "Tabs",
+    "Modal",
+    "Code",
+    "HtmlReport"
+]
