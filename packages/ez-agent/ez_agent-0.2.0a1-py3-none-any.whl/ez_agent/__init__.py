a3a145b93e1b16ce404ba2e4001aa70d550d453e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:28 +0000

    Add package ez-agent/0.2.0a1/ez_agent-0.2.0a1-py3-none-any.whl

---
+++ packages/ez-agent/ez_agent-0.2.0a1-py3-none-any.whl/ez_agent/__init__.py
@@ -0,0 +1,26 @@
+__author__ = "captainSuo"
+__version__ = "0.2.0a1"
+__all__ = [
+    "Agent",
+    "AsyncAgent",
+    "AsyncFunctionTool",
+    "Tool",
+    "FoldableAsyncFunctionTool",
+    "FoldableFunctionTool",
+    "FoldableMCPTool",
+    "FunctionTool",
+    "MCPClient",
+    "MCPTool",
+]
+
+
+from .agent.agent import Agent
+from .agent.agent_async import AsyncAgent
+from .agent.function_tool import (
+    FunctionTool,
+    FoldableFunctionTool,
+    AsyncFunctionTool,
+    FoldableAsyncFunctionTool,
+)
+from .agent.mcp_tool import MCPClient, MCPTool, FoldableMCPTool
+from .agent.base_tool import Tool
