af44e794748208309e5ad955c342c0f71b87f0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:36 +0000

    Add package pyObsFind/0.4.0/pyobsfind-0.4.0.tar.gz

---
+++ packages/pyObsFind/pyobsfind-0.4.0.tar.gz/pyobsfind-0.4.0/README.md
@@ -0,0 +1,18 @@
+# READ ME
+
+## Purpose:
+
+The purpose of this code is to find when a list of solar system objects are observable from a given observatory on Earth in order to aid proposal writing.
+
+It will create:
+* A csv of the JPL Horizons call for all the objects across the semester
+* A csv summary of the median value for each night (to do)
+* A pdf of nightly elevation charts and summary tables for all objects visible
+* A pdf with summary charts to show how the observability and target properties change over the course of the full semester (to do)
+
+The documentation can be found at [pyObsFind documentation](https://pyobsfind.readthedocs.io/en/latest/index.html).
+
+
+[![Made at Code Astro](https://img.shields.io/badge/Made%20at-Code/Astro-blueviolet.svg)](https://semaphorep.github.io/codeastro/)
+[![DOI](https://zenodo.org/badge/1031821874.svg)](https://doi.org/10.5281/zenodo.16754829)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ packages/pyObsFind/pyobsfind-0.4.0.tar.gz/pyobsfind-0.4.0/obsfind/plotting.py
@@ -0,0 +1,106 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import itertools
+
+def marker_list(target_names):
+    """
+    Creates a list of markers and colours for plotting targets.
+
+    Inputs
+        target_names : List of target names to be plotted.
+
+    Output
+        DataFrame with target names, markers, and colours.
+    """
+    # Marker set up now that number of targets that will be plotted is known
+    marker_opt = ['o','v','s','X','<','P','*','h','>','H','+','^','x','D']
+    colour_opt = ['b','g','r','c','m','y']
+    markers    = list(itertools.product(marker_opt, colour_opt))  # List of unique tuples
+    markerlist = [pair[0] for pair in markers[:len(target_names)]]
+    colourlist = [pair[1] for pair in markers[:len(target_names)]]
+    target_list_data = {'targets'  : target_names,
+                        'markers'  : markerlist,
+                        'colours'  : colourlist}
+    target_plot_info = pd.DataFrame(data = target_list_data)
+
+    return target_plot_info
+
+
+def elevation_chart(twilight_times, eph_night, target_plot_info, elevation_limit, show_plot=False, fig_path='./temp_airmass'):
+    """
+    Creates an elevation chart for a given night.
+    
+    Inputs
+        twilight_times : DataFrame with twilight times for the night.
+        eph_night      : DataFrame with ephemerides for the night.
+        target_plot_info : DataFrame with target names, markers, and colours.
+        elevation_limit : Minimum elevation limit for plotting.
+        show_plot      : Boolean to show the plot (default: False).
+        fig_path       : Path to save the figure (default: './temp_airmass').
+
+    Output
+        Saves the elevation chart as a PNG file if fig_path is provided.
+    """
+    night = twilight_times['night']
+
+    # Create figure
+    fig, ax = plt.subplots(figsize=(18,15))
+    ax = plt.gca()
+
+    elevation_ticks = [tick for tick in np.arange(0, 91, 10) if tick>=elevation_limit]
+    airmass_values = 1 / np.sin(np.radians(elevation_ticks))
+    airmass_labels = [f'{a:.2f}' for a in airmass_values]
+
+    # Plot targets above threshold
+    for obj in eph_night['target'].unique():
+        
+        eph_night_tar = eph_night[eph_night.target==obj]
+
+        if obj == '301':
+            eph_night_tar.plot(x='datetime_str', y='elevation',
+                            label='Moon', ax=ax,
+                            linestyle='--', color='black', marker='', lw=7, alpha=0.75)
+        else:
+            colour = target_plot_info[target_plot_info['targets']==obj]['colours'].values[0]
+            marker = target_plot_info[target_plot_info['targets']==obj]['markers'].values[0]
+            eph_night_tar.plot(x='datetime_str', y='elevation',
+                            label=obj, ax=ax,
+                            marker=marker, color=colour, markersize=8)
+    
+    # Format plot
+    ax.axvspan(twilight_times['sun_set'].isoformat(), twilight_times['civil_set'].isoformat(), alpha=.30)
+    ax.axvspan(twilight_times['civil_set'].isoformat(), twilight_times['nautical_set'].isoformat(), alpha=.20)
+    ax.axvspan(twilight_times['nautical_set'].isoformat(), twilight_times['astronomical_set'].isoformat(), alpha=.10)
+    ax.axvspan(twilight_times['astronomical_rise'].isoformat(), twilight_times['nautical_rise'].isoformat(), alpha=.10)
+    ax.axvspan(twilight_times['nautical_rise'].isoformat(), twilight_times['civil_rise'].isoformat(), alpha=.20)
+    ax.axvspan(twilight_times['civil_rise'].isoformat(), twilight_times['sun_rise'].isoformat(), alpha=.30)
+    # ax.axhspan(0,elevation_limit,alpha=.5,color='grey')
+    ax.set_xlabel(f"Universal Time (hours) - Night begins on {night.strftime('%d %b %Y')} UT", fontsize=20)
+    ax.set_ylabel("Elevation", fontsize=20)
+    ax.set_ylim(elevation_limit,90)
+    ax.set_yticks(elevation_ticks)
+    ax.xaxis.set_ticks_position('both')
+    ax.yaxis.set_tick_params(direction='in', labelsize=20)
+    ax.xaxis.set_tick_params(labeltop=True, which='both', direction='in', labelsize=20)
+    ax.set_xlim(twilight_times['sun_set'].isoformat(), twilight_times['sun_rise'].isoformat())
+    ax2 = ax.twinx()
+    ax2.set_ylim(ax.get_ylim())
+    ax2.set_yticks(elevation_ticks)
+    ax2.set_yticklabels(airmass_labels)
+    ax2.set_ylabel("Airmass", fontsize=20)
+    ax2.yaxis.set_tick_params(direction='in', labelsize=20)
+
+    # ax.legend(bbox_to_anchor=(1.1, 1), loc='upper left',prop={'size': 20})
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15),
+              ncol=6, prop={'size': 20})
+
+    plt.grid(which='both',axis='both')
+    plt.tight_layout()
+    if fig_path:
+        plt.savefig(f'{fig_path}/elevation_{night.strftime("%Y%m%d")}.png')
+    if show_plot:
+        plt.show()
+    plt.close()
+
+    return
