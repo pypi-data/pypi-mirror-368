dd7385ee828e974bacadc4f4b55f14b45c83e3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:15 +0000

    Add package mtcli/1.12.0/mtcli-1.12.0.tar.gz

---
+++ packages/mtcli/mtcli-1.12.0.tar.gz/mtcli-1.12.0/mtcli/extensions/media_movel/command.py
@@ -0,0 +1,30 @@
+"""Calcula a média móvel simples."""
+
+import click
+
+from . import conf
+from mtcli.models import model_rates
+from .models import model_moving_average
+
+
+@click.command()
+@click.argument("symbol")
+@click.option(
+    "--period",
+    "-p",
+    type=click.Choice(conf.timeframes, case_sensitive=False),
+    default="D1",
+    help="Tempo grafico, default D1.",
+)
+@click.option("--count", "-c", default=20, help="Quantidade de barras, default 20.")
+def mm(symbol, period, count):
+    """Calcula a media movel simples."""
+    rates = model_rates.RatesModel(symbol, period)
+    rates = rates.lista
+    ma = model_moving_average.MovingAverageModel(rates, count)
+    ma = ma.average()
+    click.echo(ma)
+
+
+if __name__ == "__main__":
+    mm()
