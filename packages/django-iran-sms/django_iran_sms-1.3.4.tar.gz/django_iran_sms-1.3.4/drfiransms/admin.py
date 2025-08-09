99b133da31939239f41ccd913c82bb4d1c978f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:05:06 +0000

    Add package django-iran-sms/1.3.4/django_iran_sms-1.3.4.tar.gz

---
+++ packages/django-iran-sms/django_iran_sms-1.3.4.tar.gz/django_iran_sms-1.3.4/drfiransms/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import User, OTPCode, Session
+
+@admin.register(User)
+class MobileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user__id', 'user__username', 'mobile', 'user__is_active']
+
+
+@admin.register(OTPCode)
+class OTPCodeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'code', 'mobile', 'created_at']
+    ordering = ('-created_at', )
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user__id', 'user__name', 'ip_address', 'last_seen']
+    ordering = ('-created_at', )
+
