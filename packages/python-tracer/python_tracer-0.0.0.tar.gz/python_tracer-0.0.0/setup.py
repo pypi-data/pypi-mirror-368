51d7a430d1acf1e942398398707cd86b2a77ec88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:04:50 +0000

    Add package python-tracer/0.0.0/python_tracer-0.0.0.tar.gz

---
+++ packages/python-tracer/python_tracer-0.0.0.tar.gz/python_tracer-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    # name="python-tracer",
+    # version="0.1.0",
+    # description="Python Tracer and Performance Monitoring Tool with HTML Reports",
+    # author="Gaurao Bharambe",
+    # packages=find_packages(),
+    # install_requires=[],
+    # python_requires=">=3.7",
+)
