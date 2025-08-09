5f3cca16d718976d213fad40411fc960aa37fd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:56 +0000

    Add package bitquery-pb2-kafka-package/0.1.9/bitquery_pb2_kafka_package-0.1.9.tar.gz

---
+++ packages/bitquery-pb2-kafka-package/bitquery_pb2_kafka_package-0.1.9.tar.gz/bitquery_pb2_kafka_package-0.1.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bitquery-pb2-kafka-package',
+    version='0.1.9',
+    packages=find_packages(),
+    author='Bitquery',
+    author_email='divyasshree@bitquery.io',
+    description='This package contains the pb2 files necessary to interact with Bitquery Kafka Protobuf messages',
+    long_description=open("README.md", "r", encoding="utf-8").read(),  # Ensuring UTF-8 encoding
+    long_description_content_type="text/markdown",  # Explicitly set Markdown format
+    url='https://github.com/bitquery/streaming-protobuf-python', 
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
