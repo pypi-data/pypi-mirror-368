3e63f2f9e3a8979089aa3383a679112c178d9c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:41 +0000

    Add package pyasic/0.74.1/pyasic-0.74.1.tar.gz

---
+++ packages/pyasic/pyasic-0.74.1.tar.gz/pyasic-0.74.1/pyasic/miners/backends/whatsminer.py
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2022 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+from pyasic.miners.backends.btminer import BTMiner
+
+
+class M7X(BTMiner):
+    supports_autotuning = True
+
+
+class M6X(BTMiner):
+    supports_autotuning = True
+
+
+class M5X(BTMiner):
+    supports_autotuning = True
+
+
+class M3X(BTMiner):
+    supports_autotuning = True
+
+
+class M2X(BTMiner):
+    pass
