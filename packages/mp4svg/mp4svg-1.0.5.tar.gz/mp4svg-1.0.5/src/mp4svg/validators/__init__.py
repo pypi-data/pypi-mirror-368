00693fd1882ce11d9d22833d7ddcc9f97e5ca288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:31 +0000

    Add package mp4svg/1.0.5/mp4svg-1.0.5.tar.gz

---
+++ packages/mp4svg/mp4svg-1.0.5.tar.gz/mp4svg-1.0.5/src/mp4svg/validators/__init__.py
@@ -0,0 +1,11 @@
+"""
+SVG validation utilities for mp4svg
+"""
+
+from .svg_validator import SVGValidator
+from .integrity_validator import IntegrityValidator
+
+__all__ = [
+    'SVGValidator',
+    'IntegrityValidator'
+]
