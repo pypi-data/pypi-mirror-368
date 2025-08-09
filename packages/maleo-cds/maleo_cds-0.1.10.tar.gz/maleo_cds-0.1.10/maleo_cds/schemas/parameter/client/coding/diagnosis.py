8a0b88b68e97733b617e09fa7d9c1a8a991aa35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:31 +0000

    Add package maleo-cds/0.1.10/maleo_cds-0.1.10.tar.gz

---
+++ packages/maleo-cds/maleo_cds-0.1.10.tar.gz/maleo_cds-0.1.10/maleo_cds/schemas/parameter/client/coding/diagnosis.py
@@ -0,0 +1,36 @@
+from maleo_soma.mixins.parameter import (
+    OptionalListOfIds,
+    OptionalListOfUuids,
+    OptionalListOfUserIds,
+    OptionalListOfOrganizationIds,
+    OptionalListOfCodes,
+    Search,
+)
+from maleo_soma.schemas.parameter.service import (
+    ReadPaginatedMultipleParameterSchema,
+    ReadPaginatedMultipleQueryParameterSchema,
+)
+
+
+class ReadMultipleParameter(
+    ReadPaginatedMultipleParameterSchema,
+    Search,
+    OptionalListOfCodes,
+    OptionalListOfUserIds,
+    OptionalListOfOrganizationIds,
+    OptionalListOfUuids,
+    OptionalListOfIds,
+):
+    pass
+
+
+class ReadMultipleQueryParameter(
+    ReadPaginatedMultipleQueryParameterSchema,
+    Search,
+    OptionalListOfCodes,
+    OptionalListOfUserIds,
+    OptionalListOfOrganizationIds,
+    OptionalListOfUuids,
+    OptionalListOfIds,
+):
+    pass
