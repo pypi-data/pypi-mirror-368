203c893af9174688d302df7d23e943bd9be85ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:54 +0000

    Add package xvideos-api/1.6.4/xvideos_api-1.6.4-py3-none-any.whl

---
+++ packages/xvideos-api/xvideos_api-1.6.4-py3-none-any.whl/xvideos_api/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Client", "Video", "Pornstar", "sorting",
+           "errors.py", "consts"]
+
+
+from xvideos_api.xvideos_api import Client, Video, Pornstar
+from xvideos_api.modules import sorting, errors, consts
