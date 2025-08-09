a1fe24a3e4799a201e2cf4be3c8837913d8b51c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 08:04:21 +0000

    Add package LSMS_Library/0.2.4/lsms_library-0.2.4.tar.gz

---
+++ packages/LSMS_Library/lsms_library-0.2.4.tar.gz/lsms_library-0.2.4/lsms_library/countries/Uganda/2019-20/_/household_characteristics.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from lsms_library.local_tools import to_parquet
+
+import sys
+sys.path.append('../../_/')
+import pandas as pd
+import numpy as np
+from uganda import age_sex_composition
+
+myvars = dict(fn='../Data/HH/gsec2.dta',
+              HHID='hhid',
+              sex='h2q3',
+              age='h2q8',
+              months_spent='h2q5')
+
+df = age_sex_composition(**myvars)
+
+df = df.filter(regex='ales ')
+
+N = df.sum(axis=1)
+
+df['log HSize'] = np.log(N[N>0])
+
+to_parquet(df, 'household_characteristics.parquet')
