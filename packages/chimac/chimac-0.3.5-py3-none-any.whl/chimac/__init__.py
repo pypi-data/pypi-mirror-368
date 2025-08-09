bf09dbea7fca177b80a89aed3d2779a5b03e21ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:04:47 +0000

    Add package chimac/0.3.5/chimac-0.3.5-py3-none-any.whl

---
+++ packages/chimac/chimac-0.3.5-py3-none-any.whl/chimac/__init__.py
@@ -0,0 +1,19 @@
+from chimac.chimac import ChiMAC
+from chimac.augmentor import DatasetAugmenter
+from chimac.utils import *
+
+__all__ = [
+    "ChiMAC",
+    "DatasetAugmenter",
+    "Logger",
+    "seed_all",
+    "adjust_and_sample",
+    "op_brightness",
+    "op_contrast",
+    "op_flip",
+    "op_hue",
+    "op_rotate",
+    "op_saturation",
+    "op_scale",
+    "op_translate",
+]
