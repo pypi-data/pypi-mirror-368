58254f4b71ea3374562742be6ba7ec1025508860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:24 +0000

    Add package kinglet/1.1.0/kinglet-1.1.0-py3-none-any.whl

---
+++ packages/kinglet/kinglet-1.1.0-py3-none-any.whl/kinglet/__init__.py
@@ -0,0 +1,9 @@
+"""
+Kinglet - A lightweight routing framework for Python Workers
+"""
+
+# Import everything from the main module
+from .kinglet import *
+
+__version__ = "1.1.0"
+__author__ = "Mitchell Currie"
