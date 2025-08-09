7172328466023aed7d6194f71ccab48fb3ae2b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:33 +0000

    Add package tnbs-stim-clean/0.1.6/tnbs_stim_clean-0.1.6.tar.gz

---
+++ packages/tnbs-stim-clean/tnbs_stim_clean-0.1.6.tar.gz/tnbs_stim_clean-0.1.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tnbs_stim_clean",  # Name of your package
+    version="0.1.6",  # Version of your package
+    packages=find_packages(),  # Automatically find packages
+    install_requires=[  # External dependencies
+        "numpy",       
+        "matplotlib",  
+        "mne",         # Required for EEG/MEG data import
+    ],
+    long_description=open("README.md").read(),  # Long description for PyPI
+    long_description_content_type='text/markdown',  # Optional, set to markdown
+    classifiers=[  # Help users find your package
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.1',  # Minimum Python version
+)
