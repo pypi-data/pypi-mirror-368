9a40ba7d18bbcb4563710dca53ff69b4d9600259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:04:52 +0000

    Add package bluer-journal/5.74.1/bluer_journal-5.74.1.tar.gz

---
+++ packages/bluer-journal/bluer_journal-5.74.1.tar.gz/bluer_journal-5.74.1/bluer_journal/.abcli/git/pull.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+function bluer_journal_git_pull() {
+    local options=$1
+    local do_pull=$(bluer_ai_option_int "$options" pull 1)
+    local is_webhook=$(bluer_ai_option_int "$webhook" webhook 0)
+
+    local repo_name
+    for repo_name in \
+        $BLUER_JOURNAL_REPO \
+        $BLUER_JOURNAL_REPO.wiki; do
+        if [[ ! -d "$abcli_path_git/$repo_name" ]]; then
+            if [[ "$abcli_is_github_workflow" == true ]]; then
+                pushd $abcli_path_git >/dev/null
+                if [[ "$is_webhook" == 1 ]]; then
+                    git clone https://x-access-token:$GITHUB_TOKEN@github.com/$repo_name.wiki.git wiki
+                else
+                    git clone https://github.com/kamangir/$repo_name.git
+                fi
+                [[ $? -ne 0 ]] && return 1
+                popd >/dev/null
+            else
+                bluer_ai_git_clone $repo_name
+                [[ $? -ne 0 ]] && return 1
+            fi
+        fi
+    done
+
+    [[ "$do_pull" == 0 ]] &&
+        return 0
+
+    bluer_ai_git \
+        $BLUER_JOURNAL_REPO.wiki \
+        pull \
+        ~all
+}
