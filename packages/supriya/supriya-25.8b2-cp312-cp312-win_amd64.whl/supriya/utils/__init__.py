7052b65b6a74f1963dff53b412400e5c1ebeddf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:05:01 +0000

    Add package supriya/25.8b2/supriya-25.8b2-cp312-cp312-win_amd64.whl

---
+++ packages/supriya/supriya-25.8b2-cp312-cp312-win_amd64.whl/supriya/utils/__init__.py
@@ -0,0 +1,29 @@
+"""
+Utility functions.
+
+These will be migrated out into a base package at some point.
+"""
+
+from .intervals import Interval, IntervalTree, Moment
+from .iterables import (
+    expand,
+    expand_deep,
+    flatten,
+    group_by_count,
+    iterate_nwise,
+    repeat_to_length,
+    zip_cycled,
+)
+
+__all__ = [
+    "Interval",
+    "IntervalTree",
+    "Moment",
+    "expand",
+    "expand_deep",
+    "flatten",
+    "group_by_count",
+    "iterate_nwise",
+    "repeat_to_length",
+    "zip_cycled",
+]
