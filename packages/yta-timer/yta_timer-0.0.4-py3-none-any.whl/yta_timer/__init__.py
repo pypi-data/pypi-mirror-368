27b23a1000f77f685189afb499641c3da8117327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:05:15 +0000

    Add package yta-timer/0.0.4/yta_timer-0.0.4-py3-none-any.whl

---
+++ packages/yta-timer/yta_timer-0.0.4-py3-none-any.whl/yta_timer/__init__.py
@@ -0,0 +1,100 @@
+import time
+
+
+END_TIME_NO_VALUE: float = 999999.9
+
+class Timer:
+    """
+    Class to measure how long certain methods take
+    to execute and complete. Just call the
+    `.start()` method before the code is executed
+    and the `.stop()` when it finishes.
+
+    It can be used as a context manager with this:
+    - `with Timer():`
+    """
+
+    @property
+    def time_elapsed(
+        self
+    ) -> float:
+        """
+        The time elapsed between the 'start' and the
+        'stop' methods call.
+        """
+        return self.end_t - self.start_t
+
+    @property
+    def time_elapsed_str(
+        self
+    ) -> str:
+        """
+        The time elapsed between the 'start' and the
+        'stop' method call, but as a printable string.
+        """
+        return str(round(self.time_elapsed, 2))
+
+    def __init__(
+        self,
+        is_silent_as_context: bool = False
+    ):
+        self.start_t: float = 0
+        """
+        The start time moment.
+        """
+        self.end_t: float = END_TIME_NO_VALUE
+        """
+        The end time moment.
+        """
+        self._is_silent_as_context: bool = is_silent_as_context
+        """
+        Flag to indicate if it must print the info
+        or not when used as a context and finished.
+        """
+
+    def start(
+        self
+    ) -> None:
+        """
+        Start the timer.
+        """
+        self.start_t = time.perf_counter()
+        self.end_t = END_TIME_NO_VALUE
+
+    def stop(
+        self
+    ) -> None:
+        """
+        Stop the timer. This will raise an exception
+        if the timer hasn't been started previously.
+        """
+        if self.start_t is None:
+            raise Exception('The timer was not started.')
+        
+        self.end_t = time.perf_counter()
+    
+    def print(
+        self
+    ) -> None:
+        """
+        Print the time elapsed in the console.
+        """
+        print(f'Time elapsed: {self.time_elapsed_str}')
+
+    # Allowing 'with Timer():' context below
+    def __enter__(
+        self
+    ) -> 'Timer':
+        self.start()
+
+        return self
+
+    def __exit__(
+        self,
+        exc_type,
+        exc_val,
+        exc_tb
+    ):
+        self.stop()
+        if not self._is_silent_as_context:
+            self.print()
