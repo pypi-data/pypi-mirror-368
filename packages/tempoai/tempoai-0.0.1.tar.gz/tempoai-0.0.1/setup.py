0ddf3d143216beba5a53afdfd7986ed5e4e2290f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:37 +0000

    Add package tempoai/0.0.1/tempoai-0.0.1.tar.gz

---
+++ packages/tempoai/tempoai-0.0.1.tar.gz/tempoai-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tempoai",
+    version="0.0.1",
+    description="Reserved placeholder for the Tempo deep learning system",
+    author="Pedro Silvestre",
+    packages=["tempoai"],
+    python_requires=">=3.10",
+)
+
