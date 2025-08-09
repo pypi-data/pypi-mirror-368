5700c373591b8a652144704d3a28a2aa63ef1a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:48 +0000

    Add package near-omni-client/0.1.9/near_omni_client-0.1.9-py3-none-any.whl

---
+++ packages/near-omni-client/near_omni_client-0.1.9-py3-none-any.whl/near_omni_client/adapters/cctp/__init__.py
@@ -0,0 +1,6 @@
+from .attestation_service import AttestationService
+from .messenger_contract import MessengerContract
+from .transmitter_contract import TransmitterContract
+from .usdc_contract import USDCContract
+
+__all__ = ["AttestationService", "MessengerContract", "TransmitterContract", "USDCContract"]
