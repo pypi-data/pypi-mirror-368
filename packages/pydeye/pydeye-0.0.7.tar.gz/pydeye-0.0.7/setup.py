9c8445bfcfa727919f844bedb21e24d5bfd5f873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:04:48 +0000

    Add package pydeye/0.0.7/pydeye-0.0.7.tar.gz

---
+++ packages/pydeye/pydeye-0.0.7.tar.gz/pydeye-0.0.7/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pydeye",
+    version="0.0.7",
+    description="Python deye inverter interface",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Jakob Salmič",
+    author_email="salmic.jakob@gmail.com",
+    maintainer=", ".join(("Jakob Salmič <salmic.jakob@gmail.com>",)),
+    license="GPL",
+    url="https://github.com/UnknownHero99/pydeye",
+    python_requires=">=3.8",
+    packages=find_packages(),
+    keywords=["homeautomation", "deye", "inverter", "modbus", "solar"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Home Automation",
+    ],
+    install_requires=["pymodbus"],
+)
