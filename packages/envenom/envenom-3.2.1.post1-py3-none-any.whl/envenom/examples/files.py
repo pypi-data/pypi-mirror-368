a45ef1511e103b1c73b4715af2b7f455e12d18c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:05:12 +0000

    Add package envenom/3.2.1.post1/envenom-3.2.1.post1-py3-none-any.whl

---
+++ packages/envenom/envenom-3.2.1.post1-py3-none-any.whl/envenom/examples/files.py
@@ -0,0 +1,31 @@
+# `envenom` - an elegant application configurator for the more civilized age
+# Copyright (C) 2024-2025 Artur Ciesielski <artur.ciesielski@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from envenom import config, optional, required
+from envenom.examples import print_config_tree
+
+
+@config()
+class MainCfg:
+    some_value: str = required()
+    other_value: str | None = optional(file=False)
+
+
+if __name__ == "__main__":
+    cfg = MainCfg()
+
+    print_config_tree(cfg)
