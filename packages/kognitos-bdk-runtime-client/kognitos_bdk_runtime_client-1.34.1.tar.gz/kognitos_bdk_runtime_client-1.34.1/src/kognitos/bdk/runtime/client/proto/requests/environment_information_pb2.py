439cbceccae07297544ace5ab442acbaff87826c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:05:09 +0000

    Add package kognitos-bdk-runtime-client/1.34.1/kognitos_bdk_runtime_client-1.34.1.tar.gz

---
+++ packages/kognitos-bdk-runtime-client/kognitos_bdk_runtime_client-1.34.1.tar.gz/kognitos_bdk_runtime_client-1.34.1/src/kognitos/bdk/runtime/client/proto/requests/environment_information_pb2.py
@@ -0,0 +1,17 @@
+"""Generated protocol buffer code."""
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
+from google.protobuf import symbol_database as _symbol_database
+from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 27, 3, '', 'requests/environment_information.proto')
+_sym_db = _symbol_database.Default()
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n&requests/environment_information.proto\x12\x08protocol"\x1f\n\x1dEnvironmentInformationRequestB\x1fZ\x1dgithub.com/kognitos/bdk-protob\x06proto3')
+_globals = globals()
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'requests.environment_information_pb2', _globals)
+if not _descriptor._USE_C_DESCRIPTORS:
+    _globals['DESCRIPTOR']._loaded_options = None
+    _globals['DESCRIPTOR']._serialized_options = b'Z\x1dgithub.com/kognitos/bdk-proto'
+    _globals['_ENVIRONMENTINFORMATIONREQUEST']._serialized_start = 52
+    _globals['_ENVIRONMENTINFORMATIONREQUEST']._serialized_end = 83
