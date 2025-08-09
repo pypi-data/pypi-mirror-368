4cc21132b4b7423c999adad0d38a59fb183c284e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:39 +0000

    Add package odoo14-addon-ssi-odoo-implementation-data-requirement/14.0.1.0.1.dev1/odoo14_addon_ssi_odoo_implementation_data_requirement-14.0.1.0.1.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-odoo-implementation-data-requirement/odoo14_addon_ssi_odoo_implementation_data_requirement-14.0.1.0.1.dev1-py3-none-any.whl/odoo/addons/ssi_odoo_implementation_data_requirement/models/odoo_feature_issue.py
@@ -0,0 +1,18 @@
+# Copyright 2025 OpenSynergy Indonesia
+# Copyright 2025 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0-standalone.html).
+
+from odoo import models
+
+
+class OdooFeatureIssue(models.Model):
+    _name = "odoo_feature_issue"
+    _inherit = [
+        "odoo_feature_issue",
+        "mixin.data_requirement",
+    ]
+
+    _data_requirement_create_page = True
+    _data_requirement_configurator_field_name = "type_id"
+    _data_requirement_partner_field_name = "commercial_partner_id"
+    _data_requirement_contact_field_name = "partner_id"
