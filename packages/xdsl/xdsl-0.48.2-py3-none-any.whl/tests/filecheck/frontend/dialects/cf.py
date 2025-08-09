080d8e906ede320b1130f92415af9f8494a14964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:59 +0000

    Add package xdsl/0.48.2/xdsl-0.48.2-py3-none-any.whl

---
+++ packages/xdsl/xdsl-0.48.2-py3-none-any.whl/tests/filecheck/frontend/dialects/cf.py
@@ -0,0 +1,37 @@
+# RUN: python %s | filecheck %s
+
+
+from xdsl.dialects import bigint, builtin
+from xdsl.frontend.pyast.context import CodeContext
+from xdsl.frontend.pyast.program import FrontendProgram
+from xdsl.frontend.pyast.utils.exceptions import CodeGenerationException
+
+p = FrontendProgram()
+p.register_type(bool, builtin.i1)
+p.register_type(int, bigint.bigint)
+with CodeContext(p):
+    # CHECK: cf.assert %{{.*}}, ""
+    def test_assert_I(cond: bool):
+        assert cond
+        return
+
+    # CHECK: cf.assert %{{.*}}, "some message"
+    def test_assert_II(cond: bool):
+        assert cond, "some message"
+        return
+
+
+p.compile(desymref=False)
+print(p.textual_format())
+
+try:
+    with CodeContext(p):
+        # CHECK: Expected a string constant for assertion message, found 'ast.Name'
+        def test_assert_message_type(cond: bool, a: int):
+            assert cond, a
+            return
+
+    p.compile(desymref=False)
+    print(p.textual_format())
+except CodeGenerationException as e:
+    print(e.msg)
