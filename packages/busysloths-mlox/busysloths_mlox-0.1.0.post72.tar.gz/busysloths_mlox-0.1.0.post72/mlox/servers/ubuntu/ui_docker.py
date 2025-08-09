7b95885cf688eafce8f81797228712b762f812d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:36 +0000

    Add package busysloths-mlox/0.1.0.post72/busysloths_mlox-0.1.0.post72.tar.gz

---
+++ packages/busysloths-mlox/busysloths_mlox-0.1.0.post72.tar.gz/busysloths_mlox-0.1.0.post72/mlox/servers/ubuntu/ui_docker.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+from typing import Dict
+
+from mlox.infra import Infrastructure, Bundle
+from mlox.config import ServiceConfig
+from mlox.servers.ubuntu.docker import UbuntuDockerServer
+from mlox.servers.ubuntu.ui_native import setup as setup_native
+
+
+def setup(infra: Infrastructure, config: ServiceConfig) -> Dict:
+    return setup_native(infra, config)
+
+
+def settings(infra: Infrastructure, bundle: Bundle, server: UbuntuDockerServer):
+    st.markdown(f"#### {bundle.name}")
+
+    if server.mlox_user:
+        st.write(f"ssh {server.mlox_user.name}@{server.ip}")
+        st.write(server.mlox_user.pw)
