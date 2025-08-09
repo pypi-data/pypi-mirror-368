6f25df4a481466a2c5b2b661c3f89754de6030c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:42 +0000

    Add package NeZha2/0.1.1/nezha2-0.1.1.tar.gz

---
+++ packages/NeZha2/nezha2-0.1.1.tar.gz/nezha2-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NeZha2",
+    version="0.1.1",
+    packages=find_packages(),
+    package_data={
+        'nezha2.data': ['*.txt']
+    },
+    description="将二进制魔丸数据转换为GIF动画",
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ququ85005702/NeZha2",
+    author="ququ",
+    author_email="85005702@qq.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+    entry_points={}
+)
