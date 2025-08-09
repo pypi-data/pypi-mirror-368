0b04225a41477da430d9d92e1bf81d41a4e2d7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:23 +0000

    Add package LayerZero/0.1.0/layerzero-0.1.0.tar.gz

---
+++ packages/LayerZero/layerzero-0.1.0.tar.gz/layerzero-0.1.0/LayerZero/nnprinter.py
@@ -0,0 +1,8 @@
+# fastnn/nnprinter.py
+
+class NNPrinter:
+    """Tiny helper used in fast deployments to confirm the package is reachable."""
+
+    def print(self) -> None:
+        """Print a simple status message to stdout."""
+        print("yes i am working")
