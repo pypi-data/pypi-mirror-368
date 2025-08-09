df8a912d851126dc58a54b28aef99a9564e6172f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:09:52 +0000

    Add package kognitos-bdk-linter/1.22.1/kognitos_bdk_linter-1.22.1-py3-none-any.whl

---
+++ packages/kognitos-bdk-linter/kognitos_bdk_linter-1.22.1-py3-none-any.whl/kognitos/bdk/linter/procedure/procedure_rule_factory.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from .rules.procedure_rules import ProcedureExamplesRule, ProcedureRule
+
+
+class ProcedureRuleFactory(ABC):
+    @abstractmethod
+    def get_rules(self) -> List[ProcedureRule]:
+        pass
+
+
+class DefaultProcedureRuleFactory(ProcedureRuleFactory):
+    def get_rules(self) -> List[ProcedureRule]:
+        return [ProcedureExamplesRule()]
