15edeeae87a933967b8862fee35048da7200a3b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 14:04:52 +0000

    Add package fabricatio/0.19.2/fabricatio-0.19.2-cp313-cp313-manylinux_2_34_aarch64.whl

---
+++ packages/fabricatio/fabricatio-0.19.2-cp313-cp313-manylinux_2_34_aarch64.whl/fabricatio/__init__.py
@@ -0,0 +1,23 @@
+"""Fabricatio is a Python library for building llm app using event-based agent structure."""
+
+from fabricatio_core import parser, utils
+from fabricatio_core.journal import logger
+from fabricatio_core.models.action import Action, WorkFlow
+from fabricatio_core.models.role import Role
+from fabricatio_core.models.task import Task
+from fabricatio_core.rust import CONFIG, TEMPLATE_MANAGER, Event
+from fabricatio_tool import fs
+
+__all__ = [
+    "CONFIG",
+    "TEMPLATE_MANAGER",
+    "Action",
+    "Event",
+    "Role",
+    "Task",
+    "WorkFlow",
+    "fs",
+    "logger",
+    "parser",
+    "utils",
+]
