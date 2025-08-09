2b1a6e75d259c0ce41952bd2dc66ac7754fefa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:43 +0000

    Add package bkmr/4.30.0/bkmr-4.30.0.tar.gz

---
+++ packages/bkmr/bkmr-4.30.0.tar.gz/bkmr-4.30.0/bkmr/src/lib.rs
@@ -0,0 +1,20 @@
+// src/lib.rs
+#![crate_type = "lib"]
+#![crate_name = "bkmr"]
+
+extern crate skim;
+
+// Core modules
+pub mod application;
+pub mod domain;
+pub mod infrastructure;
+
+// CLI modules
+pub mod app_state;
+pub mod cli;
+pub mod config;
+pub mod exitcode;
+pub mod util;
+
+#[cfg(test)]
+mod tests {}
