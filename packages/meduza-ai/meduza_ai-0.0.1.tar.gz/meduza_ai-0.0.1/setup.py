39cf5377dd299680653b2d4dc285569b4356417e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:16 +0000

    Add package meduza-ai/0.0.1/meduza_ai-0.0.1.tar.gz

---
+++ packages/meduza-ai/meduza_ai-0.0.1.tar.gz/meduza_ai-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="meduza-ai",
+    version="0.0.1",
+    author="Matt Aliev",
+    description="Build AI Agent - coming soon",
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
