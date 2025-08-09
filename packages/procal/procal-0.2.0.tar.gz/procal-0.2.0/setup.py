2e613cda6b81241044d19b59e722920095a43f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:08 +0000

    Add package procal/0.2.0/procal-0.2.0.tar.gz

---
+++ packages/procal/procal-0.2.0.tar.gz/procal-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+  description = f.read()
+
+setup(
+  name='procal',
+  version='0.2.0',
+  packages=find_packages(),
+  install_requires=[],
+  long_description=description,
+  long_description_content_type="text/markdown",
+)
