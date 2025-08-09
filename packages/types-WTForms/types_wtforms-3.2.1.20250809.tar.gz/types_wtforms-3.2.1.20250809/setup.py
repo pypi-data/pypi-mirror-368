690b1ad363c0617d194fec08c3929b136c10460e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:08 +0000

    Add package types-WTForms/3.2.1.20250809/types_wtforms-3.2.1.20250809.tar.gz

---
+++ packages/types-WTForms/types_wtforms-3.2.1.20250809.tar.gz/types_wtforms-3.2.1.20250809/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'wtforms-stubs': ['__init__.pyi', 'csrf/__init__.pyi', 'csrf/core.pyi', 'csrf/session.pyi', 'fields/__init__.pyi', 'fields/choices.pyi', 'fields/core.pyi', 'fields/datetime.pyi', 'fields/form.pyi', 'fields/list.pyi', 'fields/numeric.pyi', 'fields/simple.pyi', 'form.pyi', 'i18n.pyi', 'meta.pyi', 'utils.pyi', 'validators.pyi', 'widgets/__init__.pyi', 'widgets/core.pyi', 'METADATA.toml', 'py.typed']})
