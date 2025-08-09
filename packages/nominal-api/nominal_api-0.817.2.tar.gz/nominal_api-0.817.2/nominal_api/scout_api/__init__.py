0f19bafb55e1c8accdbd6265c6aca6ca78d9fe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:28 +0000

    Add package nominal-api/0.817.2/nominal_api-0.817.2.tar.gz

---
+++ packages/nominal-api/nominal_api-0.817.2.tar.gz/nominal_api-0.817.2/nominal_api/scout_api/__init__.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from .._impl import (
+    scout_api_ChannelLocator as ChannelLocator,
+    scout_api_ClosedWithFurtherActionDispositionState as ClosedWithFurtherActionDispositionState,
+    scout_api_ClosedWithIgnoreDispositionState as ClosedWithIgnoreDispositionState,
+    scout_api_DataSourceRefName as DataSourceRefName,
+    scout_api_DispositionState as DispositionState,
+    scout_api_DispositionStateVisitor as DispositionStateVisitor,
+    scout_api_HexColor as HexColor,
+    scout_api_PendingReviewDispositionState as PendingReviewDispositionState,
+    scout_api_Priority as Priority,
+)
+
+__all__ = [
+    'ChannelLocator',
+    'ClosedWithFurtherActionDispositionState',
+    'ClosedWithIgnoreDispositionState',
+    'DataSourceRefName',
+    'DispositionState',
+    'DispositionStateVisitor',
+    'HexColor',
+    'PendingReviewDispositionState',
+    'Priority',
+]
+
