970b2fc4dc103db716298cd3b8e8151480d7c708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:05:02 +0000

    Add package secret-ai-sdk/0.1.4/secret_ai_sdk-0.1.4-py3-none-any.whl

---
+++ packages/secret-ai-sdk/secret_ai_sdk-0.1.4-py3-none-any.whl/secret_ai_sdk/_config.py
@@ -0,0 +1,31 @@
+# config.py
+
+"""
+Module Secret AI SDK configurations and constants
+"""
+
+import logging
+
+LOG_LEVEL = 'SECRET_SDK_LOG_LEVEL' # log level env var
+
+# Define a dictionary to map log level names to their corresponding logging levels
+LOG_LEVELS = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warn': logging.WARNING,
+    'warning': logging.WARNING,  # Allow both 'warn' and 'warning' as valid values
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+    'fatal': logging.CRITICAL
+}
+
+API_KEY = 'SECRET_AI_API_KEY' # api key env var
+
+# SECRET
+SECRET_CHAIN_ID_DEFAULT = 'secret-4'
+SECRET_WORKER_SMART_CONTRACT_DEFAULT = 'secret1xv90yettghx8uv6ug23knaf5mjqwlsghau6aqa'
+SECRET_NODE_URL_DEFAULT = 'https://lcd.secret.tactus.starshell.net/'
+
+SECRET_CHAIN_ID = 'SECRET_CHAIN_ID' # points to the name of the env var for secret chain id
+SECRET_WORKER_SMART_CONTRACT = 'SECRET_WORKER_SMART_CONTRACT' #points to the env var for the smart contract address for secret worker management
+SECRET_NODE_URL = 'SECRET_NODE_URL' # points to the name of the env var for secret node url
