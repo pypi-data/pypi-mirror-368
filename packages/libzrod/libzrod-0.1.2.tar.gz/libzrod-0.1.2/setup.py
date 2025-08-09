1d85016fead488b072091bbea09cbe4f49e555dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:58 +0000

    Add package libzrod/0.1.2/libzrod-0.1.2.tar.gz

---
+++ packages/libzrod/libzrod-0.1.2.tar.gz/libzrod-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from skbuild import setup
+setup(
+    name="libzrod",
+    version="0.1.0",
+    description="ZROD Library",
+    author="Walter Phillips",
+    license="Commercial - Contact Me",
+    packages=[
+        "libzrod",
+    ],
+    python_requires=">=3.8",
+)
+'''
+${LIBAWS_PATH}/libaws-c-auth.a
+${LIBAWS_PATH}/libaws-c-cal.a
+${LIBAWS_PATH}/libaws-c-common.a
+${LIBAWS_PATH}/libaws-c-compression.a
+${LIBAWS_PATH}/libaws-c-event-stream.a
+${LIBAWS_PATH}/libaws-c-http.a
+${LIBAWS_PATH}/libaws-c-io.a
+${LIBAWS_PATH}/libaws-c-mqtt.a
+${LIBAWS_PATH}/libaws-c-s3.a
+${LIBAWS_PATH}/libaws-c-sdkutils.a
+${LIBAWS_PATH}/libaws-checksums.a
+${LIBAWS_PATH}/libaws-cpp-sdk-cognito-identity.a
+${LIBAWS_PATH}/libaws-cpp-sdk-core.a
+${LIBAWS_PATH}/libaws-cpp-sdk-dynamodb.a
+${LIBAWS_PATH}/libaws-cpp-sdk-s3.a
+${LIBAWS_PATH}/libaws-crt-cpp.a
+'''
