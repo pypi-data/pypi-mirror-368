cae9b6d9660efb417210a51473ee9e67f8434b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 14:04:37 +0000

    Add package pvevti/2025.8.8.2/pvevti-2025.8.8.2.tar.gz

---
+++ packages/pvevti/pvevti-2025.8.8.2.tar.gz/pvevti-2025.8.8.2/src/pvevti/__init__.py
@@ -0,0 +1,20 @@
+# Define the __all__ variable
+__all__ = ["csvutil", "genutil", "jsonutil", "pdfutil"]
+__version__ = "2025.8.5.7"
+
+#s = time.time()
+#print('[pvevti] Import csvutil module')
+#from . import csvutil
+#print('       Done ({:.0f}ms)'.format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import genutil module')
+#from . import genutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import jsonutil module')
+#from . import jsonutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
+#s = time.time()
+#print('[pvevti] Import pdfutil module')
+#from . import pdfutil
+#print("       Done ({:.0f}ms)".format(1000*(time.time()-s)))
