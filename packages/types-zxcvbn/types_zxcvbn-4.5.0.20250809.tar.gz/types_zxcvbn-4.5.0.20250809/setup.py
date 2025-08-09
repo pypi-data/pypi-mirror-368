f57cd55317358cbea1e52df17d41446f80beb196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:05 +0000

    Add package types-zxcvbn/4.5.0.20250809/types_zxcvbn-4.5.0.20250809.tar.gz

---
+++ packages/types-zxcvbn/types_zxcvbn-4.5.0.20250809.tar.gz/types_zxcvbn-4.5.0.20250809/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'zxcvbn-stubs': ['__init__.pyi', 'adjacency_graphs.pyi', 'feedback.pyi', 'frequency_lists.pyi', 'matching.pyi', 'scoring.pyi', 'time_estimates.pyi', 'METADATA.toml', 'py.typed']})
