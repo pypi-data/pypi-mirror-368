cb468939fb4e7943d48a7d6bbdadcd5be7fed18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 14:05:02 +0000

    Add package PySEMO/0.2/pysemo-0.2.tar.gz

---
+++ packages/PySEMO/pysemo-0.2.tar.gz/pysemo-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PySEMO",
+    version="0.2",
+    packages=find_packages(),
+    install_requires=[
+        "pycryptodome"
+    ],
+    description="TikTok signing library",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Kameran",
+    author_email="hh@gmail.com",
+    license="MIT",
+)
