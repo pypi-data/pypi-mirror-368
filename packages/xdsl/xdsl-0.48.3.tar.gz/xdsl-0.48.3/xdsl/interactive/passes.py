323c1f00b9462062dab9d2e41b93cba54a5b2725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:05:06 +0000

    Add package xdsl/0.48.3/xdsl-0.48.3.tar.gz

---
+++ packages/xdsl/xdsl-0.48.3.tar.gz/xdsl-0.48.3/xdsl/interactive/passes.py
@@ -0,0 +1,70 @@
+from collections.abc import Callable
+from typing import NamedTuple
+
+from xdsl.context import Context
+from xdsl.dialects import builtin
+from xdsl.ir import Dialect
+from xdsl.passes import ModulePass
+from xdsl.transforms.mlir_opt import MLIROptPass
+
+
+class AvailablePass(NamedTuple):
+    """
+    Type alias for the attributes that describe a pass, namely the display name of the
+    pass, the module pass and pass spec.
+    """
+
+    module_pass: type[ModulePass] | ModulePass
+
+    def __str__(self) -> str:
+        module_pass = self.module_pass
+        if isinstance(module_pass, ModulePass):
+            return str(module_pass.pipeline_pass_spec())
+        else:
+            return module_pass.name
+
+
+def get_new_registered_context(
+    all_dialects: tuple[tuple[str, Callable[[], Dialect]], ...],
+) -> Context:
+    """
+    Generates a new Context, registers it and returns it.
+    """
+    ctx = Context(True)
+    for dialect_name, dialect_factory in all_dialects:
+        ctx.register_dialect(dialect_name, dialect_factory)
+    return ctx
+
+
+def iter_condensed_passes(
+    ctx: Context,
+    input: builtin.ModuleOp,
+    all_passes: tuple[tuple[str, type[ModulePass]], ...],
+):
+    for _, pass_type in all_passes:
+        if pass_type is MLIROptPass:
+            # Always keep MLIROptPass as an option in condensed list
+            yield AvailablePass(pass_type)
+            continue
+        cloned_module = input.clone()
+        cloned_ctx = ctx.clone()
+        try:
+            pass_instance = pass_type()
+            pass_instance.apply(cloned_ctx, cloned_module)
+            if input.is_structurally_equivalent(cloned_module):
+                continue
+        except Exception:
+            continue
+        yield AvailablePass(pass_instance)
+
+
+def get_condensed_pass_list(
+    ctx: Context,
+    input: builtin.ModuleOp,
+    all_passes: tuple[tuple[str, type[ModulePass]], ...],
+) -> tuple[AvailablePass, ...]:
+    """
+    Function that returns the condensed pass list for a given ModuleOp, i.e. the passes that
+    change the ModuleOp.
+    """
+    return tuple(iter_condensed_passes(ctx, input, all_passes))
