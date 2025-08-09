60dc0e59c6b8601145def9afb3fdd7eb274cf80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:04 +0000

    Add package mcp-add-project/0.1.0/mcp_add_project-0.1.0-py3-none-any.whl

---
+++ packages/mcp-add-project/mcp_add_project-0.1.0-py3-none-any.whl/mcp_add_project/__init__.py
@@ -0,0 +1,20 @@
+from mcp.server.fastmcp import FastMCP
+
+# Create an MCP server
+mcp = FastMCP("Demo")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    """Get a personalized greeting"""
+    return f"Hello, {name}!"
+
+
+def main() -> None:
+    mcp.run(transport='stdio')
