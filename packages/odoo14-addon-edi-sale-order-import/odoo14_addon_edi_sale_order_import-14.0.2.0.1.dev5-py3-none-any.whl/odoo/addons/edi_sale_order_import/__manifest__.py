a45f26f6ab316d58400a32504ff5657be5dbecf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:33 +0000

    Add package odoo14-addon-edi-sale-order-import/14.0.2.0.1.dev5/odoo14_addon_edi_sale_order_import-14.0.2.0.1.dev5-py3-none-any.whl

---
+++ packages/odoo14-addon-edi-sale-order-import/odoo14_addon_edi_sale_order_import-14.0.2.0.1.dev5-py3-none-any.whl/odoo/addons/edi_sale_order_import/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Camptocamp SA
+# @author: Simone Orsi <simone.orsi@camptocamp.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EDI Sale order import",
+    "summary": """
+    MODULE DEPRECATED: use `edi_sale_oca`.
+
+    Plug sale_order_import into EDI machinery.""",
+    "version": "14.0.2.0.0",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/edi",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "maintainers": ["simahawk"],
+    # Depend on edi_sale_oca which now provides the component.
+    "depends": ["edi_sale_oca"],
+    "auto_install": False,
+    "data": ["templates/exchange_chatter_msg.xml"],
+    "post_load": "post_load_hook",
+}
