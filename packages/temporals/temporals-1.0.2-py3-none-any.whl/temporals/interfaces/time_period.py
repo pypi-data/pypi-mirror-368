bb3e5bcec8dd6e105ae938b1e0eef94dfc5f8885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:13 +0000

    Add package temporals/1.0.2/temporals-1.0.2-py3-none-any.whl

---
+++ packages/temporals/temporals-1.0.2-py3-none-any.whl/temporals/interfaces/time_period.py
@@ -0,0 +1,12 @@
+from abc import abstractmethod
+from .base_period import AbstractPeriod
+
+
+class AbstractTimePeriod(AbstractPeriod):
+    """ A period of time within a 24-hour day that does not overflow into the next day. Implementing periods must also
+    implement the `combine` method which allows a TimePeriod to be combined with a DatePeriod to create a DateTimePeriod
+    """
+
+    @abstractmethod
+    def combine(self, other):
+        ...
