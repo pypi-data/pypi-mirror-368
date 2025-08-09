d9b3a37321d12b78be2822b35868599d25d6d453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:00 +0000

    Add package dosctl/0.2.0/dosctl-0.2.0-py3-none-any.whl

---
+++ packages/dosctl/dosctl-0.2.0-py3-none-any.whl/dosctl/commands/list.py
@@ -0,0 +1,25 @@
+import click
+from dosctl.lib.decorators import ensure_cache
+from dosctl.lib.display import sort_games, display_games
+from dosctl.config import INSTALLED_DIR
+
+@click.command(name="list")
+@click.option('-s', '--sort-by', type=click.Choice(['name', 'year'], case_sensitive=False), default='name', help='Sort the list by name or year.')
+@click.option('-i', '--installed', is_flag=True, default=False, help='Only list installed games.')
+@ensure_cache
+def list_games(collection, sort_by, installed):
+    """Lists all available games from the local cache."""
+    
+    games = collection.get_games()
+
+    if installed:
+        installed_ids = {p.name for p in INSTALLED_DIR.iterdir() if p.is_dir()}
+        games = [g for g in games if g['id'] in installed_ids]
+
+    if not games:
+        message = "No games are currently installed." if installed else "No games found in cache."
+        click.echo(message)
+        return
+
+    games = sort_games(games, sort_by)
+    display_games(games)
