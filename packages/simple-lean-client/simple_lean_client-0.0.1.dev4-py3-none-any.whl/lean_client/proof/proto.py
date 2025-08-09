b13d77072ddd5592a3919e08c8062b5c713a759a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:10 +0000

    Add package simple-lean-client/0.0.1.dev4/simple_lean_client-0.0.1.dev4-py3-none-any.whl

---
+++ packages/simple-lean-client/simple_lean_client-0.0.1.dev4-py3-none-any.whl/lean_client/proof/proto.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class ProofConfig(BaseModel):
+    all_tactics: bool = False
+    ast: bool = False
+    tactics: bool = False
+    premises: bool = False
+    timeout: float = 300.0
+
+
+class LeanProofStatus(Enum):
+    PENDING = "pending"
+    RUNNING = "running"
+    FINISHED = "finished"
+    ERROR = "error"
+
+
+class ProofResult(BaseModel):
+    success: bool | None = None
+    status: LeanProofStatus
+    result: dict | None = None
+    error_message: str | None = None
+
+
+class Proof(BaseModel):
+    id: str
