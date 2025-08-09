4aaa26640bf0ef19922b6ad64a9164d1711b0491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 14:04:41 +0000

    Add package boto3-stubs-full/1.40.6/boto3_stubs_full-1.40.6-py3-none-any.whl

---
+++ packages/boto3-stubs-full/boto3_stubs_full-1.40.6-py3-none-any.whl/mypy_boto3_evs/client.pyi
@@ -0,0 +1,250 @@
+"""
+Type annotations for evs service Client.
+
+[Documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_evs.client import EVSClient
+
+    session = Session()
+    client: EVSClient = session.client("evs")
+    ```
+"""
+
+from __future__ import annotations
+
+import sys
+from typing import Any, overload
+
+from botocore.client import BaseClient, ClientMeta
+from botocore.errorfactory import BaseClientExceptions
+from botocore.exceptions import ClientError as BotocoreClientError
+
+from .paginator import (
+    ListEnvironmentHostsPaginator,
+    ListEnvironmentsPaginator,
+    ListEnvironmentVlansPaginator,
+)
+from .type_defs import (
+    CreateEnvironmentHostRequestTypeDef,
+    CreateEnvironmentHostResponseTypeDef,
+    CreateEnvironmentRequestTypeDef,
+    CreateEnvironmentResponseTypeDef,
+    DeleteEnvironmentHostRequestTypeDef,
+    DeleteEnvironmentHostResponseTypeDef,
+    DeleteEnvironmentRequestTypeDef,
+    DeleteEnvironmentResponseTypeDef,
+    GetEnvironmentRequestTypeDef,
+    GetEnvironmentResponseTypeDef,
+    ListEnvironmentHostsRequestTypeDef,
+    ListEnvironmentHostsResponseTypeDef,
+    ListEnvironmentsRequestTypeDef,
+    ListEnvironmentsResponseTypeDef,
+    ListEnvironmentVlansRequestTypeDef,
+    ListEnvironmentVlansResponseTypeDef,
+    ListTagsForResourceRequestTypeDef,
+    ListTagsForResourceResponseTypeDef,
+    TagResourceRequestTypeDef,
+    UntagResourceRequestTypeDef,
+)
+
+if sys.version_info >= (3, 9):
+    from builtins import dict as Dict
+    from builtins import type as Type
+    from collections.abc import Mapping
+else:
+    from typing import Dict, Mapping, Type
+if sys.version_info >= (3, 12):
+    from typing import Literal, Unpack
+else:
+    from typing_extensions import Literal, Unpack
+
+__all__ = ("EVSClient",)
+
+class Exceptions(BaseClientExceptions):
+    ClientError: Type[BotocoreClientError]
+    ResourceNotFoundException: Type[BotocoreClientError]
+    ServiceQuotaExceededException: Type[BotocoreClientError]
+    TagPolicyException: Type[BotocoreClientError]
+    ThrottlingException: Type[BotocoreClientError]
+    TooManyTagsException: Type[BotocoreClientError]
+    ValidationException: Type[BotocoreClientError]
+
+class EVSClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs.html#EVS.Client)
+    [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/)
+    """
+
+    meta: ClientMeta
+
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        EVSClient exceptions.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs.html#EVS.Client)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#exceptions)
+        """
+
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/can_paginate.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#can_paginate)
+        """
+
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/generate_presigned_url.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#generate_presigned_url)
+        """
+
+    def create_environment(
+        self, **kwargs: Unpack[CreateEnvironmentRequestTypeDef]
+    ) -> CreateEnvironmentResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/create_environment.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#create_environment)
+        """
+
+    def create_environment_host(
+        self, **kwargs: Unpack[CreateEnvironmentHostRequestTypeDef]
+    ) -> CreateEnvironmentHostResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/create_environment_host.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#create_environment_host)
+        """
+
+    def delete_environment(
+        self, **kwargs: Unpack[DeleteEnvironmentRequestTypeDef]
+    ) -> DeleteEnvironmentResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/delete_environment.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#delete_environment)
+        """
+
+    def delete_environment_host(
+        self, **kwargs: Unpack[DeleteEnvironmentHostRequestTypeDef]
+    ) -> DeleteEnvironmentHostResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/delete_environment_host.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#delete_environment_host)
+        """
+
+    def get_environment(
+        self, **kwargs: Unpack[GetEnvironmentRequestTypeDef]
+    ) -> GetEnvironmentResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/get_environment.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#get_environment)
+        """
+
+    def list_environment_hosts(
+        self, **kwargs: Unpack[ListEnvironmentHostsRequestTypeDef]
+    ) -> ListEnvironmentHostsResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/list_environment_hosts.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#list_environment_hosts)
+        """
+
+    def list_environment_vlans(
+        self, **kwargs: Unpack[ListEnvironmentVlansRequestTypeDef]
+    ) -> ListEnvironmentVlansResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/list_environment_vlans.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#list_environment_vlans)
+        """
+
+    def list_environments(
+        self, **kwargs: Unpack[ListEnvironmentsRequestTypeDef]
+    ) -> ListEnvironmentsResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/list_environments.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#list_environments)
+        """
+
+    def list_tags_for_resource(
+        self, **kwargs: Unpack[ListTagsForResourceRequestTypeDef]
+    ) -> ListTagsForResourceResponseTypeDef:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/list_tags_for_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#list_tags_for_resource)
+        """
+
+    def tag_resource(self, **kwargs: Unpack[TagResourceRequestTypeDef]) -> Dict[str, Any]:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/tag_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#tag_resource)
+        """
+
+    def untag_resource(self, **kwargs: Unpack[UntagResourceRequestTypeDef]) -> Dict[str, Any]:
+        """
+        <note> <p>Amazon EVS is in public preview release and is subject to change.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/untag_resource.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#untag_resource)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_environment_hosts"]
+    ) -> ListEnvironmentHostsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/get_paginator.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_environment_vlans"]
+    ) -> ListEnvironmentVlansPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/get_paginator.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#get_paginator)
+        """
+
+    @overload  # type: ignore[override]
+    def get_paginator(  # type: ignore[override]
+        self, operation_name: Literal["list_environments"]
+    ) -> ListEnvironmentsPaginator:
+        """
+        Create a paginator for an operation.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/evs/client/get_paginator.html)
+        [Show boto3-stubs-full documentation](https://youtype.github.io/boto3_stubs_docs/mypy_boto3_evs/client/#get_paginator)
+        """
