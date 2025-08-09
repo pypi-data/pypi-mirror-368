511ad48940993e8a446615b490aabc3ca362f8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:28 +0000

    Add package epeira/0.1.1/epeira-0.1.1-py3-none-any.whl

---
+++ packages/epeira/epeira-0.1.1-py3-none-any.whl/epeira/helpers/parsing.py
@@ -0,0 +1,9 @@
+"""String parsing tools."""
+
+def get_default_archetype_prefix():
+    """Return standard string prefix for archetype classes, to be replaced with domain strings."""
+    return "ARCH"
+
+def get_subcomponent_indicator():
+    """Return string that indicates a model is meant to be a subcomponent."""
+    return "subcomponent"
