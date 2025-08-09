46201d5f6edaa7cc7751c5fbf8ae4d94ba617575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:15 +0000

    Add package pointless/1.0.28/pointless-1.0.28.tar.gz

---
+++ packages/pointless/pointless-1.0.28.tar.gz/pointless-1.0.28/include/pointless/pointless_recreate.h
@@ -0,0 +1,13 @@
+#ifndef __POINTLESS__RECREATE__H__
+#define __POINTLESS__RECREATE__H__
+
+#include <stdlib.h>
+
+#include <pointless/pointless_reader.h>
+#include <pointless/pointless_create.h>
+
+uint32_t pointless_recreate_value(pointless_t* p_in, pointless_value_t* v_in, pointless_create_t* c_out, const char** error);
+
+int pointless_recreate_64(const char* fname_in, const char* fname_out, const char** error);
+
+#endif
