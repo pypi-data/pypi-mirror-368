03babe9dd675399fe5b06bfe82bb1b85fb26b383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 14:04:51 +0000

    Add package gamesolverlib/1.0.0/gamesolverlib-1.0.0-py3-none-any.whl

---
+++ packages/gamesolverlib/gamesolverlib-1.0.0-py3-none-any.whl/gamesolverlib/__init__.py
@@ -0,0 +1,7 @@
+from ._wordfinder_lib.wordfinder import WordFinder
+from .__about__ import *
+from .__about__ import __all__ as _about_all
+
+
+__all__: list[str] = ["WordFinder"]
+__all__.extend(_about_all)
---
+++ packages/gamesolverlib/gamesolverlib-1.0.0-py3-none-any.whl/gamesolverlib/__init__.pyi
@@ -0,0 +1,4 @@
+from .__about__ import *
+from ._wordfinder_lib.wordfinder import WordFinder as WordFinder
+
+__all__ = ['WordFinder']
