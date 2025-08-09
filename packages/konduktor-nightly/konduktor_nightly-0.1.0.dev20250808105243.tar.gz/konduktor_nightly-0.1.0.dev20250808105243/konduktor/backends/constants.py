068135a7a4cda27b85c2c20e09103ba2dda92544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:26 +0000

    Add package konduktor-nightly/0.1.0.dev20250808105243/konduktor_nightly-0.1.0.dev20250808105243.tar.gz

---
+++ packages/konduktor-nightly/konduktor_nightly-0.1.0.dev20250808105243.tar.gz/konduktor_nightly-0.1.0.dev20250808105243/konduktor/backends/constants.py
@@ -0,0 +1,13 @@
+KONDUKTOR_SSH_PORT = 2222
+
+# Common labels used across JobSets and Deployments
+JOB_NAME_LABEL = 'trainy.ai/job-name'
+DEPLOYMENT_NAME_LABEL = 'trainy.ai/deployment-name'
+AIBRIX_NAME_LABEL = 'model.aibrix.ai/name'
+USERID_LABEL = 'trainy.ai/user-id'
+USER_LABEL = 'trainy.ai/username'
+ACCELERATOR_LABEL = 'trainy.ai/accelerator'
+NUM_ACCELERATORS_LABEL = 'trainy.ai/num-accelerators'
+
+# Secret labels
+SECRET_BASENAME_LABEL = 'konduktor/basename'
