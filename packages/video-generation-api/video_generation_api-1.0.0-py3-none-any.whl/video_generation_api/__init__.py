347741f73e9c5ac7e5f5c32e1a6fc8303b9ebdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:27 +0000

    Add package video-generation-api/1.0.0/video_generation_api-1.0.0-py3-none-any.whl

---
+++ packages/video-generation-api/video_generation_api-1.0.0-py3-none-any.whl/video_generation_api/__init__.py
@@ -0,0 +1,33 @@
+"""
+Video Generation API
+A powerful API for intelligent video generation with professional effects and subtitles.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Leo Wang"
+__email__ = "preangelleo@gmail.com"
+
+from .core_functions import (
+    create_video_with_subtitles_onestep,
+    merge_audio_image_to_video_with_effects,
+    add_subtitles_to_video,
+    add_subtitles_to_video_portrait,
+    AfterEffectsProcess,
+    get_local_font,
+    get_output_filename,
+    EFFECTS
+)
+
+from .api_client import VideoGenerationClient
+
+__all__ = [
+    "create_video_with_subtitles_onestep",
+    "merge_audio_image_to_video_with_effects", 
+    "add_subtitles_to_video",
+    "add_subtitles_to_video_portrait",
+    "AfterEffectsProcess",
+    "get_local_font",
+    "get_output_filename",
+    "EFFECTS",
+    "VideoGenerationClient"
+]
