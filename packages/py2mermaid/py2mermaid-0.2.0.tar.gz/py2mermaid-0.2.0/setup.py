47a18edb7d6dcef21c2abb3998cfddc6999ce52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:08 +0000

    Add package py2mermaid/0.2.0/py2mermaid-0.2.0.tar.gz

---
+++ packages/py2mermaid/py2mermaid-0.2.0.tar.gz/py2mermaid-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py2mermaid",
+    version="0.2.0",
+    author="raghuvasanthrao",
+    author_email="k.s.raghuvsanthrao@gmail.com",
+    description="A Python helper library to create Mermaid.js diagrams",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/raghuvasanthrao/mermaidpy",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
