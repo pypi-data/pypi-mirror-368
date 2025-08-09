3243cda9536780c57523de016de847206d460f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:30 +0000

    Add package pygeai/0.4.0b5/pygeai-0.4.0b5-py3-none-any.whl

---
+++ packages/pygeai/pygeai-0.4.0b5-py3-none-any.whl/pygeai/lab/clients.py
@@ -0,0 +1,17 @@
+from pygeai.core.base.clients import BaseClient
+
+
+class AILabClient(BaseClient):
+
+    def __init__(self, api_key: str = None, base_url: str = None, alias: str = None):
+        super().__init__(api_key, base_url, alias)
+        self.project_id = self.__get_project_id()
+
+    def __get_project_id(self):
+        response = None
+        try:
+            response = AdminClient().validate_api_token()
+            return response.get("projectId")
+        except Exception as e:
+            logger.error(f"Error retrieving project_id from GEAI. Response: {response}: {e}")
+            raise APIError(f"Error retrieving project_id from GEAI: {e}")
