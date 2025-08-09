601b76490a994c8c1bd004b05d5d8f042aa98380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:04:46 +0000

    Add package maleo-cds/0.1.17/maleo_cds-0.1.17.tar.gz

---
+++ packages/maleo-cds/maleo_cds-0.1.17.tar.gz/maleo_cds-0.1.17/maleo_cds/schemas/data/coding/record.py
@@ -0,0 +1,95 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from maleo_soma.mixins.data import DataIdentifier, DataStatus, DataTimestamp
+from maleo_soma.mixins.general import UserId, OptionalOrganizationId, OptionalAge
+from maleo_metadata.schemas.data.gender import OptionalSimpleGender
+from maleo_cds.mixins.general import (
+    ChiefComplaint,
+    OptionalAdditionalComplaint,
+    OptionalPainScale,
+    OptionalOnset,
+    OptionalChronology,
+    OptionalLocation,
+    OptionalAggravatingFactor,
+    OptionalRelievingFactor,
+    OptionalPersonalMedicalHistory,
+    OptionalFamilyMedicalHistory,
+    OptionalHabitActivityOccupation,
+    OptionalConsumedMedication,
+    OptionalSystolicBloodPressure,
+    OptionalDiastolicBloodPressure,
+    OptionalTemperature,
+    OptionalRespirationRate,
+    OptionalHeartRate,
+    OptionalOxygenSaturation,
+    OptionalAbdominalCircumference,
+    OptionalWaistCircumference,
+    OptionalWeight,
+    OptionalHeight,
+    OptionalBodyMassIndex,
+    OptionalOrganExaminationDetail,
+    OptionalOverallPlan,
+    OptionalTreatment,
+)
+from maleo_cds.dtos.coding.diagnosis import ListOfDiagnosesMixin
+from .procedure import ProcedureRecommendationSchemaMixin
+
+
+class RecordDataSchema(
+    ProcedureRecommendationSchemaMixin,
+    OptionalTreatment,
+    OptionalOverallPlan,
+    ListOfDiagnosesMixin,
+    OptionalOrganExaminationDetail,
+    OptionalBodyMassIndex,
+    OptionalHeight,
+    OptionalWeight,
+    OptionalWaistCircumference,
+    OptionalAbdominalCircumference,
+    OptionalOxygenSaturation,
+    OptionalHeartRate,
+    OptionalRespirationRate,
+    OptionalTemperature,
+    OptionalDiastolicBloodPressure,
+    OptionalSystolicBloodPressure,
+    OptionalConsumedMedication,
+    OptionalHabitActivityOccupation,
+    OptionalFamilyMedicalHistory,
+    OptionalPersonalMedicalHistory,
+    OptionalRelievingFactor,
+    OptionalAggravatingFactor,
+    OptionalLocation,
+    OptionalChronology,
+    OptionalOnset,
+    OptionalPainScale,
+    OptionalAdditionalComplaint,
+    ChiefComplaint,
+    OptionalAge,
+    OptionalSimpleGender,
+    UserId,
+    OptionalOrganizationId,
+    DataTimestamp,
+    DataStatus,
+    DataIdentifier,
+):
+    pass
+
+
+class ExpandedRecord(BaseModel):
+    record: RecordDataSchema = Field(..., description="Single record data")
+
+
+class OptionalExpandedRecord(BaseModel):
+    record: Optional[RecordDataSchema] = Field(
+        None, description="Single record data. (Optional)"
+    )
+
+
+class ListOfExpandedRecords(BaseModel):
+    records: List[RecordDataSchema] = Field(..., description="Multiple records data")
+
+
+class OptionalListOfExpandedRecords(BaseModel):
+    records: Optional[List[RecordDataSchema]] = Field(
+        None, description="Multiple records data. (Optional)"
+    )
---
+++ packages/maleo-cds/maleo_cds-0.1.17.tar.gz/maleo_cds-0.1.17/maleo_cds/schemas/parameter/general/coding/record.py
@@ -0,0 +1,92 @@
+from maleo_soma.mixins.general import OptionalAge, OptionalOrganizationId, UserId
+from maleo_soma.mixins.parameter import (
+    IdentifierType as IdentifierTypeMixin,
+    IdentifierValue as IdentifierValueMixin,
+)
+from maleo_soma.schemas.parameter.general import ReadSingleParameterSchema
+from maleo_metadata.schemas.data.gender import OptionalSimpleGender
+from maleo_cds.enums.coding.record import IdentifierType
+from maleo_cds.mixins.general import (
+    ChiefComplaint,
+    OptionalAdditionalComplaint,
+    OptionalPainScale,
+    OptionalOnset,
+    OptionalChronology,
+    OptionalLocation,
+    OptionalAggravatingFactor,
+    OptionalRelievingFactor,
+    OptionalPersonalMedicalHistory,
+    OptionalFamilyMedicalHistory,
+    OptionalHabitActivityOccupation,
+    OptionalConsumedMedication,
+    OptionalSystolicBloodPressure,
+    OptionalDiastolicBloodPressure,
+    OptionalTemperature,
+    OptionalRespirationRate,
+    OptionalHeartRate,
+    OptionalOxygenSaturation,
+    OptionalAbdominalCircumference,
+    OptionalWaistCircumference,
+    OptionalWeight,
+    OptionalHeight,
+    OptionalBodyMassIndex,
+    OptionalOrganExaminationDetail,
+    OptionalOverallPlan,
+    OptionalTreatment,
+)
+from maleo_cds.dtos.coding.diagnosis import ListOfDiagnosesMixin
+from maleo_cds.schemas.data.coding.procedure import ProcedureRecommendationSchemaMixin
+from maleo_cds.types.base.coding.record import IdentifierValueType
+
+
+class ReadSingleParameter(
+    ReadSingleParameterSchema[IdentifierType, IdentifierValueType]
+):
+    pass
+
+
+class CreateOrUpdateBody(
+    ProcedureRecommendationSchemaMixin,
+    OptionalTreatment,
+    OptionalOverallPlan,
+    ListOfDiagnosesMixin,
+    OptionalOrganExaminationDetail,
+    OptionalBodyMassIndex,
+    OptionalHeight,
+    OptionalWeight,
+    OptionalWaistCircumference,
+    OptionalAbdominalCircumference,
+    OptionalOxygenSaturation,
+    OptionalHeartRate,
+    OptionalRespirationRate,
+    OptionalTemperature,
+    OptionalDiastolicBloodPressure,
+    OptionalSystolicBloodPressure,
+    OptionalConsumedMedication,
+    OptionalHabitActivityOccupation,
+    OptionalFamilyMedicalHistory,
+    OptionalPersonalMedicalHistory,
+    OptionalRelievingFactor,
+    OptionalAggravatingFactor,
+    OptionalLocation,
+    OptionalChronology,
+    OptionalOnset,
+    OptionalPainScale,
+    OptionalAdditionalComplaint,
+    ChiefComplaint,
+    OptionalAge,
+    OptionalSimpleGender,
+):
+    pass
+
+
+class CreateParameter(CreateOrUpdateBody, UserId, OptionalOrganizationId):
+    pass
+
+
+class UpdateParameter(
+    CreateOrUpdateBody,
+    IdentifierValueMixin[IdentifierValueType],
+    IdentifierTypeMixin[IdentifierType],
+):
+    pass
