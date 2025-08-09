8e211a8afa4da26d1f667c6092e598ac585fc0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:04:50 +0000

    Add package quick-sentiments/0.2.3/quick_sentiments-0.2.3-py3-none-any.whl

---
+++ packages/quick-sentiments/quick_sentiments-0.2.3-py3-none-any.whl/quick_sentiments/__init__.py
@@ -0,0 +1,8 @@
+# quick_sentiments/__init__.py
+from .pipeline import run_pipeline       # Expose pipeline function
+from .predict import make_predictions # Expose prediction function
+from .preprocess import pre_process
+from .spacy_preprocess import pre_process_spacy  # Expose preprocessing function
+
+__all__ = ['run_pipeline', 'make_predictions', 'pre_process', 'pre_process_spacy']  # Controls what's available in *
+
