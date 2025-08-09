8ee43655375075899f01df54a43c4285e76508fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:21 +0000

    Add package python-servo-gpiod/1.1.1/python_servo_gpiod-1.1.1-py3-none-any.whl

---
+++ packages/python-servo-gpiod/python_servo_gpiod-1.1.1-py3-none-any.whl/python_servo_gpiod/__init__.py
@@ -0,0 +1,18 @@
+"""
+Python Servo Control Library using gpiod
+
+A comprehensive Python implementation for controlling servo motors using the gpiod
+library on Raspberry Pi and other Linux systems with GPIO support.
+
+Author: Svndsn
+License: MIT
+"""
+
+from .servo import Servo, ServoError
+
+__version__ = "1.1.1"
+__author__ = "Svndsn"
+__email__ = "simon.egeris.svendsen@gmail.com"
+__license__ = "MIT"
+
+__all__ = ["Servo", "ServoError"]
