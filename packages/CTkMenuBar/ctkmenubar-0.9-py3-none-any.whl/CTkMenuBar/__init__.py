cc7ef5acadd304eed3abcee4a80888727a81742e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:04:46 +0000

    Add package CTkMenuBar/0.9/ctkmenubar-0.9-py3-none-any.whl

---
+++ packages/CTkMenuBar/ctkmenubar-0.9-py3-none-any.whl/CTkMenuBar/__init__.py
@@ -0,0 +1,12 @@
+"""
+MenuBar widget for customtkinter
+by Akascape
+
+Homepage: https://github.com/Akascape/CTkMenuBar
+"""
+
+__version__ = '0.9'
+
+from .menu_bar import CTkMenuBar
+from .title_menu_win import CTkTitleMenu
+from .dropdown_menu import CustomDropdownMenu
