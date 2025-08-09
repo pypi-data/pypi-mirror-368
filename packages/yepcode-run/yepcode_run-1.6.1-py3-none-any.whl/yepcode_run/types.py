b72aba37314718325a645a803f74344a3ec14c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:49 +0000

    Add package yepcode-run/1.6.1/yepcode_run-1.6.1-py3-none-any.whl

---
+++ packages/yepcode-run/yepcode_run-1.6.1-py3-none-any.whl/yepcode_run/types.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Optional
+from enum import Enum
+
+
+class ExecutionStatus(Enum):
+    CREATED = "CREATED"
+    RUNNING = "RUNNING"
+    FINISHED = "FINISHED"
+    KILLED = "KILLED"
+    REJECTED = "REJECTED"
+    ERROR = "ERROR"
+
+
+@dataclass
+class YepCodeApiConfig:
+    api_host: Optional[str] = None
+    timeout: Optional[int] = None
+    access_token: Optional[str] = None
+    api_token: Optional[str] = None
+    client_id: Optional[str] = None
+    client_secret: Optional[str] = None
+    team_id: Optional[str] = None
+
+
+@dataclass
+class Log:
+    timestamp: str
+    level: str
+    message: str
+
+
+@dataclass
+class ExecutionError:
+    message: Optional[str] = None
+
+
+@dataclass
+class TimelineEvent:
+    status: ExecutionStatus
+    timestamp: str
+    explanation: Optional[str] = None
