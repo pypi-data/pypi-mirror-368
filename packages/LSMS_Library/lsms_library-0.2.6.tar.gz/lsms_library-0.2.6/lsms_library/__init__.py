cc521ff8777b12d51b85c20c807f3f17605fe481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 15:04:23 +0000

    Add package LSMS_Library/0.2.6/lsms_library-0.2.6.tar.gz

---
+++ packages/LSMS_Library/lsms_library-0.2.6.tar.gz/lsms_library-0.2.6/lsms_library/__init__.py
@@ -0,0 +1,7 @@
+from . import country
+from . country import Country, Wave
+from . import local_tools as tools
+#from . import countries
+#from .countries import GhanaLSS
+from .categorical_mapping import ai_agent
+from . import transformations
---
+++ packages/LSMS_Library/lsms_library-0.2.6.tar.gz/lsms_library-0.2.6/lsms_library/countries/Uganda/2018-19/_/household_characteristics.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from lsms_library.local_tools import to_parquet
+
+import sys
+sys.path.append('../../_/')
+import pandas as pd
+import numpy as np
+from uganda import age_sex_composition
+import dvc.api
+
+myvars = dict(fn='../Data/GSEC2.dta',
+              HHID='hhid',
+              sex='h2q3',
+              age='h2q8',
+              months_spent='h2q5')
+
+df = age_sex_composition(**myvars)
+
+df = df.filter(regex='ales ')
+
+N = df.sum(axis=1)
+
+df['log HSize'] = np.log(N[N>0])
+
+# Switch to hhid that is constant across waves...
+#with dvc.api.open('../Data/GSEC1.dta',mode='rb') as dta:
+ #   id = pd.read_stata(dta,convert_categoricals=False)
+
+to_parquet(df, 'household_characteristics.parquet')
