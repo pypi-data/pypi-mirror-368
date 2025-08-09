05cb1a117555d8acfe46a7f815ecb3f20a7c4001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:04:54 +0000

    Add package agentrun-sdk/0.1.2/agentrun_sdk-0.1.2-py3-none-any.whl

---
+++ packages/agentrun-sdk/agentrun_sdk-0.1.2-py3-none-any.whl/agentrun_operation_sdk/cli/cli.py
@@ -0,0 +1,19 @@
+import typer
+from ..utils.logging_config import setup_toolkit_logging
+from .runtime.commands import create, configure
+
+app = typer.Typer(name="agentrun", help="HWAgentRun CLI", add_completion=False, rich_markup_mode="rich")
+
+setup_toolkit_logging(mode="cli")
+
+app.command("create")(create)
+app.command("configure")(configure)
+
+
+def main():
+    """Entry point for the CLI application."""
+    app()
+
+
+if __name__ == "__main__":
+    main()
