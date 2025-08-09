6397a2b0b377a2d9bd34e16fcca156916f7ac1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:46 +0000

    Add package pycona/0.4/pycona-0.4-py3-none-any.whl

---
+++ packages/pycona/pycona-0.4-py3-none-any.whl/pycona/active_algorithms/mineacq.py
@@ -0,0 +1,142 @@
+import time
+import cpmpy as cp
+
+from cpmpy.transformations.get_variables import get_variables
+
+from .algorithm_core import AlgorithmCAInteractive
+from .utils import is_clique, can_be_clique
+from ..ca_environment.active_ca import ActiveCAEnv
+from ..utils import get_relation, get_scope, get_kappa
+from ..problem_instance import ProblemInstance
+from ..answering_queries import Oracle, UserOracle
+from .. import Metrics
+
+
+class MineAcq(AlgorithmCAInteractive):
+
+    """
+    QuAcq variation algorithm, using mine&Ask to detect types of variables and ask generalization queries. From:
+    "Detecting Types of Variables for Generalization in Constraint Acquisition", ICTAI 2015.
+    """
+
+    def __init__(self, ca_env: ActiveCAEnv = None, qg_max=10):
+        """
+        Initialize the MineAcq algorithm with an optional constraint acquisition environment.
+
+        :param ca_env: An instance of ActiveCAEnv, default is None.
+        :param qg_max: maximum number of generalization queries
+        """
+        super().__init__(ca_env)
+        self._negativeQ = []
+        self._qg_max = qg_max
+
+    def learn(self, instance: ProblemInstance, oracle: Oracle = UserOracle(), verbose=0, metrics: Metrics = None, X=None):
+        """
+        Learn constraints using the MineAcq algorithm by generating queries and analyzing the results. 
+        Using mine&ask to detect types of variables and ask generalization queries.
+
+        :param instance: the problem instance to acquire the constraints for
+        :param oracle: An instance of Oracle, default is to use the user as the oracle.
+        :param verbose: Verbosity level, default is 0.
+        :param metrics: statistics logger during learning
+        :param X: List of variables to consider for learning. If None, uses all variables from the instance.
+        :return: the learned instance
+        """
+        self.env.init_state(instance, oracle, verbose, metrics)
+
+        if X is None:
+            X = list(self.env.instance.variables.flat)
+
+        if len(self.env.instance.bias) == 0:
+            self.env.instance.construct_bias(X)
+
+        while True:
+            if self.env.verbose > 0:
+                print("Size of CL: ", len(self.env.instance.cl))
+                print("Size of B: ", len(self.env.instance.bias))
+                print("Number of Queries: ", self.env.metrics.total_queries)
+
+            gen_start = time.time()
+            Y = self.env.run_query_generation(X)
+            gen_end = time.time()
+
+            if len(Y) == 0:
+                # if no query can be generated it means we have (prematurely) converged to the target network -----
+                self.env.metrics.finalize_statistics()
+                if self.env.verbose >= 1:
+                    print(f"\nLearned {self.env.metrics.cl} constraints in "
+                          f"{self.env.metrics.total_queries} queries.")
+                self.env.instance.bias = []
+                return self.env.instance
+
+            self.env.metrics.increase_generation_time(gen_end - gen_start)
+            self.env.metrics.increase_generated_queries()
+            self.env.metrics.increase_top_queries()
+            kappaB = get_kappa(self.env.instance.bias, Y)
+
+            answer = self.env.ask_membership_query(Y)
+            if answer:
+                # it is a solution, so all candidates violated must go
+                # B <- B \setminus K_B(e)
+                self.env.remove_from_bias(kappaB)
+
+            else:  # user says UNSAT
+
+                scope = self.env.run_find_scope(Y)
+                c = self.env.run_findc(scope)
+                self.env.add_to_cl(c)
+                self.mineAsk(get_relation(c, self.env.instance.language))
+
+
+    def mineAsk(self, r):
+        """
+        Mine&Ask function presented in
+        "Detecting Types of Variables for Generalization in Constraint Acquisition", ICTAI 2015.
+
+        :param r: The index of a relation in gamma.
+        :return: List of learned constraints.
+        """
+        try:
+            import networkx as nx
+        except ImportError:
+            raise ImportError("To use the predictAsk function of PQuAcq, networkx needs to be installed")
+        
+        gq_counter = 0
+
+        C = [c for c in self.env.instance.cl if get_relation(c, self.env.instance.language) == r]
+
+        # Project Y to those in C that have relation r
+        Y = [v.name for v in get_variables(C)]
+        E = [tuple([v.name for v in get_scope(c)]) for c in C]  # all scopes
+
+        # Create the graph
+        G = nx.Graph()
+        G.add_nodes_from(Y)
+        G.add_edges_from(E)
+
+        T = [comp for comp in nx.components.connected_components(G) if not is_clique(G.subgraph(comp))]
+
+        while len(T) > 0 and gq_counter < self._qg_max:
+            Y = T.pop()
+            gen_flag = False
+            B = [c for c in self.env.instance.bias if
+                 get_relation(c, self.env.instance.language) == r and frozenset(get_scope(c)).issubset(Y)]
+
+            D = [tuple([v.name for v in get_scope(c)]) for c in B]  # missing edges that can be completed (exist in B)
+
+            # If one of the following conditions is true, continue to next:
+            # already a subset of it was negative, cannot be completed to a clique, does not add any constraint or
+            # potentially generalizing leads to UNSAT
+            new_CL = self.env.instance.cl.copy()
+            new_CL += B
+            if not (any(Y2.issubset(Y) for Y2 in self._negativeQ) or not (can_be_clique(G.subgraph(Y), D) and (len(B) > 0) and cp.Model(new_CL).solve())):
+                if self.env.ask_generalization_query(self.env.instance.language[r], B):
+                    gen_flag = True
+                    self.env.add_to_cl(B)
+                else:
+                    gq_counter += 1
+                    self._negativeQ.append(Y)
+
+            if not gen_flag:
+                communities = nx.community.greedy_modularity_communities(G.subgraph(Y))
+                [T.append(com) for com in communities if 2 < len(com) < len(Y)]
---
+++ packages/pycona/pycona-0.4-py3-none-any.whl/pycona/find_scope/findscope2.py
@@ -0,0 +1,102 @@
+from ..ca_environment.active_ca import ActiveCAEnv
+from ..ca_environment.acive_ca_proba import ProbaActiveCAEnv
+from .findscope_core import FindScopeBase
+from ..utils import get_kappa, get_con_subset
+from ..find_scope.findscope_obj import split_proba, split_half
+
+class FindScope2(FindScopeBase):
+    """
+    This is the version of the FindScope function that was presented in
+    Bessiere, Christian, et al., "Learning constraints through partial queries", AIJ 2023
+    """
+
+    def __init__(self, ca_env: ActiveCAEnv = None, split_func=None, time_limit=0.2):
+        """
+        Initialize the FindScope2 class.
+
+        :param ca_env: The constraint acquisition environment.
+        :param time_limit: The time limit for findscope query generation.
+        :param split_func: The function used to split the variables in findscope.
+        """
+
+        if split_func is None:
+            split_func = split_proba if isinstance(ca_env, ProbaActiveCAEnv) else split_half
+
+        super().__init__(ca_env, time_limit, split_func=split_func)
+        self._kappaB = []
+
+    def run(self, Y, kappa=None):
+        """
+        Run the FindScope2 algorithm.
+
+        :param Y: A set of variables.
+        :return: The scope of the partial example.
+        :raises Exception: If the partial example is not a negative example.
+        """
+        assert self.ca is not None
+
+        kappaB = kappa if kappa is not None else get_kappa(self.ca.instance.bias, Y)
+        self._kappaB = kappaB
+        if len(self._kappaB) == 0:
+            raise Exception(f"The partial example e_Y, on the subset of variables Y given in FindScope, "
+                            f"must be a negative example")
+        scope = self._find_scope(set(), Y)
+
+        return scope
+
+    def _find_scope(self, R, Y):
+        """
+        Find the scope of the partial example.
+
+        :param R: A set of variables.
+        :param Y: A set of variables.
+        :return: The scope of the partial example.
+        :raises Exception: If kappaB is not part of the bias.
+        """
+
+        RY = R.union(Y)
+        kappaBRY = get_con_subset(self._kappaB, list(RY))
+
+        # if ask(e_R) = yes: B \setminus K(e_R)
+        # need to project 'e' down to vars in R,
+        # will show '0' for None/na/" ", should create object nparray instead
+        kappaBR = get_con_subset(kappaBRY, list(R))
+        if len(kappaBR) > 0:
+
+            self.ca.metrics.increase_findscope_queries()
+            if self.ca.ask_membership_query(R):
+                self.ca.remove_from_bias(kappaBR)
+                self._kappaB = list(set(self._kappaB) - set(kappaBR))
+                kappaBRY = get_con_subset(self._kappaB, list(RY))
+
+            else:
+                return set()
+
+        if len(Y) == 1:
+            return set(Y)
+
+
+        # Create Y1, Y2 -------------------------
+        proba = self.ca.bias_proba if hasattr(self.ca, 'bias_proba') else []
+        Y1, Y2 = self.split_func(Y=Y, R=R, kappaB=kappaBRY, P_c=proba)
+
+        S1 = set()
+        S2 = set()
+
+        # R U Y1
+        RY1 = R.union(Y1)
+        kappaBRY1 = get_con_subset(kappaBRY, RY1)
+
+        if len(kappaBRY1) < len(kappaBRY):
+            S1 = self._find_scope(RY1, Y2)
+            kappaBRY = get_con_subset(self._kappaB, list(RY))  # update in case constraints were removed
+
+        # R U S1
+        RS1 = R.union(S1)
+
+        kappaBRS1 = get_con_subset(kappaBRY, RS1)
+
+        if len(kappaBRS1) < len(kappaBRY):
+            S2 = self._find_scope(RS1, Y1)
+
+        return S1.union(S2)
