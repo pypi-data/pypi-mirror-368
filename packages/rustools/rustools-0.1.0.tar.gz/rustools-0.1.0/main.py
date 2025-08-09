8468d717f991d2d27ef2e2e03c6ed2411508cf13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:28 +0000

    Add package rustools/0.1.0/rustools-0.1.0.tar.gz

---
+++ packages/rustools/rustools-0.1.0.tar.gz/rustools-0.1.0/main.py
@@ -0,0 +1,4 @@
+from rustools import sum_as_string
+
+if __name__ == "__main__":
+    print(sum_as_string(2, 10))
---
+++ packages/rustools/rustools-0.1.0.tar.gz/rustools-0.1.0/python/rustools/rustools.pyi
@@ -0,0 +1,5 @@
+
+def sum_as_string(a: int, b: int) -> str:
+    """
+    两数之和
+    """
