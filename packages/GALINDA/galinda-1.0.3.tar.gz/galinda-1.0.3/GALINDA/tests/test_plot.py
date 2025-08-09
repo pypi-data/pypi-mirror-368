ab929c870c1961305ff81b5319905d378839949f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 05:07:40 +0000

    Add package GALINDA/1.0.3/galinda-1.0.3.tar.gz

---
+++ packages/GALINDA/galinda-1.0.3.tar.gz/galinda-1.0.3/GALINDA/tests/test_plot.py
@@ -0,0 +1,29 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import glob
+import h5py
+from matplotlib import rc
+rc("animation", html = "html5")
+from GALINDA import GALINDA
+
+# %%
+def test_plot():
+    """
+    Test that plot function produces a plot of the screenshot file for animation.
+    This function will also assert that the given attributes fig and im were assigned.
+
+    We'll test on the 0th file.
+    """
+    gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
+    plot = gal.plot(0)
+    
+    assert hasattr(gal,"fig") == True
+    assert hasattr(gal,"im") == True
+
+    return plot
+
+if __name__ == "__main__":
+    pl = test_plot()
+    plt.show()
