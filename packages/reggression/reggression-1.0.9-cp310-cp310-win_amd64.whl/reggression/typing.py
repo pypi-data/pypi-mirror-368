32a0c4c1ac87c1ad050e7b3fcaf7aaf0705b3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:05:14 +0000

    Add package reggression/1.0.9/reggression-1.0.9-cp310-cp310-win_amd64.whl

---
+++ packages/reggression/reggression-1.0.9-cp310-cp310-win_amd64.whl/reggression/typing.py
@@ -0,0 +1,14 @@
+import sys
+
+from typing import List
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Protocol
+else:
+    from typing import Protocol
+
+
+class Session(Protocol):
+    def version(self) -> str: ...
+    def main(self, args: List[str] = []) -> int: ...
+    def reggression_run(self, myCmd : str, dataset : str, testData : str, loss : str, loadFrom : str, dumpTo : str, parseCSV : str, parseParams : int, calcDL : int, calcFit : int, varnames : str) -> str: ...
