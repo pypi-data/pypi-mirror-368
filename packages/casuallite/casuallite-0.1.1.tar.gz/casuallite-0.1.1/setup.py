550c6c97d10bc5482b301c8a4f15bceeef90296d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:09:52 +0000

    Add package casuallite/0.1.1/casuallite-0.1.1.tar.gz

---
+++ packages/casuallite/casuallite-0.1.1.tar.gz/casuallite-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="casuallite",
+    version="0.1.1",
+    author="Project Genesis",
+    author_email="srinimagi99@gmail.com",
+    description="CasualLite: The world's first Hybrid Meta Causal Learning Algorithm",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/theAiAlgoRepository/casuallite",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    license="Apache-2.0",
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "seaborn",
+        "pandas",
+        "matplotlib",
+        "scikit-learn",
+        "scipy",
+        "torch",
+        "networkx"
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
