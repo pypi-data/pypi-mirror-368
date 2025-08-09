7371f031cc7f2c46c4c2dfc5f832c86dabe0925f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:14 +0000

    Add package luxonis-ml/0.8.0/luxonis_ml-0.8.0-py3-none-any.whl

---
+++ packages/luxonis-ml/luxonis_ml-0.8.0-py3-none-any.whl/luxonis_ml/enums/enums.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class DatasetType(str, Enum):
+    LDF = "ldf"
+    COCO = "coco"
+    VOC = "voc"
+    DARKNET = "darknet"
+    YOLOV6 = "yolov6"
+    YOLOV4 = "yolov4"
+    CREATEML = "createml"
+    TFCSV = "tfcsv"
+    CLSDIR = "clsdir"
+    SEGMASK = "segmask"
+    SOLO = "solo"
+    NATIVE = "native"
