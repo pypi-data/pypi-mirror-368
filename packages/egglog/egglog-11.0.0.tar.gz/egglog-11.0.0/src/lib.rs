e3c65b98a9734a6298280664bb3fb318031b2f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 07:04:56 +0000

    Add package egglog/11.0.0/egglog-11.0.0.tar.gz

---
+++ packages/egglog/egglog-11.0.0.tar.gz/egglog-11.0.0/src/lib.rs
@@ -0,0 +1,31 @@
+mod conversions;
+mod egraph;
+mod error;
+mod py_object_sort;
+mod serialize;
+mod termdag;
+mod utils;
+
+use pyo3::prelude::*;
+
+/// Bindings for egglog rust library
+#[pymodule]
+fn bindings(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(1)
+        .build_global()
+        .unwrap();
+
+    pyo3_log::init();
+
+    m.add_class::<crate::py_object_sort::PyObjectSort>()?;
+    m.add_class::<crate::serialize::SerializedEGraph>()?;
+    m.add_class::<crate::egraph::EGraph>()?;
+    m.add_class::<crate::error::EggSmolError>()?;
+    m.add_class::<crate::termdag::TermDag>()?;
+
+    crate::conversions::add_structs_to_module(m)?;
+    crate::conversions::add_enums_to_module(m)?;
+
+    Ok(())
+}
