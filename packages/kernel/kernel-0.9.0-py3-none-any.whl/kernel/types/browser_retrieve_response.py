60d887fedcec0192896f7ee9d5d0b31208e99af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:17 +0000

    Add package kernel/0.9.0/kernel-0.9.0-py3-none-any.whl

---
+++ packages/kernel/kernel-0.9.0-py3-none-any.whl/kernel/types/browser_retrieve_response.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+
+from .._models import BaseModel
+from .browser_persistence import BrowserPersistence
+
+__all__ = ["BrowserRetrieveResponse"]
+
+
+class BrowserRetrieveResponse(BaseModel):
+    cdp_ws_url: str
+    """Websocket URL for Chrome DevTools Protocol connections to the browser session"""
+
+    session_id: str
+    """Unique identifier for the browser session"""
+
+    browser_live_view_url: Optional[str] = None
+    """Remote URL for live viewing the browser session.
+
+    Only available for non-headless browsers.
+    """
+
+    persistence: Optional[BrowserPersistence] = None
+    """Optional persistence configuration for the browser session."""
