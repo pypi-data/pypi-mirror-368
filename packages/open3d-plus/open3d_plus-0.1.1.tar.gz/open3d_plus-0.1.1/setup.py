3e29e2ccb7c81f70b381cf2f95240ac56344586f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 16:05:18 +0000

    Add package open3d-plus/0.1.1/open3d_plus-0.1.1.tar.gz

---
+++ packages/open3d-plus/open3d_plus-0.1.1.tar.gz/open3d_plus-0.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+setup(
+    name="open3d_plus",
+    version="0.1.1",
+    description="open3d plus functions",
+    author="Minghao Gou",
+    author_email="gouminghao@gmail.com",
+    url="",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "open3d>=0.8.0.0",
+    ],
+)
