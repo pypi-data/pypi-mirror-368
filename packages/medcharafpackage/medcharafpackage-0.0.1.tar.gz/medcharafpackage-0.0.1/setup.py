695f7bf509bdf5b3fc3f726d501ab1098491f8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:54 +0000

    Add package medcharafpackage/0.0.1/medcharafpackage-0.0.1.tar.gz

---
+++ packages/medcharafpackage/medcharafpackage-0.0.1.tar.gz/medcharafpackage-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup , find_packages
+classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+],
+setup(
+    name ='medcharafpackage',
+    version='0.0.1',
+    description='basic package',
+    author_email='medcharaf011@gmail.com',
+    license='MIT',
+    # classifiers=classifiers,
+    keywords='simplepackage',
+    install_requires=['']    
+    
+    
+)
+
+
