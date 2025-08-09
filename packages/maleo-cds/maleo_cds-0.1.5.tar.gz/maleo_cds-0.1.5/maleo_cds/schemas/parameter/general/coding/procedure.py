ce76901c28f4f401522e5331cc73a533f55fc7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 03:28:55 +0000

    Add package maleo-cds/0.1.5/maleo_cds-0.1.5.tar.gz

---
+++ packages/maleo-cds/maleo_cds-0.1.5.tar.gz/maleo_cds-0.1.5/maleo_cds/schemas/parameter/general/coding/procedure.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from maleo_soma.schemas.parameter.general import ReadSingleParameterSchema
+from maleo_cds.enums.coding.procedure import IdentifierType
+from maleo_cds.types.base.coding.procedure import IdentifierValueType
+
+
+class ReadSingleParameter(
+    ReadSingleParameterSchema[IdentifierType, IdentifierValueType]
+):
+    pass
+
+
+class GenerateSuggestion(BaseModel):
+    overall_plan: str = Field(..., description="Overall plan")
