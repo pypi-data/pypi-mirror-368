345acddd07b118d515f2a479f813ce30a022fb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 04:10:41 +0000

    Add package charmed-kubeflow-chisme/0.4.12/charmed_kubeflow_chisme-0.4.12-py3-none-any.whl

---
+++ packages/charmed-kubeflow-chisme/charmed_kubeflow_chisme-0.4.12-py3-none-any.whl/charmed_kubeflow_chisme/testing/__init__.py
@@ -0,0 +1,50 @@
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Utilities for testing charms."""
+
+from .charm_spec import CharmSpec, generate_context_from_charm_spec_list
+from .cos_integration import (
+    ALERT_RULES_DIRECTORY,
+    APP_GRAFANA_DASHBOARD,
+    APP_LOGGING,
+    APP_METRICS_ENDPOINT,
+    GRAFANA_AGENT_APP,
+    GRAFANA_AGENT_GRAFANA_DASHBOARD,
+    GRAFANA_AGENT_LOGGING_PROVIDER,
+    GRAFANA_AGENT_METRICS_ENDPOINT,
+    assert_alert_rules,
+    assert_grafana_dashboards,
+    assert_logging,
+    assert_metrics_endpoint,
+    deploy_and_assert_grafana_agent,
+    get_alert_rules,
+    get_grafana_dashboards,
+)
+from .serialized_data_interface import (
+    RelationMetadata,
+    add_data_to_sdi_relation,
+    add_sdi_relation_to_harness,
+)
+
+__all__ = [
+    RelationMetadata,
+    CharmSpec,
+    add_data_to_sdi_relation,
+    add_sdi_relation_to_harness,
+    assert_alert_rules,
+    assert_grafana_dashboards,
+    assert_logging,
+    assert_metrics_endpoint,
+    deploy_and_assert_grafana_agent,
+    generate_context_from_charm_spec_list,
+    get_alert_rules,
+    get_grafana_dashboards,
+    GRAFANA_AGENT_APP,
+    GRAFANA_AGENT_METRICS_ENDPOINT,
+    GRAFANA_AGENT_GRAFANA_DASHBOARD,
+    GRAFANA_AGENT_LOGGING_PROVIDER,
+    APP_METRICS_ENDPOINT,
+    APP_GRAFANA_DASHBOARD,
+    APP_LOGGING,
+    ALERT_RULES_DIRECTORY,
+]
