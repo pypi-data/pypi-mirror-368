3c4af3032f59049af9da9a0fd0f1f3ad2d259362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-09 06:04:52 +0000

    Add package gitgenius/0.1.3/gitgenius-0.1.3.tar.gz

---
+++ packages/gitgenius/gitgenius-0.1.3.tar.gz/gitgenius-0.1.3/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gitgenius',
+    version='0.1.3',
+    description='GitGenius CLI - Understand Git errors in plain English',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Selva Neyas U',
+    author_email='selvaneyas@gmail.com',  # update with your email
+    url='https://github.com/selvaneyas/gitgenius',
+    project_urls={  # optional extra links
+        'Bug Tracker': 'https://github.com/selvaneyas/gitgenius/issues',
+        'Documentation': 'https://selvaneyas.github.io/gitgenius/',
+    },
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'GitPython==3.1.32',
+        'gitdb==4.0.10',
+        'rich==13.7.1'
+    ],
+    entry_points={
+        'console_scripts': [
+            'gitgenius = gitgenius.cli:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Environment :: Console',
+        'Topic :: Software Development :: Version Control :: Git'
+    ],
+    python_requires='>=3.7',
+)
