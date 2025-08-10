55bc4a5f4c9365176c9c60109ac70b48c436aa78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 04:19:24 +0000

    Add package mandelbrot-synthesizer/0.1.11/mandelbrot_synthesizer-0.1.11-py3-none-any.whl

---
+++ packages/mandelbrot-synthesizer/mandelbrot_synthesizer-0.1.11-py3-none-any.whl/mandelbrot_synthesizer/MandelbrotSynthesizer.py
@@ -0,0 +1,9 @@
+#This is the run file
+#I want to thank everyone using this application because I worked extremely hard on the development process and I had spent more than 20 hours doing so so please if you like it. consider giving it a star on the summer of making site.
+from .UI import Main
+
+def main():
+    Main().run()
+
+if __name__ == "__main__":
+    main()
