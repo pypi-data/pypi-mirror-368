b4174a651f118114a54c10f55120b813273edc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:35 +0000

    Add package fastvol/0.1.1/fastvol-0.1.1.tar.gz

---
+++ packages/fastvol/fastvol-0.1.1.tar.gz/fastvol-0.1.1/fastvol/_core/build/CMakeFiles/fastvol.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/valerio/Desktop/fastvol/core/src/detail/bench/american/bopm.cpp" "CMakeFiles/fastvol.dir/src/detail/bench/american/bopm.cpp.o" "gcc" "CMakeFiles/fastvol.dir/src/detail/bench/american/bopm.cpp.o.d"
+  "/Users/valerio/Desktop/fastvol/core/src/detail/bench/american/psor.cpp" "CMakeFiles/fastvol.dir/src/detail/bench/american/psor.cpp.o" "gcc" "CMakeFiles/fastvol.dir/src/detail/bench/american/psor.cpp.o.d"
+  "/Users/valerio/Desktop/fastvol/core/src/detail/bench/american/ttree.cpp" "CMakeFiles/fastvol.dir/src/detail/bench/american/ttree.cpp.o" "gcc" "CMakeFiles/fastvol.dir/src/detail/bench/american/ttree.cpp.o.d"
+  "/Users/valerio/Desktop/fastvol/core/src/detail/bench/european/bsm.cpp" "CMakeFiles/fastvol.dir/src/detail/bench/european/bsm.cpp.o" "gcc" "CMakeFiles/fastvol.dir/src/detail/bench/european/bsm.cpp.o.d"
+  "/Users/valerio/Desktop/fastvol/core/src/ffi.cpp" "CMakeFiles/fastvol.dir/src/ffi.cpp.o" "gcc" "CMakeFiles/fastvol.dir/src/ffi.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ packages/fastvol/fastvol-0.1.1.tar.gz/fastvol-0.1.1/fastvol/_core/build/CMakeFiles/fastvol.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/bopm.cpp.o"
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/bopm.cpp.o.d"
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/psor.cpp.o"
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/psor.cpp.o.d"
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/ttree.cpp.o"
+  "CMakeFiles/fastvol.dir/src/detail/bench/american/ttree.cpp.o.d"
+  "CMakeFiles/fastvol.dir/src/detail/bench/european/bsm.cpp.o"
+  "CMakeFiles/fastvol.dir/src/detail/bench/european/bsm.cpp.o.d"
+  "CMakeFiles/fastvol.dir/src/ffi.cpp.o"
+  "CMakeFiles/fastvol.dir/src/ffi.cpp.o.d"
+  "libfastvol.a"
+  "libfastvol.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fastvol.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
