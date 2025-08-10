6dce289b62827941fe510483853a6864d0baef2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:22 +0000

    Add package LLM00/0.1.0/LLM00-0.1.0-py3-none-any.whl

---
+++ packages/LLM00/LLM00-0.1.0-py3-none-any.whl/LLM00/test.py
@@ -0,0 +1,9 @@
+
+# シンプルなLLMインターフェース [LLM00]
+# 【動作確認 / 使用例】
+
+import sys
+import ezpip
+LLM00 = ezpip.load_develop("LLM00", "../", develop_flag = True)
+
+print(LLM00("ずばり簡潔に、タコの足は何本？"))	# AIへの問いかけ [LLM00]
