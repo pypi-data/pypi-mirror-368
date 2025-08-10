617d1b1cdb2ffd21c446a4ef0c7a3965b9ff50a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:04:58 +0000

    Add package accrete/0.0.154/accrete-0.0.154-py3-none-any.whl

---
+++ packages/accrete/accrete-0.0.154-py3-none-any.whl/accrete/contrib/ui/__init__.py
@@ -0,0 +1,19 @@
+from . import widgets
+from .filter import Filter
+from .response import (
+    Response,
+    WindowResponse,
+    ListResponse,
+    ListEntryResponse,
+    TableResponse,
+    TableRowResponse,
+    DetailResponse,
+    ModalResponse,
+    OobResponse,
+    TriggerResponse,
+    ClientTrigger,
+    search_select_response,
+    message_response,
+    add_trigger,
+    update
+)
