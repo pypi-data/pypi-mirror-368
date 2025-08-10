07fff31d2493fe5bcd0e518da665d8d882710a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:03 +0000

    Add package git-narrate/1.0.3/git_narrate-1.0.3-py3-none-any.whl

---
+++ packages/git-narrate/git_narrate-1.0.3-py3-none-any.whl/git_narrate/__init__.py
@@ -0,0 +1,9 @@
+"""
+Git-Narrate: The Repository Storyteller
+
+A tool that analyzes a git repository and generates a human-readable story of its development.
+"""
+
+__version__ = "1.0.3"
+__author__ = "Sithum Sathsara Rajapakshe | 000x"
+__email__ = "SITHUMSS9122@gmail.com"
