e862d7df003ebeb2725f5ad529610ac54bcacaad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:16 +0000

    Add package mtcli/1.12.2/mtcli-1.12.2.tar.gz

---
+++ packages/mtcli/mtcli-1.12.2.tar.gz/mtcli-1.12.2/mtcli/mt.py
@@ -0,0 +1,38 @@
+"""
+Módulo principal do aplicativo.
+
+Frontcontroller do mtcli.
+"""
+
+import click
+
+from mtcli.bars import bars
+from mtcli.conf import conf
+from mtcli.extensions import agressao
+from mtcli.extensions import volume_medio
+from mtcli.extensions import media_movel
+from mtcli.extensions import range_medio
+from mtcli.extensions import moving_average
+from mtcli import __version__
+
+
+@click.group(invoke_without_command=True)
+@click.option("--version", "-v", is_flag=True, help="Exibe a versao do mtcli.")
+def mt(version):
+    """Exibe o grafico de velas do MetaTrader 5 em texto."""
+    if version:
+        click.echo("mtcli %s" % __version__)
+        return
+
+
+mt.add_command(bars)
+mt.add_command(conf)
+mt.add_command(media_movel.mm)
+mt.add_command(range_medio.rm)
+mt.add_command(moving_average.ma)
+mt.add_command(volume_medio.vm)
+mt.add_command(agressao.sa)
+
+
+if __name__ == "__main__":
+    mt()
