0295af35cdebc2ca507521fa25f70a7ee027c72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 04:19:47 +0000

    Add package publicity-idl/0.0.2/publicity_idl-0.0.2-py3-none-any.whl

---
+++ packages/publicity-idl/publicity_idl-0.0.2-py3-none-any.whl/publicity_idl/demo.py
@@ -0,0 +1,9 @@
+class Demo:
+    def __init__(self, number):
+        self.number = number
+
+    def demo_add_one(self):
+        return self.number + 1
+
+def add_one(number):
+    return number + 1
