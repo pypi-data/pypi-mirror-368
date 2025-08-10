5c0e5c2f9ef11b59038ff4c055ff719e96fd742a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:22 +0000

    Add package azpytools/1.1.10/azpytools-1.1.10.tar.gz

---
+++ packages/azpytools/azpytools-1.1.10.tar.gz/azpytools-1.1.10/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'azpytools',
+    version = '1.1.10',
+    author = 'AndyZhu',
+    author_email = 'andyzhu@live.com',  
+    description='Tools for Python development',  
+    # packages = ['azpytools'],
+    packages=find_packages(),
+)
