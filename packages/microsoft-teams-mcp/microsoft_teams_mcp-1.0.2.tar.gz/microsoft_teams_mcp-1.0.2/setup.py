637a59d3a7d40765e8a19c0f0150f94ba7ccb001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:26 +0000

    Add package microsoft-teams-mcp/1.0.2/microsoft_teams_mcp-1.0.2.tar.gz

---
+++ packages/microsoft-teams-mcp/microsoft_teams_mcp-1.0.2.tar.gz/microsoft_teams_mcp-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="microsoft-teams-mcp",   # The package name users will install
+    version="1.0.2",              # Version of your package
+    packages=find_packages(),     # Automatically find all packages in the folder
+    install_requires=[            # Your dependencies here
+        "fastmcp==2.11.2",
+        "requests==2.32.4",
+        "httpx==0.28.1",
+        "python-dateutil==2.9.0.post0",
+        "pytz==2025.2",
+        "msgraph-core==1.3.5",
+        "azure-identity==1.24.0",
+        "python-dotenv==1.1.1",
+        "msgraph-sdk==1.40.0",
+    ],
+    author="Naveen Chowdary Aliveli", 
+    author_email="naveenchowdary5175@gmail.com",
+    description="Microsoft Teams meeting scheduler MCP tool using Microsoft Graph API",
+    url="https://github.com/alivnavc/Microsoft-Teams-Meetings-MCP-Server",
+    classifiers=[                 # Optional metadata
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
