46a7f2ca2c9966b5f3d408fb5dc6afb9b4dc9749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:06 +0000

    Add package vpnflow/1.0.1/vpnflow-1.0.1-py3-none-any.whl

---
+++ packages/vpnflow/vpnflow-1.0.1-py3-none-any.whl/vpnflow/misc.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from logging import getLogger
+from logging.config import dictConfig
+
+import yaml
+
+logger = getLogger(__name__)
+
+
+def load_yaml(file_path: str) -> dict:
+    """⭐"""
+    with open(file_path, 'r', encoding="UTF-8") as f:
+        return yaml.safe_load(f.read())
+
+
+def load_log_conf(file_path: str) -> None:
+    """⭐"""
+    dictConfig(load_yaml(file_path))
+
+
+def days_verbose(days: int) -> str:
+    """⭐"""
+    if days > 20:
+        days %= 10
+    if days == 1:
+        return "день"
+    elif 2 <= days <= 4:
+        return "дня"
+    return "дней"
+
+
+def slice_generator(s, l):
+    """⭐"""
+    for i in range(0, len(s), l):
+        yield s[i:i + l]
