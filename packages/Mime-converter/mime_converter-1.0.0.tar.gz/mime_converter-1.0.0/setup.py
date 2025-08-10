4124b7ae89eee5edb8f8fa0704be06fda74064ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:52 +0000

    Add package Mime-converter/1.0.0/mime_converter-1.0.0.tar.gz

---
+++ packages/Mime-converter/mime_converter-1.0.0.tar.gz/mime_converter-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Mime_converter',
+    version='1.0.0',
+    description='Converts mime to file extensions',
+    author='Jacob D',
+    author_email='d8008385@gmail.com',
+    packages=find_packages(),
+    python_requires='>=3.11',
+)
