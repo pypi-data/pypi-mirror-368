46c99d0a6187c456db59c856d43f14ecbcc38077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:36 +0000

    Add package internal-rdbms/0.1.27/internal_rdbms-0.1.27-py3-none-any.whl

---
+++ packages/internal-rdbms/internal_rdbms-0.1.27-py3-none-any.whl/internal/model/base_history_model.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base_model import InternalBaseModel
+
+
+class BaseHistoryModel(InternalBaseModel):
+    operator_id: Mapped[str] = mapped_column(String(100))
+    operator_name: Mapped[str] = mapped_column(String(100))
+    operator_type: Mapped[str] = mapped_column(String(100))
+    operate: Mapped[str] = mapped_column(Text(2000))
