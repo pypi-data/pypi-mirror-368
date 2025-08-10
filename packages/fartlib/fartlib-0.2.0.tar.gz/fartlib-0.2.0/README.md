ca6c4b387329556f60286b5506592c63a8bedb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:32 +0000

    Add package fartlib/0.2.0/fartlib-0.2.0.tar.gz

---
+++ packages/fartlib/fartlib-0.2.0.tar.gz/fartlib-0.2.0/README.md
@@ -0,0 +1,12 @@
+# fartlib
+
+the official fartlib package.
+
+## A cool example
+
+```py
+from fartlib import Fart, Smell
+
+fart = Fart(smell=Smell.HORRIBLE)
+fart.rip(power=100)
+```
---
+++ packages/fartlib/fartlib-0.2.0.tar.gz/fartlib-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fartlib",
+    version="0.2.0",
+    author="Adonis",
+    description="the official fartlib package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/adondonis/fartlib",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
