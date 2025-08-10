919bf3f85d5d411e3624ed391810fbd0d112b9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:01 +0000

    Add package yta-video-moviepy/0.0.9/yta_video_moviepy-0.0.9.tar.gz

---
+++ packages/yta-video-moviepy/yta_video_moviepy-0.0.9.tar.gz/yta_video_moviepy-0.0.9/src/yta_video_moviepy/t.py
@@ -0,0 +1,9 @@
+"""
+Module to simplify the way we work with
+the moviepy video time moment 't'.
+"""
+# TODO: Refactor this in the other libraries
+# to use the new 'yta_video_frame_time' 
+# instead of this library, it is lighter
+# I want to export this as it is
+from yta_video_frame_time import T
