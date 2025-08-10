6209ff50cebd0ff0aa14436a7f22be22b4640eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:11 +0000

    Add package financial-reports-generated-client/1.2.6/financial_reports_generated_client-1.2.6-py3-none-any.whl

---
+++ packages/financial-reports-generated-client/financial_reports_generated_client-1.2.6-py3-none-any.whl/financial_reports_generated_client/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from financial_reports_generated_client.api.companies_api import CompaniesApi
+from financial_reports_generated_client.api.filing_types_api import FilingTypesApi
+from financial_reports_generated_client.api.filings_api import FilingsApi
+from financial_reports_generated_client.api.isic_classifications_api import ISICClassificationsApi
+from financial_reports_generated_client.api.processed_filings_api import ProcessedFilingsApi
+from financial_reports_generated_client.api.sources_api import SourcesApi
+from financial_reports_generated_client.api.watchlist_api import WatchlistApi
+from financial_reports_generated_client.api.health_api import HealthApi
+from financial_reports_generated_client.api.schema_api import SchemaApi
+
