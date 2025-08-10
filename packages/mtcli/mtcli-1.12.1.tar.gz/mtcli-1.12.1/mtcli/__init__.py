0af0971c02a2d50fc2633e81e89a3200b12aca71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:08 +0000

    Add package mtcli/1.12.1/mtcli-1.12.1.tar.gz

---
+++ packages/mtcli/mtcli-1.12.1.tar.gz/mtcli-1.12.1/mtcli/__init__.py
@@ -0,0 +1,11 @@
+"""Aplicativo de linha de comando para exibir graficos do MetaTrader 5 em texto."""
+
+from . import models
+from . import views
+from . import data
+
+from . import bars
+from . import conf
+from .extensions import range_medio, media_movel, moving_average, volume_medio, agressao
+
+__version__ = "1.12.1"
