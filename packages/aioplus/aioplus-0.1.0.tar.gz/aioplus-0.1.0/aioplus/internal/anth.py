0754a84d562129249cffd38c033d5f862cea5f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 04:19:27 +0000

    Add package aioplus/0.1.0/aioplus-0.1.0.tar.gz

---
+++ packages/aioplus/aioplus-0.1.0.tar.gz/aioplus-0.1.0/aioplus/internal/anth.py
@@ -0,0 +1,78 @@
+from collections.abc import AsyncIterable
+from typing import Any, SupportsIndex, TypeVar, overload
+
+from aioplus.internal import cast
+from aioplus.internal.aenumerate import aenumerate
+
+
+T1 = TypeVar("T1")
+T2 = TypeVar("T2")
+
+
+SENTINEL = object()
+
+
+@overload
+async def anth(aiterable: AsyncIterable[T1], /, *, n: SupportsIndex) -> T1: ...
+
+
+@overload
+async def anth(aiterable: AsyncIterable[T1], /, *, n: SupportsIndex, default: T2) -> T1 | T2: ...
+
+
+async def anth(
+    aiterable: AsyncIterable[Any],
+    /,
+    *,
+    n: SupportsIndex,
+    default: Any = SENTINEL,
+) -> Any:
+    """Return the nth item or a default value.
+
+    Parameters
+    ----------
+    aiterable : AsyncIterable[T1]
+        An asynchronous iterable to retrieve the nth item from.
+
+    n : SupportsIndex
+        The index of the item to retrieve, starting from 0.
+
+    default : T2, optional
+        A default value to return if the nth item does not exist.
+        If not provided, :obj:`IndexError` will be raised if the nth item is not found.
+
+    Returns
+    -------
+    T1 or T2
+        The nth item or the default value.
+
+    Examples
+    --------
+    >>> import asyncio
+    >>>
+    >>> from aioplus import anth, arange
+    >>>
+    >>> async def main() -> None:
+    >>>     '''Run the program.'''
+    >>>     aiterable = arange(23)
+    >>>     value = await anth(aiterable, n=4)
+    >>>     print(f'value = {value}')
+    >>>
+    >>> if __name__ == '__main__':
+    >>>     asyncio.run(main())
+    """
+    if not isinstance(aiterable, AsyncIterable):
+        detail = "'aiterable' must be 'AsyncIterable'"
+        raise TypeError(detail)
+
+    n = cast.to_non_negative_int(n, variable_name="n")
+
+    async for index, value in aenumerate(aiterable):
+        if index == n:
+            return value
+
+    if default is not SENTINEL:
+        return default
+
+    detail = "The iterable contains fewer than 'n' items"
+    raise IndexError(detail)
---
+++ packages/aioplus/aioplus-0.1.0.tar.gz/aioplus-0.1.0/aioplus/internal/awaitify.py
@@ -0,0 +1,73 @@
+from asyncio import get_running_loop
+from collections.abc import Awaitable, Callable
+from concurrent.futures import Executor
+from contextvars import copy_context
+from functools import partial, wraps
+from typing import ParamSpec, TypeVar
+
+
+ReturnT = TypeVar("ReturnT")
+ParamsT = ParamSpec("ParamsT")
+
+
+def awaitify(
+    func: Callable[ParamsT, ReturnT],
+    /,
+    *,
+    executor: Executor | None = None,
+) -> Callable[ParamsT, Awaitable[ReturnT]]:
+    """Make a function asynchronous.
+
+    Parameters
+    ----------
+    func : Callable
+        A callable to be wrapped for asynchronous execution.
+
+    executor : Executor, optional
+        An optional :class:`concurrent.futures.Executor` to run the function in. If :obj:`None`, the
+        default executor is used (usually a thread pool).
+
+    Returns
+    -------
+    Callable
+        An asynchronous callable that, when awaited, runs the original function in the executor.
+
+    Examples
+    --------
+    >>> import asyncio
+    >>>
+    >>> from aioplus import awaitify
+    >>>
+    >>> def func(num: int) -> None:
+    >>>     '''Print the number.'''
+    >>>     print(f'Num: {num}')
+    >>>
+    >>> async def main() -> None:
+    >>>     '''Run the program.'''
+    >>>     afunc = awaitify(func)
+    >>>     await afunc(num=2304)
+    >>>
+    >>> if __name__ == '__main__':
+    >>>     asyncio.run(main())
+
+    See Also
+    --------
+    :meth:`asyncio.loop.run_in_executor`
+    """
+    if not callable(func):
+        detail = "'func' must be callable"
+        raise TypeError(detail)
+
+    if executor is not None and not isinstance(executor, Executor):
+        detail = "'executor' must be 'Executor' or 'None'"
+        raise TypeError(detail)
+
+    @wraps(func)
+    async def afunc(*args: ParamsT.args, **kwargs: ParamsT.kwargs) -> ReturnT:
+        """Run the function asynchronously."""
+        loop = get_running_loop()
+        context = copy_context()
+        execute = partial(context.run, func, *args, **kwargs)
+        return await loop.run_in_executor(executor, execute)
+
+    return afunc
