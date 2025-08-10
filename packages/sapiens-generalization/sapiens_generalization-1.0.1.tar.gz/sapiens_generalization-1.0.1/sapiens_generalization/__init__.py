615d0ef4eb8fd83d2f5eee694607768bb74308b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:12 +0000

    Add package sapiens-generalization/1.0.1/sapiens_generalization-1.0.1.tar.gz

---
+++ packages/sapiens-generalization/sapiens_generalization-1.0.1.tar.gz/sapiens_generalization-1.0.1/sapiens_generalization/__init__.py
@@ -0,0 +1,3 @@
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .sapiens_generalization import *
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
---
+++ packages/sapiens-generalization/sapiens_generalization-1.0.1.tar.gz/sapiens_generalization-1.0.1/setup.py
@@ -0,0 +1,13 @@
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from setuptools import setup, find_packages
+package_name = 'sapiens_generalization'
+version = '1.0.1'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    url='https://github.com/sapiens-technology/SapiensGeneralization',
+    license='Proprietary Software'
+)
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
