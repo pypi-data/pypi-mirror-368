86b1fc6526d91d08abeaa6d4fb56165f3433bd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:18 +0000

    Add package loglocal/1.0.4/loglocal-1.0.4.tar.gz

---
+++ packages/loglocal/loglocal-1.0.4.tar.gz/loglocal-1.0.4/src/loglocal/models/__init__.py
@@ -0,0 +1,9 @@
+from loglocal.models._config_models import LogConfig, LogLocalConfig, LogLocalOptions, LogLocalTraceOptions
+
+__all__ = [
+    "LogConfig",
+    "LogLocalConfig",
+    "LogLocalOptions",
+    "LogLocalTraceOptions"
+]
+
