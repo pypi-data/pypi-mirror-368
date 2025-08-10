3e4ec1bcf440bc75c1cb6c374b88fdb220b482e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:26 +0000

    Add package spymx-kernels/0.2.3/spymx_kernels-0.2.3.tar.gz

---
+++ packages/spymx-kernels/spymx_kernels-0.2.3.tar.gz/spymx_kernels-0.2.3/README.md
@@ -0,0 +1,41 @@
+# IPython kernels for Spyder plugin for modelx
+
+A custom IPython kernels for the `spyder-modelx` package, which is a plug-in for Spyder,
+a Scientific Python Development Environment.
+
+These kernels can launched either through Spyder itself or in an independent
+Python session, and allow for interactive or file-based execution of Python
+code inside Spyder.
+
+Visit the following sites for more details:
+
+* [modelx documentation](https://docs.modelx.io/en/latest/)
+* [modelx home page](https://modelx.io)
+* [modelx github repository](https://github.com/fumitoh/modelx)
+* [spyder-modelx github repository](https://github.com/fumitoh/spyder-modelx)
+
+## Installation
+
+To install this package, you can use either the ``pip`` or ``conda`` package
+managers, as follows:
+
+Using conda (the recommended way!):
+
+```
+conda install -c conda-forge spymx-kernels
+```
+
+Using pip:
+
+```
+pip install spymx-kernels
+```
+
+## Dependencies
+
+This project depends on:
+
+* [spyder-kernels](https://github.com/spyder-ide/spyder-kernels)
+
+
+
---
+++ packages/spymx-kernels/spymx_kernels-0.2.3.tar.gz/spymx_kernels-0.2.3/setup.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018-2025 Fumito Hamamura <fumito.ham@gmail.com>
+
+# This library is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 3.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+# The source code contains parts copied and modified from Spyder project:
+# https://github.com/spyder-ide/spyder-kernels
+# See below for the original copyright notice.
+
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009- Spyder Kernels Contributors
+#
+# Licensed under the terms of the MIT License
+# (see spymx_kernels/__init__.py for details)
+# -----------------------------------------------------------------------------
+
+"""Jupyter Kernels for the Spyder consoles."""
+
+# Standard library imports
+import ast
+import io
+import os
+
+# Third party imports
+from setuptools import find_packages, setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+with io.open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+def get_version(module='spymx_kernels'):
+    """Get version."""
+    with open(os.path.join(HERE, module, '_version.py'), 'r') as f:
+        data = f.read()
+    lines = data.split('\n')
+    for line in lines:
+        if line.startswith('VERSION_INFO'):
+            version_tuple = ast.literal_eval(line.split('=')[-1].strip())
+            version = '.'.join(map(str, version_tuple))
+            break
+    return version
+
+
+REQUIREMENTS = [
+    'spyder-kernels>=1.8.1'
+]
+
+TEST_REQUIREMENTS = [
+    # 'codecov',
+    # 'cython',
+    # 'dask[distributed]',
+    # 'flaky',
+    # 'matplotlib',
+    # 'mock',
+    # 'numpy',
+    # 'pandas',
+    # 'pytest',
+    # 'pytest-cov',
+    # 'scipy',
+    # 'xarray',
+    # 'pillow',
+]
+
+setup(
+    name='spymx-kernels',
+    version=get_version(),
+    keywords='modelx spyder jupyter kernel ipython console',
+    url='https://github.com/fumitoh/spymx-kernels',
+    download_url="https://pypi.org/project/spymx-kernels",
+    license='LGPLv3',
+    author='Fumito Hamamura',
+    author_email="fumito.ham@gmail.com",
+    description="Jupyter kernels for Spyder plugin for modelx",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['docs', '*tests']),
+    install_requires=REQUIREMENTS,
+    extras_require={'test': TEST_REQUIREMENTS},
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Jupyter',
+        'Framework :: IPython',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Topic :: Software Development :: Interpreters',
+    ]
+)
