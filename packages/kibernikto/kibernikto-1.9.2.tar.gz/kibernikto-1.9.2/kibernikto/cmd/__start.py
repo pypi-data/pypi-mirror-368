b4a110742837f870b16af80d5fdf464d55398257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:18 +0000

    Add package kibernikto/1.9.2/kibernikto-1.9.2.tar.gz

---
+++ packages/kibernikto/kibernikto-1.9.2.tar.gz/kibernikto-1.9.2/kibernikto/cmd/__start.py
@@ -0,0 +1,36 @@
+import logging
+import argparse
+from dotenv import load_dotenv
+
+
+def start(outer_env=False):
+    """
+    Run main dispatcher, connect to Telegram and start listening for messages.
+
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Run Kibernikto')
+    parser.add_argument('--env_file_path', metavar='env_file_path', required=False,
+                        help='env file location', default='.env')
+
+    args = parser.parse_args()
+
+    if not outer_env:
+        load_dotenv(dotenv_path=args.env_file_path)
+
+    from kibernikto.utils.environment import configure_logger, print_banner
+    from kibernikto.telegram import dispatcher
+    configure_logger()
+    print_banner()
+
+    # for service messages
+    from kibernikto.telegram import service
+    # for default commands
+    from kibernikto.telegram import commands
+    from kibernikto.bots.cybernoone import Kibernikto
+    dispatcher.start(bot_class=Kibernikto)
+
+
+# Initialize bot and dispatcher
+if __name__ == '__main__':
+    start()
