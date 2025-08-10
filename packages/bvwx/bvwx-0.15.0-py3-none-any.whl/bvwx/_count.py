005153a1f77832c84b89bd9587b5a69267eb27e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:20 +0000

    Add package bvwx/0.15.0/bvwx-0.15.0-py3-none-any.whl

---
+++ packages/bvwx/bvwx-0.15.0-py3-none-any.whl/bvwx/_count.py
@@ -0,0 +1,53 @@
+"""Counting"""
+
+from ._bits import BitsLike, Vector, expect_bits, vec_size
+from ._util import clog2, mask
+
+
+def cpop(x: BitsLike) -> Vector:
+    """Count population: return number of set bits."""
+    x = expect_bits(x)
+
+    n = clog2(x.size + 1)
+    V = vec_size(n)
+
+    if x.has_x():
+        return V.xes()
+    if x.has_w():
+        return V.dcs()
+
+    d1 = x.data[1].bit_count()
+    return V(d1 ^ mask(n), d1)
+
+
+def clz(x: BitsLike) -> Vector:
+    """Count leading zeros."""
+    x = expect_bits(x)
+
+    n = clog2(x.size + 1)
+    V = vec_size(n)
+
+    if x.has_x():
+        return V.xes()
+    if x.has_w():
+        return V.dcs()
+
+    d1 = x.size - clog2(x.data[1] + 1)
+    return V(d1 ^ mask(n), d1)
+
+
+def ctz(x: BitsLike) -> Vector:
+    """Count trailing zeros."""
+    x = expect_bits(x)
+
+    n = clog2(x.size + 1)
+    V = vec_size(n)
+
+    if x.has_x():
+        return V.xes()
+    if x.has_w():
+        return V.dcs()
+
+    d = (1 << x.size) - x.data[1]
+    d1 = clog2(-d & d)
+    return V(d1 ^ mask(n), d1)
---
+++ packages/bvwx/bvwx-0.15.0-py3-none-any.whl/bvwx/_union.py
@@ -0,0 +1,118 @@
+"""Bits Union data type."""
+
+from functools import partial
+from typing import Any
+
+from ._bits import Bits, BitsLike, Composite, expect_bits
+from ._util import mask
+
+
+class _UnionMeta(type):
+    """Union Metaclass: Create union base classes."""
+
+    def __new__(mcs, name: str, bases: tuple[type], attrs: dict[str, Any]):
+        # Base case for API
+        if name == "Union":
+            return super().__new__(mcs, name, bases, attrs)
+
+        # TODO(cjdrake): Support multiple inheritance?
+        assert len(bases) == 1
+
+        # Get field_name: field_type items
+        try:
+            annotations: dict[str, type[Bits]] = attrs["__annotations__"]
+        except KeyError as e:
+            raise ValueError("Empty Union is not supported") from e
+
+        fields = list(annotations.items())
+
+        # Create Union class
+        size = max(field_type.size for _, field_type in fields)
+        union = super().__new__(mcs, name, bases, {"size": size})
+
+        # Help the type checker
+        assert issubclass(union, Composite)
+
+        # Override Bits.__init__ method
+        def _init(self: Union, arg: BitsLike):
+            x = expect_bits(arg)
+            ts = {ft for _, ft in fields}
+            if not isinstance(x, tuple(ts)):
+                s = ", ".join(t.__name__ for t in ts)
+                s = f"Expected arg to be {{{s}}}, or str literal"
+                raise TypeError(s)
+            self._data = x.data  # pyright: ignore[reportPrivateUsage]
+
+        setattr(union, "__init__", _init)
+
+        # Override Bits.__repr__ method
+        def _repr(self: Union) -> str:
+            parts = [f"{name}("]
+            for fn, _ in fields:
+                x = getattr(self, fn)
+                r = "\n    ".join(repr(x).splitlines())
+                parts.append(f"    {fn}={r},")
+            parts.append(")")
+            return "\n".join(parts)
+
+        setattr(union, "__repr__", _repr)
+
+        # Override Bits.__str__ method
+        def _str(self: Union) -> str:
+            parts = [f"{name}("]
+            for fn, _ in fields:
+                x = getattr(self, fn)
+                s = "\n    ".join(str(x).splitlines())
+                parts.append(f"    {fn}={s},")
+            parts.append(")")
+            return "\n".join(parts)
+
+        setattr(union, "__str__", _str)
+
+        # Create Union fields
+        def _fget(ft: type[Bits], self: Union):
+            m = mask(ft.size)
+            d0 = self._data[0] & m  # pyright: ignore[reportPrivateUsage]
+            d1 = self._data[1] & m  # pyright: ignore[reportPrivateUsage]
+            return ft.cast_data(d0, d1)
+
+        for fn, ft in fields:
+            setattr(union, fn, property(fget=partial(_fget, ft)))
+
+        return union
+
+
+class Union(Composite, metaclass=_UnionMeta):
+    """User defined union data type.
+
+    Compose a type from the union of other types.
+
+    Extend from ``Union`` to define a union:
+
+    >>> from bvwx import Vec
+    >>> class Response(Union):
+    ...     error: Vec[4]
+    ...     data: Vec[8]
+
+    Use the new type's constructor to create ``Union`` instances:
+
+    >>> rsp = Response("8h0f")
+
+    Access individual fields using attributes:
+
+    >>> rsp.error
+    bits("4b1111")
+    >>> rsp.data
+    bits("8b0000_1111")
+
+    ``Unions`` have a ``size``, but no ``shape``.
+    They do **NOT** implement a ``__len__`` method.
+
+    >>> Response.size
+    8
+
+    ``Union`` slicing behaves like a ``Vector``:
+
+    >>> rsp[3:5]
+    bits("2b01")
+    """
