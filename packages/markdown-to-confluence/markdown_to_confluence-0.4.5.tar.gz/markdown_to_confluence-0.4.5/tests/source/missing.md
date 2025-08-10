b5a7f6883f3439eb86c47f0f6458347f35201dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:22 +0000

    Add package markdown-to-confluence/0.4.5/markdown_to_confluence-0.4.5.tar.gz

---
+++ packages/markdown-to-confluence/markdown_to_confluence-0.4.5.tar.gz/markdown_to_confluence-0.4.5/tests/source/missing.md
@@ -0,0 +1,7 @@
+<!-- confluence-page-id: 00000000000 -->
+
+## Broken links
+
+Relative URLs to [locations not exported](missing.md) may be skipped.
+
+![Missing PNG image](missing.png)
---
+++ packages/markdown-to-confluence/markdown_to_confluence-0.4.5.tar.gz/markdown_to_confluence-0.4.5/tests/test_scanner.py
@@ -0,0 +1,57 @@
+"""
+Publish Markdown files to Confluence wiki.
+
+Copyright 2022-2025, Levente Hunyadi
+
+:see: https://github.com/hunyadi/md2conf
+"""
+
+import logging
+import unittest
+from pathlib import Path
+
+from md2conf.extra import override
+from md2conf.scanner import Scanner
+from tests.utility import TypedTestCase
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(funcName)s [%(lineno)d] - %(message)s",
+)
+
+
+class TestScanner(TypedTestCase):
+    sample_dir: Path
+
+    @override
+    def setUp(self) -> None:
+        self.maxDiff = 1024
+
+        test_dir = Path(__file__).parent
+        parent_dir = test_dir.parent
+
+        self.sample_dir = parent_dir / "sample"
+
+    def test_tag(self) -> None:
+        document = Scanner().read(self.sample_dir / "index.md")
+        self.assertIsNotNone(document.page_id)
+        self.assertIsNone(document.space_key)
+        self.assertIsNone(document.title)
+
+    def test_json_frontmatter(self) -> None:
+        document = Scanner().read(self.sample_dir / "parent" / "index.md")
+        self.assertEqual(document.page_id, "1966122")
+        self.assertEqual(document.space_key, "~hunyadi")
+        self.assertEqual(document.title, "🏠 Markdown parent page")
+
+    def test_yaml_frontmatter(self) -> None:
+        document = Scanner().read(self.sample_dir / "sibling.md")
+        self.assertIsNotNone(document.page_id)
+        self.assertIsNone(document.space_key)
+        self.assertEqual(document.generated_by, "This page has been generated with md2conf.")
+        self.assertEqual(document.title, "Markdown example document")
+        self.assertEqual(document.tags, ["markdown", "confluence", "md", "wiki"])
+
+
+if __name__ == "__main__":
+    unittest.main()
