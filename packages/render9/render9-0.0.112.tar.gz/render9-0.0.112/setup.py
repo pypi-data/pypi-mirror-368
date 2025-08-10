beb84f315f242d029da3bd71ee38eea71de475b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 03:34:06 +0000

    Add package render9/0.0.112/render9-0.0.112.tar.gz

---
+++ packages/render9/render9-0.0.112.tar.gz/render9-0.0.112/setup.py
@@ -0,0 +1,15 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='render9',
+    version='0.0.112',
+    description="Render9 OTP is a lightweight and easy-to-use Python package for sending OTP messages via the Render9 OTP API.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv',
+    ],
+)
