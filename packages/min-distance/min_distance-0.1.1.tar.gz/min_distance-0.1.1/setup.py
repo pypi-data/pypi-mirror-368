d2cb86b8b1bdb29cf834ad72a11b82d14b151ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 04:19:11 +0000

    Add package min-distance/0.1.1/min_distance-0.1.1.tar.gz

---
+++ packages/min-distance/min_distance-0.1.1.tar.gz/min_distance-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="min_distance",
+    version="0.1.1",
+    author="Vansh",
+    author_email="vanshbhardwajhere@gmail.com",
+    description="A simple Python package to calculate the minimum distance between two geographic points",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vanshbhardwajhere/min_distance",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
