444ead865eda2dfe2d2755e289682ef592601b07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 05:07:46 +0000

    Add package mcsrvstatus/1.0.4/mcsrvstatus-1.0.4-py3-none-any.whl

---
+++ packages/mcsrvstatus/mcsrvstatus-1.0.4-py3-none-any.whl/mcsrvstatus/__init__.py
@@ -0,0 +1,35 @@
+"""Python library for mcsrvstat.us API - Minecraft server status checker."""
+
+from .client import MinecraftServerStatus
+from .async_client import AsyncMinecraftServerStatus
+from .exceptions import (
+    MCSrvStatError,
+    ServerNotFoundError,
+    APIError,
+    ConnectionError
+)
+from .models import (
+    ServerStatus,
+    BedrockServerStatus,
+    PlayerInfo,
+    ServerVersion,
+    ServerMotd
+)
+
+__version__ = "1.0.4"
+__author__ = "Towux"
+__description__ = "Python library for mcsrvstat.us API"
+
+__all__ = [
+    "MinecraftServerStatus",
+    "AsyncMinecraftServerStatus",
+    "MCSrvStatError", 
+    "ServerNotFoundError",
+    "APIError",
+    "ConnectionError",
+    "ServerStatus",
+    "BedrockServerStatus",
+    "PlayerInfo",
+    "ServerVersion",
+    "ServerMotd"
+]
