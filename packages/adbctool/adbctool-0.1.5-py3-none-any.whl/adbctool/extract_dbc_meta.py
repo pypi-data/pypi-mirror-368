646d52730885709b7f104c37408765e5c3d720d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:25 +0000

    Add package adbctool/0.1.5/adbctool-0.1.5-py3-none-any.whl

---
+++ packages/adbctool/adbctool-0.1.5-py3-none-any.whl/adbctool/extract_dbc_meta.py
@@ -0,0 +1,274 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# Copyright 2017 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+import re
+import shlex
+import sys
+import yaml
+import chardet
+import codecs
+
+MAX_CAN_ID = 4096000000  # include can extended ID
+STANDARD_CAN_ID = 2048
+
+
+def extract_var_info(items):
+    """
+       Desp: extract var info from line split items.
+    """
+    car_var = {}
+    car_var["name"] = items[1]
+    car_var["bit"] = int(items[3].split('|')[0])
+    car_var["len"] = int(items[3].split('|')[1].split('@')[0])
+    order_sign = items[3].split('|')[1].split('@')[1]
+    if order_sign == "0+":
+        car_var["order"] = "motorola"
+        car_var["is_signed_var"] = False
+    elif order_sign == "0-":
+        car_var["order"] = "motorola"
+        car_var["is_signed_var"] = True
+    elif order_sign == "1+":
+        car_var["order"] = "intel"
+        car_var["is_signed_var"] = False
+    elif order_sign == "1-":
+        car_var["order"] = "intel"
+        car_var["is_signed_var"] = True
+    car_var["offset"] = float(items[4].split(',')[1].split(')')[0])
+    car_var["precision"] = float(items[4].split(',')[0].split('(')[1])
+    car_var["physical_range"] = items[5]
+    car_var["physical_unit"] = items[6].replace('_', ' ')
+    if car_var["len"] == 1:
+        car_var["type"] = "bool"
+    elif car_var["physical_range"].find(
+            ".") != -1 or car_var["precision"] != 1.0:
+        car_var["type"] = "double"
+    else:
+        car_var["type"] = "int"
+
+    return car_var
+
+
+def detect_file_encoding(file_path,
+                         default_encoding='utf-8',
+                         confidence_threshold=0.5):
+    """Detect file encoding
+
+    Args:
+        file_path (_type_): _description_
+        default_encoding (str, optional): _description_. Defaults to 'utf-8'.
+        confidence_threshold (float, optional): _description_. Defaults to 0.5.
+
+    Returns:
+        _type_: _description_
+    """
+    with open(file_path, 'rb') as f:
+        raw_data = f.read()
+        result = chardet.detect(raw_data)
+
+        encoding = result['encoding']
+        confidence = result['confidence']
+
+        if confidence < confidence_threshold or encoding is None:
+            return default_encoding, confidence
+        else:
+            return encoding, confidence
+
+
+def parse_signal_comment(items, protocols):
+    """
+    Parse a signal comment line from the DBC file.
+    """
+    protocol_id = "%x" % int(items[2])
+    if int(items[2]) > MAX_CAN_ID:
+        return
+    if int(items[2]) > STANDARD_CAN_ID:
+        protocol_id = gen_can_id_extended(protocol_id)
+    for var in protocols[protocol_id]["vars"]:
+        if var["name"] == items[3]:
+            var["description"] = items[4][:-1]
+
+
+def parse_enum_values(items, protocols):
+    """
+    Parse enumeration values from the DBC file.
+    """
+    protocol_id = "%x" % int(items[1])
+    if int(items[1]) > MAX_CAN_ID:
+        return
+    if int(items[1]) > STANDARD_CAN_ID:
+        protocol_id = gen_can_id_extended(protocol_id)
+    for var in protocols[protocol_id]["vars"]:
+        if var["name"] == items[2]:
+            var["type"] = "enum"
+            var["enum"] = {}
+            for idx in range(3, len(items) - 1, 2):
+                enumtype = re.sub(r'\W+', ' ',
+                                  items[idx + 1]).strip().replace(" ",
+                                                                  "_").upper()
+                enumtype = f"{items[2].upper()}_{enumtype}"
+                var["enum"][int(items[idx])] = enumtype
+
+
+def parse_message_cycle_time(items, protocols):
+    """Parse message cycle time from the DBC file.
+    """
+    protocol_id = '%x' % int(items[3])
+    if int(items[3]) > MAX_CAN_ID:
+        return
+    if int(items[3]) > STANDARD_CAN_ID:
+        protocol_id = gen_can_id_extended(protocol_id)
+    period = items[4].rstrip(' ;')
+    protocols[protocol_id]['period'] = period
+
+
+def adjust_reserved_keywords(protocols):
+    """
+    Adjust variable names that are reserved C++ keywords.
+    """
+    CPP_RESERVED_KEYWORDS = ['minor', 'major', 'long', 'int']
+    for protocol in protocols.values():
+        for var in protocol["vars"]:
+            if var["name"].lower() in CPP_RESERVED_KEYWORDS:
+                var["name"] = f"MY_{var['name']}"
+
+
+def log_summary(car_type, out_file, protocols):
+    """
+    Log a summary of the parsed protocols.
+    """
+    control_protocol_num = sum(1 for p in protocols.values()
+                               if p["protocol_type"] == "control")
+    report_protocol_num = sum(1 for p in protocols.values()
+                              if p["protocol_type"] == "report")
+
+    print(
+        f"Extracted car_type: {car_type.upper()}'s protocol meta info to file: {out_file}"
+    )
+    print(f"Total parsed protocols: {len(protocols)}")
+    print(f"Control protocols: {control_protocol_num}")
+    print(f"Report protocols: {report_protocol_num}")
+
+
+def extract_dbc_meta(dbc_file, out_file, car_type, black_list, sender_list,
+                     sender, can_interface="socketcan"):
+    """
+        the main gen_config func, use dbc file to gen a yaml file
+        parse every line, if the line is:
+        eg:BO_ 1104 BMS_0x450: 8 VCU
+        5 segments, and segments[0] is "BO_", then begin parse every signal in the following line
+
+    """
+    sender_list = {format(int(item, 16), "x") for item in sender_list}
+
+    # Get the file character encoding
+    encoding, _ = detect_file_encoding(dbc_file)
+
+    with codecs.open(dbc_file, encoding=encoding) as f:
+        in_protocol = False
+        protocols = {}
+        protocol = {}
+        p_name = ""
+
+        try:
+            for line_num, line in enumerate(f, start=1):
+                items = shlex.split(line)
+
+                if len(items) == 5 and items[0] == "BO_":
+                    p_name = items[2][:-1].lower()
+                    protocol = {}
+                    if int(items[1]) > MAX_CAN_ID:
+                        continue
+                    protocol["id"] = "%x" % int(items[1])
+                    if int(items[1]) > STANDARD_CAN_ID:
+                        protocol["id"] = gen_can_id_extended(protocol["id"])
+                    protocol["name"] = "%s_%s" % (p_name, protocol["id"])
+                    protocol["sender"] = items[4]
+                    if protocol["id"] in black_list:
+                        continue
+                    protocol["protocol_type"] = "report"
+                    if protocol["id"] in sender_list or protocol["sender"] == sender:
+                        protocol["protocol_type"] = "control"
+                    protocol["vars"] = []
+                    in_protocol = True
+                elif in_protocol:
+                    if len(items) > 3 and items[0] == "SG_":
+                        if items[2] == ":":
+                            var_info = extract_var_info(items)
+                            # current we can't process than 4 byte value
+                            if var_info["len"] <= 32:
+                                protocol["vars"].append(var_info)
+                    else:
+                        in_protocol = False
+                        if len(protocol) != 0 and len(
+                                protocol["vars"]) != 0 and len(
+                                    protocol["vars"]) < 65:
+                            protocols[protocol["id"]] = protocol
+                            # print protocol
+                            protocol = {}
+
+                if len(items) == 5 and items[0] == "CM_" and items[1] == "SG_":
+                    parse_signal_comment(items, protocols)
+
+                if len(items) > 2 and items[0] == "VAL_":
+                    parse_enum_values(items, protocols)
+
+                if len(items) == 5 and items[0] == 'BA_' and items[
+                        1] == 'GenMsgCycleTime' and items[2] == 'BO_':
+                    parse_message_cycle_time(items, protocols)
+
+        except (ValueError, UnicodeDecodeError) as e:
+            print(f"Error occurred on line {line_num}: {e}")
+            return False
+
+        adjust_reserved_keywords(protocols)
+
+        # save protocols
+        config = {"car_type": car_type, "protocols": protocols}
+        config["can_interface"] = can_interface
+        with open(out_file, 'w') as fp:
+            yaml.dump(config, fp)
+
+        # summary
+        log_summary(car_type, out_file, protocols)
+        return True
+
+
+def gen_can_id_extended(str):
+    """
+        id string:
+    """
+    int_id = int(str, 16)
+    int_id &= 0x1FFFFFFF
+    str = hex(int_id).replace('0x', '')
+    return str
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage:\npython %s your_car_parse_config_file.yml" % sys.argv[0])
+        sys.exit(0)
+    with open(sys.argv[1], 'r') as fp:
+        conf = yaml.safe_load(fp)
+    dbc_file = conf["dbc_file"]
+    protocol_conf_file = conf["protocol_conf"]
+    car_type = conf["car_type"]
+    black_list = conf["black_list"]
+    sender_list = conf["sender_list"]
+    sender = conf["sender"]
+    extract_dbc_meta(dbc_file, protocol_conf_file, car_type, black_list,
+                     sender_list, sender, can_interface)
---
+++ packages/adbctool/adbctool-0.1.5-py3-none-any.whl/adbctool/gen.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# Copyright 2017 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# -*- coding:utf-8 -*-
+
+import sys
+import argparse
+import ast
+
+from adbctool.extract_dbc_meta import extract_dbc_meta
+from adbctool.gen_proto_file import gen_proto_file
+from adbctool.gen_protocols import gen_protocols
+from adbctool.gen_vehicle_controller_and_manager import gen_vehicle_controller_and_manager
+
+
+def main(args=sys.argv):
+    """
+    Main function to generate vehicle protocol files from a DBC file.
+    It parses arguments, validates them, and then orchestrates the generation process.
+    """
+    parser = argparse.ArgumentParser(
+        description="A tool to generate Apollo vehicle protocol from a DBC file.",
+        prog="gen.py",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    # --- Required Arguments ---
+    parser.add_argument("-f", "--dbc_file",
+                        action="store",
+                        type=str,
+                        required=True,
+                        help="Specify the path to the input DBC file.")
+    parser.add_argument("-t", "--car_type",
+                        action="store",
+                        type=str,
+                        required=True,
+                        help="Specify the car type or name (e.g., LINCOLN_MKZ_2017).")
+
+    # --- Control Message Definition (User must choose one of the following) ---
+    control_group = parser.add_argument_group(
+        'Control Message Definition (Choose ONE method)',
+        'Define which CAN messages are control commands.'
+    )
+    control_group.add_argument("--sender",
+                               action="store",
+                               type=str,
+                               default=None,  # Default to None to check if it was provided
+                               help="""The name of the node (ECU) that sends control messages.
+All messages from this sender will be marked as control messages.
+(e.g., --sender MAB)""")
+    control_group.add_argument(
+        "--sender_list",
+        nargs='+',
+        type=str,
+        default=None,
+        help="""A list of CAN message IDs in hexadecimal format (e.g., 0x180 0x25A).
+    Use this for fine-grained control or when commands come from multiple ECUs.
+    Example: --sender_list 0x180 0x25A""")
+
+    # --- Optional Arguments ---
+    parser.add_argument("-b", "--black_list",
+                        action="store",
+                        type=str,      # Receive as string
+                        default='[]',  # Default to a string representation of an empty list
+                        help="A Python-style list of message names to exclude from processing.")
+    parser.add_argument("--can_interface",
+                        choices=["socketcan", "esdcan"],
+                        default="socketcan",
+                        help="Specify the CAN interface type (default: socketcan)."
+                        )
+    parser.add_argument("-o", "--output_dir",
+                        action="store",
+                        type=str,
+                        default="output/",
+                        help="Specify the directory to save the generated files (default: ./output/).")
+
+    parsed_args = parser.parse_args(args[1:])
+
+    # --- Argument Validation ---
+    if parsed_args.sender is None and parsed_args.sender_list is None:
+        parser.error(
+            "You must define the source of control messages. Please provide either --sender or --sender_list.")
+
+    # Safely parse string arguments into Python lists
+    try:
+        black_list = ast.literal_eval(parsed_args.black_list)
+        sender_list = parsed_args.sender_list if parsed_args.sender_list else []
+    except (ValueError, SyntaxError) as e:
+        print(f"Error: Invalid list format for --black_list or --sender_list. Please use Python list syntax.", file=sys.stderr)
+        print(f"Details: {e}", file=sys.stderr)
+        sys.exit(1)
+
+    # --- Generation Logic ---
+    print("🚀 Starting vehicle protocol generation...")
+    protocol_conf_file = "dbc.yml"
+    if not extract_dbc_meta(parsed_args.dbc_file, protocol_conf_file, parsed_args.car_type,
+                            black_list, sender_list, parsed_args.sender):
+        print("❌ Failed to extract DBC meta information.", file=sys.stderr)
+        return
+
+    # Generate proto file
+    proto_dir = parsed_args.output_dir + "proto/"
+    gen_proto_file(protocol_conf_file, proto_dir)
+
+    # Generate protocol files
+    protocol_dir = parsed_args.output_dir + "vehicle/" + \
+        parsed_args.car_type.lower() + "/protocol/"
+    gen_protocols(protocol_conf_file, protocol_dir)
+
+    # Generate vehicle controller and protocol_manager
+    vehicle_dir = parsed_args.output_dir + \
+        "vehicle/" + parsed_args.car_type.lower() + "/"
+    gen_vehicle_controller_and_manager(protocol_conf_file, vehicle_dir)
+
+    print(f"\n✅ Vehicle protocol generation complete!")
+    print(f"Files saved in: {parsed_args.output_dir}")
+
+
+if __name__ == '__main__':
+    main()
