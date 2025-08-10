c9e190508fce543506a80036331724777892806c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 14:05:06 +0000

    Add package liblaf-melon/0.5.1/liblaf_melon-0.5.1-py3-none-any.whl

---
+++ packages/liblaf-melon/liblaf_melon-0.5.1-py3-none-any.whl/liblaf/melon/io/wrap/polygons/_utils.py
@@ -0,0 +1,11 @@
+import os
+from pathlib import Path
+
+from liblaf import grapes
+
+
+def get_polygons_path(path: str | os.PathLike[str]) -> Path:
+    path: Path = grapes.as_path(path)
+    if path.suffix != ".json":
+        return path.with_suffix(".polygons.json")
+    return path
