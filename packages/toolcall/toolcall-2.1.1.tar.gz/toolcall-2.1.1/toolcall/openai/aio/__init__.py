69c1495fa18b6cf272eb73b18607939c020b38ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:04:58 +0000

    Add package toolcall/2.1.1/toolcall-2.1.1.tar.gz

---
+++ packages/toolcall/toolcall-2.1.1.tar.gz/toolcall-2.1.1/toolcall/openai/aio/__init__.py
@@ -0,0 +1,13 @@
+from .._common import *
+from ._tool import *
+from ._group import *
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import deprecated
+
+    @deprecated("Renamed ToolGroup")
+    class FunctionToolGroup[In, Out](ToolGroup[In, Out]): ...
+else:
+    FunctionToolGroup = ToolGroup
