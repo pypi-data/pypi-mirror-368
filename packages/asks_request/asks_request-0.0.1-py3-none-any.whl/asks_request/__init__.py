c2ffb3d9650235aa4a8182000627a585faff4141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:29 +0000

    Add package asks_request/0.0.1/asks_request-0.0.1-py3-none-any.whl

---
+++ packages/asks_request/asks_request-0.0.1-py3-none-any.whl/asks_request/__init__.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+__author__ = "ChenyangGao <https://chenyanggao.github.io>"
+__version__ = (0, 0, 1)
+__all__ = ["request"]
+
+from collections import UserString
+from collections.abc import Buffer, Callable, Iterable, Mapping
+from http.cookiejar import CookieJar
+from http.cookies import SimpleCookie
+from inspect import isawaitable
+from os import PathLike
+from types import EllipsisType
+from typing import cast, overload, Any, Final, Literal
+
+from argtools import argcount
+from cookietools import cookies_to_dict, update_cookies
+from dicttools import get_all_items
+from ensure import ensure_acm
+from filewrap import bio_chunk_iter, SupportsRead
+from http_request import normalize_request_args, SupportsGeturl
+from http_response import parse_response
+from asks.response_objects import BaseResponse, StreamBody # type: ignore
+from asks.sessions import Session # type: ignore
+from yarl import URL
+
+
+type string = Buffer | str | UserString
+
+_REQUEST_KWARGS: Final = {
+    "method", "url", "data", "params", "headers", "encoding", "json", "files", 
+    "multipart", "cookies", "callback", "timeout", "retries", "max_redirects", 
+    "follow_redirects", "persist_cookies", "auth", "stream", 
+}
+
+def __del__(self, /):
+    from asynctools import run_async
+    run_async(self.close())
+
+if "__del__" not in Session.__dict__:
+    setattr(Session, "__del__", __del__)
+if "__del__" not in StreamBody.__dict__:
+    setattr(StreamBody, "__del__", __del__)
+
+_DEFAULT_SESSION = Session(connections=64)
+
+
+@overload
+async def request(
+    url: string | SupportsGeturl | URL, 
+    method: string = "GET", 
+    params: None | string | Mapping | Iterable[tuple[Any, Any]] = None, 
+    data: Any = None, 
+    json: Any = None, 
+    files: None | Mapping[string, Any] | Iterable[tuple[string, Any]] = None, 
+    headers: None | Mapping[string, string] | Iterable[tuple[string, string]] = None, 
+    follow_redirects: bool = True, 
+    raise_for_status: bool = True, 
+    stream: bool = True, 
+    cookies: None | CookieJar | SimpleCookie = None, 
+    session: None | Session = _DEFAULT_SESSION, 
+    *, 
+    parse: None | EllipsisType = None, 
+    **request_kwargs, 
+) -> BaseResponse:
+    ...
+@overload
+async def request(
+    url: string | SupportsGeturl | URL, 
+    method: string = "GET", 
+    params: None | string | Mapping | Iterable[tuple[Any, Any]] = None, 
+    data: Any = None, 
+    json: Any = None, 
+    files: None | Mapping[string, Any] | Iterable[tuple[string, Any]] = None, 
+    headers: None | Mapping[string, string] | Iterable[tuple[string, string]] = None, 
+    follow_redirects: bool = True, 
+    raise_for_status: bool = True, 
+    stream: bool = True, 
+    cookies: None | CookieJar | SimpleCookie = None, 
+    session: None | Session = _DEFAULT_SESSION, 
+    *, 
+    parse: Literal[False], 
+    **request_kwargs, 
+) -> bytes:
+    ...
+@overload
+async def request(
+    url: string | SupportsGeturl | URL, 
+    method: string = "GET", 
+    params: None | string | Mapping | Iterable[tuple[Any, Any]] = None, 
+    data: Any = None, 
+    json: Any = None, 
+    files: None | Mapping[string, Any] | Iterable[tuple[string, Any]] = None, 
+    headers: None | Mapping[string, string] | Iterable[tuple[string, string]] = None, 
+    follow_redirects: bool = True, 
+    raise_for_status: bool = True, 
+    stream: bool = True, 
+    cookies: None | CookieJar | SimpleCookie = None, 
+    session: None | Session = _DEFAULT_SESSION, 
+    *, 
+    parse: Literal[True], 
+    **request_kwargs, 
+) -> bytes | str | dict | list | int | float | bool | None:
+    ...
+@overload
+async def request[T](
+    url: string | SupportsGeturl | URL, 
+    method: string = "GET", 
+    params: None | string | Mapping | Iterable[tuple[Any, Any]] = None, 
+    data: Any = None, 
+    json: Any = None, 
+    files: None | Mapping[string, Any] | Iterable[tuple[string, Any]] = None, 
+    headers: None | Mapping[string, string] | Iterable[tuple[string, string]] = None, 
+    follow_redirects: bool = True, 
+    raise_for_status: bool = True, 
+    stream: bool = True, 
+    cookies: None | CookieJar | SimpleCookie = None, 
+    session: None | Session = _DEFAULT_SESSION, 
+    *, 
+    parse: Callable[[BaseResponse, bytes], T] | Callable[[BaseResponse], T], 
+    **request_kwargs, 
+) -> T:
+    ...
+async def request[T](
+    url: string | SupportsGeturl | URL, 
+    method: string = "GET", 
+    params: None | string | Mapping | Iterable[tuple[Any, Any]] = None, 
+    data: Any = None, 
+    json: Any = None, 
+    files: None | Mapping[string, Any] | Iterable[tuple[string, Any]] = None, 
+    headers: None | Mapping[string, string] | Iterable[tuple[string, string]] = None, 
+    follow_redirects: bool = True, 
+    raise_for_status: bool = True, 
+    stream: bool = True, 
+    cookies: None | CookieJar | SimpleCookie = None, 
+    session: None | Session = _DEFAULT_SESSION, 
+    *, 
+    parse: None | EllipsisType| bool | Callable[[BaseResponse, bytes], T] | Callable[[BaseResponse], T] = None, 
+    **request_kwargs, 
+) -> BaseResponse | bytes | str | dict | list | int | float | bool | None | T:
+    request_kwargs["follow_redirects"] = follow_redirects
+    request_kwargs["stream"] = stream
+    if session is None:
+        session = Session()
+    if isinstance(data, PathLike):
+        data = bio_chunk_iter(open(data, "rb"))
+    elif isinstance(data, SupportsRead):
+        data = bio_chunk_iter(data)
+    request_kwargs.update(normalize_request_args(
+        method=method, 
+        url=url, 
+        params=params, 
+        data=data, 
+        json=json, 
+        files=files, 
+        headers=headers, 
+    ))
+    if cookies is not None:
+        request_kwargs["cookies"] = cookies_to_dict(cookies, predicate=request_kwargs["url"])
+    response = await session.request(
+        **dict(get_all_items(request_kwargs, *_REQUEST_KWARGS)))
+    setattr(response, "session", session)
+    if cookies is not None and response.cookies:
+        update_cookies(cookies, response.cookies) # type: ignore
+    if raise_for_status:
+        response.raise_for_status()
+    if parse is None:
+        return response
+    elif parse is ...:
+        body = response.body
+        if not isinstance(body, Buffer):
+            await body.close()
+        return response
+    async with ensure_acm(response.body):
+        if isinstance(parse, bool):
+            content = response.content
+            if parse:
+                return parse_response(response, content)
+            return content
+        ac = argcount(parse)
+        if ac == 1:
+            ret = cast(Callable[[BaseResponse], T], parse)(response)
+        else:
+            ret = cast(Callable[[BaseResponse, bytes], T], parse)(
+                response, response.content)
+        if isawaitable(ret):
+            ret = await ret
+        return ret
+
