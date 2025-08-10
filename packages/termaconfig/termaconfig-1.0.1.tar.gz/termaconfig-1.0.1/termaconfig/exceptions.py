3c1d0e0235a6c29aa1c7e39270f1c166bb13a6d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:04:58 +0000

    Add package termaconfig/1.0.1/termaconfig-1.0.1.tar.gz

---
+++ packages/termaconfig/termaconfig-1.0.1.tar.gz/termaconfig-1.0.1/termaconfig/exceptions.py
@@ -0,0 +1,13 @@
+# termaconfig/exceptions.py
+
+
+class TableTypeError(Exception):
+    """Raised when a terminaltables class instance is invalid"""
+
+    pass
+
+
+class ConfigValidationError(Exception):
+    """Raised by the TermaConfig class when an errortree exists (invalid config)"""
+
+    pass
