581fb6942d965fc9951c7b6b39f3e2e74297cff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:05:15 +0000

    Add package robosystems-sdk/0.1.8/robosystems_sdk-0.1.8.tar.gz

---
+++ packages/robosystems-sdk/robosystems_sdk-0.1.8.tar.gz/robosystems_sdk-0.1.8/robosystems_client/models/subscription_info.py
@@ -0,0 +1,180 @@
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, Any, TypeVar, Union, cast
+
+from attrs import define as _attrs_define
+from attrs import field as _attrs_field
+
+from ..types import UNSET, Unset
+
+if TYPE_CHECKING:
+  from ..models.subscription_info_metadata import SubscriptionInfoMetadata
+
+
+T = TypeVar("T", bound="SubscriptionInfo")
+
+
+@_attrs_define
+class SubscriptionInfo:
+  """User subscription information.
+
+  Attributes:
+      id (str): Subscription ID
+      user_id (str): User ID
+      addon_type (str): Add-on type
+      addon_tier (str): Subscription tier
+      is_active (bool): Whether subscription is active
+      is_trial (bool): Whether this is a trial subscription
+      activated_at (str): Activation date (ISO format)
+      monthly_price_cents (int): Monthly price in cents
+      features (list[str]): List of features
+      metadata (SubscriptionInfoMetadata): Additional metadata
+      trial_ends_at (Union[None, Unset, str]): Trial expiration date (ISO format)
+      expires_at (Union[None, Unset, str]): Expiration date (ISO format)
+  """
+
+  id: str
+  user_id: str
+  addon_type: str
+  addon_tier: str
+  is_active: bool
+  is_trial: bool
+  activated_at: str
+  monthly_price_cents: int
+  features: list[str]
+  metadata: "SubscriptionInfoMetadata"
+  trial_ends_at: Union[None, Unset, str] = UNSET
+  expires_at: Union[None, Unset, str] = UNSET
+  additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
+
+  def to_dict(self) -> dict[str, Any]:
+    id = self.id
+
+    user_id = self.user_id
+
+    addon_type = self.addon_type
+
+    addon_tier = self.addon_tier
+
+    is_active = self.is_active
+
+    is_trial = self.is_trial
+
+    activated_at = self.activated_at
+
+    monthly_price_cents = self.monthly_price_cents
+
+    features = self.features
+
+    metadata = self.metadata.to_dict()
+
+    trial_ends_at: Union[None, Unset, str]
+    if isinstance(self.trial_ends_at, Unset):
+      trial_ends_at = UNSET
+    else:
+      trial_ends_at = self.trial_ends_at
+
+    expires_at: Union[None, Unset, str]
+    if isinstance(self.expires_at, Unset):
+      expires_at = UNSET
+    else:
+      expires_at = self.expires_at
+
+    field_dict: dict[str, Any] = {}
+    field_dict.update(self.additional_properties)
+    field_dict.update(
+      {
+        "id": id,
+        "user_id": user_id,
+        "addon_type": addon_type,
+        "addon_tier": addon_tier,
+        "is_active": is_active,
+        "is_trial": is_trial,
+        "activated_at": activated_at,
+        "monthly_price_cents": monthly_price_cents,
+        "features": features,
+        "metadata": metadata,
+      }
+    )
+    if trial_ends_at is not UNSET:
+      field_dict["trial_ends_at"] = trial_ends_at
+    if expires_at is not UNSET:
+      field_dict["expires_at"] = expires_at
+
+    return field_dict
+
+  @classmethod
+  def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
+    from ..models.subscription_info_metadata import SubscriptionInfoMetadata
+
+    d = dict(src_dict)
+    id = d.pop("id")
+
+    user_id = d.pop("user_id")
+
+    addon_type = d.pop("addon_type")
+
+    addon_tier = d.pop("addon_tier")
+
+    is_active = d.pop("is_active")
+
+    is_trial = d.pop("is_trial")
+
+    activated_at = d.pop("activated_at")
+
+    monthly_price_cents = d.pop("monthly_price_cents")
+
+    features = cast(list[str], d.pop("features"))
+
+    metadata = SubscriptionInfoMetadata.from_dict(d.pop("metadata"))
+
+    def _parse_trial_ends_at(data: object) -> Union[None, Unset, str]:
+      if data is None:
+        return data
+      if isinstance(data, Unset):
+        return data
+      return cast(Union[None, Unset, str], data)
+
+    trial_ends_at = _parse_trial_ends_at(d.pop("trial_ends_at", UNSET))
+
+    def _parse_expires_at(data: object) -> Union[None, Unset, str]:
+      if data is None:
+        return data
+      if isinstance(data, Unset):
+        return data
+      return cast(Union[None, Unset, str], data)
+
+    expires_at = _parse_expires_at(d.pop("expires_at", UNSET))
+
+    subscription_info = cls(
+      id=id,
+      user_id=user_id,
+      addon_type=addon_type,
+      addon_tier=addon_tier,
+      is_active=is_active,
+      is_trial=is_trial,
+      activated_at=activated_at,
+      monthly_price_cents=monthly_price_cents,
+      features=features,
+      metadata=metadata,
+      trial_ends_at=trial_ends_at,
+      expires_at=expires_at,
+    )
+
+    subscription_info.additional_properties = d
+    return subscription_info
+
+  @property
+  def additional_keys(self) -> list[str]:
+    return list(self.additional_properties.keys())
+
+  def __getitem__(self, key: str) -> Any:
+    return self.additional_properties[key]
+
+  def __setitem__(self, key: str, value: Any) -> None:
+    self.additional_properties[key] = value
+
+  def __delitem__(self, key: str) -> None:
+    del self.additional_properties[key]
+
+  def __contains__(self, key: str) -> bool:
+    return key in self.additional_properties
