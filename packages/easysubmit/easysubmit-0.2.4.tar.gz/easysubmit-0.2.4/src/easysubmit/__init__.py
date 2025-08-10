245ea4ce9d30d49be920d0f3b42bd7c0d1d0892b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 04:19:25 +0000

    Add package easysubmit/0.2.4/easysubmit-0.2.4.tar.gz

---
+++ packages/easysubmit/easysubmit-0.2.4.tar.gz/easysubmit-0.2.4/src/easysubmit/__init__.py
@@ -0,0 +1,14 @@
+from easysubmit.entities import Cluster, Job, Task, TaskConfig, AutoTask
+from easysubmit.slurm import SLURMCluster, SLURMConfig
+
+__version__ = "0.2.4"
+
+__all__ = [
+    "Task",
+    "TaskConfig",
+    "AutoTask",
+    "Job",
+    "Cluster",
+    "SLURMCluster",
+    "SLURMConfig",
+]
