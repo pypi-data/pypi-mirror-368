6230e36ef9407337b07c1d46be39e349c83035cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-10 15:04:57 +0000

    Add package m_crawler/0.1.4/m_crawler-0.1.4-py3-none-any.whl

---
+++ packages/m_crawler/m_crawler-0.1.4-py3-none-any.whl/m_crawler/engine/playwright_engine.pyi
@@ -0,0 +1,20 @@
+from _typeshed import Incomplete
+from m_crawler.config import PlayWeightConfig as PlayWeightConfig
+from m_crawler.engine.abstract_engine import AbstractEngine as AbstractEngine
+from m_crawler.engine.abstract_request import AbstractRequest as AbstractRequest
+from m_crawler.engine.abstract_response import AbstractResponse as AbstractResponse
+from m_crawler.engine.request import PlayWeightRequest as PlayWeightRequest
+from m_crawler.engine.response import BaseResponse as BaseResponse
+from playwright.sync_api import (
+    Browser as Browser,
+    BrowserContext as BrowserContext,
+    Playwright as Playwright,
+)
+
+class PlayWeightEngine(AbstractEngine):
+    settings: Incomplete
+    browser: Browser | None
+    browser_context: BrowserContext | None
+    def __init__(self, settings: PlayWeightConfig) -> None: ...
+    def download(self, request: AbstractRequest) -> AbstractResponse: ...
+    def after_download_hook(self, result: AbstractResponse) -> AbstractResponse: ...
