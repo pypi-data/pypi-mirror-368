f6bd763044386e3203f2fe5cd63a416a29e567db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 04:20:30 +0000

    Add package fixedwidthfile/0.8/fixedwidthfile-0.8.tar.gz

---
+++ packages/fixedwidthfile/fixedwidthfile-0.8.tar.gz/fixedwidthfile-0.8/00-package-pypi.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# instruction: https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+# packages needed for upload
+# python3 -m pip install --upgrade build twine
+
+python3 -m build
+
+# QA
+# python3 -m twine upload --repository testpypi dist/*
+
+# prod
+python3 -m twine upload dist/*
---
+++ packages/fixedwidthfile/fixedwidthfile-0.8.tar.gz/fixedwidthfile-0.8/01-testpipinstall.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+PKGNAME="fixedwidthfile"
+
+# QA
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps "$PKGNAME"
