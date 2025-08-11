1984afba22dab95668d55ba1b925e2df8a013646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 05:10:57 +0000

    Add package topsis-Mridul/1.0.0/topsis_mridul-1.0.0-py3-none-any.whl

---
+++ packages/topsis-Mridul/topsis_mridul-1.0.0-py3-none-any.whl/topsis/__init__.py
@@ -0,0 +1,14 @@
+"""
+TOPSIS (Technique for Order of Preference by Similarity to Ideal Solution)
+
+A Python library for implementing the Technique for Order of Preference by Similarity to Ideal Solution,
+a multi-criteria decision-making method.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Mridul Mahajan"
+__email__ = "mridulmahajan16@gmail.com"
+
+from .topsis import Topsis
+
+__all__ = ["Topsis"]
