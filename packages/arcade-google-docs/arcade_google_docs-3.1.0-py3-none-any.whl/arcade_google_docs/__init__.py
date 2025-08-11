896ac61bd6c8ed61129555bf777c29ba22250cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 06:05:20 +0000

    Add package arcade-google-docs/3.1.0/arcade_google_docs-3.1.0-py3-none-any.whl

---
+++ packages/arcade-google-docs/arcade_google_docs-3.1.0-py3-none-any.whl/arcade_google_docs/__init__.py
@@ -0,0 +1,21 @@
+from arcade_google_docs.tools import (
+    comment_on_document,
+    create_blank_document,
+    create_document_from_text,
+    get_document_by_id,
+    insert_text_at_end_of_document,
+    list_document_comments,
+    search_and_retrieve_documents,
+    search_documents,
+)
+
+__all__ = [
+    "create_blank_document",
+    "create_document_from_text",
+    "comment_on_document",
+    "list_document_comments",
+    "get_document_by_id",
+    "insert_text_at_end_of_document",
+    "search_and_retrieve_documents",
+    "search_documents",
+]
