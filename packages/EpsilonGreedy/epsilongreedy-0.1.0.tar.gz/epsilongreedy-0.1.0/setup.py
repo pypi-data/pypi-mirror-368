8971b110fdb6e287e42db32a59da6b2f6ed5a453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:35 +0000

    Add package EpsilonGreedy/0.1.0/epsilongreedy-0.1.0.tar.gz

---
+++ packages/EpsilonGreedy/epsilongreedy-0.1.0.tar.gz/epsilongreedy-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EpsilonGreedy",
+    version="0.1.0",
+    author="Priyanshi Furiya",
+    author_email="furiyapriyanshi@gmail.com",
+    description="A simple Epsilon-Greedy multi-armed bandit implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # Replace with your project's URL
+    url="https://github.com/priyanshi-furiya/epsilon-greedy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'epsilon-greedy-sim=EpsilonGreedy.main:main',
+        ],
+    },
+)
