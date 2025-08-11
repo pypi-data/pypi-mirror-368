13a4fb743c3c63e8232ea0b38d5897e9c375b371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:37 +0000

    Add package PaddockTS/0.2.0/paddockts-0.2.0-py3-none-any.whl

---
+++ packages/PaddockTS/paddockts-0.2.0-py3-none-any.whl/PaddockTS/PaddockSegmentation/segment_paddocks.py
@@ -0,0 +1,21 @@
+from PaddockTS.PaddockSegmentation._1_presegment import presegment
+from PaddockTS.PaddockSegmentation._2_segment import segment
+from PaddockTS.query import Query
+
+def get_paddocks(
+    query: Query,
+    min_area_ha: int = 10,
+    max_area_ha: int = 1500,
+    max_perim_area_ratio: int = 30,
+    device='cpu'
+):
+    presegment(query)
+    segment(query, min_area_ha, max_area_ha, max_perim_area_ratio, device='cpu')
+def test():
+    from PaddockTS.query import get_example_query
+    
+    query = get_example_query()
+    get_paddocks(query, device='cpu')
+
+if __name__ == '__main__':
+    test()
