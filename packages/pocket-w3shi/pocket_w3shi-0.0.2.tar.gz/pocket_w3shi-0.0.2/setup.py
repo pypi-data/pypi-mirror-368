abc202910541516c0b2e45002f3b203e5030f688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:40 +0000

    Add package pocket-w3shi/0.0.2/pocket_w3shi-0.0.2.tar.gz

---
+++ packages/pocket-w3shi/pocket_w3shi-0.0.2.tar.gz/pocket_w3shi-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='pocket_w3shi',
+    version='0.0.2',
+    packages=['pocket_w3shi'],
+    description='Demo package for PyPI publishing test',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your@email.com',
+    url='https://github.com/slvignesh05/pocket_w3shi',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.6',
+)
