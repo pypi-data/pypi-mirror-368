52811af27f7278c213326c08528dc190df0eb0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:01 +0000

    Add package jpegdata/0.1.0/jpegdata-0.1.0-py3-none-any.whl

---
+++ packages/jpegdata/jpegdata-0.1.0-py3-none-any.whl/jpegdata/enumerations/__init__.py
@@ -0,0 +1,11 @@
+from jpegdata.enumerations.format import Format
+from jpegdata.enumerations.encoding import Encoding
+from jpegdata.enumerations.marker import Marker
+from jpegdata.enumerations.state import State
+
+__all__ = [
+    "Format",
+    "Encoding",
+    "Marker",
+    "State",
+]
