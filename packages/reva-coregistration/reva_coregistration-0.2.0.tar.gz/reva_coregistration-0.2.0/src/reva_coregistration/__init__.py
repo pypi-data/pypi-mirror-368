6e7442fe7dd5f8ec861f882ed579711821b32bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:59 +0000

    Add package reva-coregistration/0.2.0/reva_coregistration-0.2.0.tar.gz

---
+++ packages/reva-coregistration/reva_coregistration-0.2.0.tar.gz/reva_coregistration-0.2.0/src/reva_coregistration/__init__.py
@@ -0,0 +1,9 @@
+"""
+Reva Coregistration Package
+
+A Python package for image coregistration and coordinate transformation.
+"""
+
+__version__ = "0.2.0"
+__author__ = "Robert Toth, PhD"
+__email__ = "robert.toth@thetatech.ai"
