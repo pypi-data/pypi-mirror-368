b3e8a1d425e8378b520a63d1b397a62cca67f8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:16 +0000

    Add package onboarding-buddy-client/2.1.0/onboarding_buddy_client-2.1.0-py3-none-any.whl

---
+++ packages/onboarding-buddy-client/onboarding_buddy_client-2.1.0-py3-none-any.whl/onboarding_buddy_client/api/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+# import apis into api package
+from onboarding_buddy_client.api.batch_api import BatchApi
+from onboarding_buddy_client.api.file_api import FileApi
+from onboarding_buddy_client.api.sanctions_api import SanctionsApi
+from onboarding_buddy_client.api.validation_api import ValidationApi
+
