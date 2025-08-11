bda84a8f4d78a3078ee46f00e040646bd4fcb9d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:12 +0000

    Add package simple-blogger/0.2.9/simple_blogger-0.2.9.tar.gz

---
+++ packages/simple-blogger/simple_blogger-0.2.9.tar.gz/simple_blogger-0.2.9/simple_blogger/generator/deepseek.py
@@ -0,0 +1,22 @@
+from simple_blogger.generator import File, TextGenerator
+from openai import OpenAI
+from io import StringIO
+import os
+
+class DeepSeekTextGenerator(TextGenerator):
+    def __init__(self, system_prompt, api_key_name ='DEEPSEEK_API_KEY', model_name='deepseek-chat'):
+        super().__init__(system_prompt=system_prompt)
+        self.base_url='https://api.deepseek.com'
+        self.api_key = os.environ.get(api_key_name)
+        self.model_name=model_name
+
+    def generate(self, prompt, **_):
+        client = OpenAI(api_key=self.api_key, base_url=self.base_url)
+        text = client.chat.completions.create(
+                    model=self.model_name,
+                    messages=[
+                        { "role": "system", "content": self.system_prompt },
+                        { "role": "user", "content": prompt },
+                    ]
+                ).choices[0].message.content
+        return File('txt', StringIO(text))
