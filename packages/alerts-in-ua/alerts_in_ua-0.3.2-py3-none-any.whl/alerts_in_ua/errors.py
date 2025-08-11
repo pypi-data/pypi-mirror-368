4c3978ccb1fff591dd4fde56e489a756da723a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:37 +0000

    Add package alerts-in-ua/0.3.2/alerts_in_ua-0.3.2-py3-none-any.whl

---
+++ packages/alerts-in-ua/alerts_in_ua-0.3.2-py3-none-any.whl/alerts_in_ua/errors.py
@@ -0,0 +1,19 @@
+class ApiError(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(f"{message}")
+
+class UnauthorizedError(ApiError):
+    pass
+
+class RateLimitError(ApiError):
+    pass
+
+class InternalServerError(ApiError):
+    pass
+
+class ForbiddenError(ApiError):
+    pass
+
+class InvalidParameterException(Exception):
+    pass
