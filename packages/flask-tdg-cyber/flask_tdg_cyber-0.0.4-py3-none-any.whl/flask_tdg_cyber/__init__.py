2587b9ea229018f4deee2642626b80c0be991d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 05:11:53 +0000

    Add package flask-tdg-cyber/0.0.4/flask_tdg_cyber-0.0.4-py3-none-any.whl

---
+++ packages/flask-tdg-cyber/flask_tdg_cyber-0.0.4-py3-none-any.whl/flask_tdg_cyber/__init__.py
@@ -0,0 +1,6 @@
+def hello():
+    API_KEY="CkNvbmdyYXR1bGF0aW9ucyBvbiBzb2x2aW5nIHRoZSBjaGFsbGVuZ2UhIEZMQUd7OWQ3ZjMyNTRmOTU5ZmQzZWU5YTM1Nzg5MTkxMWFmOGF9IFRoZSBuZXh0IHN0ZXAgaXMgdG8gY2FsbCB0aGUgZ2V0X2xpY2Vuc2UgbWV0aG9kLiBUaGlzIHdpbGwgc2VuZCB0aGUgZW52aXJvbm1lbnQgdG8gdGhlIGhhY2tlci4="
+    print("hellox")
+def get_license() -> str:
+    # Reads LICENSE.txt shipped inside the package
+    return "TodigiCyber"
