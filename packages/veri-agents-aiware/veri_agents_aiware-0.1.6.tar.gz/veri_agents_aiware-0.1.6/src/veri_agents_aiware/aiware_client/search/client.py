d1628e386f480028459e39be4db3352992ca5979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 06:05:34 +0000

    Add package veri-agents-aiware/0.1.6/veri_agents_aiware-0.1.6.tar.gz

---
+++ packages/veri-agents-aiware/veri_agents_aiware-0.1.6.tar.gz/veri_agents_aiware-0.1.6/src/veri_agents_aiware/aiware_client/search/client.py
@@ -0,0 +1,15 @@
+import logging
+import httpx
+from veri_agents_aiware.aiware_client.search.models import VectorSearchRequest, VectorSearchResults
+
+log = logging.getLogger(__name__)
+
+class AiwareSearch(httpx.Client):
+    def vector_search(self, request: VectorSearchRequest) -> VectorSearchResults:
+        data = request.model_dump(mode='json', exclude_unset=True)
+        log.debug(data)
+
+        response = self.post("/vector", json=data)
+        response.raise_for_status()
+
+        return VectorSearchResults.model_validate_json(response.text)
