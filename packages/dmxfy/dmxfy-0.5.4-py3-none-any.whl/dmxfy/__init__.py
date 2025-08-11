6c8167a2eb26bf59ef0ff1d7b5ffade93ca1c65e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:55 +0000

    Add package dmxfy/0.5.4/dmxfy-0.5.4-py3-none-any.whl

---
+++ packages/dmxfy/dmxfy-0.5.4-py3-none-any.whl/dmxfy/__init__.py
@@ -0,0 +1,11 @@
+from dmxfy.cli.cli import CLI
+
+
+def main() -> None:
+    """Main entry point for the DMXFY application"""
+    cli = CLI()
+    cli.run()
+
+
+if __name__ == "__main__":
+    main()
