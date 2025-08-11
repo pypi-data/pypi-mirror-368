e7b407b8e69b4ae4646549fa31a024c389fa4381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:15 +0000

    Add package qlib-learn/1.0.3/qlib_learn-1.0.3-py3-none-any.whl

---
+++ packages/qlib-learn/qlib_learn-1.0.3-py3-none-any.whl/search/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module provides the ObjectSearch class for implementing search algorithms.
+
+Exports:
+    ObjectSearch: Main class for object search operations.
+"""
+
+__author__ = "Eric Santos <ericshantos13@gmail.com>"
---
+++ packages/qlib-learn/qlib_learn-1.0.3-py3-none-any.whl/utils/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module provides utility classes and functions for geographic coordinate manipulation.
+
+Exports:
+    GeoCoordinate: A class for representing and handling geographic coordinates.
+"""
+
+__author__ = "Eric Santos <ericshantos13@gmail.com>"
