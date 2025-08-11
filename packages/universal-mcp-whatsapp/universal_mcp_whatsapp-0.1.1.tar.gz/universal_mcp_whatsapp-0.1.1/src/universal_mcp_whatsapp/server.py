9c823cca4d033d5bd779e32b2bd286e9c7862f70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:31 +0000

    Add package universal-mcp-whatsapp/0.1.1/universal_mcp_whatsapp-0.1.1.tar.gz

---
+++ packages/universal-mcp-whatsapp/universal_mcp_whatsapp-0.1.1.tar.gz/universal_mcp_whatsapp-0.1.1/src/universal_mcp_whatsapp/server.py
@@ -0,0 +1,22 @@
+
+
+from universal_mcp.servers import SingleMCPServer
+from universal_mcp.integrations import AgentRIntegration
+from universal_mcp.stores import EnvironmentStore
+
+from universal_mcp_whatsapp.app import WhatsappApp
+
+env_store = EnvironmentStore()
+integration_instance = AgentRIntegration(name="whatsapp", store=env_store)
+
+
+app_instance = WhatsappApp(integration=None)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
+
+
