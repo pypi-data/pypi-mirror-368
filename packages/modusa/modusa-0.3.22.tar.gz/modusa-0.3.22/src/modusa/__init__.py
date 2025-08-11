aed360de941a3fd0e21f69b75dab61d42cb1dc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 16:06:23 +0000

    Add package modusa/0.3.22/modusa-0.3.22.tar.gz

---
+++ packages/modusa/modusa-0.3.22.tar.gz/modusa-0.3.22/src/modusa/__init__.py
@@ -0,0 +1,8 @@
+from modusa.utils import excp, config
+
+#=====Giving access to plot functions to plot multiple signals.=====
+from modusa.tools import plot1d, plot2d
+from modusa.tools import play, convert
+from modusa.tools import download
+from modusa.tools import load
+#=====
---
+++ packages/modusa/modusa-0.3.22.tar.gz/modusa-0.3.22/src/modusa/tools/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from .plotter import plot1d, plot2d
+from .audio_player import play
+from .audio_converter import convert
+from .youtube_downloader import download
+from .audio_loader import load
