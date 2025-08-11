a45ce335edd82c256016d3e9168c6e09f4e47ae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:35 +0000

    Add package xp3-tool/2.0/xp3_tool-2.0-py3-none-any.whl

---
+++ packages/xp3-tool/xp3_tool-2.0-py3-none-any.whl/xp3_tool/__init__.py
@@ -0,0 +1,8 @@
+from .xp3_tool import uuid, CallAi
+
+__all__ = [
+    "uuid",
+    "CallAi"
+]
+
+__version__ = "2.0"
---
+++ packages/xp3-tool/xp3_tool-2.0-py3-none-any.whl/xp3_tool/xp3_tool.py
@@ -0,0 +1,37 @@
+from openai import OpenAI
+
+def uuid(length=16):
+    chars = string.ascii_letters + string.digits
+    return ''.join(random.choice(chars) for _ in range(length))
+
+class CallAi:
+    def __init__(self,api_key,base_url,model=None):
+        self.api_key = api_key
+        self.base_url = base_url
+        self.client = OpenAI(
+            api_key = api_key,
+            base_url= base_url,
+        )
+        self.model =  model if model else 'qwen-plus'
+        self._prompt = ""
+        self.inquiry = ""
+
+    @property
+    def prompt(self):
+        return self._prompt
+
+    @prompt.setter
+    def prompt(self,content):
+        self._prompt = content
+
+    def chat(self,text,top_p = 0.9, temperature = 0.7):
+        completion = self.client.chat.completions.create(
+            model= self.model,
+            messages=[
+                {'role': 'system', 'content': f'{self._prompt}'},
+                {'role': 'user', 'content': text}],
+            temperature = temperature,
+            top_p = top_p
+        )
+        reply = completion.choices[0].message.content
+        return reply
