f23bb4f37f51bd38bd4abc59a2f2a59672cae638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:18 +0000

    Add package lunary/1.4.26a3/lunary-1.4.26a3-py3-none-any.whl

---
+++ packages/lunary/lunary-1.4.26a3-py3-none-any.whl/lunary/graph_manager.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+  from langchain_core.runnables.graph import Graph as LangChainGraph
+else:
+  LangChainGraph = any 
+
+GraphID = str
+
+## TODO: subgraphs (xray=True)
+
+class GraphManager:
+  def __init__(self):
+    self.graphs: Dict[GraphID, LangChainGraph] = {}
+    
+  def add_graph(self, id: GraphID, graph: LangChainGraph):
+    self.graphs[id] = graph
+    
+  def get_graph(self, id: GraphID) -> LangChainGraph | None:
+    return self.graphs.get(id)
+  
+  
