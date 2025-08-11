4848d1fc4cd1a744abb1c19eaaf8fefe97cd7072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 16:05:23 +0000

    Add package gameAs/0.1/gameas-0.1.tar.gz

---
+++ packages/gameAs/gameas-0.1.tar.gz/gameas-0.1/GameMenu/__init__.py
@@ -0,0 +1,5 @@
+from .CowsABull import play_bulls_and_cows
+from .GuessNum import guess_the_number
+from .RockPaper import play_rps
+
+__add__=["play_bulls_and_cows", "guess_the_number", "play_rps"]
