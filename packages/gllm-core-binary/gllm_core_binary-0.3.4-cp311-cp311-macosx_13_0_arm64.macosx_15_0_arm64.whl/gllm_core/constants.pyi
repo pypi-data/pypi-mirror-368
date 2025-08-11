0effcf4474a300f24ca7cc7e4df3f43d1b003586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:31 +0000

    Add package gllm-core-binary/0.3.4/gllm_core_binary-0.3.4-cp311-cp311-macosx_13_0_arm64.macosx_15_0_arm64.whl

---
+++ packages/gllm-core-binary/gllm_core_binary-0.3.4-cp311-cp311-macosx_13_0_arm64.macosx_15_0_arm64.whl/gllm_core/constants.pyi
@@ -0,0 +1,22 @@
+from enum import Enum, IntEnum
+
+class EventLevel(IntEnum):
+    """Defines event levels for the event emitter module."""
+    TRACE = 0
+    DEBUG = 1
+    INFO = 2
+    WARN = 3
+    ERROR = 4
+    FATAL = 5
+
+class EventType(str, Enum):
+    """Defines event types for the event emitter module."""
+    STATUS = 'status'
+    RESPONSE = 'response'
+    DATA = 'data'
+
+class DefaultChunkMetadata:
+    """Defines constants for default chunk metadata keys."""
+    CHUNK_ID: str
+    PREV_CHUNK_ID: str
+    NEXT_CHUNK_ID: str
