06289bf72eb783f432c67a0d055a98fe180d0a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 16:06:42 +0000

    Add package exejs/0.0.5/exejs-0.0.5-py3-none-any.whl

---
+++ packages/exejs/exejs-0.0.5-py3-none-any.whl/exejs/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.5"
+__author__ = "UlionTse"
+
+from exejs.runtimes import compile, execute, evaluate
+from exejs.runtimes import reset_runtime, find_all_runtime_name_list
+from exejs.runtimes import get_current_runtime, get_current_runtime_name
