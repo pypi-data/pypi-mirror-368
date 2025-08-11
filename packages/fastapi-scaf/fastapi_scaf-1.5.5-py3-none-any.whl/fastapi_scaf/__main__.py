67452c8eb883f99fef949f248d3db9d2aae36bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:47 +0000

    Add package fastapi-scaf/1.5.5/fastapi_scaf-1.5.5-py3-none-any.whl

---
+++ packages/fastapi-scaf/fastapi_scaf-1.5.5-py3-none-any.whl/fastapi_scaf/__main__.py
@@ -0,0 +1,17 @@
+"""
+@author axiner
+@version v1.0.0
+@created 2024/07/29 22:22
+@abstract main
+@description
+@history
+"""
+from fastapi_scaff.__main__ import main as _main
+
+
+def main():
+    _main()
+
+
+if __name__ == "__main__":
+    main()
