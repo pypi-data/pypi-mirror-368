ffea46b32d2701f7129fd9cbadf7178a03a199b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:40 +0000

    Add package streamll/0.1.0/streamll-0.1.0-py3-none-any.whl

---
+++ packages/streamll/streamll-0.1.0-py3-none-any.whl/streamll/__init__.py
@@ -0,0 +1,12 @@
+"""
+streamll: Production Streaming Infrastructure for DSPy
+
+Transform black-box LLM pipelines into observable, real-time systems.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Nick"
+__email__ = "nick@glssn.co.uk"
+
+# This is a reserved package. Full implementation coming soon.
+# Visit https://github.com/glssn/streamll for updates.
