39d6dad1a1f976c701901660f85956b6783e0661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 05:12:30 +0000

    Add package busylight-for-humans/0.41.3/busylight_for_humans-0.41.3.tar.gz

---
+++ packages/busylight-for-humans/busylight_for_humans-0.41.3.tar.gz/busylight_for_humans-0.41.3/src/busylight/api/models.py
@@ -0,0 +1,28 @@
+"""API Response Models"""
+
+from typing import Dict, Optional, Tuple, Union
+
+from pydantic import BaseModel
+
+
+class LightOperation(BaseModel):
+    light_id: Union[int, str]
+    action: Optional[str] = None
+    color: Optional[str] = None
+    rgb: Optional[Tuple[int, int, int]] = None
+    speed: Optional[str] = None
+    name: Optional[str] = None
+    dim: Optional[float] = 1.0
+
+
+class LightDescription(BaseModel):
+    light_id: int
+    name: str
+    info: Dict[str, Union[int, str, Tuple[int, int]]]
+    is_on: bool
+    color: str
+    rgb: Tuple[int, int, int]
+
+
+class EndPoint(BaseModel):
+    path: str
