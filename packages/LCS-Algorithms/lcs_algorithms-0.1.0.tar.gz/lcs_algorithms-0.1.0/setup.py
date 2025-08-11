d926b30176618e6fc5cec2efcf24ebbf87cbaec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:41 +0000

    Add package LCS-Algorithms/0.1.0/lcs_algorithms-0.1.0.tar.gz

---
+++ packages/LCS-Algorithms/lcs_algorithms-0.1.0.tar.gz/lcs_algorithms-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lcs",
+    version="0.1.0",
+    author="Zeshan Khan",
+    author_email="zeshankhanalvi@gmail.com",
+    description="Finds the longest common subsequence among multiple strings.",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
