5567dcd7b158c416c66464e7bc6fd7508d52c9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 06:05:03 +0000

    Add package lt-utils/0.0.8/lt_utils-0.0.8.tar.gz

---
+++ packages/lt-utils/lt_utils-0.0.8.tar.gz/lt_utils-0.0.8/lt_utils/common.py
@@ -0,0 +1,41 @@
+__all__ = [
+    "ABC",
+    "Path",
+    "Any",
+    "Type",
+    "Dict",
+    "List",
+    "Union",
+    "Tuple",
+    "Deque",
+    "AnyStr",
+    "Number",
+    "Literal",
+    "Sequence",
+    "Optional",
+    "Callable",
+    "PathLike",
+    "TypeAlias",
+    "abstractmethod",
+]
+
+from abc import ABC, abstractmethod
+
+from typing import (
+    Any,
+    Dict,
+    List,
+    Tuple,
+    Type,
+    Deque,
+    Union,
+    Literal,
+    Sequence,
+    Optional,
+    Callable,
+    TypeAlias,
+    AnyStr,
+)
+from numbers import Number
+from pathlib import Path
+from os import PathLike
