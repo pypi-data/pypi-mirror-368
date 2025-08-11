c68c5471f06a384e0250d91309306c3d1b598455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:13 +0000

    Add package django-coolify/0.1.5/django_coolify-0.1.5.tar.gz

---
+++ packages/django-coolify/django_coolify-0.1.5.tar.gz/django_coolify-0.1.5/django_coolify/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DjangoCoolifyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_coolify"
+    verbose_name = "Django Coolify"
