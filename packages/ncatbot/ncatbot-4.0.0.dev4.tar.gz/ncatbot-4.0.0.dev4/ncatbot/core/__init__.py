d544e36f24c753eb5ba17602559d6e8f271a5c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 16:05:22 +0000

    Add package ncatbot/4.0.0.dev4/ncatbot-4.0.0.dev4.tar.gz

---
+++ packages/ncatbot/ncatbot-4.0.0.dev4.tar.gz/ncatbot-4.0.0.dev4/ncatbot/core/__init__.py
@@ -0,0 +1,44 @@
+from .client import BotClient
+from .event import GroupMessageEvent, PrivateMessageEvent, RequestEvent, NoticeEvent, MetaEvent
+from .helper import ForwardConstructor
+from .legacy import GroupMessage, PrivateMessage, MessageChain, BaseMessage
+from .event import (
+    Text,
+    At,
+    Image,
+    Face,
+    Reply,
+    File,
+    Record,
+    Video,
+    Node,
+    Forward,
+    MessageArray,
+    MessageSegment,
+)
+
+__all__ = [
+    "BaseMessage",
+    "GroupMessage",
+    "PrivateMessage",
+    "BotClient",
+    "GroupMessageEvent",
+    "PrivateMessageEvent",
+    "RequestEvent",
+    "NoticeEvent",
+    "MetaEvent",
+    "ForwardConstructor",
+    "MessageChain",
+    "Text",
+    "At",
+    "Image",
+    "Face",
+    "Reply",
+    "File",
+    "Record",
+    "Video",
+    "Node",
+    "Forward",
+    "MessageArray",
+    "MessageSegment",
+]
