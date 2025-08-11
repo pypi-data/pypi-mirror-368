ad4b70faccc30f81e980ce237b71a16ff9dd6f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:11 +0000

    Add package drf-pydantic/2.9.0/drf_pydantic-2.9.0.tar.gz

---
+++ packages/drf-pydantic/drf_pydantic-2.9.0.tar.gz/drf_pydantic-2.9.0/src/drf_pydantic/__init__.py
@@ -0,0 +1,8 @@
+__all__ = [
+    "BaseModel",
+    "DrfPydanticSerializer",
+]
+__version__ = "2.9.0"
+
+from drf_pydantic.base_model import BaseModel
+from drf_pydantic.base_serializer import DrfPydanticSerializer
