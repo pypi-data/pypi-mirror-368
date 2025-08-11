77d72c168d4f15b1feeabdb2b4a9fa61ceaf5a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:46 +0000

    Add package systempy/0.1.6/systempy-0.1.6-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/systempy/systempy-0.1.6-cp312-cp312-manylinux_2_39_x86_64.whl/systempy/libsystempy/enums.py
@@ -0,0 +1,32 @@
+from enum import Enum, unique
+
+
+@unique
+class DIRECTION(str, Enum):
+    """
+    Public enum containing supported directions. Rely on your IDE's help
+    """
+
+    FORWARD = "forward"
+    BACKWARD = "backward"
+    GATHER = "gather"
+
+
+@unique
+class TYPE(str, Enum):
+    """
+    Private internal enum
+    """
+
+    SYNC = "sync"
+    ASYNC = "async"
+
+
+@unique
+class ROLE(str, Enum):
+    APP = "app"
+    UNIT = "unit"
+    MIXIN = "mixin"
+    TARGET = "target"
+    BUILTINS = "builtins"
+    METACLASS = "metaclass"
