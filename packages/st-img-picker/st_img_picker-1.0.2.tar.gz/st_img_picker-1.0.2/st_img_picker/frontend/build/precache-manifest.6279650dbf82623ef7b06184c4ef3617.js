615ec4ce04c1bffd4ab101b363a6bc503e0a7e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:32 +0000

    Add package st-img-picker/1.0.2/st_img_picker-1.0.2.tar.gz

---
+++ packages/st-img-picker/st_img_picker-1.0.2.tar.gz/st_img_picker-1.0.2/st_img_picker/frontend/build/precache-manifest.6279650dbf82623ef7b06184c4ef3617.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1177e5abe9342bb6be93b71c4a60f306",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c7ca612e710df352115",
+    "url": "./static/js/2.e31505f2.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.e31505f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee7079db81a9ba696bf",
+    "url": "./static/js/main.52e540fe.chunk.js"
+  },
+  {
+    "revision": "08bfe9a8f9db97a558b1",
+    "url": "./static/js/runtime-main.fce4c018.js"
+  }
+]);
