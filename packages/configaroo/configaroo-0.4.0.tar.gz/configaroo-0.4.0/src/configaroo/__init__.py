b0a03656091b8ecb1dfe0b2c3625505721458192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:48 +0000

    Add package configaroo/0.4.0/configaroo-0.4.0.tar.gz

---
+++ packages/configaroo/configaroo-0.4.0.tar.gz/configaroo-0.4.0/src/configaroo/__init__.py
@@ -0,0 +1,18 @@
+"""Bouncy configuration handling."""
+
+from configaroo.configuration import Configuration, print_configuration
+from configaroo.exceptions import (
+    ConfigarooError,
+    MissingEnvironmentVariableError,
+    UnsupportedLoaderError,
+)
+
+__all__ = [
+    "ConfigarooError",
+    "Configuration",
+    "MissingEnvironmentVariableError",
+    "UnsupportedLoaderError",
+    "print_configuration",
+]
+
+__version__ = "0.4.0"
