61dee7fc0f9537a2f6c683ea2b784b83d750bfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 16:06:31 +0000

    Add package norpm/1.2/norpm-1.2.tar.gz

---
+++ packages/norpm/norpm-1.2.tar.gz/norpm-1.2/tests/test_expression.py
@@ -0,0 +1,98 @@
+"""
+Test rpmmacro parsing in spec-files.
+"""
+
+from norpm.specfile import specfile_expand
+from norpm.macro import MacroRegistry
+
+
+def test_expand_expression():
+    """ Normal expression expansion """
+    assert specfile_expand("""\
+%if 1 - 1
+1
+%endif
+%if 1+1
+2
+%endif
+%if 3*3/3-3 > -1
+3
+%endif
+%if 1 && 0 || 1
+4
+%endif
+%if 1 && 0 || 1 && 0
+5
+%endif
+%if 1 && (0 || 1) && 1
+6
+%endif
+%if 1 && !(0 || !1) && 1
+7
+%endif
+""", MacroRegistry()) == """\
+2
+3
+4
+6
+7
+"""
+
+
+def test_macro_inexpression():
+    """ Normal expression expansion """
+    assert specfile_expand("""\
+%global foo 1
+%if 1 - %foo
+1
+%endif
+%if 1 + %foo
+2
+%endif
+""", MacroRegistry()) == """\
+2
+"""
+
+
+def test_with_statement():
+    """ Normal expression expansion """
+    assert specfile_expand("""\
+%bcond_without system_ntirpc
+%if 0%{?with_system_ntirpc}
+1
+%else
+Not yet working.
+%endif
+""", MacroRegistry()) == """\
+%bcond_without system_ntirpc
+Not yet working.
+"""
+
+
+def test_else_and_comment():
+    """ Normal expression expansion """
+    assert specfile_expand("""\
+%if 0
+%else  # foo
+1
+%endif  # bar
+post
+""", MacroRegistry()) == """\
+1
+post
+"""
+
+
+def test_expression_expansion():
+    """ Normal expression expansion """
+    assert specfile_expand("%[ 1 > 2 ]\n", MacroRegistry()) == "0\n"
+    assert specfile_expand("%[ 1 > 2 + 2 ]\n", MacroRegistry()) == "0\n"
+    assert specfile_expand("%[ 2 + 2 ]\n", MacroRegistry()) == "4\n"
+    assert specfile_expand("%[ 2 + 2 * 3 ]\n", MacroRegistry()) == "8\n"
+    db = MacroRegistry()
+    db["foo"] = "11"
+    assert specfile_expand("%[ 2 + 2 * %foo ]\n", db) == "24\n"
+    assert specfile_expand('%[ 1 ? "a" : "b" ]', MacroRegistry()) == "a"
+    assert specfile_expand('%[ 0 ? "a" : "b" ]', MacroRegistry()) == "b"
+    assert specfile_expand('%[ 1 + 10 ? 2 : 3 ]', MacroRegistry()) == "2"
+    assert specfile_expand('%[!(0%{?rhel} >= 10)]', MacroRegistry()) == "1"
