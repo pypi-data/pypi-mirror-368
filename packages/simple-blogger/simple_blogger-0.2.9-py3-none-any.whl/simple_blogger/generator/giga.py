8caa3dedbf684e53321653d30eb6cf635cfc77fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 03:34:12 +0000

    Add package simple-blogger/0.2.9/simple_blogger-0.2.9-py3-none-any.whl

---
+++ packages/simple-blogger/simple_blogger-0.2.9-py3-none-any.whl/simple_blogger/generator/giga.py
@@ -0,0 +1,56 @@
+from simple_blogger.generator import File, ImageGenerator, TextGenerator
+from gigachat import GigaChat
+from io import StringIO, BytesIO
+from bs4 import BeautifulSoup
+import os, base64
+
+class GigaChatTextGenerator(TextGenerator):
+    def __init__(self, system_prompt, api_key_name ='GIGACHAT_CREDENTIALS', model_name='GigaChat-2'):
+        super().__init__(system_prompt=system_prompt)
+        self.api_key = os.environ.get(api_key_name)
+        self.model_name=model_name
+
+    def generate(self, prompt, **_):
+        giga = GigaChat(
+            credentials=self.api_key,
+            verify_ssl_certs=False,
+        )
+        text = giga.chat({
+                                "model": f"{self.model_name}",
+                                "messages": [
+                                    { "role": "system", "content": f"{self.system_prompt}" },
+                                    { "role": "user", "content": f"{prompt}" },
+                                ]
+                            }).choices[0].message.content
+        return File('txt', StringIO(text))
+    
+class GigaChatImageGenerator(ImageGenerator):
+    def __init__(self, api_key_name ='GIGACHAT_CREDENTIALS', model_name='GigaChat-2', system_prompt=None, style_prompt=None):
+        super().__init__()
+        self.api_key = os.environ.get(api_key_name)
+        self.model_name=model_name
+        self.system_prompt=system_prompt
+        self.style_prompt=style_prompt
+
+    def generate(self, prompt, **_):
+        giga = GigaChat(
+            credentials=self.api_key,
+            verify_ssl_certs=False,
+        )
+        prompt = f"{prompt}{'\n' + self.style_prompt if self.style_prompt else ''}"
+        response = giga.chat({
+                                "model": f"{self.model_name}",
+                                "messages": [
+                                    { "role": "system", "content": f"{self.system_prompt}" },
+                                    { "role": "user", "content": f"{prompt}" },
+                                ],
+                                "function_call": "auto"
+                            }).choices[0].message.content
+        
+        file_id = BeautifulSoup(response, "html.parser").find('img').get("src")
+        image = giga.get_image(file_id)
+
+        return File(self.ext(), BytesIO(base64.b64decode(image.content)))
+    
+    def ext(self):
+        return 'jpeg'
