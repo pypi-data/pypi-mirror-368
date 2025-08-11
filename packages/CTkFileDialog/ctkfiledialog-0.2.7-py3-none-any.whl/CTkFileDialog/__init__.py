316cb88c1a904ca7f69187466093a0283ddb6cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 14:06:40 +0000

    Add package CTkFileDialog/0.2.7/ctkfiledialog-0.2.7-py3-none-any.whl

---
+++ packages/CTkFileDialog/ctkfiledialog-0.2.7-py3-none-any.whl/CTkFileDialog/__init__.py
@@ -0,0 +1,22 @@
+'''
+File dialog created in custom Tkinter and inspired by the Tkinter dialog.
+
+- Version: 0.2.7 
+- Author: Flick
+- Github: https://github.com/FlickGMD/CTkFileDialog
+'''
+from ._functions import askopenfilename, askdirectory, askopenfile, askopenfiles, askopenfilenames, asksaveasfile, asksaveasfilename 
+from . import Constants
+
+version = '0.2.7'
+__all__ = [
+        'askopenfilename',
+        'askdirectory',
+        'askopenfile',
+        'askopenfiles',
+        'askopenfilenames',
+        'asksaveasfile',
+        'asksaveasfilename', 
+        'Constants'
+        ]
+
