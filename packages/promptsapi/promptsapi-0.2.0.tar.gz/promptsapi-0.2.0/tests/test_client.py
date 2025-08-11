53e20232da6336663499716be14c122a6dde9b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-11 15:05:37 +0000

    Add package promptsapi/0.2.0/promptsapi-0.2.0.tar.gz

---
+++ packages/promptsapi/promptsapi-0.2.0.tar.gz/promptsapi-0.2.0/tests/test_client.py
@@ -0,0 +1,14 @@
+import pytest
+from promptsapi import generate_prompt, reverse_prompt
+
+def test_generate_prompt_exists():
+    """
+    Test that the generate_prompt function can be imported and exists.
+    """
+    assert callable(generate_prompt)
+
+def test_reverse_prompt_exists():
+    """
+    Test that the reverse_prompt function can be imported and exists.
+    """
+    assert callable(reverse_prompt)
