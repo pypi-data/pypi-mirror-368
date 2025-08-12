033753bdccd07a52c9a014cd98077ee2bb23f303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:06:35 +0000

    Add package piegy/2.3.11/piegy-2.3.11.tar.gz

---
+++ packages/piegy/piegy-2.3.11.tar.gz/piegy-2.3.11/src/piegy/build_info.py
@@ -0,0 +1,11 @@
+"""
+Contains build info, whether it's local built, or a pre-compiled wheel.
+Auto-generated at compile time.
+"""
+
+build_info = {
+    "version": "2.3.10",
+    "build date": "2025-08-11 20:12:18",
+    "python used": "3.11.10",
+    "platform": "darwin"
+}
