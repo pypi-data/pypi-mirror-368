962eb871382269f678ae1c71d388eed5e8cb05c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:27:58 +0000

    Add package synapsefrc/2025.0.0a1/synapsefrc-2025.0.0a1-py3-none-any.whl

---
+++ packages/synapsefrc/synapsefrc-2025.0.0a1-py3-none-any.whl/synapse_ui/_next/static/chunks/app/dashboard/page-a7043ad99dc3711b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[105],{4811:(e,s,_)=>{Promise.resolve().then(_.bind(_,1254))}},e=>{var s=s=>e(e.s=s);e.O(0,[361,292,481,830,357,538,254,441,684,358],()=>s(4811)),_N_E=e.O()}]);
