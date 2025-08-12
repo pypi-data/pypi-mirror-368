fc047d0016c154df76f8892c01089f42b8ac65fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:54 +0000

    Add package batchstats/0.4.5/batchstats-0.4.5-py3-none-any.whl

---
+++ packages/batchstats/batchstats-0.4.5-py3-none-any.whl/batchstats/nanstats.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+from ._misc import NoValidSamplesError
+from .core import BatchNanStat
+
+
+class BatchNanSum(BatchNanStat):
+    """
+    Class for calculating the sum of batches of data that can contain NaN values.
+
+    """
+
+    def __init__(self, axis=0):
+        """
+        Initialize the BatchNanSum object.
+        """
+        super().__init__(axis=axis)
+        self.sum = None
+
+    def update_batch(self, batch):
+        """
+        Update the sum with a new batch of data that can contain NaN values.
+
+        Args:
+            batch (numpy.ndarray): Input batch.
+
+        Returns:
+            BatchNanSum: Updated BatchNanSum object.
+
+        """
+        batch = self._process_batch(batch)
+        if self.sum is None:
+            self.sum = np.nansum(batch, axis=self.axis)
+        else:
+            self.sum += np.nansum(batch, axis=self.axis)
+        return self
+
+    def __call__(self):
+        """
+        Calculate the sum of the batches that can contain NaN values.
+
+        Returns:
+            numpy.ndarray: Sum of the batches.
+
+        Raises:
+            NoValidSamplesError: If no valid samples are available.
+
+        """
+        if self.sum is None:
+            raise NoValidSamplesError()
+        else:
+            return np.where(self.n_samples > 0, self.sum, np.nan)
+
+
+class BatchNanMean(BatchNanStat):
+    """
+    Class for calculating the mean of batches of data that can contain NaN values.
+
+    """
+
+    def __init__(self, axis=0):
+        """
+        Initialize the BatchNanMean object.
+        """
+        super().__init__(axis=axis)
+        self.sum = BatchNanSum(axis=axis)
+
+    def update_batch(self, batch):
+        """
+        Update the mean with a new batch of data that can contain NaN values.
+
+        Args:
+            batch (numpy.ndarray): Input batch.
+
+        Returns:
+            BatchNanMean: Updated BatchNanMean object.
+
+        """
+        self.sum.update_batch(batch)
+        return self
+
+    def __call__(self):
+        """
+        Calculate the mean of the batches that can contain NaN values.
+
+        Returns:
+            numpy.ndarray: Mean of the batches.
+
+        """
+        return self.sum() / self.sum.n_samples
