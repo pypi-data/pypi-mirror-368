eb3e9d9d17ad143f3516357921901c144daff214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 05:07:21 +0000

    Add package xyz-swayam/0.2.0/xyz_swayam-0.2.0.tar.gz

---
+++ packages/xyz-swayam/xyz_swayam-0.2.0.tar.gz/xyz_swayam-0.2.0/xyz/download.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+def main():
+    # Get path to Downloads folder
+    downloads_folder = os.path.expanduser("~/Downloads")
+    
+    # Path to sample.pdf (inside package folder)
+    current_dir = os.path.dirname(__file__)
+    src_file = os.path.join(current_dir, "sample.pdf")  # FIXED here
+
+    # Destination path
+    dest_file = os.path.join(downloads_folder, "sample.pdf")
+    
+    # Copy file
+    shutil.copy(src_file, dest_file)
+    print(f"✅ sample.pdf has been copied to: {downloads_folder}")
