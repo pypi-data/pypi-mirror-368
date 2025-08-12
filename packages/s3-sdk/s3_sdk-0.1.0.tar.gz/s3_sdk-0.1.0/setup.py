b8d31c7e385fc19395862337a8e41ab67d26ac67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:27:56 +0000

    Add package s3-sdk/0.1.0/s3_sdk-0.1.0.tar.gz

---
+++ packages/s3-sdk/s3_sdk-0.1.0.tar.gz/s3_sdk-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='s3_sdk',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=['boto3>=1.26.0'],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple SDK for interacting with AWS S3',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/s3_sdk',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
