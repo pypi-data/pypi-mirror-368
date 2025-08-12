031bad37464d05fa53e161ef198b3cf7210fdea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:28:20 +0000

    Add package model-registry/0.2.22/model_registry-0.2.22.tar.gz

---
+++ packages/model-registry/model_registry-0.2.22.tar.gz/model_registry-0.2.22/src/model_registry/core.py
@@ -0,0 +1,464 @@
+"""Client for the model registry."""
+
+from __future__ import annotations
+
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from typing import TypeVar, cast
+
+from typing_extensions import overload
+
+from mr_openapi import (
+    ApiClient,
+    Configuration,
+    ModelRegistryServiceApi,
+)
+from mr_openapi import (
+    exceptions as mr_exceptions,
+)
+
+from ._utils import required_args
+from .types import (
+    Artifact,
+    ListOptions,
+    ModelArtifact,
+    ModelVersion,
+    RegisteredModel,
+)
+
+ArtifactT = TypeVar("ArtifactT", bound=Artifact)
+
+
+@dataclass
+class ModelRegistryAPIClient:
+    """Model registry API."""
+
+    config: Configuration
+
+    @classmethod
+    def secure_connection(
+        cls,
+        server_address: str,
+        port: int = 443,
+        *,
+        user_token: str,
+        custom_ca: str | None = None,
+    ) -> ModelRegistryAPIClient:
+        """Constructor.
+
+        Args:
+            server_address: Server address.
+            port: Server port. Defaults to 443.
+
+        Keyword Args:
+            user_token: The PEM-encoded user token as a string.
+            custom_ca: The path to a PEM-
+        """
+        return cls(
+            Configuration(
+                f"{server_address}:{port}",
+                access_token=user_token,
+                ssl_ca_cert=custom_ca,
+            )
+        )
+
+    @classmethod
+    def insecure_connection(
+        cls,
+        server_address: str,
+        port: int,
+        user_token: str | None = None,
+    ) -> ModelRegistryAPIClient:
+        """Constructor.
+
+        Args:
+            server_address: Server address.
+            port: Server port.
+            user_token: The PEM-encoded user token as a string.
+        """
+        return cls(
+            Configuration(host=f"{server_address}:{port}", access_token=user_token, verify_ssl=False)
+        )
+
+    @asynccontextmanager
+    async def get_client(self) -> AsyncIterator[ModelRegistryServiceApi]:
+        """Get a client for the model registry."""
+        api_client = ApiClient(self.config)
+        client = ModelRegistryServiceApi(api_client)
+
+        try:
+            yield client
+        finally:
+            await api_client.close()
+
+    async def upsert_registered_model(
+        self, registered_model: RegisteredModel
+    ) -> RegisteredModel:
+        """Upsert a registered model.
+
+        Updates or creates a registered model on the server.
+
+        Args:
+            registered_model: Registered model.
+
+        Returns:
+            New registered model.
+        """
+        async with self.get_client() as client:
+            if registered_model.id:
+                rm = await client.update_registered_model(
+                    registered_model.id, registered_model.update()
+                )
+            else:
+                rm = await client.create_registered_model(registered_model.create())
+
+        return RegisteredModel.from_basemodel(rm)
+
+    async def get_registered_model_by_id(self, id: str) -> RegisteredModel | None:
+        """Fetch a registered model by its ID.
+
+        Args:
+            id: Registered model ID.
+
+        Returns:
+            Registered model.
+        """
+        async with self.get_client() as client:
+            try:
+                rm = await client.get_registered_model(id)
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return RegisteredModel.from_basemodel(rm)
+
+    @overload
+    async def get_registered_model_by_params(self, name: str): ...
+
+    @overload
+    async def get_registered_model_by_params(self, *, external_id: str): ...
+
+    @required_args(("name",), ("external_id",))
+    async def get_registered_model_by_params(
+        self, name: str | None = None, external_id: str | None = None
+    ) -> RegisteredModel | None:
+        """Fetch a registered model by its name or external ID.
+
+        Args:
+            name: Registered model name.
+            external_id: Registered model external ID.
+
+        Returns:
+            Registered model.
+        """
+        async with self.get_client() as client:
+            try:
+                rm = await client.find_registered_model(
+                    name=name, external_id=external_id
+                )
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return RegisteredModel.from_basemodel(rm)
+
+    async def get_registered_models(
+        self, options: ListOptions | None = None
+    ) -> list[RegisteredModel]:
+        """Fetch registered models.
+
+        Args:
+            options: Options for listing registered models.
+
+        Returns:
+            Registered models.
+        """
+        async with self.get_client() as client:
+            rm_list = await client.get_registered_models(
+                **(options or ListOptions()).as_options()
+            )
+
+        if options:
+            options.next_page_token = rm_list.next_page_token
+
+        return [RegisteredModel.from_basemodel(rm) for rm in rm_list.items or []]
+
+    async def upsert_model_version(
+        self, model_version: ModelVersion, registered_model_id: str | None = None
+    ) -> ModelVersion:
+        """Upsert a model version.
+
+        Updates or creates a model version on the server.
+
+        Args:
+            model_version: Model version to upsert.
+            registered_model_id: ID of the registered model this version will be associated to. Can be None when updating an existing model version.
+
+        Returns:
+            New model version.
+        """
+        async with self.get_client() as client:
+            if model_version.id:
+                mv = await client.update_model_version(
+                    model_version.id, model_version.update()
+                )
+            elif registered_model_id:
+                mv = await client.create_model_version(
+                    model_version.create(registered_model_id=registered_model_id)
+                )
+            else:
+                msg = f"Registered model ID required for creating a new model version: {model_version}"
+                raise ValueError(msg)
+
+        return ModelVersion.from_basemodel(mv)
+
+    async def get_model_version_by_id(
+        self, model_version_id: str
+    ) -> ModelVersion | None:
+        """Fetch a model version by its ID.
+
+        Args:
+            model_version_id: Model version ID.
+
+        Returns:
+            Model version.
+        """
+        async with self.get_client() as client:
+            try:
+                mv = await client.get_model_version(model_version_id)
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return ModelVersion.from_basemodel(mv)
+
+    async def get_model_versions(
+        self, registered_model_id: str, options: ListOptions | None = None
+    ) -> list[ModelVersion]:
+        """Fetch model versions by registered model ID.
+
+        Args:
+            registered_model_id: Registered model ID.
+            options: Options for listing model versions.
+
+        Returns:
+            Model versions.
+        """
+        async with self.get_client() as client:
+            mv_list = await client.get_registered_model_versions(
+                registered_model_id, **(options or ListOptions()).as_options()
+            )
+
+        if options:
+            options.next_page_token = mv_list.next_page_token
+
+        return [ModelVersion.from_basemodel(mv) for mv in mv_list.items or []]
+
+    @overload
+    async def get_model_version_by_params(
+        self, registered_model_id: str, name: str
+    ): ...
+
+    @overload
+    async def get_model_version_by_params(self, *, external_id: str): ...
+
+    @required_args(
+        (
+            "registered_model_id",
+            "name",
+        ),
+        ("external_id",),
+    )
+    async def get_model_version_by_params(
+        self,
+        registered_model_id: str | None = None,
+        name: str | None = None,
+        external_id: str | None = None,
+    ) -> ModelVersion | None:
+        """Fetch a model version by associated parameters.
+
+        Either fetches by using external ID or by using registered model ID and version name.
+
+        Args:
+            registered_model_id: Registered model ID.
+            name: Model version.
+            external_id: Model version external ID.
+
+        Returns:
+            Model version.
+        """
+        async with self.get_client() as client:
+            try:
+                mv = await client.find_model_version(
+                    name=name,
+                    external_id=external_id,
+                    parent_resource_id=registered_model_id,
+                )
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return ModelVersion.from_basemodel(mv)
+
+    async def upsert_model_artifact(
+        self, model_artifact: ModelArtifact
+    ) -> ModelArtifact:
+        """Upsert a model artifact.
+
+        Updates or creates a model artifact on the server.
+
+        Args:
+            model_artifact: Model artifact to upsert.
+            model_version_id: ID of the model version this artifact will be associated to.
+
+        Returns:
+            New model artifact.
+        """
+        async with self.get_client() as client:
+            if not model_artifact.id:
+                ma = await client.create_model_artifact(model_artifact.create())
+            else:
+                ma = await client.update_model_artifact(
+                    model_artifact.id, model_artifact.update()
+                )
+        return ModelArtifact.from_basemodel(ma)
+
+    async def upsert_model_version_artifact(
+        self, artifact: ArtifactT, model_version_id: str
+    ) -> ArtifactT:
+        """Creates a model version artifact.
+
+        Creates a model version artifact on the server.
+
+        Args:
+            artifact: Model version artifact to upsert.
+            model_version_id: ID of the model version this artifact will be associated to.
+
+        Returns:
+            New model version artifact.
+        """
+        async with self.get_client() as client:
+            return cast(
+                ArtifactT,
+                Artifact.validate_artifact(
+                    await client.upsert_model_version_artifact(
+                        model_version_id, artifact.wrap()
+                    )
+                ),
+            )
+
+    async def get_model_artifact_by_id(self, id: str) -> ModelArtifact | None:
+        """Fetch a model artifact by its ID.
+
+        Args:
+            id: Model artifact ID.
+
+        Returns:
+            Model artifact.
+        """
+        async with self.get_client() as client:
+            try:
+                ma = await client.get_model_artifact(id)
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return ModelArtifact.from_basemodel(ma)
+
+    @overload
+    async def get_model_artifact_by_params(
+        self,
+        name: str,
+        model_version_id: str,
+    ): ...
+
+    @overload
+    async def get_model_artifact_by_params(self, *, external_id: str): ...
+
+    @required_args(
+        (
+            "name",
+            "model_version_id",
+        ),
+        ("external_id",),
+    )
+    async def get_model_artifact_by_params(
+        self,
+        name: str | None = None,
+        model_version_id: str | None = None,
+        external_id: str | None = None,
+    ) -> ModelArtifact | None:
+        """Fetch a model artifact either by external ID or by its name and the ID of its associated model version.
+
+        Args:
+            name: Model artifact name.
+            model_version_id: ID of the associated model version.
+            external_id: Model artifact external ID.
+
+        Returns:
+            Model artifact.
+        """
+        async with self.get_client() as client:
+            try:
+                ma = await client.find_model_artifact(
+                    name=name,
+                    parent_resource_id=model_version_id,
+                    external_id=external_id,
+                )
+            except mr_exceptions.NotFoundException:
+                return None
+
+        return ModelArtifact.from_basemodel(ma)
+
+    async def get_model_artifacts(
+        self,
+        model_version_id: str | None = None,
+        options: ListOptions | None = None,
+    ) -> list[ModelArtifact]:
+        """Fetches model artifacts.
+
+        Args:
+            model_version_id: ID of the associated model version.
+            options: Options for listing model artifacts.
+
+        Returns:
+            Model artifacts.
+        """
+        async with self.get_client() as client:
+            if model_version_id:
+                art_list = await client.get_model_version_artifacts(
+                    model_version_id, **(options or ListOptions()).as_options()
+                )
+                if options:
+                    options.next_page_token = art_list.next_page_token
+                models = []
+                for art in art_list.items or []:
+                    converted = Artifact.validate_artifact(art)
+                    if isinstance(converted, ModelArtifact):
+                        models.append(converted)
+                return models
+
+            ma_list = await client.get_model_artifacts(
+                **(options or ListOptions()).as_options()
+            )
+            if options:
+                options.next_page_token = ma_list.next_page_token
+            return [ModelArtifact.from_basemodel(ma) for ma in ma_list.items or []]
+
+    async def get_model_version_artifacts(
+        self,
+        model_version_id: str,
+        options: ListOptions | None = None,
+    ) -> list[Artifact]:
+        """Fetches model artifacts.
+
+        Args:
+            model_version_id: ID of the associated model version.
+            options: Options for listing model artifacts.
+
+        Returns:
+            Model artifacts.
+        """
+        async with self.get_client() as client:
+            art_list = await client.get_model_version_artifacts(
+                model_version_id, **(options or ListOptions()).as_options()
+            )
+        if options:
+            options.next_page_token = art_list.next_page_token
+        return [Artifact.validate_artifact(art) for art in art_list.items or []]
---
+++ packages/model-registry/model_registry-0.2.22.tar.gz/model_registry-0.2.22/src/model_registry/types/artifacts.py
@@ -0,0 +1,228 @@
+"""Artifact types for model registry.
+
+Artifacts represent pieces of data.
+This could be datasets, models, metrics, or any other piece of data produced or consumed by an
+execution, such as an experiment run.
+
+Those types are used to map between proto types based on artifacts and Python objects.
+
+Todo:
+    * Move part of the description to API Reference docs (#120).
+"""
+
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import Any, TypeVar
+
+from typing_extensions import override
+
+from mr_openapi import (
+    Artifact as ArtifactBaseModel,
+)
+from mr_openapi import (
+    ArtifactState,
+    DocArtifactCreate,
+    DocArtifactUpdate,
+    ModelArtifactCreate,
+    ModelArtifactUpdate,
+)
+from mr_openapi import (
+    DocArtifact as DocArtifactBaseModel,
+)
+from mr_openapi import (
+    ModelArtifact as ModelArtifactBaseModel,
+)
+
+from .base import BaseResourceModel
+
+A = TypeVar("A", bound="Artifact")
+
+
+class Artifact(BaseResourceModel, ABC):
+    """Base class for all artifacts.
+
+    Attributes:
+        name: Name of the artifact.
+        uri: URI of the artifact.
+        state: State of the artifact.
+    """
+
+    name: str | None = None
+    uri: str | None = None
+    state: ArtifactState = ArtifactState.UNKNOWN
+
+    @classmethod
+    def from_artifact(cls: type[A], source: ArtifactBaseModel) -> A:
+        """Convert a base artifact."""
+        model = source.actual_instance
+        assert model
+        return cls.from_basemodel(model)
+
+    @staticmethod
+    def validate_artifact(source: ArtifactBaseModel) -> DocArtifact | ModelArtifact:
+        """Validate an artifact."""
+        model = source.actual_instance
+        assert model
+        if isinstance(model, DocArtifactBaseModel):
+            return DocArtifact.from_basemodel(model)
+        return ModelArtifact.from_basemodel(model)
+
+    @abstractmethod
+    def as_basemodel(self) -> Any:
+        """Wrap the object in a BaseModel object."""
+
+    def wrap(self) -> ArtifactBaseModel:
+        """Wrap the object in a ArtifactBaseModel object."""
+        return ArtifactBaseModel(self.as_basemodel())
+
+
+class DocArtifact(Artifact):
+    """Represents a Document Artifact.
+
+    Attributes:
+        name: Name of the document.
+        uri: URI of the document.
+        description: Description of the object.
+        external_id: Customizable ID. Has to be unique among instances of the same type.
+    """
+
+    @override
+    def create(self, **kwargs) -> Any:
+        """Create a new DocArtifactCreate object."""
+        return DocArtifactCreate(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("id", "custom_properties")),
+            artifactType="doc-artifact",
+            **kwargs,
+        )
+
+    @override
+    def update(self, **kwargs) -> Any:
+        """Create a new DocArtifactUpdate object."""
+        return DocArtifactUpdate(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("id", "name", "custom_properties")),
+            artifactType="doc-artifact",
+            **kwargs,
+        )
+
+    @override
+    def as_basemodel(self) -> DocArtifactBaseModel:
+        return DocArtifactBaseModel(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("custom_properties")),
+            artifactType="doc-artifact",
+        )
+
+    @classmethod
+    @override
+    def from_basemodel(cls, source: DocArtifactBaseModel) -> DocArtifact:
+        assert source.name
+        assert source.state
+        return cls(
+            id=source.id,
+            name=source.name,
+            description=source.description,
+            external_id=source.external_id,
+            create_time_since_epoch=source.create_time_since_epoch,
+            last_update_time_since_epoch=source.last_update_time_since_epoch,
+            uri=source.uri,
+            state=source.state,
+            custom_properties=cls._unmap_custom_properties(source.custom_properties)
+            if source.custom_properties
+            else None,
+        )
+
+
+class ModelArtifact(Artifact):
+    """Represents a Model.
+
+    Attributes:
+        name: Name of the model.
+        uri: URI of the model.
+        description: Description of the object.
+        external_id: Customizable ID. Has to be unique among instances of the same type.
+        model_format_name: Name of the model format.
+        model_format_version: Version of the model format.
+        storage_key: Storage secret name.
+        storage_path: Storage path of the model.
+        service_account_name: Name of the service account with storage secret.
+        model_source_kind: A string identifier describing the source kind.
+        model_source_class: A subgroup within the source kind.
+        model_source_group: This identifies a source group for models from source class.
+        model_source_id: A unique identifier for a source model within kind, class, and group.
+        model_source_name: A human-readable name for the source model.
+    """
+
+    # TODO: this could be an enum of valid formats
+    model_format_name: str | None = None
+    model_format_version: str | None = None
+    storage_key: str | None = None
+    storage_path: str | None = None
+    service_account_name: str | None = None
+    model_source_kind: str | None = None
+    model_source_class: str | None = None
+    model_source_group: str | None = None
+    model_source_id: str | None = None
+    model_source_name: str | None = None
+
+    _model_version_id: str | None = None
+
+    @override
+    def create(self, **kwargs) -> ModelArtifactCreate:
+        """Create a new ModelArtifactCreate object."""
+        return ModelArtifactCreate(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("id", "custom_properties")),
+            artifactType="model-artifact",
+            **kwargs,
+        )
+
+    @override
+    def update(self, **kwargs) -> ModelArtifactUpdate:
+        """Create a new ModelArtifactUpdate object."""
+        return ModelArtifactUpdate(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("id", "name", "custom_properties")),
+            artifactType="model-artifact",
+            **kwargs,
+        )
+
+    @override
+    def as_basemodel(self) -> ModelArtifactBaseModel:
+        return ModelArtifactBaseModel(
+            customProperties=self._map_custom_properties(),
+            **self._props_as_dict(exclude=("custom_properties")),
+            artifactType="model-artifact",
+        )
+
+    @classmethod
+    @override
+    def from_basemodel(cls, source: ModelArtifactBaseModel) -> ModelArtifact:
+        """Create a new ModelArtifact object from a BaseModel object."""
+        assert source.name
+        assert source.state
+        return cls(
+            id=source.id,
+            name=source.name,
+            description=source.description,
+            external_id=source.external_id,
+            create_time_since_epoch=source.create_time_since_epoch,
+            last_update_time_since_epoch=source.last_update_time_since_epoch,
+            uri=source.uri,
+            model_format_name=source.model_format_name,
+            model_format_version=source.model_format_version,
+            storage_key=source.storage_key,
+            storage_path=source.storage_path,
+            service_account_name=source.service_account_name,
+            model_source_kind=source.model_source_kind,
+            model_source_class=source.model_source_class,
+            model_source_group=source.model_source_group,
+            model_source_id=source.model_source_id,
+            model_source_name=source.model_source_name,
+            state=source.state,
+            custom_properties=cls._unmap_custom_properties(source.custom_properties)
+            if source.custom_properties
+            else None,
+        )
