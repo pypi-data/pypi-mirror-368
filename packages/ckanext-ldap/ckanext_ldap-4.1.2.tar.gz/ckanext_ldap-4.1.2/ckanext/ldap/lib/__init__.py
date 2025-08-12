07fa35bd6792f01bc48a56e48419158a3977b203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:28:30 +0000

    Add package ckanext-ldap/4.1.2/ckanext_ldap-4.1.2.tar.gz

---
+++ packages/ckanext-ldap/ckanext_ldap-4.1.2.tar.gz/ckanext_ldap-4.1.2/ckanext/ldap/lib/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-ldap
+# Created by the Natural History Museum in London, UK
---
+++ packages/ckanext-ldap/ckanext_ldap-4.1.2.tar.gz/ckanext_ldap-4.1.2/ckanext/ldap/lib/helpers.py
@@ -0,0 +1,42 @@
+# !/usr/bin/env python
+# encoding: utf-8
+#
+# This file is part of ckanext-ldap
+# Created by the Natural History Museum in London, UK
+from urllib.parse import parse_qs, urlparse
+
+from ckan.common import session
+from ckan.plugins import toolkit
+
+
+def is_ldap_user():
+    """
+    Helper function for determining if current user is LDAP user.
+
+    :returns: boolean
+    """
+    return 'ckanext-ldap-user' in session
+
+
+def get_login_action():
+    """
+    Returns ldap login handler.
+
+    Preserves parameter `came_from` as stored in context object's
+    login_handler.
+    """
+    if hasattr(toolkit.c, 'login_handler'):
+        came_from = parse_qs(urlparse(toolkit.c.login_handler).query).get('came_from')
+    else:
+        came_from = None
+    if came_from:
+        action = toolkit.url_for('ldap.login_handler', came_from=str(came_from[0]))
+    else:
+        action = toolkit.url_for('ldap.login_handler')
+    return action
+
+
+def decode_str(s, encoding='utf-8'):
+    if isinstance(s, bytes):
+        return s.decode(encoding)
+    return s
