3217a79c195fcf9c639df05500ef9d340e8022f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:55 +0000

    Add package asteroid-odyssey/1.0.3/asteroid_odyssey-1.0.3.tar.gz

---
+++ packages/asteroid-odyssey/asteroid_odyssey-1.0.3.tar.gz/asteroid_odyssey-1.0.3/src/asteroid_odyssey/openapi_client/api/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+# import apis into api package
+from asteroid_odyssey.openapi_client.api.api_api import APIApi
+from asteroid_odyssey.openapi_client.api.execution_api import ExecutionApi
+from asteroid_odyssey.openapi_client.api.sdk_api import SDKApi
+
