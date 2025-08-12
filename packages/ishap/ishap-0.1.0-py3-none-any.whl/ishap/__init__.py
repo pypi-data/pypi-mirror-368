177c3deac10a2c4dda9ca32f4389ad74be3c54b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:46 +0000

    Add package ishap/0.1.0/ishap-0.1.0-py3-none-any.whl

---
+++ packages/ishap/ishap-0.1.0-py3-none-any.whl/ishap/__init__.py
@@ -0,0 +1,5 @@
+# Import the main explainer class to make it accessible as `ishap.IShapExplainer`
+from .explainer import IShapExplainer
+
+# You can also import plotting functions if you want them at the top level
+# from .plots import plot_ishap
