89febb9b0af84fc37450740f298f6d20b2f9f069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:45 +0000

    Add package parivaha/0.1.16/parivaha-0.1.16-py3-none-any.whl

---
+++ packages/parivaha/parivaha-0.1.16-py3-none-any.whl/parivaha/__init__.py
@@ -0,0 +1,10 @@
+# ───────────────────────── src/parivaha/__init__.py ─────────────────────────
+"""Parivaha package root."""
+__all__ = [
+     "config",
+     "vault",
+     "obsidian_io",
+     "sync",
+     "utils",
+     "progress"
+     ]
