deaba84d8d89f9be35718922e2d22c575c63f2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:06:35 +0000

    Add package firetrack-py-lib/0.1.0/firetrack_py_lib-0.1.0.tar.gz

---
+++ packages/firetrack-py-lib/firetrack_py_lib-0.1.0.tar.gz/firetrack_py_lib-0.1.0/firetrack_py_lib/__init__.py
@@ -0,0 +1,3 @@
+from .youtube_lib import say_hello
+
+__all__ = ["say_hello"]
---
+++ packages/firetrack-py-lib/firetrack_py_lib-0.1.0.tar.gz/firetrack_py_lib-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="firetrack_py_lib",  # Replace with unique name
+    version="0.1.0",
+    description="A simple greeting package",
+    author="Your Name",
+    author_email="you@example.com",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
