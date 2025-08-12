0004a3b262b5a41dd7dabe7c462f2aacd04ef9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 07:05:44 +0000

    Add package pysui/0.88.0/pysui-0.88.0-py3-none-any.whl

---
+++ packages/pysui/pysui-0.88.0-py3-none-any.whl/bin/genprot.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#    Copyright Frank V. Castellucci
+#    SPDX-License-Identifier: Apache-2.0
+
+base_dir=${PWD##*/}
+if test "$base_dir" = "pysui";
+then
+    protoc -I./suiprot/ -I./suiprot/sui/rpc/v2beta2/ --python_betterproto2_opt=typing.310 --python_betterproto2_opt=client_generation=async --python_betterproto2_out=pysui/sui/sui_grpc/suimsgs ./suiprot/sui/rpc/v2beta2/*.proto    
+else
+    echo "Command must run from pysui folder."
+fi
