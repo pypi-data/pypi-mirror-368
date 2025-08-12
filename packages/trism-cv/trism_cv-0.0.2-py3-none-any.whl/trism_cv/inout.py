7c52fec75ad9e2dd8c024cd24128941d4b031929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 14:07:22 +0000

    Add package trism-cv/0.0.2/trism_cv-0.0.2-py3-none-any.whl

---
+++ packages/trism-cv/trism_cv-0.0.2-py3-none-any.whl/trism_cv/inout.py
@@ -0,0 +1,40 @@
+import numpy as np
+from typing import Union
+
+import tritonclient.grpc as grpcclient
+import tritonclient.http as httpclient
+
+from trism_cv import types
+
+
+
+class Inout:
+
+  @property
+  def name(self) -> str:
+    return self.__name
+  
+  @property
+  def shape(self) -> tuple:
+    return self.__shape
+  
+  @property
+  def dtype(self) -> np.dtype:
+    return self.__dtype
+
+  def __init__(self, name: str, shape: tuple, dtype: str, *args, **kwds):
+    self.__name = name
+    self.__shape = shape
+    self.__dtype = types.trt2np(dtype)
+
+  def make_input(self, client, data: np.ndarray) -> Union[grpcclient.InferInput, httpclient.InferInput]:
+    infer = client.InferInput(
+      name=self.name,
+      shape=data.shape,
+      datatype=types.np2trt(self.dtype)
+    )
+    infer.set_data_from_numpy(data.astype(self.dtype))
+    return infer
+  
+  def make_output(self, client) -> Union[grpcclient.InferRequestedOutput, httpclient.InferRequestedOutput]:
+    return client.InferRequestedOutput(name=self.name)
