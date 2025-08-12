f6037cf6deeed71109ede28399d16d02297931fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:27:32 +0000

    Add package ha-mqtt-publisher/0.2.3/ha_mqtt_publisher-0.2.3-py3-none-any.whl

---
+++ packages/ha-mqtt-publisher/ha_mqtt_publisher-0.2.3-py3-none-any.whl/ha_mqtt_publisher/__init__.py
@@ -0,0 +1,28 @@
+"""An MQTT publisher package with Home Assistant Discovery support"""
+
+__version__ = "0.2.3"
+__author__ = "ronschaeffer"
+
+# Import main components for easy access
+from .config import Config, MQTTConfig
+
+# Import HA discovery components
+from .ha_discovery import (
+    Device,
+    DiscoveryManager,
+    Entity,
+    StatusSensor,
+    publish_discovery_configs,
+)
+from .publisher import MQTTPublisher
+
+__all__ = [
+    "Config",
+    "Device",
+    "DiscoveryManager",
+    "Entity",
+    "MQTTConfig",
+    "MQTTPublisher",
+    "StatusSensor",
+    "publish_discovery_configs",
+]
