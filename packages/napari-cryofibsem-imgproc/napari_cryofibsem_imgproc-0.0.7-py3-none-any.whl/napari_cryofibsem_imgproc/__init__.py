9c48c5e17deb4a0c9c17b7c91d94a00b73fa4586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 05:06:59 +0000

    Add package napari-cryofibsem-imgproc/0.0.7/napari_cryofibsem_imgproc-0.0.7-py3-none-any.whl

---
+++ packages/napari-cryofibsem-imgproc/napari_cryofibsem_imgproc-0.0.7-py3-none-any.whl/napari_cryofibsem_imgproc/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.0.7"
+from ._sample_data import make_sample_data
+from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from .brightness import brightness
+from .contrast import contrast
+from .decharge import decharge
+from .decurtain import decurtain
+from .denoise import denoise
+from .medianblur import medianblur
+
+__all__ = (
+    "make_sample_data",
+    "ExampleQWidget",
+    "ImageThreshold",
+    "threshold_autogenerate_widget",
+    "threshold_magic_widget",
+    "brightness"
+    "contrast",
+    "decharge",
+    "decurtain",
+    "denoise",
+    "medianblur"
+)
