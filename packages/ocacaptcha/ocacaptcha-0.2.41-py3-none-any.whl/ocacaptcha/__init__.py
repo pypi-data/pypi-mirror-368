b67cf07370bc1516c47860d92381ff020ffa1fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:28:04 +0000

    Add package ocacaptcha/0.2.41/ocacaptcha-0.2.41-py3-none-any.whl

---
+++ packages/ocacaptcha/ocacaptcha-0.2.41-py3-none-any.whl/ocacaptcha/__init__.py
@@ -0,0 +1,5 @@
+from .captcha_solver import oca_solve_captcha
+from .captcha_selenium import solve_captcha_selenium
+from .captcha_playwright import solve_captcha_playwright
+
+__all__ = ['oca_solve_captcha']
