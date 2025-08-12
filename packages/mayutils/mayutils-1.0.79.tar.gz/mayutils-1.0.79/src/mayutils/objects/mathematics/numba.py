db28ba7714ea4e58a2b98fbae85bcb4da7848c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 16:05:45 +0000

    Add package mayutils/1.0.79/mayutils-1.0.79.tar.gz

---
+++ packages/mayutils/mayutils-1.0.79.tar.gz/mayutils-1.0.79/src/mayutils/objects/mathematics/numba.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from numba import njit
+import numpy as np
+from numpy.typing import NDArray
+
+
+@njit
+def choice_replacement(
+    arr: NDArray,
+    p: Optional[NDArray] = None,
+    size: Optional[tuple[int, ...]] = None,
+    seed: Optional[int] = None,
+) -> NDArray:
+    if seed is not None:
+        np.random.seed(seed=seed)
+
+    if p is None:
+        return np.random.choice(a=arr, size=size)
+
+    return arr[
+        np.searchsorted(
+            np.cumsum(p),
+            np.random.uniform(size=size),
+            side="right",
+        )
+    ]
