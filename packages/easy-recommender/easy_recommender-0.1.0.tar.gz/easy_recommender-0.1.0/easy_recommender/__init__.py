9dd84b31850fdc8b9951ce7ccd5f4c9f11fd2c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:19 +0000

    Add package easy-recommender/0.1.0/easy_recommender-0.1.0.tar.gz

---
+++ packages/easy-recommender/easy_recommender-0.1.0.tar.gz/easy_recommender-0.1.0/easy_recommender/__init__.py
@@ -0,0 +1,9 @@
+"""
+Easy Recommender - A simple recommendation system using implicit library
+"""
+
+__version__ = "0.1.0"
+
+from .recommender import recommend, process_df, build_feature_data
+
+__all__ = ["recommend", "process_df", "build_feature_data"]
