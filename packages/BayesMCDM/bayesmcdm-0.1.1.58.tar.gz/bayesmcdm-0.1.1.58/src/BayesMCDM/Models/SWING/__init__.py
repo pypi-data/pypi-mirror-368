92b40d76da7a7dea69de7d90c0f699b084fb41e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 16:06:22 +0000

    Add package BayesMCDM/0.1.1.58/bayesmcdm-0.1.1.58.tar.gz

---
+++ packages/BayesMCDM/bayesmcdm-0.1.1.58.tar.gz/bayesmcdm-0.1.1.58/src/BayesMCDM/Models/SWING/__init__.py
@@ -0,0 +1,6 @@
+from .StandardSWING import StandardSWING
+from .GaussianSWING import GaussianSWING
+from .IntervalSWING import IntervalSWING
+from .TriangularSWING import TriangularSWING
+
+__all__ = ["StandardSWING", "GaussianSWING", "IntervalSWING", "TriangularSWING"]
