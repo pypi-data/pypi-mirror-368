5c6e27032eb901b34a89b61fce658d054691e158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:38 +0000

    Add package ini-cfg-parser/0.1.8/ini_cfg_parser-0.1.8.tar.gz

---
+++ packages/ini-cfg-parser/ini_cfg_parser-0.1.8.tar.gz/ini_cfg_parser-0.1.8/ini_cfg_parser/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1.8"
+
+from .ini_cfg_parser import IniParser, DieMode, IniDict, IniItem, IniType, IniValue, IniParserError
+
+__all__ = [
+    'IniParser',
+    'DieMode',
+    'IniDict',
+    'IniItem',
+    'IniType',
+    'IniValue',
+    'IniParserError',
+]
+
