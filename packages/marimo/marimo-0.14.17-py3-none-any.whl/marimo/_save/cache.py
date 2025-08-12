96d23c18b0d5bdc26b2f039201aa05a7588544ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:18 +0000

    Add package marimo/0.14.17/marimo-0.14.17-py3-none-any.whl

---
+++ packages/marimo/marimo-0.14.17-py3-none-any.whl/marimo/_save/cache.py
@@ -0,0 +1,237 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import importlib
+import inspect
+import re
+import textwrap
+from collections import namedtuple
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Literal, Optional, get_args
+
+from marimo._plugins.ui._core.ui_element import S, T, UIElement
+from marimo._runtime.context import ContextNotInitializedError, get_context
+from marimo._runtime.state import SetFunctor
+
+if TYPE_CHECKING:
+    from marimo._ast.visitor import Name
+    from marimo._save.hash import HashKey
+
+# NB. Increment on cache breaking changes.
+MARIMO_CACHE_VERSION: int = 3
+
+CacheType = Literal[
+    "ContextExecutionPath",
+    "ContentAddressed",
+    "ExecutionPath",
+    "Pure",
+    "Deferred",
+    "Unknown",
+]
+# Easy visual identification of cache type.
+CACHE_PREFIX: dict[CacheType, str] = {
+    "ContextExecutionPath": "X_",
+    "ContentAddressed": "C_",
+    "ExecutionPath": "E_",
+    "Pure": "P_",
+    "Deferred": "D_",
+    "Unknown": "U_",
+}
+
+ValidCacheSha = namedtuple("ValidCacheSha", ("sha", "cache_type"))
+MetaKey = Literal["return", "version"]
+
+
+class ModuleStub:
+    def __init__(self, module: Any) -> None:
+        self.name = module.__name__
+
+    def load(self) -> Any:
+        return importlib.import_module(self.name)
+
+
+class FunctionStub:
+    def __init__(self, function: Any) -> None:
+        self.code = textwrap.dedent(inspect.getsource(function))
+
+    def load(self, glbls: dict[str, Any]) -> Any:
+        # TODO: Fix line cache and associate with the correct module.
+        code_obj = compile(self.code, "<string>", "exec")
+        lcls: dict[str, Any] = {}
+        exec(code_obj, glbls, lcls)
+        # Update the global scope with the function.
+        for value in lcls.values():
+            return value
+
+
+class UIElementStub:
+    def __init__(self, element: UIElement[S, T]) -> None:
+        self.args = element._args
+        self.cls = element.__class__
+        # Ideally only hashable attributes are stored on the subclass level.
+        defaults = set(self.cls.__new__(self.cls).__dict__.keys())
+        defaults |= {"_ctx"}
+        self.data = {
+            k: v
+            for k, v in element.__dict__.items()
+            if hasattr(v, "__hash__") and k not in defaults
+        }
+
+    def load(self) -> UIElement[S, T]:
+        return self.cls.from_args(self.data, self.args)  # type: ignore
+
+
+# BaseException because "raise _ as e" is utilized.
+class CacheException(BaseException):
+    pass
+
+
+@dataclass
+class Cache:
+    defs: dict[Name, Any]
+    hash: str
+    cache_type: CacheType
+    stateful_refs: set[str]
+    hit: bool
+    # meta corresponds to internally used data, kept as a dictionary to allow
+    # for backwards pickle compatibility with future entries.
+    # TODO: Utilize to store code and output in cache.
+    # TODO: Consider storing graph information such that execution history can
+    # be explored and visualized.
+    meta: dict[MetaKey, Any]
+
+    def restore(self, scope: dict[str, Any]) -> None:
+        """Restores values from cache, into scope."""
+        for var, lookup in self.contextual_defs():
+            value = self.defs.get(var, None)
+            # If it's a module we must replace with a stub.
+            if isinstance(value, ModuleStub):
+                scope[lookup] = value.load()
+            elif isinstance(value, FunctionStub):
+                scope[lookup] = value.load(scope)
+            elif isinstance(value, UIElementStub):
+                # UIElementStub is a placeholder for UIElement, which cannot be
+                # restored directly.
+                scope[lookup] = value.load()
+            else:
+                scope[lookup] = self.defs[var]
+
+        defs = {**globals(), **scope}
+        for ref in self.stateful_refs:
+            if ref not in defs:
+                raise CacheException(
+                    "Failure while restoring cached values. "
+                    "Cache expected a reference to a "
+                    f"variable that is not present ({ref})."
+                )
+            value = defs[ref]
+            if isinstance(value, SetFunctor):
+                value(self.defs[ref])
+            # UI Values cannot be easily programmatically set, so only update
+            # state values.
+            elif not isinstance(value, UIElement):
+                raise CacheException(
+                    "Failure while restoring cached values. "
+                    "Unexpected stateful reference type "
+                    f"({type(ref)}:{ref})."
+                )
+
+    def update(
+        self,
+        scope: dict[str, Any],
+        meta: Optional[dict[MetaKey, Any]] = None,
+    ) -> None:
+        """Loads values from scope, updating the cache."""
+        for var, lookup in self.contextual_defs():
+            if lookup not in scope:
+                raise CacheException(
+                    "Failure while saving cached values. "
+                    "Cache expected a reference to a "
+                    f"variable that is not present ({lookup})."
+                )
+            self.defs[var] = scope[lookup]
+
+        self.meta = {}
+        if meta is not None:
+            for metakey, metavalue in meta.items():
+                if metakey not in get_args(MetaKey):
+                    raise CacheException(f"Unexpected meta key: {metakey}")
+                self.meta[metakey] = metavalue
+        self.meta["version"] = MARIMO_CACHE_VERSION
+
+        defs = {**globals(), **scope}
+        for ref in self.stateful_refs:
+            if ref not in defs:
+                raise CacheException(
+                    "Failure while saving cached values. "
+                    "Cache expected a reference to a "
+                    f"variable that is not present ({ref})."
+                )
+            value = defs[ref]
+            if isinstance(value, SetFunctor):
+                self.defs[ref] = value._state()
+            elif isinstance(value, UIElement):
+                self.defs[ref] = value.value
+            else:
+                raise CacheException(
+                    "Failure while saving cached values. "
+                    "Unexpected stateful reference type "
+                    f"({type(value)}:{ref})."
+                )
+
+        for key, value in self.defs.items():
+            # If it's a module we must replace with a stub.
+            if inspect.ismodule(value):
+                self.defs[key] = ModuleStub(value)
+            elif inspect.isfunction(value):
+                self.defs[key] = FunctionStub(value)
+            elif isinstance(value, UIElement):
+                # UIElement cannot be restored directly, so we store a stub.
+                self.defs[key] = UIElementStub(value)
+
+    def contextual_defs(self) -> dict[tuple[Name, Name], Any]:
+        """Uses context to resolve private variable names."""
+        try:
+            context = get_context().execution_context
+            assert context is not None, "Context could not be resolved"
+            private_prefix = f"_cell_{context.cell_id}_"
+        except (ContextNotInitializedError, AssertionError):
+            private_prefix = r"^_cell_\w+_"
+
+        return {
+            (var, re.sub(r"^_", private_prefix, var)): value
+            for var, value in self.defs.items()
+            if var not in self.stateful_refs
+        }
+
+    @property
+    def key(self) -> HashKey:
+        from marimo._save.hash import HashKey
+
+        return HashKey(hash=self.hash, cache_type=self.cache_type)
+
+    @classmethod
+    def empty(
+        cls, *, key: HashKey, defs: set[str], stateful_refs: set[str]
+    ) -> Cache:
+        return Cache(
+            defs={d: None for d in defs},
+            hash=key.hash,
+            cache_type=key.cache_type,
+            stateful_refs=stateful_refs,
+            hit=False,
+            meta={},
+        )
+
+    @classmethod
+    def new(
+        cls, *, loaded: Cache, key: HashKey, stateful_refs: set[str]
+    ) -> Cache:
+        return Cache(
+            defs=loaded.defs,
+            hash=key.hash,
+            cache_type=key.cache_type,
+            stateful_refs=stateful_refs,
+            hit=True,
+            meta=loaded.meta,
+        )
---
+++ packages/marimo/marimo-0.14.17-py3-none-any.whl/marimo/_utils/formatter.py
@@ -0,0 +1,108 @@
+# Copyright 2024 Marimo. All rights reserved.
+from __future__ import annotations
+
+import subprocess
+import sys
+
+from marimo import _loggers
+from marimo._dependencies.dependencies import DependencyManager
+from marimo._types.ids import CellId_t
+
+LOGGER = _loggers.marimo_logger()
+
+
+CellCodes = dict[CellId_t, str]
+
+
+def ruff(codes: CellCodes, *cmd: str) -> CellCodes:
+    # Try with sys.executable first
+    ruff_cmd = [sys.executable, "-m", "ruff"]
+    process = subprocess.run([*ruff_cmd, "--help"], capture_output=True)
+
+    # If that fails, try global ruff
+    if process.returncode != 0:
+        ruff_cmd = ["ruff"]
+        process = subprocess.run([*ruff_cmd, "--help"], capture_output=True)
+        if process.returncode != 0:
+            raise ModuleNotFoundError(
+                "To enable code formatting, please install ruff", name="ruff"
+            )
+
+    formatted_codes: CellCodes = {}
+    for key, code in codes.items():
+        try:
+            process = subprocess.run(
+                [
+                    *ruff_cmd,
+                    *cmd,
+                    "-",
+                ],
+                input=code.encode(),
+                capture_output=True,
+                check=True,
+            )
+            if process.returncode != 0:
+                raise FormatError("Failed to format code with ruff")
+
+            formatted = process.stdout.decode()
+            formatted_codes[key] = formatted.strip()
+        except Exception as e:
+            LOGGER.error("Failed to format code with ruff")
+            LOGGER.debug(e)
+            continue
+
+    return formatted_codes
+
+
+class Formatter:
+    def __init__(self, line_length: int) -> None:
+        self.line_length = line_length
+
+    def format(self, codes: CellCodes) -> CellCodes:
+        return codes
+
+
+class DefaultFormatter(Formatter):
+    """
+    Tries ruff, then black, then no formatting.
+    """
+
+    def format(self, codes: CellCodes) -> CellCodes:
+        # Ruff may be installed in venv or globally
+        if DependencyManager.ruff.has() or DependencyManager.which("ruff"):
+            return RuffFormatter(self.line_length).format(codes)
+        # Black must be installed in venv
+        elif DependencyManager.black.has():
+            return BlackFormatter(self.line_length).format(codes)
+        else:
+            raise ModuleNotFoundError(
+                "To enable code formatting, please install ruff or black",
+                name="ruff",
+            )
+
+
+class RuffFormatter(Formatter):
+    def format(self, codes: CellCodes) -> CellCodes:
+        return ruff(codes, "format", "--line-length", str(self.line_length))
+
+
+class BlackFormatter(Formatter):
+    def format(self, codes: CellCodes) -> CellCodes:
+        DependencyManager.black.require("to enable code formatting")
+
+        import black
+
+        formatted_codes: CellCodes = {}
+        for key, code in codes.items():
+            try:
+                mode = black.Mode(line_length=self.line_length)  # type: ignore
+                formatted = black.format_str(code, mode=mode)
+                formatted_codes[key] = formatted.strip()
+            except Exception:
+                formatted_codes[key] = code
+
+        return formatted_codes
+
+
+class FormatError(Exception):
+    pass
