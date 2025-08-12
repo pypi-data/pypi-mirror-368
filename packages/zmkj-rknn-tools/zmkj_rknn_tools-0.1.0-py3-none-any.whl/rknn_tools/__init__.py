4b522823328e0a2e3ca9a46c818d9efdf9967889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:27:58 +0000

    Add package zmkj-rknn-tools/0.1.0/zmkj_rknn_tools-0.1.0-py3-none-any.whl

---
+++ packages/zmkj-rknn-tools/zmkj_rknn_tools-0.1.0-py3-none-any.whl/rknn_tools/__init__.py
@@ -0,0 +1,8 @@
+# RKNN-Tools: 瑞芯微端侧的辅助快速验证工具
+# 版本: 0.1.0
+
+__version__ = '0.1.0'
+
+# 导入主要模块，方便用户直接使用
+from rknn_tools.config import update_config, get_config
+from rknn_tools.detector import YOLOv8Detector
