e5a257bb01f543c79464d6256e83ae9a1f29f5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 05:06:53 +0000

    Add package wexample-app/0.0.46/wexample_app-0.0.46.tar.gz

---
+++ packages/wexample-app/wexample_app-0.0.46.tar.gz/wexample_app-0.0.46/wexample_app/common/command.py
@@ -0,0 +1,34 @@
+from typing import Callable, Any, TYPE_CHECKING
+
+from pydantic import BaseModel
+
+from wexample_app.common.abstract_kernel_child import AbstractKernelChild
+from wexample_app.helpers.response import response_normalize
+
+if TYPE_CHECKING:
+    from wexample_app.response.abstract_response import AbstractResponse
+    from wexample_app.common.command_request import CommandRequest
+    from wexample_app.common.abstract_kernel import AbstractKernel
+
+
+class Command(AbstractKernelChild, BaseModel):
+    function: Callable[..., Any] = None
+
+    def __init__(self, kernel: "AbstractKernel", **kwargs):
+        BaseModel.__init__(self, **kwargs)
+        AbstractKernelChild.__init__(self, kernel=kernel)
+
+    def execute_request(self, request: "CommandRequest") -> Any:
+        # Basic way to execute command.
+        return self.function(
+            kernel=self.kernel,
+            arguments=request.arguments
+        )
+
+    def execute_request_and_normalize(self, request: "CommandRequest") -> "AbstractResponse":
+        return response_normalize(
+            kernel=self.kernel,
+            response=self.execute_request(
+                request=request
+            )
+        )
