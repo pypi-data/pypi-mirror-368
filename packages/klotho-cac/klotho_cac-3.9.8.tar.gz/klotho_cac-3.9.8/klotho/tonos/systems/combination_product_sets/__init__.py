175e4241103ebd5300bf8566e47accb6301299b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:34 +0000

    Add package klotho-cac/3.9.8/klotho_cac-3.9.8.tar.gz

---
+++ packages/klotho-cac/klotho_cac-3.9.8.tar.gz/klotho_cac-3.9.8/klotho/tonos/systems/combination_product_sets/__init__.py
@@ -0,0 +1,11 @@
+from .cps import CombinationProductSet
+from .nkany import Hexany, Dekany, Pentadekany, Eikosany, Hebdomekontany
+
+__all__ = [
+    'CombinationProductSet',
+    'Hexany',
+    'Dekany',
+    'Pentadekany',
+    'Eikosany',
+    'Hebdomekontany',
+]
