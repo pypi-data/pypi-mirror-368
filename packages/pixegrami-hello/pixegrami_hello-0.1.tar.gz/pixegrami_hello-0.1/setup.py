226591ea0d0ca7f748c0e11159e7fcdd76a71327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:35 +0000

    Add package pixegrami-hello/0.1/pixegrami_hello-0.1.tar.gz

---
+++ packages/pixegrami-hello/pixegrami_hello-0.1.tar.gz/pixegrami_hello-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pixegrami_hello',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        #Add dependecies here.
+        #e.g. 'numpy>=1.11.1'
+    ],
+)
