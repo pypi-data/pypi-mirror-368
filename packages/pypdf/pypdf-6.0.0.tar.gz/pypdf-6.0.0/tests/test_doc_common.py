dfedce2b2ff8d24574170bf13c81747e8e14310e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:15 +0000

    Add package pypdf/6.0.0/pypdf-6.0.0.tar.gz

---
+++ packages/pypdf/pypdf-6.0.0.tar.gz/pypdf-6.0.0/tests/test_doc_common.py
@@ -0,0 +1,179 @@
+"""Test the pypdf._doc_common module."""
+import re
+import shutil
+import subprocess
+from io import BytesIO
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+from pypdf import PdfReader, PdfWriter
+from pypdf.generic import EmbeddedFile, NullObject, ViewerPreferences
+from tests import get_data_from_url
+
+TESTS_ROOT = Path(__file__).parent.resolve()
+PROJECT_ROOT = TESTS_ROOT.parent
+SAMPLE_ROOT = PROJECT_ROOT / "sample-files"
+RESOURCES_ROOT = PROJECT_ROOT / "resources"
+
+PDFATTACH_BINARY = shutil.which("pdfattach")
+
+
+@pytest.mark.skipif(PDFATTACH_BINARY is None, reason="Requires poppler-utils")
+def test_attachments(tmpdir):
+    # No attachments.
+    clean_path = SAMPLE_ROOT / "002-trivial-libre-office-writer" / "002-trivial-libre-office-writer.pdf"
+    with PdfReader(clean_path) as pdf:
+        assert pdf._list_attachments() == []
+        assert list(pdf.attachment_list) == []
+
+    # UF = name.
+    attached_path = tmpdir / "attached.pdf"
+    file_path = tmpdir / "test.txt"
+    file_path.write_binary(b"Hello World\n")
+    subprocess.run([PDFATTACH_BINARY, clean_path, file_path, attached_path])  # noqa: S603
+    with PdfReader(str(attached_path)) as pdf:
+        assert pdf._list_attachments() == ["test.txt"]
+        assert pdf._get_attachments("test.txt") == {"test.txt": b"Hello World\n"}
+        assert [(x.name, x.content) for x in pdf.attachment_list] == [("test.txt", b"Hello World\n")]
+        assert next(pdf.attachment_list).alternative_name == "test.txt"
+
+    # UF != name.
+    different_path = tmpdir / "different.pdf"
+    different_path.write_binary(re.sub(rb" /UF [^/]+ /", b" /UF(my-file.txt) /", attached_path.read_binary()))
+    with PdfReader(str(different_path)) as pdf:
+        assert pdf._list_attachments() == ["test.txt", "my-file.txt"]
+        assert pdf._get_attachments("test.txt") == {"test.txt": b"Hello World\n"}
+        assert pdf._get_attachments("my-file.txt") == {"my-file.txt": b"Hello World\n"}
+        assert [(x.name, x.content) for x in pdf.attachment_list] == [("test.txt", b"Hello World\n")]
+        assert next(pdf.attachment_list).alternative_name == "my-file.txt"
+
+    # Only name.
+    no_f_path = tmpdir / "no-f.pdf"
+    no_f_path.write_binary(re.sub(rb" /UF [^/]+ /", b" /", attached_path.read_binary()))
+    with PdfReader(str(no_f_path)) as pdf:
+        assert pdf._list_attachments() == ["test.txt"]
+        assert pdf._get_attachments("test.txt") == {"test.txt": b"Hello World\n"}
+        assert [(x.name, x.content) for x in pdf.attachment_list] == [("test.txt", b"Hello World\n")]
+        assert next(pdf.attachment_list).alternative_name is None
+
+    # UF and F.
+    uf_f_path = tmpdir / "uf-f.pdf"
+    uf_f_path.write_binary(attached_path.read_binary().replace(b" /UF ", b"/F(file.txt) /UF "))
+    with PdfReader(str(uf_f_path)) as pdf:
+        assert pdf._list_attachments() == ["test.txt"]
+        assert pdf._get_attachments("test.txt") == {"test.txt": b"Hello World\n"}
+        assert [(x.name, x.content) for x in pdf.attachment_list] == [("test.txt", b"Hello World\n")]
+        assert next(pdf.attachment_list).alternative_name == "test.txt"
+
+    # Only F.
+    only_f_path = tmpdir / "f.pdf"
+    only_f_path.write_binary(attached_path.read_binary().replace(b" /UF ", b" /F "))
+    with PdfReader(str(only_f_path)) as pdf:
+        assert pdf._list_attachments() == ["test.txt"]
+        assert pdf._get_attachments("test.txt") == {"test.txt": b"Hello World\n"}
+        assert [(x.name, x.content) for x in pdf.attachment_list] == [("test.txt", b"Hello World\n")]
+        assert next(pdf.attachment_list).alternative_name == "test.txt"
+
+
+def test_get_attachments__same_attachment_more_than_twice():
+    writer = PdfWriter()
+    writer.add_blank_page(100, 100)
+    for i in range(5):
+        writer.add_attachment("test.txt", f"content{i}")
+    assert writer._get_attachments("test.txt") == {
+        "test.txt": [b"content0", b"content1", b"content2", b"content3", b"content4"]
+    }
+    assert [(x.name, x.content) for x in writer.attachment_list] == [
+        ("test.txt", b"content0"),
+        ("test.txt", b"content1"),
+        ("test.txt", b"content2"),
+        ("test.txt", b"content3"),
+        ("test.txt", b"content4"),
+    ]
+
+
+def test_get_attachments__alternative_name_is_none():
+    writer = PdfWriter()
+    attachment = EmbeddedFile(name="test.txt", pdf_object=writer.root_object)
+    assert attachment.alternative_name is None
+    with mock.patch(
+            "pypdf._writer.PdfWriter.attachment_list",
+            new_callable=mock.PropertyMock(return_value=[attachment])
+    ), mock.patch(
+            "pypdf.generic._files.EmbeddedFile.content",
+            new_callable=mock.PropertyMock(return_value=b"content")
+    ):
+        assert writer._get_attachments() == {"test.txt": b"content"}
+
+
+@pytest.mark.enable_socket
+def test_byte_encoded_named_destinations():
+    url = "https://github.com/user-attachments/files/19820164/pypdf_issue.pdf"
+    name = "issue3261.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url=url, name=name)))
+
+    page = reader.pages[0]
+    for annotation in page.annotations:
+        if annotation.get("/Subtype") == "/Link":
+            action = annotation["/A"]
+            if action["/S"] == "/GoTo":
+                named_dest = action["/D"]
+                assert str(named_dest) in reader.named_destinations
+
+    assert reader.named_destinations == {
+        "Doc-Start": {
+            "/Title": "Doc-Start",
+            "/Page": page.indirect_reference,
+            "/Type": "/XYZ",
+            "/Left": 133.768,
+            "/Top": 667.198,
+            "/Zoom": NullObject()
+        },
+        "楣整搮捡귃㉫㈰爵捡牥汦杩瑨敷杩瑨瑳瑡捩慤慴": {
+            "/Title": "楣整搮捡귃㉫㈰爵捡牥汦杩瑨敷杩瑨瑳瑡捩慤慴",
+            "/Page": page.indirect_reference,
+            "/Type": "/XYZ",
+            "/Left": 133.768,
+            "/Top": 614.424,
+            "/Zoom": NullObject()
+        },
+        "page.1": {
+            "/Title": "page.1",
+            "/Page": page.indirect_reference,
+            "/Type": "/XYZ",
+            "/Left": 132.768,
+            "/Top": 705.06,
+            "/Zoom": NullObject()
+        },
+        "section*.1": {
+            "/Title": "section*.1",
+            "/Page": page.indirect_reference,
+            "/Type": "/XYZ",
+            "/Left": 133.768,
+            "/Top": 642.222,
+            "/Zoom": NullObject()
+        }
+    }
+
+
+def test_viewer_preferences__indirect_reference():
+    input_path = RESOURCES_ROOT / "git.pdf"
+    reader = PdfReader(input_path)
+    assert (0, 24) not in reader.resolved_objects
+    viewer_preferences = reader.viewer_preferences
+    assert isinstance(viewer_preferences, ViewerPreferences)
+    assert viewer_preferences == {"/DisplayDocTitle": True}
+    assert (0, 24) in reader.resolved_objects
+    assert id(viewer_preferences) == id(reader.viewer_preferences)
+    assert id(viewer_preferences) == id(reader.resolved_objects[(0, 24)])
+
+
+@pytest.mark.enable_socket
+def test_named_destinations__tree_is_null_object():
+    url = "https://github.com/user-attachments/files/20885216/test.pdf"
+    name = "issue3330.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url=url, name=name)))
+
+    assert reader.named_destinations == {}
---
+++ packages/pypdf/pypdf-6.0.0.tar.gz/pypdf-6.0.0/tests/test_xmp.py
@@ -0,0 +1,417 @@
+"""Test the pypdf.xmp module."""
+from datetime import datetime
+from io import BytesIO
+from pathlib import Path
+
+import pytest
+
+import pypdf.generic
+import pypdf.xmp
+from pypdf import PdfReader, PdfWriter
+from pypdf.errors import PdfReadError
+from pypdf.generic import NameObject, StreamObject
+from pypdf.xmp import XmpInformation
+
+from . import get_data_from_url
+
+TESTS_ROOT = Path(__file__).parent.resolve()
+PROJECT_ROOT = TESTS_ROOT.parent
+RESOURCE_ROOT = PROJECT_ROOT / "resources"
+SAMPLE_ROOT = Path(PROJECT_ROOT) / "sample-files"
+
+
+@pytest.mark.samples
+@pytest.mark.parametrize(
+    "src",
+    [
+        (SAMPLE_ROOT / "020-xmp/output_with_metadata_pymupdf.pdf"),
+    ],
+)
+def test_read_xmp_metadata_samples(src):
+    reader = PdfReader(src)
+    xmp = reader.xmp_metadata
+    assert xmp
+    assert xmp.dc_contributor == []
+    assert xmp.dc_creator == ["John Doe"]
+    assert xmp.dc_source == "Martin Thoma"  # attribute node
+    assert xmp.dc_description == {"x-default": "This is a text"}
+    assert xmp.dc_date == [datetime(1990, 4, 28, 0, 0)]
+    assert xmp.dc_title == {"x-default": "Sample PDF with XMP Metadata"}
+    assert xmp.custom_properties == {
+        "Style": "FooBarStyle",
+        "other": "worlds",
+        "⏰": "time",
+    }
+
+
+@pytest.mark.samples
+def test_writer_xmp_metadata_samples():
+    writer = PdfWriter(SAMPLE_ROOT / "020-xmp/output_with_metadata_pymupdf.pdf")
+    xmp = writer.xmp_metadata
+    assert xmp
+    assert xmp.dc_contributor == []
+    assert xmp.dc_creator == ["John Doe"]
+    assert xmp.dc_source == "Martin Thoma"  # attribute node
+    assert xmp.dc_description == {"x-default": "This is a text"}
+    assert xmp.dc_date == [datetime(1990, 4, 28, 0, 0)]
+    assert xmp.dc_title == {"x-default": "Sample PDF with XMP Metadata"}
+    assert xmp.custom_properties == {
+        "Style": "FooBarStyle",
+        "other": "worlds",
+        "⏰": "time",
+    }
+    co = pypdf.generic.ContentStream(None, None)
+    co.set_data(
+        xmp.stream.get_data().replace(
+            b'dc:source="Martin Thoma"', b'dc:source="Pubpub-Zz"'
+        )
+    )
+    writer.xmp_metadata = pypdf.xmp.XmpInformation(co)
+    b = BytesIO()
+    writer.write(b)
+    reader = PdfReader(b)
+    xmp2 = reader.xmp_metadata
+    assert xmp2.dc_source == "Pubpub-Zz"
+
+
+@pytest.mark.parametrize(
+    ("src", "has_xmp"),
+    [
+        (RESOURCE_ROOT / "commented-xmp.pdf", True),
+        (RESOURCE_ROOT / "crazyones.pdf", False),
+    ],
+)
+def test_read_xmp_metadata(src, has_xmp):
+    """Read XMP metadata from PDF files."""
+    reader = PdfReader(src)
+    xmp = reader.xmp_metadata
+    assert (xmp is None) == (not has_xmp)
+    if has_xmp:
+        for _ in xmp.get_element(
+            about_uri="", namespace=pypdf.xmp.RDF_NAMESPACE, name="Artist"
+        ):
+            pass
+
+        assert get_all_tiff(xmp) == {"tiff:Artist": ["me"]}
+        assert xmp.dc_contributor == []
+
+
+def get_all_tiff(xmp: pypdf.xmp.XmpInformation):
+    """Return all TIFF metadata as a dictionary."""
+    data = {}
+    tiff_ns = xmp.get_nodes_in_namespace(
+        about_uri="", namespace="http://ns.adobe.com/tiff/1.0/"
+    )
+    for tag in tiff_ns:
+        contents = [content.data for content in tag.childNodes]
+        data[tag.tagName] = contents
+    return data
+
+
+def test_converter_date():
+    """
+    _converter_date returns the correct datetime.
+
+    This is a regression test for issue #774.
+    """
+    date = pypdf.xmp._converter_date("2021-04-28T12:23:34.123Z")
+    assert date == datetime(2021, 4, 28, 12, 23, 34, 123000)
+
+    with pytest.raises(ValueError) as exc:
+        pypdf.xmp._converter_date("today")
+    assert exc.value.args[0].startswith("Invalid date format")
+
+    date = pypdf.xmp._converter_date("2021-04-28T12:23:01-03:00")
+    assert date == datetime(2021, 4, 28, 15, 23, 1)
+
+
+def test_modify_date():
+    """
+    xmp_modify_date is extracted correctly.
+
+    This is a regression test for issue #914.
+    """
+    path = RESOURCE_ROOT / "issue-914-xmp-data.pdf"
+    reader = PdfReader(path)
+    assert reader.xmp_metadata.xmp_modify_date == datetime(2022, 4, 9, 15, 22, 43)
+
+
+@pytest.mark.parametrize(
+    "x",
+    ["a", 42, 3.141, False, True],
+)
+def test_identity_function(x):
+    """The identity is returning its input."""
+    assert pypdf.xmp._identity(x) == x
+
+
+@pytest.mark.enable_socket
+@pytest.mark.parametrize(
+    ("url", "name", "xmpmm_instance_id"),
+    [
+        (
+            None,
+            "tika-955562.pdf",
+            "uuid:ca96e032-c2af-49bd-a71c-95889bafbf1d",
+        )
+    ],
+)
+def test_xmpmm_instance_id(url, name, xmpmm_instance_id):
+    """XMPMM instance id is correctly extracted."""
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    xmp_metadata = reader.xmp_metadata
+    assert xmp_metadata.xmpmm_instance_id == xmpmm_instance_id
+    # cache hit:
+    assert xmp_metadata.xmpmm_instance_id == xmpmm_instance_id
+
+
+@pytest.mark.enable_socket
+def test_xmp_dc_description_extraction():
+    """XMP dc_description is correctly extracted."""
+    url = "https://github.com/user-attachments/files/18381721/tika-953770.pdf"
+    name = "tika-953770.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    xmp_metadata = reader.xmp_metadata
+    assert xmp_metadata.dc_description == {
+        "x-default": "U.S. Title 50 Certification Form"
+    }
+    # cache hit:
+    assert xmp_metadata.dc_description == {
+        "x-default": "U.S. Title 50 Certification Form"
+    }
+
+
+@pytest.mark.enable_socket
+def test_dc_creator_extraction():
+    """XMP dc_creator is correctly extracted."""
+    url = "https://github.com/user-attachments/files/18381721/tika-953770.pdf"
+    name = "tika-953770.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    xmp_metadata = reader.xmp_metadata
+    assert xmp_metadata.dc_creator == ["U.S. Fish and Wildlife Service"]
+    # cache hit:
+    assert xmp_metadata.dc_creator == ["U.S. Fish and Wildlife Service"]
+
+
+@pytest.mark.enable_socket
+def test_custom_properties_extraction():
+    """XMP custom_properties is correctly extracted."""
+    url = "https://github.com/user-attachments/files/18381764/tika-986065.pdf"
+    name = "tika-986065.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    xmp_metadata = reader.xmp_metadata
+    assert xmp_metadata.custom_properties == {"Style": "Searchable Image (Exact)"}
+    # cache hit:
+    assert xmp_metadata.custom_properties == {"Style": "Searchable Image (Exact)"}
+
+
+@pytest.mark.enable_socket
+def test_dc_subject_extraction():
+    """XMP dc_subject is correctly extracted."""
+    url = "https://github.com/user-attachments/files/18381730/tika-959519.pdf"
+    name = "tika-959519.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    xmp_metadata = reader.xmp_metadata
+    assert xmp_metadata.dc_subject == [
+        "P&P",
+        "manual",
+        "1240.2325",
+        "CVM",
+        "PROCEDURES ON MEDIA INQUIRIES",
+        "animal",
+        "media",
+        "procedures",
+        "inquiries",
+    ]
+    # Cache hit:
+    assert xmp_metadata.dc_subject == [
+        "P&P",
+        "manual",
+        "1240.2325",
+        "CVM",
+        "PROCEDURES ON MEDIA INQUIRIES",
+        "animal",
+        "media",
+        "procedures",
+        "inquiries",
+    ]
+
+
+@pytest.mark.enable_socket
+def test_invalid_xmp_information_handling():
+    """
+    Invalid XML in xmp_metadata is gracefully handled.
+
+    This is a regression test for issue #585.
+    """
+    url = "https://github.com/py-pdf/pypdf/files/5536984/test.pdf"
+    name = "pypdf-5536984.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+    with pytest.raises(PdfReadError) as exc:
+        reader.xmp_metadata
+    assert exc.value.args[0].startswith("XML in XmpInformation was invalid")
+
+
+def test_xmp_getter_bag_function():
+    """xmp._getter_bag does not crash."""
+    f = pypdf.xmp._getter_bag("namespace", "name")
+
+    class Tst:  # to replace pdf
+        strict = False
+
+    reader = PdfReader(RESOURCE_ROOT / "commented-xmp.pdf")
+    xmp_info = reader.xmp_metadata
+    # <?xpacket begin='ï»¿' id='W5M0MpCehiHzreSzNTczkc9d'?>
+    # <x:xmpmeta xmlns:x='adobe:ns:meta/' x:xmptk='Image::ExifTool 11.88'>
+    # <rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'>
+
+    # <rdf:Description rdf:about=''
+    # xmlns:tiff='http://ns.adobe.com/tiff/1.0/'>
+    # <tiff:Artist>me</tiff:Artist>
+    # </rdf:Description>
+    # </rdf:RDF>
+    # </x:xmpmeta>
+
+    assert xmp_info is not None
+    f(xmp_info)
+
+
+@pytest.mark.samples
+def test_pdfa_xmp_metadata_with_values():
+    """Test PDF/A XMP metadata extraction from a file with PDF/A metadata."""
+    reader = PdfReader(SAMPLE_ROOT / "021-pdfa" / "crazyones-pdfa.pdf")
+    xmp = reader.xmp_metadata
+
+    assert xmp is not None
+    assert xmp.pdfaid_part == "1"
+    assert xmp.pdfaid_conformance == "B"
+
+
+@pytest.mark.samples
+def test_pdfa_xmp_metadata_without_values():
+    """Test PDF/A XMP metadata extraction from a file without PDF/A metadata."""
+    reader = PdfReader(SAMPLE_ROOT / "020-xmp" / "output_with_metadata_pymupdf.pdf")
+    xmp = reader.xmp_metadata
+
+    assert xmp is not None
+    assert xmp.pdfaid_part is None
+    assert xmp.pdfaid_conformance is None
+
+
+@pytest.mark.enable_socket
+def test_xmp_metadata__content_stream_is_dictionary_object():
+    url = "https://github.com/user-attachments/files/18943249/testing.pdf"
+    name = "issue3107.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+
+    with pytest.raises(
+            PdfReadError,
+            match="XML in XmpInformation was invalid: 'DictionaryObject' object has no attribute 'get_data'"
+    ):
+        assert reader.xmp_metadata is not None
+
+
+@pytest.mark.enable_socket
+def test_dc_creator__bag_instead_of_seq():
+    url = "https://github.com/user-attachments/files/18381698/tika-924562.pdf"
+    name = "tika-924562.pdf"
+    reader = PdfReader(BytesIO(get_data_from_url(url, name=name)))
+
+    assert reader.xmp_metadata is not None
+    assert reader.xmp_metadata.dc_creator == ["William J. Hussar"]
+
+
+@pytest.mark.enable_socket
+def test_dc_language__no_bag_container():
+    reader = PdfReader(BytesIO(get_data_from_url(name="iss2138.pdf")))
+
+    assert reader.xmp_metadata is not None
+    assert reader.xmp_metadata.dc_language == ["x-unknown"]
+
+
+def test_reading_does_not_destroy_root_object():
+    """Test for #3391."""
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "commented-xmp.pdf")
+    xmp = writer.xmp_metadata
+    assert xmp is not None
+    assert not isinstance(writer.root_object["/Metadata"], XmpInformation)
+    assert isinstance(writer.root_object["/Metadata"].get_object(), StreamObject)
+
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    assert b"\n/Metadata 27 0 R\n" in output_bytes
+
+
+def test_xmp_information__write_to_stream():
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "commented-xmp.pdf")
+    xmp = writer.xmp_metadata
+
+    output = BytesIO()
+    with pytest.warns(
+            DeprecationWarning,
+            match=(
+                r"^XmpInformation\.write_to_stream is deprecated and will be removed in pypdf 6\.0\.0\. "
+                r"Use PdfWriter\.xmp_metadata instead\.$"
+            )
+    ):
+        xmp.write_to_stream(output)
+    output_bytes = output.getvalue()
+    assert output_bytes.startswith(b"<<\n/Type /Metadata\n/Subtype /XML\n/Length 2786\n>>\nstream\n<?xpacket begin")
+
+
+def test_pdf_writer__xmp_metadata_setter():
+    # Clear existing metadata.
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "commented-xmp.pdf")
+    assert writer.xmp_metadata is not None
+    original_metadata = writer.xmp_metadata.stream.get_data()
+    writer.xmp_metadata = None
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    reader = PdfReader(BytesIO(output_bytes))
+    assert reader.xmp_metadata is None
+
+    # Attempt to clear again.
+    writer = PdfWriter(clone_from=reader)
+    assert writer.xmp_metadata is None
+    writer.xmp_metadata = None
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    reader = PdfReader(BytesIO(output_bytes))
+    assert reader.xmp_metadata is None
+
+    # Set new metadata from bytes.
+    writer = PdfWriter(clone_from=reader)
+    assert writer.xmp_metadata is None
+    writer.xmp_metadata = original_metadata
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    reader = PdfReader(BytesIO(output_bytes))
+    assert get_all_tiff(reader.xmp_metadata) == {"tiff:Artist": ["me"]}
+
+    # Set metadata from XmpInformation.
+    writer = PdfWriter(clone_from=reader)
+    xmp_metadata = writer.xmp_metadata
+    assert get_all_tiff(xmp_metadata) == {"tiff:Artist": ["me"]}
+    new_metadata = original_metadata.replace(b"<tiff:Artist>me</tiff:Artist>", b"<tiff:Artist>Foo Bar</tiff:Artist>")
+    xmp_metadata.stream.set_data(new_metadata)
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    reader = PdfReader(BytesIO(output_bytes))
+    assert get_all_tiff(reader.xmp_metadata) == {"tiff:Artist": ["Foo Bar"]}
+
+    # Fix metadata not being an IndirectObject before.
+    writer = PdfWriter(clone_from=RESOURCE_ROOT / "commented-xmp.pdf")
+    writer.root_object[NameObject("/Metadata")] = writer.root_object["/Metadata"].get_object()
+    assert "/XML" in str(writer.root_object)
+    writer.xmp_metadata = new_metadata
+    output = BytesIO()
+    writer.write(output)
+    output_bytes = output.getvalue()
+    reader = PdfReader(BytesIO(output_bytes))
+    assert get_all_tiff(reader.xmp_metadata) == {"tiff:Artist": ["Foo Bar"]}
+    assert "/XML" not in str(writer.root_object)
