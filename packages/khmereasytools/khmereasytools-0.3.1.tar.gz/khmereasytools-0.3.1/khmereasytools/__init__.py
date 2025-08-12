4445e24a5fbf6e9d4f7fd76118b424d0a134fbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:54 +0000

    Add package khmereasytools/0.3.1/khmereasytools-0.3.1.tar.gz

---
+++ packages/khmereasytools/khmereasytools-0.3.1.tar.gz/khmereasytools-0.3.1/khmereasytools/__init__.py
@@ -0,0 +1,13 @@
+
+from .tools import (
+    get_default_stop_words,
+    load_stop_words_from_file,
+    khfilter,
+    khseg,
+    syllable_segment,
+    pos_tag,
+    ocr_from_image,
+    is_khmer
+)
+
+__version__ = "0.3.1"
