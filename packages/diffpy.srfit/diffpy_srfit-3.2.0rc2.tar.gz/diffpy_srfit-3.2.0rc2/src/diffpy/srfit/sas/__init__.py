a968b5ce93dfbf6b616b4772fbb56ace7ad3aae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 05:06:19 +0000

    Add package diffpy.srfit/3.2.0rc2/diffpy_srfit-3.2.0rc2.tar.gz

---
+++ packages/diffpy.srfit/diffpy_srfit-3.2.0rc2.tar.gz/diffpy_srfit-3.2.0rc2/src/diffpy/srfit/sas/__init__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.srfit      by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2008 The Trustees of Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Chris Farrow
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE_DANSE.txt for license information.
+#
+##############################################################################
+"""SAS calculation tools."""
+
+__all__ = [
+    "SASGenerator",
+    "SASParser",
+    "SASProfile",
+    "PrCalculator",
+    "CFCalculator",
+]
+
+from .prcalculator import CFCalculator, PrCalculator
+from .sasgenerator import SASGenerator
+from .sasparser import SASParser
+from .sasprofile import SASProfile
+
+# End of file
