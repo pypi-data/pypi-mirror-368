45d59ec69d6a02b020de77cec5573ddab14ded2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:58 +0000

    Add package cartography-client/0.3.1/cartography_client-0.3.1-py3-none-any.whl

---
+++ packages/cartography-client/cartography_client-0.3.1-py3-none-any.whl/cartography/types/workflows/request/crawl_create_params.py
@@ -0,0 +1,32 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Iterable
+from typing_extensions import Required, TypedDict
+
+__all__ = ["CrawlCreateParams"]
+
+
+class CrawlCreateParams(TypedDict, total=False):
+    bucket_name: Required[str]
+
+    crawl_id: Required[str]
+
+    engines: Required[Iterable[Dict[str, object]]]
+
+    url: Required[str]
+
+    absolute_only: bool
+
+    batch_size: int
+
+    depth: int
+
+    keep_external: bool
+
+    max_urls: int
+
+    max_workers: int
+
+    visit_external: bool
