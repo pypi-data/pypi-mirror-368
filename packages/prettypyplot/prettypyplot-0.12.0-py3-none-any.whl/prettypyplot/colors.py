2cf5a8601e723f7040120607aac8c1df9f5482aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 07:05:53 +0000

    Add package prettypyplot/0.12.0/prettypyplot-0.12.0-py3-none-any.whl

---
+++ packages/prettypyplot/prettypyplot-0.12.0-py3-none-any.whl/prettypyplot/colors.py
@@ -0,0 +1,408 @@
+# -*- coding: utf-8 -*-
+# BSD 3-Clause License
+# Copyright (c) 2020-2023, Daniel Nagel
+# All rights reserved.
+"""Set-up matplotlib environment."""
+
+# ~~~ IMPORT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from collections import namedtuple
+
+import matplotlib as mpl
+import numpy as np
+from matplotlib import colors as clr
+from matplotlib import pyplot as plt
+
+# importing colormaps
+from prettypyplot._cmaps.bownair import _bownair
+from prettypyplot._cmaps.discrete import (
+    _argon,
+    _cbf4,
+    _cbf5,
+    _cbf8,
+    _pastel5,
+    _pastel6,
+    _pastel_autumn,
+    _pastel_rainbow,
+    _pastel_spring,
+    _paula,
+    _paula_cbf,
+    _paula_lime,
+    _summertimes,
+    _ufcd,
+)
+from prettypyplot._cmaps.gdv_discrete import (
+    _gdv_rag,
+    _gdv_rag_cvd,
+    _gdv_palette,
+    _gdv_6a,
+    _gdv_5a,
+    _gdv_4a,
+    _gdv_4b,
+    _gdv_3a,
+    _gdv_3b,
+    _gdv_2a,
+    _gdv_2b,
+    _gdv_s1,
+    _gdv_s2,
+    _gdv_s3,
+    _gdv_m1,
+    _gdv_m2,
+    _gdv_m3,
+    _gdv_d1,
+    _gdv_d2,
+    _gdv_d3,
+    _gdv_d4,
+    _gdv_mars,
+    _gdv_moon,
+)
+from prettypyplot._cmaps.macaw import _macaw
+from prettypyplot._cmaps.paula2 import _paula2
+from prettypyplot._cmaps.tol_discrete import (
+    _tol_bright,
+    _tol_high_contrast,
+    _tol_light,
+    _tol_medium_contrast,
+    _tol_muted,
+    _tol_vibrant,
+)
+from prettypyplot._cmaps.turbo import _turbo
+from prettypyplot.tools import is_number
+
+
+# ~~~ COLORS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class GrayTones(namedtuple('GrayTones', 'dark light')):
+    """Class for holding light and dark gray tone."""
+
+
+black_grays = GrayTones('#000000', '#dddfe5')
+black_grays_darkmode = GrayTones('#ffffff', '#22201a')
+default_grays = GrayTones('#4d4f53', '#dddfe5')
+default_grays_darkmode = GrayTones('#b2b0ac', '#22201a')
+
+
+# ~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def _get_cmap(cmap):
+    """Wrapper for get_cmap with mpl <=3.2 and >=3.4."""
+    if hasattr(mpl, 'colormaps') and hasattr(mpl.colormaps, 'get_cmap'):
+        return mpl.colormaps.get_cmap(cmap)
+    return mpl.cm.get_cmap(cmap)  # pragma: no cover
+
+
+def _register_cmap(cmap):
+    """Wrapper for register_cmap with mpl <=3.2 and >=3.4."""
+    if hasattr(mpl, 'colormaps') and hasattr(mpl.colormaps, 'register'):
+        mpl.colormaps.register(cmap)
+    else:  # pragma: no cover
+        mpl.cm.register_cmap(cmap=cmap)
+
+
+def load_cmaps():
+    """Load and include custom colormaps to matplotlib.
+
+    Add sequential colormaps `pastel5`, `pastel6`, `cbf4`, `cbf5`, `cbf8`,
+    and `ufcd` as an corporate design. Except of `ufcd` all palettes should be
+    'color-blind-friendly'.
+
+    Add continuous colormaps macaw, Turbo. The Copyright of those are given on
+    top of the data.
+
+    !!! see
+        Choosing an [cmaps](../../gallery/cmaps).
+
+    """
+    colormaps = (
+        _argon(),
+        _pastel5(),
+        _pastel6(),
+        _cbf4(),
+        _cbf5(),
+        _cbf8(),
+        _gdv_rag(),
+        _gdv_rag_cvd(),
+        _gdv_palette(),
+        _gdv_6a(),
+        _gdv_5a(),
+        _gdv_4a(),
+        _gdv_4b(),
+        _gdv_3a(),
+        _gdv_3b(),
+        _gdv_2a(),
+        _gdv_2b(),
+        _gdv_s1(),
+        _gdv_s2(),
+        _gdv_s3(),
+        _gdv_m1(),
+        _gdv_m2(),
+        _gdv_m3(),
+        _gdv_d1(),
+        _gdv_d2(),
+        _gdv_d3(),
+        _gdv_d4(),
+        _gdv_mars(),
+        _gdv_moon(),
+        _pastel_autumn(),
+        _pastel_rainbow(),
+        _pastel_spring(),
+        _paula(),
+        _paula2(),
+        _paula_cbf(),
+        _paula_lime(),
+        _summertimes(),
+        _tol_bright(),
+        _tol_high_contrast(),
+        _tol_light(),
+        _tol_medium_contrast(),
+        _tol_muted(),
+        _tol_vibrant(),
+        _ufcd(),
+        _turbo(),
+        _macaw(),
+        _bownair(),
+    )
+    # register own continuous and discrete cmaps
+    for colormap in colormaps:
+        # add cmap and reverse cmap
+        for cmap in (colormap, colormap.reversed()):
+            try:
+                _get_cmap(cmap.name)
+            except ValueError:
+                _register_cmap(cmap=cmap)
+
+
+def load_colors():
+    """Load and include custom colors to matplotlib.
+
+    Add colors of `pastel5` which can be accessed via `pplt:blue`, `pplt:red`,
+    `pplt:green`, `pplt:orange`, `pplt:lightblue`, `pplt:gray` and
+    `pplt:lightgray`. Further, the current colors will be added `pplt:axes`,
+    `pplt:text`, `pplt:grid`.
+
+    !!! see
+        Choosing an [cmaps](../../gallery/cmaps).
+
+    """
+    # register own colors
+    pplt_colors = {
+        'pplt:blue': _pastel5().colors[0],
+        'pplt:red': _pastel5().colors[1],
+        'pplt:green': _pastel5().colors[2],
+        'pplt:orange': _pastel5().colors[3],
+        'pplt:lightblue': _pastel5().colors[4],
+        'pplt:gray': default_grays.dark,
+        'pplt:grey': default_grays.dark,
+        'pplt:lightgray': default_grays.light,
+        'pplt:lightgrey': default_grays.light,
+        'pplt:axes': plt.rcParams['axes.edgecolor'],
+        'pplt:text': plt.rcParams['text.color'],
+        'pplt:grid': plt.rcParams['grid.color'],
+    }
+    clr._colors_full_map.update(pplt_colors)
+
+
+def categorical_cmap(nc, nsc, *, cmap=None, return_colors=False):
+    """Generate categorical colors of given cmap.
+
+    Exract from a predefined colormap colors and generate for each the desired
+    number of shades.
+
+    Parameters
+    ----------
+    nc : int
+        Number of colors
+    nsc : int
+        Number of shades per colors
+    cmap : `matplotlib.colors.Colormap` or str, optional
+        Matplotlib colormap to take colors from. The default is the active
+        color cycle.
+    return_colors : bool, optional
+        Return an array of rgb colors. Each color together with its shades are
+        in an own row.
+
+    Returns
+    -------
+    scolors : `matplotlib.colors.Colormap` or np.ndarray
+        Return discrete colormap. If return_colors, a 2d representation will
+        be returned instead.
+
+    """
+    # check correct data type
+    _is_number_in_range(
+        nc,
+        name='nc',
+        dtype=int,
+        low=1,
+        high=np.iinfo(int).max,
+    )
+    _is_number_in_range(
+        nsc,
+        name='nsc',
+        dtype=int,
+        low=1,
+        high=np.iinfo(int).max,
+    )
+    nc, nsc = int(nc), int(nsc)
+
+    # get cmap
+    if cmap is not None:
+        cmap = plt.get_cmap(cmap)
+    else:
+        cmap = clr.ListedColormap(
+            plt.rcParams['axes.prop_cycle'].by_key()['color'],
+        )
+    if nc > cmap.N:
+        raise ValueError('Too many categories for colormap.')
+
+    # extract colors from cmap
+    if isinstance(cmap, clr.LinearSegmentedColormap):
+        colors = cmap(np.linspace(0, 1, nc))
+    elif isinstance(cmap, clr.ListedColormap):
+        colors = cmap(np.arange(nc, dtype=int))
+
+    # get shades of colors
+    scolors = np.empty((nc, nsc, 3))
+    for idx, color in enumerate(colors):
+        scolors[idx] = categorical_color(nsc, color)
+
+    if return_colors:
+        return scolors
+    return clr.ListedColormap(np.concatenate(scolors))
+
+
+def categorical_color(nsc, color, *, return_hex=False):
+    """Generate categorical shades of given colors.
+
+    Generate for each provided color the number of specified shades. The shaded
+    colors are interpolated linearly in HSV colorspace. This function is based
+    on following post: https://stackoverflow.com/a/47232942
+
+    Parameters
+    ----------
+    nsc : int
+        Number of shades per color.
+    color : RGB color or matplotlib predefined color
+        Color used for generating shades.
+    return_hex : bool, optional
+        Return colors in hex format instead of rgb.
+
+    Returns
+    -------
+    colors_rgb : list of RGB colors
+        A list containing shaded colors. Where the list is sorted from the
+        original color at the beginning to the most shaded one at the end.
+        The default color encoding is rgb and hex if specified.
+
+    """
+    # check correct data type
+    color = clr.to_rgb(color)
+    _is_number_in_range(
+        nsc,
+        name='nsc',
+        dtype=int,
+        low=1,
+        high=np.iinfo(int).max,
+    )
+    nsc = int(nsc)
+
+    # genrate shades of colors
+    color_hsv = clr.rgb_to_hsv(color)
+    colors_hsv = np.tile(color_hsv, nsc).reshape(nsc, 3)
+    colors_hsv[:, 1] = np.linspace(color_hsv[1], 1 / 4, nsc)
+    colors_hsv[:, 2] = np.linspace(color_hsv[2], 1, nsc)
+    colors_rgb = clr.hsv_to_rgb(colors_hsv)
+
+    # check if color is greyscale value, need to fix arbitrary hue value of 0
+    if is_greyshade(color):
+        colors_rgb[:, 0] = colors_rgb[:, 1]
+
+    if return_hex:
+        return [clr.to_hex(color) for color in colors_rgb]
+    return colors_rgb
+
+
+def text_color(bgcolor, colors=('#000000', '#ffffff')):
+    """Select textcolor with maximal contrast on background.
+
+    All parameters needs to be colors accepted by matplotlib, see
+    [matplotlib.colors](https://matplotlib.org/api/colors_api.html).
+    The formulas are taken from W3C [WCAG 2.1](https://www.w3.org/TR/WCAG21)
+    (Web Content Accessibility Guidelines).
+
+    Parameters
+    ----------
+    bgcolor : matplotlib color
+        Background color to which the contrast is maximized.
+    colors : list of matplotlib colors, optional
+        Selection of textcolors to choose from.
+
+    Returns
+    -------
+    color : matplotlib color
+        Color of colors which has the highest contrast on the given bgcolor.
+
+    """
+    # check input by casting to matplotlib colors
+    bgcolor = clr.to_rgb(bgcolor)
+    colors_rgb = [clr.to_rgb(color) for color in colors]
+
+    # calculate the (luminances)
+    bgL = _relative_luminance(bgcolor)
+    Ls = [_relative_luminance(color) for color in colors_rgb]
+
+    # calculate contrast between bgcolor and all colors
+    contrast = [_contrast(bgL, Luminance) for Luminance in Ls]
+
+    # return color corresponding to greatest contrast
+    idx = contrast.index(max(contrast))
+    return colors[idx]
+
+
+def _channel_transf(channel):
+    """Transform channel for luminance calculation."""
+    if channel < 0.03928:
+        return channel / 12.92
+    return ((channel + 0.055) / 1.055) ** 2.4
+
+
+def _relative_luminance(color):
+    """Calculate luminance from rgb color, each channel [0, 1]."""
+    for rgbc in color:
+        _is_number_in_range(rgbc, name='RGB channel')
+    rgb = np.array([_channel_transf(channel) for channel in color])
+    rgb_weights = np.array([0.2126, 0.7152, 0.0722])
+    return np.sum(rgb_weights * rgb)
+
+
+def _contrast(L1, L2):
+    """L1 and L2 should be luminances [0, 1]."""
+    for lum in (L1, L2):
+        _is_number_in_range(lum, name='Luminace')
+    L1, L2 = float(L1), float(L2)
+
+    L_offset = 0.05
+    if L1 < L2:
+        L1, L2 = L2, L1
+    return (L1 + L_offset) / (L2 + L_offset)
+
+
+def _is_number_in_range(num, *, dtype=float, name='Variable', low=0, high=1):
+    """Check if number is in range [low, high]."""
+    if not is_number(num, dtype=dtype):
+        raise TypeError(
+            '{0} needs to be {1} but given '.format(name, dtype.__name__)
+            + '{num}'.format(num=num),
+        )
+    num = dtype(num)
+    if num < low or num > high:
+        raise ValueError(
+            '{name} needs to be within [{low}'.format(name=name, low=low)
+            + ', {high}] but given {num}'.format(high=high, num=num),
+        )
+
+
+def is_greyshade(color):
+    """Check if color is a greyscale value including bw."""
+    # check if color is greyscale value, need to fix arbitrary hue value
+    color = clr.to_rgb(color)
+    if np.min(color) == np.max(color):
+        return True
+    return False
---
+++ packages/prettypyplot/prettypyplot-0.12.0-py3-none-any.whl/prettypyplot/style.py
@@ -0,0 +1,507 @@
+# -*- coding: utf-8 -*-
+# BSD 3-Clause License
+# Copyright (c) 2020-2023, Daniel Nagel
+# All rights reserved.
+"""Set-up matplotlib environment."""
+
+# ~~~ IMPORT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import shutil
+from enum import Enum, auto
+from os import path as ospath
+
+import numpy as np
+from matplotlib import pyplot as plt
+from decorit import copy_doc_params
+
+import prettypyplot as _pplt
+from prettypyplot import tools
+from prettypyplot import colors as pclr
+
+
+# ~~~ CONSTANTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class Style(Enum):
+    """Enum for all styles defined in pplt."""
+
+    DEFAULT = auto()
+    # default style, good for scientific usage
+    MINIMAL = auto()
+    # minimal style, good for slides, posters and co.
+    NONE = auto()
+    # do not change matplotlib style
+
+    @classmethod
+    def keys_list(cls):
+        """Return list of available Style names."""
+        return list(cls.__members__.keys())
+
+
+class Mode(Enum):
+    """Enum for all modes defined in pplt."""
+
+    DEFAULT = auto()
+    # default font size, good for monitor use
+    PRINT = auto()
+    # slightly larger fonts compared to default
+    BEAMER = auto()
+    # large fonts, good for slides
+    POSTER = auto()
+    # large fonts, good for A0 posters
+
+    @classmethod
+    def keys_list(cls):
+        """Return list of available Mode names."""
+        return list(cls.__members__.keys())
+
+
+# set default mode and style
+if _pplt.MODE is None:
+    _pplt.MODE = Mode.DEFAULT
+if _pplt.STYLE is None:
+    _pplt.STYLE = Style.DEFAULT
+
+
+# ~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def update_style(  # noqa: C901
+    interactive=None,
+    colors=None,
+    cmap=None,
+    ncs=None,
+    figsize=None,
+    figratio=None,
+    mode=None,
+    style=None,
+    ipython=None,
+    true_black=None,
+    latex=None,
+    sf=None,
+):
+    """Update alternative matplotlib style.
+
+    This function updates specified parameters of `use_style` without changing
+    other.
+
+    Parameters
+    ----------
+    interactive : bool, optional
+        Set interactive mode.
+    colors : string, optional
+        Set the default color cycler from continuous or discrete maps. Use any
+        of matplotlibs defaults or specified in the colors submodule.
+    cmap : string, optional
+        Set the default colormap.
+    ncs : int, optional
+        Number of colors if continuous cmap is selected.
+    figsize : int or int tuple, optional
+        Give size of default figure in inches, either as tuple (x, y) or a
+        single float for the x-axis. The y-axis will be determined by figratio.
+    figratio : str or float, optional
+        Set ratio of figsize x:y to 1:1/'option', where 'option' is one
+        of `['sqrt(2)', 'golden', 'sqrt(3)']` or any number. Golden stands for
+        the golden ratio (1.618). This option is ignored if figsize is used
+        with tuple.
+    mode : str, optional
+        One of the following modes:
+        `'default'`: use matplotlib defaults
+        `'beamer'`: extra large fontsize
+        `'print'`: default sizes
+        `'poster'`: for Din A0 posters
+    style : str, optional
+        One of the following styles:
+        `'default'`: enables grid and upper and right spines
+        `'minimal'`: removes all unneeded lines
+        `'none'`: no changes to style
+    ipython : bool, optional
+        Deactivate high-res in jpg/png for compatibility with IPyhton, e.g.
+        jupyter notebook/lab.
+    true_black : bool, optional
+        If true black will be used for labels and co., else a dark grey.
+    latex : bool, optional
+        If true LaTeX font will be used.
+    sf : bool, optional
+        Use sans-serif font for text and latex math environment.
+
+    """
+    # set selected mode and style
+    if style is not None:
+        if isinstance(style, Style):
+            pass
+        elif isinstance(style, str) and style.upper() in Style.keys_list():
+            style = Style[style.upper()]
+        else:
+            raise ValueError(
+                'Style "{st}" is not supported, use one of {sts}.'.format(
+                    st=style,
+                    sts=Style.keys_list(),
+                ),
+            )
+        _pplt.STYLE = style
+
+    if mode is not None:
+        if isinstance(mode, Mode):
+            pass
+        elif isinstance(mode, str) and mode.upper() in Mode.keys_list():
+            mode = Mode[mode.upper()]
+        else:
+            raise ValueError(
+                'Mode "{mode}" is not supported, use one of {modes}.'.format(
+                    mode=mode,
+                    modes=Mode.keys_list(),
+                ),
+            )
+
+        _pplt.MODE = mode
+
+    # set style variables in dictionary
+    for key, val in (
+        ('interactive', interactive),
+        ('colors', colors),
+        ('cmap', cmap),
+        ('ncs', ncs),
+        ('figsize', figsize),
+        ('figratio', figratio),
+        ('ipython', ipython),
+        ('true_black', true_black),
+        ('latex', latex),
+        ('sf', sf),
+    ):
+        if val is not None:
+            _pplt.STYLE_DICT[key] = val
+
+    if _pplt.STYLE is Style.NONE:
+        _reset_style()
+    else:
+        # load static rcParams
+        _apply_style('stylelib/default.mplstyle')
+        if _pplt.STYLE is Style.MINIMAL:
+            _apply_style('stylelib/minimal.mplstyle')
+
+        # set color cycle and cmap
+        _set_rc_colors(
+            colors=colors,
+            cmap=cmap,
+            ncs=ncs,
+            true_black=true_black,
+        )
+
+        # set figsize
+        if figsize is not None:
+            _set_rc_figsize(figratio=figratio, figsize=figsize)
+
+        # increase dpi if not in iypthon
+        _set_rc_dpi(ipython)
+
+        # set interactive mode
+        _set_ineractive_mode(interactive=interactive)
+
+        # setup LaTeX font if latex is available
+        # plt.style.use can not be used.
+        if latex and shutil.which('latex'):
+            _apply_style('stylelib/latex.mplstyle')
+
+        if sf:
+            _set_rc_sansserif()
+
+    if mode is not None:
+        # change widths and fontsize depending on MODE
+        _set_rc_widths(mode)
+
+
+@copy_doc_params(update_style)
+def use_style(
+    interactive=None,
+    colors='pastel5',
+    cmap='macaw',
+    ncs=10,
+    figsize=(3,),
+    figratio='golden',
+    mode=_pplt.MODE,
+    style=_pplt.STYLE,
+    ipython=False,
+    true_black=False,
+    latex=True,
+    sf=False,
+):
+    """Define alternative matplotlib style.
+
+    This function restores first the matplolib default values and finally
+    changes depicted values to achieve a more appealing appearence.
+    It additionally loads pplts colormaps and colors in matplolib.
+
+    See [update_style][prettypyplot.update_style] for parameters.
+
+    """
+    # restore matplotlib defaults
+    _reset_style()
+
+    # register own continuous and discrete cmaps
+    pclr.load_cmaps()
+
+    # update style
+    update_style(
+        interactive=interactive,
+        colors=colors,
+        cmap=cmap,
+        ncs=ncs,
+        figsize=figsize,
+        figratio=figratio,
+        mode=mode,
+        style=style,
+        ipython=ipython,
+        true_black=true_black,
+        latex=latex,
+        sf=sf,
+    )
+
+    # register used colors
+    pclr.load_colors()
+
+
+def _set_rc_colors(colors, cmap, true_black, ncs):
+    """Set rcParams colors."""
+    # set color cycle and cmap
+    if colors is not None or ncs is not None:
+        # use values of previous call in case of None
+        colors = colors if colors is not None else _pplt.STYLE_DICT['colors']
+        ncs = ncs if ncs is not None else _pplt.STYLE_DICT['ncs']
+
+        clrs = plt.get_cmap(colors)
+        plt.rcParams['axes.prop_cycle'] = plt.cycler(
+            color=(
+                clrs.colors
+                if tools.is_discrete_cmap(colors)
+                else clrs(np.linspace(0, 1, ncs))
+            ),
+        )
+
+    if cmap is not None:
+        plt.rcParams['image.cmap'] = cmap
+
+    if true_black is not None:
+        # change default colors
+        if true_black:
+            grays = pclr.black_grays
+        else:
+            grays = pclr.default_grays
+
+        plt.rcParams.update({
+            'axes.edgecolor': grays.dark,
+            'axes.labelcolor': grays.dark,
+            'text.color': grays.dark,
+            'patch.edgecolor': grays.dark,
+            'xtick.color': grays.dark,
+            'ytick.color': grays.dark,
+            'grid.color': grays.light,
+        })
+
+
+def _set_rc_figsize(figratio, figsize):
+    """Set rcParams figsize."""
+    # setup figsize
+    figsize = _parse_figsize(figsize, figratio)
+
+    if figsize is not None:
+        plt.rcParams['figure.figsize'] = figsize
+
+
+def _set_rc_widths(mode):
+    """Set rcParams widths and fontsizes according to mode."""
+    scales = _get_scale(mode)
+    if scales is not None:
+        for scale, rcParamsVal in [
+            [
+                'small_scale',
+                [
+                    ['axes.linewidth', 0.8],
+                    ['grid.linewidth', 0.8],
+                    ['xtick.major.width', 0.8],
+                    ['xtick.minor.width', 0.6],
+                ],
+            ],
+            [
+                'tick_scale',
+                [
+                    ['xtick.major.size', 3.5],
+                    ['xtick.minor.size', 2.0],
+                    ['xtick.major.pad', 3.5],
+                    ['xtick.minor.pad', 3.4],
+                ],
+            ],
+            [
+                'medium_scale',
+                [
+                    ['patch.linewidth', 1.0],
+                    ['hatch.linewidth', 1.0],
+                    ['boxplot.flierprops.markeredgewidth', 1.0],
+                    ['boxplot.flierprops.linewidth', 1.0],
+                    ['boxplot.boxprops.linewidth', 1.0],
+                    ['boxplot.whiskerprops.linewidth', 1.0],
+                    ['boxplot.capprops.linewidth', 1.0],
+                    ['boxplot.medianprops.linewidth', 1.0],
+                    ['boxplot.meanprops.linewidth', 1.0],
+                    ['lines.markeredgewidth', 1.0],
+                ],
+            ],
+            [
+                'large_scale',
+                [
+                    ['lines.linewidth', 1.5],
+                    ['boxplot.whiskers', 1.5],
+                    ['lines.markersize', 6],
+                ],
+            ],
+            ['fontsize', [['font.size', 1]]],
+        ]:
+            scale = scales[scale]
+            for rcParam, val in rcParamsVal:
+                plt.rcParams[rcParam] = scale * val
+                # apply all changes to yticks as well
+                if rcParam.startswith('xtick'):
+                    plt.rcParams['y{0}'.format(rcParam[1:])] = plt.rcParams[rcParam]
+
+
+def _set_rc_dpi(ipython, dpi=384):
+    """Set rcParams dpi."""
+    if ipython is not None and not ipython:
+        plt.rcParams['figure.dpi'] = dpi
+
+
+def _set_rc_sansserif():
+    """Set sans serif font."""
+    plt.rcParams.update({
+        'font.family': 'sans-serif',
+        'font.sans-serif': 'Helvetica',
+    })
+    plt.rcParams['text.latex.preamble'] += r'\usepackage[helvet]{sfmath}'
+
+
+def _set_ineractive_mode(interactive):
+    """Set interative mode."""
+    if interactive is not None:
+        if interactive:
+            plt.ion()
+        else:
+            plt.ioff()
+
+
+def _get_scale(mode: Mode):
+    """Get the scaling factors."""
+    scale_dict = {
+        Mode.DEFAULT: {
+            'large_scale': 1.0,
+            'medium_scale': 1.0,
+            'small_scale': 1.0,
+            'tick_scale': 1.0,
+            'fontsize': 10.0,
+        },
+        Mode.PRINT: {
+            'large_scale': 1.5,
+            'medium_scale': 1.7,
+            'small_scale': 1.7,
+            'tick_scale': 1.7,
+            'fontsize': 12.0,
+        },
+        Mode.POSTER: {
+            'large_scale': 4.0,
+            'medium_scale': 4.0,
+            'small_scale': 4.0,
+            'tick_scale': 4.0,
+            'fontsize': 28.0,
+        },
+        Mode.BEAMER: {
+            'large_scale': 4.0,
+            'medium_scale': 4.0,
+            'small_scale': 4.0,
+            'tick_scale': 4.0,
+            'fontsize': 28.0,
+        },
+    }
+    return scale_dict.get(mode, scale_dict[Mode.DEFAULT])
+
+
+def _reset_style():
+    """Restore default matplotlib style."""
+    plt.rcdefaults()
+
+
+def _apply_style(path):
+    """Load mplstyle file at given relative path to this file."""
+    module_dir = ospath.dirname(__file__)
+    path = ospath.join(module_dir, path)
+
+    # load and apply style
+    plt.style.use(path)
+
+
+def _parse_figratio(figratio):
+    """Parse the figratio value.
+
+    Parameters
+    ----------
+    figratio : float or one of ['sqrt(2)', 'sqrt(3)', 'golden']
+        Parse the figratio to an numeric value.
+
+    Returns
+    -------
+    figratio : float
+        Numeric figratio.
+
+    """
+    if figratio is None:
+        figratio = _pplt.STYLE_DICT['figratio']
+    elif tools.is_number(figratio):
+        figratio = float(figratio)
+    else:
+        figratios = {
+            'sqrt(2)': np.sqrt(2),
+            'sqrt(3)': np.sqrt(3),
+            'golden': (1 + np.sqrt(5)) / 2,
+        }
+        if figratio not in figratios:
+            raise ValueError(
+                'figratio needs to be an numeric value or one of ['
+                + '{0}].'.format(', '.join(figratios.keys())),
+            )
+        figratio = figratios.get(figratio, None)
+    return figratio
+
+
+def _parse_figsize(figsize, figratio):
+    """Parse the figsize value.
+
+    Parameters
+    ----------
+    figsize : float or tuple of floats
+        Parse the figsize (in inches) to an numeric value. If only a single
+        value is given, the figratio will be used for the second dimension.
+    figratio : float or one of ['sqrt(2)', 'sqrt(3)', 'golden'], optional
+        Parse the figratio to an numeric value, see `parse_figsize`.
+
+    Returns
+    -------
+    figsize : tuple
+        Tuple of figsize in inches (x, y).
+
+    """
+    figsize = tuple(np.atleast_1d(figsize))
+    if not all(tools.is_number(size) for size in figsize):
+        sizetuple = None
+    elif len(figsize) == 1 and figratio is None:
+        raise ValueError(
+            'using single value for `figsize` requires `figratio` but was not'
+            ' specified yet.',
+        )
+    elif len(figsize) == 1:
+        figratio = _parse_figratio(figratio)
+        figsize = float(figsize[0])
+        sizetuple = (figsize, figsize / figratio)
+    elif len(figsize) == 2:
+        sizetuple = figsize
+    else:
+        sizetuple = None
+
+    if sizetuple is None:
+        raise ValueError(
+            'figsize needs to be an numeric value or a tuple, not '
+            + '{0}.'.format(figsize),
+        )
+    return sizetuple
