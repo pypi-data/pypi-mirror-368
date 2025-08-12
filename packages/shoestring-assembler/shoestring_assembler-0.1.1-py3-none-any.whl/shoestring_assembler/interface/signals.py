ca90fec6f40a31bf28ad19bdb4285ebf3b4bb6bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:35 +0000

    Add package shoestring-assembler/0.1.1/shoestring_assembler-0.1.1-py3-none-any.whl

---
+++ packages/shoestring-assembler/shoestring_assembler-0.1.1-py3-none-any.whl/shoestring_assembler/interface/signals.py
@@ -0,0 +1,33 @@
+from enum import Enum, unique
+
+class Signal:
+    pass
+
+
+class BackSignal(Signal):
+    pass
+
+
+@unique
+class Action(Enum):
+    DOWNLOAD = "download"
+    FIND = "find"
+    UPDATE = "update"
+    ASSEMBLE = "assemble"
+    RECONFIGURE = "reconfigure"
+    BUILD = "build"
+    SETUP = "setup"
+    START = "start"
+    STOP = "stop"
+    RESTART = "restart"
+
+
+class ActionSignal(Signal):
+    def __init__(
+        self,
+        action: Action,
+        solution = None
+    ):
+        super().__init__()
+        self.action = action
+        self.solution = solution
