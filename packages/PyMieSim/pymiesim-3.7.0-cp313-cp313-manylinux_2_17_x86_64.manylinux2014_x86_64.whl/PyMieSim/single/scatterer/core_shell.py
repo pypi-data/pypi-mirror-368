f92a7f7100812ebf4b93b3bf715e6524968244fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:54 +0000

    Add package PyMieSim/3.7.0/pymiesim-3.7.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/PyMieSim/pymiesim-3.7.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PyMieSim/single/scatterer/core_shell.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy
+import pyvista
+from PyOptik.material.base_class import BaseMaterial
+
+from PyMieSim import units
+from PyMieSim.single.scatterer.base import BaseScatterer
+from PyMieSim.binary.interface_scatterer import CORESHELL
+from PyMieSim.single.source.base import BaseSource
+
+
+class CoreShell(CORESHELL, BaseScatterer):
+    """
+    Class representing a core/shell spherical scatterer.
+
+    Parameters
+    ----------
+    core_diameter : units.Quantity
+        Diameter of the core of the scatterer.
+    shell_thickness : units.Quantity
+        Thickness of the shell surrounding the core.
+    core_property : units.Quantity | BaseMaterial
+        Defines either the refractive index (`Quantity`) or material (`BaseMaterial`) of the scatterer's core. Only one can be provided.
+    shell_property : units.Quantity | BaseMaterial
+        Defines either the refractive index (`Quantity`) or material (`BaseMaterial`) of the scatterer's shell. Only one can be provided.
+
+    """
+
+    core_diameter: units.Quantity
+    shell_thickness: units.Quantity
+    core_property: units.Quantity | BaseMaterial
+    shell_property: units.Quantity | BaseMaterial
+    medium_property: units.Quantity | BaseMaterial
+    source: BaseSource
+
+    property_names = [
+        "size_parameter", "radius", "volume", "cross_section", "g",
+        "Qsca", "Qext", "Qabs", "Qback", "Qratio", "Qpr",
+        "Csca", "Cext", "Cabs", "Cback", "Cratio", "Cpr"
+    ]
+
+    def __init__(self,
+        core_diameter: units.Quantity,
+        shell_thickness: units.Quantity,
+        core_property: units.Quantity | BaseMaterial,
+        shell_property: units.Quantity | BaseMaterial,
+        medium_property: units.Quantity | BaseMaterial,
+        source: BaseSource):
+        """
+        Initialize the CoreShell scatterer with its core and shell properties.
+
+        Parameters
+        ----------
+        core_diameter : units.Quantity
+            Diameter of the core in meters.
+        shell_thickness : units.Quantity
+            Thickness of the shell in meters.
+        core_property : units.Quantity or BaseMaterial
+            Refractive index or material of the core.
+        shell_property : units.Quantity or BaseMaterial
+            Refractive index or material of the shell.
+        medium_property : units.Quantity or BaseMaterial
+            Refractive index or material of the surrounding medium.
+        source : BaseSource
+            Source object associated with the scatterer.
+        """
+        self.core_diameter = self._validate_units(core_diameter, dimension='distance', units=units.meter)
+        self.shell_thickness = self._validate_units(shell_thickness, dimension='distance', units=units.meter)
+        self.core_property = self._validate_property(core_property)
+        self.shell_property = self._validate_property(shell_property)
+        self.medium_property = self._validate_property(medium_property)
+
+        self.source = source
+        self.core_index, self.core_material = self._assign_index_or_material(self.core_property)
+        self.shell_index, self.shell_material = self._assign_index_or_material(self.shell_property)
+        self.medium_index, self.medium_material = self._assign_index_or_material(self.medium_property)
+
+        super().__init__(
+            core_diameter=core_diameter.to(units.meter).magnitude,
+            shell_thickness=shell_thickness.to(units.meter).magnitude,
+            core_refractive_index=self.core_index.to(units.RIU).magnitude,
+            shell_refractive_index=self.shell_index.to(units.RIU).magnitude,
+            medium_refractive_index=self.medium_index.to(units.RIU).magnitude,
+            source=self.source
+        )
+
+    @property
+    def radius(self) -> units.Quantity:
+        """Return the outer radius of the scatterer."""
+        return self.core_diameter / 2 + self.shell_thickness
+
+    @property
+    def volume(self) -> units.Quantity:
+        """Return the volume of the scatterer."""
+        vol = (4/3) * numpy.pi * (self.radius ** 3)
+        return vol.to(units.meter ** 3)
+
+    def _add_to_3d_ax(self, scene: pyvista.Plotter, color: str = 'black', opacity: float = 1.0) -> None:
+        """
+        Adds a 3D cone representation to the given PyVista plotting scene.
+
+        The cone represents the acceptance angle determined by the numerical aperture (NA) of the system.
+        The cone is positioned at the origin and points downward along the z-axis.
+
+        Parameters
+        ----------
+        scene : pyvista.Plotter
+            The 3D plotting scene to which the cone will be added.
+        color : str
+            The color of the cone mesh. Default is 'red'.
+        opacity : float
+            The opacity of the cone mesh. Default is 0.8.
+
+        """
+        # Create the cone mesh
+        sphape = pyvista.Sphere(
+            center=(0.0, 0.0, 0.0),
+            radius=0.1,
+            theta_resolution=100,
+            phi_resolution=100
+        )
+
+        # Add the cone mesh to the scene
+        scene.add_mesh(sphape, color=color, opacity=opacity)
