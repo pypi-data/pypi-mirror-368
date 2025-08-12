3da93e1da269fa18fdf10ce2d74be1eddc3af80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 17:06:57 +0000

    Add package xloft/0.1.17/xloft-0.1.17-py3-none-any.whl

---
+++ packages/xloft/xloft-0.1.17-py3-none-any.whl/xloft/__init__.py
@@ -0,0 +1,10 @@
+"""(XLOFT) X-Library of tools.
+
+Modules exported by this package:
+
+- `NamedTuple`: Class imitates the behavior of the _named tuple_.
+"""
+
+__all__ = ("NamedTuple",)
+
+from xloft.namedtuple import NamedTuple
