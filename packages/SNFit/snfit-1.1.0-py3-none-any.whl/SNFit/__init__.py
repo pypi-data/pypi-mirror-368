931370e6c0ef65766732defc5f429f201f500691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:25 +0000

    Add package SNFit/1.1.0/snfit-1.1.0-py3-none-any.whl

---
+++ packages/SNFit/snfit-1.1.0-py3-none-any.whl/SNFit/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+__version__ = "1.1.0"
+
+# set Python env variable to keep track of example data dir
+SNFit_dir = os.path.dirname(__file__)
+DATADIR = os.path.join(SNFit_dir, "example_data/")
+
+
