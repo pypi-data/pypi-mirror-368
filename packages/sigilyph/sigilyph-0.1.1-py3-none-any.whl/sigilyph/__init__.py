df796c1ac0dbfab0f7d723b3cd3f5c3e0f3bcecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 17:07:02 +0000

    Add package sigilyph/0.1.1/sigilyph-0.1.1-py3-none-any.whl

---
+++ packages/sigilyph/sigilyph-0.1.1-py3-none-any.whl/sigilyph/__init__.py
@@ -0,0 +1,14 @@
+'''
+FilePath: /python-Sigilyph/sigilyph/__init__.py
+Descripttion: 
+Author: Yixiang Chen
+version: 
+Date: 2025-05-13 11:01:07
+LastEditors: Yixiang Chen
+LastEditTime: 2025-08-12 17:49:48
+'''
+
+from sigilyph.core.sigilyph_class import Sigilyph
+
+from sigilyph.core.symbols import all_phone_dict
+
