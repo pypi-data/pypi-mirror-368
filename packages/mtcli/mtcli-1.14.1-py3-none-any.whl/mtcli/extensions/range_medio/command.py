3fac67dd54c8e1b79a4901c5b821b7c08362e730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 14:06:52 +0000

    Add package mtcli/1.14.1/mtcli-1.14.1-py3-none-any.whl

---
+++ packages/mtcli/mtcli-1.14.1-py3-none-any.whl/mtcli/extensions/range_medio/command.py
@@ -0,0 +1,30 @@
+"""Calcula o range médio das barras."""
+
+import click
+
+from . import conf
+from mtcli.models import model_rates
+from .models import model_average_range
+
+
+@click.command()
+@click.argument("symbol")
+@click.option(
+    "--period",
+    "-p",
+    type=click.Choice(conf.timeframes, case_sensitive=False),
+    default="D1",
+    help="Tempo grafico, default D1.",
+)
+@click.option("--count", "-c", default=10, help="Quantidade de barras, default 10.")
+def rm(symbol, period, count):
+    """Calcula o range medio das barras."""
+    rates = model_rates.RatesModel(symbol, period)
+    rates = rates.lista
+    rm = model_average_range.AverageRangeModel(rates, count)
+    rm = rm.average()
+    click.echo(rm)
+
+
+if __name__ == "__main__":
+    rm()
