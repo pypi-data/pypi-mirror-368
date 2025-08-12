6ad8cb8ff77cb3b8050d22b7b377b26e9dc7fa6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:30 +0000

    Add package vectorize-client/0.4.0/vectorize_client-0.4.0-py3-none-any.whl

---
+++ packages/vectorize-client/vectorize_client-0.4.0-py3-none-any.whl/vectorize_client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from vectorize_client.api.ai_platform_connectors_api import AIPlatformConnectorsApi
+from vectorize_client.api.destination_connectors_api import DestinationConnectorsApi
+from vectorize_client.api.extraction_api import ExtractionApi
+from vectorize_client.api.files_api import FilesApi
+from vectorize_client.api.pipelines_api import PipelinesApi
+from vectorize_client.api.source_connectors_api import SourceConnectorsApi
+from vectorize_client.api.uploads_api import UploadsApi
+from vectorize_client.api.workspaces_api import WorkspacesApi
+
