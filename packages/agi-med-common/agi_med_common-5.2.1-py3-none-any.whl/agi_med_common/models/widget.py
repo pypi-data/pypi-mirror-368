96f17af86509696ca0e07e9c41ae83ad1f1c2905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:28:50 +0000

    Add package agi-med-common/5.2.1/agi_med_common-5.2.1-py3-none-any.whl

---
+++ packages/agi-med-common/agi_med_common-5.2.1-py3-none-any.whl/agi_med_common/models/widget.py
@@ -0,0 +1,22 @@
+from typing import List, Self, Literal
+
+from pydantic import BaseModel, model_validator
+
+
+class Widget(BaseModel):
+    type: Literal["widget"] = "widget"
+    buttons: List[List[str]] | None = None
+    ibuttons: List[List[str]] | None = None
+
+    @model_validator(mode="after")
+    def check(self) -> Self:
+        if not self.buttons and not self.ibuttons:
+            raise ValueError("Empty widget is not allowed!")
+        if not self.ibuttons:
+            return self
+        for row in self.ibuttons:
+            for btn in row:
+                if ":" in btn:
+                    continue
+                raise ValueError(f"Expected buttons like `<callback>:<caption>`, found: {btn}")
+        return self
