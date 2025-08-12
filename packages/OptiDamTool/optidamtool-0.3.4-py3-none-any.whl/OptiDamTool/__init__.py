4a7b992db513b9f07f1e526590b706c08d5d36c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:28:24 +0000

    Add package OptiDamTool/0.3.4/optidamtool-0.3.4-py3-none-any.whl

---
+++ packages/OptiDamTool/optidamtool-0.3.4-py3-none-any.whl/OptiDamTool/__init__.py
@@ -0,0 +1,13 @@
+from .watemsedem import WatemSedem
+from .network import Network
+from .analysis import Analysis
+
+
+__all__ = [
+    'WatemSedem',
+    'Network',
+    'Analysis'
+]
+
+
+__version__ = '0.3.4'
