430493d7c74f728cfc4129e141f386db0c78ad36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 07:05:46 +0000

    Add package partis-bcr/1.0.5/partis_bcr-1.0.5-py3-none-any.whl

---
+++ packages/partis-bcr/partis_bcr-1.0.5-py3-none-any.whl/bin/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo -e "\n--> running $0"
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+basedir=`dirname $SCRIPT_DIR`  # go up one level
+
+echo -e "\n--> building ig-sw"
+cd $basedir/packages/ig-sw/src/ig_align/ && scons
+cd $basedir/
+
+echo -e "\n--> building ham"
+cd $basedir/packages/ham/ && scons bcrham
+cd $basedir/
+
+if [ "$*" == "with-simulation" ]; then
+    echo -e "\n--> building bpp-newlik (only used for simulation)"
+    cd $basedir/packages/bpp-newlik/ && ./install.sh  # the bpp-phyl step is really really incredibly slow
+    cd $basedir/
+fi
+
+echo -e "\n--> test"
+if which run-test.py; then
+    run-test.py --quick  # pip[x]
+else
+    ./test/test.py --quick  # from source/docker build
+fi
