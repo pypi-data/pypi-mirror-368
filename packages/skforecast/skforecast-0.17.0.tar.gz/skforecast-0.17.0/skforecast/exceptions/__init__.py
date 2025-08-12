7f1da0c77bde5736876caba248f6f60558c5e916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:30 +0000

    Add package skforecast/0.17.0/skforecast-0.17.0.tar.gz

---
+++ packages/skforecast/skforecast-0.17.0.tar.gz/skforecast-0.17.0/skforecast/exceptions/__init__.py
@@ -0,0 +1,17 @@
+from .exceptions import (
+    DataTypeWarning,
+    DataTransformationWarning,
+    IgnoredArgumentWarning,
+    IndexWarning,
+    InputTypeWarning,
+    LongTrainingWarning,
+    MissingExogWarning,
+    MissingValuesWarning,
+    OneStepAheadValidationWarning,
+    ResidualsUsageWarning,
+    UnknownLevelWarning,
+    SaveLoadSkforecastWarning,
+    SkforecastVersionWarning,
+    warn_skforecast_categories,
+    set_warnings_style
+)
