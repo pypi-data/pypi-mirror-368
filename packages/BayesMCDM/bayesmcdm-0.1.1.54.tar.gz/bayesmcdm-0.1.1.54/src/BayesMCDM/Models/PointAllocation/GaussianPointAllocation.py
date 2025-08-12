1c8766f9eab3606f084f1ea9d39cc07213c7b35e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:07:02 +0000

    Add package BayesMCDM/0.1.1.54/bayesmcdm-0.1.1.54.tar.gz

---
+++ packages/BayesMCDM/bayesmcdm-0.1.1.54.tar.gz/bayesmcdm-0.1.1.54/src/BayesMCDM/Models/PointAllocation/GaussianPointAllocation.py
@@ -0,0 +1,7 @@
+from ..SWING.GaussianSWING import GaussianSWING
+
+class GaussianPointAllocation(GaussianSWING):
+    """
+    This class is a direct alias of GaussianSWING for Point Allocation method.
+    """
+    pass
---
+++ packages/BayesMCDM/bayesmcdm-0.1.1.54.tar.gz/bayesmcdm-0.1.1.54/src/BayesMCDM/Visualizer/__init__.py
@@ -0,0 +1,5 @@
+from .CredalRanking import CredalRanking
+from .WeightVisualizer import WeightVisualizer
+from .ClusterVisualizer import ClusterVisualizer
+
+__all__ = ["CredalRanking", "WeightVisualizer", "ClusterVisualizer"]
