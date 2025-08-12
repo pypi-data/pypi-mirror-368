663656d152c2b58bca358952db45ef0845a8d862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:19 +0000

    Add package bafser/1.5.1/bafser-1.5.1.tar.gz

---
+++ packages/bafser/bafser-1.5.1.tar.gz/bafser-1.5.1/src/bafser/scripts/__init__.py
@@ -0,0 +1,5 @@
+from .add_user_role import add_user_role
+from .add_user import add_user
+from .alembic import alembic
+from .change_user_password import change_user_password
+from .remove_user_role import remove_user_role
