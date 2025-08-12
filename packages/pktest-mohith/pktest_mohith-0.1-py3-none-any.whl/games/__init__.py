dc4d4fc6d4061201b501979c8fb363fb99fb1cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 03:27:32 +0000

    Add package pktest-mohith/0.1/pktest_mohith-0.1-py3-none-any.whl

---
+++ packages/pktest-mohith/pktest_mohith-0.1-py3-none-any.whl/games/__init__.py
@@ -0,0 +1,6 @@
+from .cowbull import generateNum, noDuplicates, numOfBullsCows, getDigits
+from .guessnum import guess_number
+from .rock import play_game
+from .bulls_and_cows import *
+
+__all__ = ["generateNum", "noDuplicates", "numOfBullsCows", "getDigits", "guess_number", "play_game"]
---
+++ packages/pktest-mohith/pktest_mohith-0.1-py3-none-any.whl/games/bulls_and_cows.py
@@ -0,0 +1,6 @@
+# Bulls and Cows game implementation
+
+def play_game(game_name):
+    if game_name == 'bulls_and_cows':
+        print('Playing Bulls and Cows!')
+        # Add your game logic here
