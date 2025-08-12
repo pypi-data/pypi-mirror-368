f4146b651a95553ad19f9b52d1423498d7a60999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:07:07 +0000

    Add package pyioweather/0.0.22/pyioweather-0.0.22.tar.gz

---
+++ packages/pyioweather/pyioweather-0.0.22.tar.gz/pyioweather-0.0.22/pyioweather/main.py
@@ -0,0 +1,138 @@
+import os
+from os import getenv
+
+import requests
+from dotenv import load_dotenv
+from rich import print
+
+WEATHER_CODES = {
+    0: "Unknown",
+    1000: "Clear, Sunny",
+    1100: "Mostly Clear",
+    1101: "Partly Cloudy",
+    1102: "Mostly Cloudy",
+    1001: "Cloudy",
+    2000: "Fog",
+    2100: "Light Fog",
+    4000: "Drizzle",
+    4001: "Rain",
+    4200: "Light Rain",
+    4201: "Heavy Rain",
+    5000: "Snow",
+    5001: "Flurries",
+    5100: "Light Snow",
+    5101: "Heavy Snow",
+    6000: "Freezing Drizzle",
+    6001: "Freezing Rain",
+    6200: "Light Freezing Rain",
+    6201: "Heavy Freezing Rain",
+    7000: "Ice Pellets",
+    7101: "Heavy Ice Pellets",
+    7102: "Light Ice Pellets",
+    8000: "Thunderstorm",
+}
+
+
+def clear_screen():
+    # For Windows
+    if os.name == "nt":
+        _ = os.system("cls")
+    # For macOS and Linux
+    else:
+        _ = os.system("clear")
+
+
+def display_weather_art(condition):
+    if condition == WEATHER_CODES[1000] or condition == WEATHER_CODES[1100]:
+        print(
+            r"""[yellow]
+  \   /  
+   .-.   
+― (   ) ―
+   `-’   
+  /   \  
+
+[/yellow]"""
+        )
+    elif (
+        condition == WEATHER_CODES[1001]
+        or condition == WEATHER_CODES[1101]
+        or condition == WEATHER_CODES[1102]
+    ):
+        print(
+            r"""
+           .--.
+         .-(    ).
+        (___.__)__)
+        """
+        )
+
+    elif (
+        condition == WEATHER_CODES[4000]
+        or condition == WEATHER_CODES[4001]
+        or condition == WEATHER_CODES[4200]
+        or condition == WEATHER_CODES[4201]
+    ):
+        print(
+            r"""[blue]
+           .--.
+         .-(    ).
+        (___.__)__)
+          ʻ ʻ ʻ ʻ
+          ʻ ʻ ʻ ʻ
+        [/blue]"""
+        )
+    elif condition == WEATHER_CODES[8000]:
+        print(
+            r"""[blue]
+           .--.
+         .-(    ).
+        (___.__)__)
+           ⚡⚡
+          ʻ ʻ ʻ
+        [/blue]"""
+        )
+
+    else:
+        print("No ascii art.")
+
+
+def main():
+    # api key
+    load_dotenv()
+    TOMORROW_IO_KEY = getenv("TOMORROW_IO_API") or input(
+        "Enter your Tomorrow.io API Key: "
+    )
+
+    # location
+    location = input("Enter a location: ").lower()
+
+    # api call
+    url = f"https://api.tomorrow.io/v4/weather/realtime?location={location}"
+    headers = {
+        "content-type": "application/json",
+        "apikey": TOMORROW_IO_KEY,
+    }
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        clear_screen()
+
+        content = response.json()
+        values = content["data"]["values"]
+
+        print(f"Location:  {content['location']['name']}")
+
+        print(f"Current temperature: {values['temperature']}\u00b0C")
+
+        condition = WEATHER_CODES[values["weatherCode"]]
+        print(f"Current conditions: {condition.lower()}.")
+        display_weather_art(condition)
+    elif response.status_code == 400:
+        print("Invalid Location.")
+    else:
+        print(f"Error: {response}")
+
+
+if __name__ == "__main__":
+    main()
