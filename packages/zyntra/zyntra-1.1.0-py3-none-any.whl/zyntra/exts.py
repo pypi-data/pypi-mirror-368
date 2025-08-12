4be9d11ba670ffb6c7d36b3b743208460c3873cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:06:38 +0000

    Add package zyntra/1.1.0/zyntra-1.1.0-py3-none-any.whl

---
+++ packages/zyntra/zyntra-1.1.0-py3-none-any.whl/zyntra/exts.py
@@ -0,0 +1,44 @@
+"""
+Here you can find most used file extensions\n
+sotred `sets` for better preformance
+"""
+
+
+EXT_TEXT = {
+    "txt", "md", "log", "csv", "tsv", "json", "xml", "yaml", "yml", "ini", "conf"
+}
+
+EXT_DOCS = {
+    "pdf", "doc", "docx", "odt", "rtf", "tex", "ppt", "pptx", "xls", "xlsx", "ods"
+}
+
+EXT_IMAGES = {
+    "jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif",
+    "webp", "ico", "svg", "heif", "heic", "avif"
+}
+
+EXT_AUDIO = {
+    "mp3", "wav", "ogg", "flac", "aac", "m4a", "wma", "aiff", "opus", "mid", "midi"
+}
+
+EXT_VIDEO = {
+    "mp4", "mkv", "webm", "mov", "avi", "flv", "wmv", "mpeg", "mpg", "m4v", "3gp"
+}
+
+EXT_ARCHIVES = {
+    "zip", "tar", "gz", "rar", "7z", "bz2", "xz", "iso", "cab"
+}
+
+EXT_CODE = {
+    "py", "pyw", "c", "cpp", "h", "hpp", "java", "class", "jar", "js", "ts",
+    "html", "htm", "css", "scss", "php", "rb", "go", "rs", "swift", "kt", "sh",
+    "bat", "pl", "lua", "sql", "jsonc"
+}
+
+EXT_DATABASE = {
+    "db", "sqlite", "sqlite3", "sql", "mdb", "accdb", "dbf"
+}
+
+EXT_EXECUTABLES = {
+    "exe", "msi", "bat", "cmd", "sh", "app", "apk", "deb", "rpm"
+}
