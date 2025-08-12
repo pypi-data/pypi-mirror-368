fd6ce547d19b1a09b6fde9ec7056131b862760fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 16:06:28 +0000

    Add package my-package-atharva98/0.1/my_package_atharva98-0.1.tar.gz

---
+++ packages/my-package-atharva98/my_package_atharva98-0.1.tar.gz/my_package_atharva98-0.1/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+with open('README.md', 'r') as f:
+    description = f.read()
+setup(
+    name='my_package_atharva98',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=description,
+    long_description_content_type='text/markdown'
+)
+    
