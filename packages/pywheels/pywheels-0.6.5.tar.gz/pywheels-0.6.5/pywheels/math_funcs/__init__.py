88b4f77847be9e9c438badc15abed720a82100f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:10 +0000

    Add package pywheels/0.6.5/pywheels-0.6.5.tar.gz

---
+++ packages/pywheels/pywheels-0.6.5.tar.gz/pywheels-0.6.5/pywheels/math_funcs/__init__.py
@@ -0,0 +1,10 @@
+from .stat import chi_squared
+from .stat import mean_squared_error
+from .integral import integral_1d_func
+
+
+__all__ = [
+    "chi_squared",
+    "mean_squared_error",
+    "integral_1d_func",
+]
