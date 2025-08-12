6bf8347b41ec4fa053d18a600ef69b75945a01d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:06:01 +0000

    Add package safep/0.1.4/safep-0.1.4.tar.gz

---
+++ packages/safep/safep-0.1.4.tar.gz/safep-0.1.4/safep/TI.py
@@ -0,0 +1,159 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scipy.integrate
+
+
+def process_TI(dataTI, restraint, Lsched):
+    '''
+    Arguments: the TI data, restraint, and lambda schedule
+    Function: Calculate the free energy for each lambda value, aggregate the result, and estimate the error
+    Returns: The free energies and associated errors as functions of lambda. Both per window and cumulative.
+    '''
+    dUs = {}
+    for key, group in dataTI.groupby('L'):
+        dUs[key] = [harmonicWall_dUdL(restraint, coord, key) for coord in group.DBC]
+
+    Lsched = np.sort(list(dUs.keys())) # FIXME parameter Lsched is ignored and overwritten
+    dL = Lsched[1] - Lsched[0]         # FIXME assumes that the lambda schedule is uniformly spaced
+    TIperWindow = pd.DataFrame(index=Lsched)
+    TIperWindow['dGdL'] = [np.mean(dUs[L]) for L in Lsched]
+    TIperWindow['error'] = [np.std(dUs[L]) for L in Lsched]
+
+    if Lsched[-1] < 1.0:
+        # TODO test lambdaExponent >= 2
+        lastPoint = pd.DataFrame({
+            "dGdL": pd.Series([0.0], index=[1.0]),
+            'error': pd.Series([0.0], index=[1.0])
+        })
+        TIperWindow = pd.concat([TIperWindow, lastPoint])
+        Lsched = np.concatenate([Lsched, [1.0]])
+
+    TIcumulative = pd.DataFrame()
+    TIcumulative['dG'] = scipy.integrate.cumulative_trapezoid(TIperWindow.dGdL, x=Lsched, initial=0)
+    TIcumulative.set_index(Lsched, inplace=True)
+
+    # Estimate square error for trapezoid rule by averaging errors in neighboring bins
+    sq_error = np.array(TIperWindow.error**2)
+    sq_error = 0.5 * (sq_error[1:] + sq_error[:-1])
+    error = np.sqrt(np.cumsum(sq_error * dL**2))
+    TIcumulative['error'] = np.concatenate([[0.0], error]) # Include initial 0 error
+
+    return TIperWindow, TIcumulative
+
+
+def plot_TI(cumulative, perWindow, width=8, height=4, PDFtype='KDE', hystLim=(-1,1), color='#0072B2', fontsize=12):
+    fig, (cumAx,eachAx) = plt.subplots(2,1, sharex='col')
+
+    # Cumulative change in kcal/mol
+    cumAx.errorbar(cumulative.index, cumulative.dG, yerr=cumulative.error,marker=None, linewidth=1, color=color, label='Cumulative Change')
+    finalEstimate = cumulative.dG[1]
+    cumAx.axhline(finalEstimate, linestyle='-', color='gray', label=f'Final Value:\n{np.round(finalEstimate,1)}kcal/mol')
+    cumAx.legend(fontsize=fontsize*0.75)                  
+    cumAx.set_ylabel(r'Cumulative $\rm\Delta G_{\lambda}$'+'\n(kcal/mol)', fontsize=fontsize)
+
+    # Per-window change in kcal/mol
+    eachAx.errorbar(perWindow.index, perWindow.dGdL, marker=None, linewidth=1, yerr=perWindow.error, color=color)
+    eachAx.set_ylabel(r'$\rm\Delta G_{\lambda}$'+'\n(kcal/mol)', fontsize=fontsize)
+
+    eachAx.set_xlabel(r'$\lambda$', fontsize=fontsize)
+
+    fig.set_figwidth(width)
+    fig.set_figheight(height*3)
+    fig.tight_layout()
+    
+    return fig, [cumAx,eachAx] 
+
+def make_harmonicWall(FC=10, targetFC=0, targetFE=1, upperWalls=1, schedule=None, numSteps=1000, targetEQ=500, name='HW', lowerWalls=None, lambdaExp=1., decoupling=None):
+    '''
+    This method should rarely be used, as long as a log file exists that can be parsed by parse_Colvars_log
+    and the result fed to make_harmonicWall_from_Colvars
+    '''
+
+    # Heuristic to set the default value of the decoupling parameter
+    if decoupling is None:
+        decoupling = (targetFC == 0 and FC != 0)
+
+    HW = {'name':name, 'targetForceConstant':targetFC, 'lambdaExponent':targetFE, 'forceConstant':FC, 'upperWalls':upperWalls, 'schedule':schedule,
+           'targetNumSteps':numSteps, 'targetEquilSteps':targetEQ, 'lowerWalls':lowerWalls, 'lambdaExp':lambdaExp, 'decoupling':decoupling}
+
+    return HW
+
+def make_harmonicWall_from_Colvars(restraint_conf):
+    '''
+    Input: a restraint configuration as a dict produced by parse_Colvars_log
+    '''
+
+    if (restraint_conf['key'] != 'harmonicwalls'):
+        keyword = restraint_conf['key']
+        print(f'Error: bias is not a harmonic wall (keyword: {keyword})')
+        return None
+
+    CVs = restraint_conf['colvars'].strip("{}").replace(",", "").split()
+    if len(CVs) != 1:
+        raise RuntimeError (f'Error: bias does not act on exactly one colvar (cvs: {CVs})')
+
+    HW = {  'name': restraint_conf['name'],
+            'colvar': CVs[0],
+            'forceConstant': float(restraint_conf['forceConstant']),
+            'targetForceConstant': float(restraint_conf['targetForceConstant']),
+            'lowerWalls': float(restraint_conf['lowerWalls'].strip('{ }')),
+            'upperWalls': float(restraint_conf['upperWalls'].strip('{ }')),
+            'targetNumSteps': int(restraint_conf['targetNumSteps']),
+            'targetNumStages': int(restraint_conf['targetNumStages']),
+            'targetEquilSteps': int(restraint_conf['targetEquilSteps']),
+            'decoupling': restraint_conf['decoupling'] in ['on', 'true', 'yes']} # interpret as Boolean
+    # Support legacy keyword
+    if 'targetForceExponent' in restraint_conf:
+        HW['lambdaExponent'] = int(restraint_conf['targetForceExponent'])
+    if 'lambdaExponent' in restraint_conf:
+        HW['lambdaExponent'] = int(restraint_conf['lambdaExponent'])
+    cleaned = restraint_conf['lambdaSchedule'].strip("{}").replace(",", "")
+    if len(cleaned.split()) > 0:
+        HW['lambdaSchedule'] = [float(num) for num in cleaned.split()]
+    else:
+        HW['lambdaSchedule'] = np.linspace(0, 1, HW['targetNumStages'] + 1)
+    return HW
+
+def harmonicWall_U(HW, coord, L):
+    d=0
+    if HW['upperWalls'] and coord>HW['upperWalls']:
+        d = coord-HW['upperWalls']
+    elif HW['lowerWalls'] and coord<HW['lowerWalls']:
+        d = coord-HW['lowerWalls']
+    
+    if d!=0:
+        alpha = HW['lambdaExponent']
+        if HW['decoupling']:
+            kL = (1.-L)**alpha * HW['forceConstant']
+        else:
+            dk = HW['targetForceConstant']-HW['forceConstant']
+            la = L**alpha
+            kL = HW['forceConstant']+la*dk
+
+        U = 0.5*kL*(d**2)
+    else:
+        U=0
+    return U
+
+def harmonicWall_dUdL(HW, coord, L):
+    d=0
+    if HW['upperWalls'] and coord>HW['upperWalls']:
+        d = coord-HW['upperWalls']
+    elif HW['lowerWalls'] and coord<HW['lowerWalls']:
+        d = coord-HW['lowerWalls']
+
+    if d!=0:
+        alpha = HW['lambdaExponent']
+        if HW['decoupling']:
+            dkL = -alpha * (1.-L)**(alpha-1) * HW['forceConstant']
+        else:
+            dk = HW['targetForceConstant']-HW['forceConstant']
+            dla = alpha*L**(alpha-1)
+            dkL = dla*dk
+
+        dU = 0.5*dkL*(d**2)
+    else:
+        dU=0
+    return dU
---
+++ packages/safep/safep-0.1.4.tar.gz/safep-0.1.4/safep/estimators.py
@@ -0,0 +1,142 @@
+# Import block
+import re
+from glob import glob  #file regexes
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from alchemlyb.estimators import BAR
+from alchemlyb.parsing import namd
+from alchemlyb.visualisation import plot_convergence
+from alchemlyb.visualisation.dF_state import plot_dF_state
+from natsort import natsorted  #for sorting "naturally" instead of alphabetically
+from numpy.lib.stride_tricks import sliding_window_view
+from scipy.optimize import curve_fit as scipyFit
+from scipy.special import erfc
+from scipy.stats import linregress as lr
+from scipy.stats import norm, skew
+from tqdm import tqdm  #for progress bars
+
+from .helpers import *
+
+
+def get_exponential(u_nk, defrag_frequency=100):
+    '''
+    Get exponential estimation of the change in free energy.
+    Arguments: u_nk in alchemlyb format
+    Returns: l[ambdas], l_mid [lambda window midpoints], dG_f [forward estimates], dG_b [backward estimates]
+    ''' 
+
+    groups = u_nk.groupby(level=1)
+    dG=pd.DataFrame([])
+
+    i = 1
+    for name, group in groups:
+        dG[name] = np.log(np.mean(np.exp(-1*group), axis=0))
+        i+=1
+        if i == defrag_frequency:
+            dG = dG.copy()
+            i=1
+
+    dG_f=np.diag(dG, k=1)
+    dG_b=np.diag(dG, k=-1)
+
+    l=dG.columns.to_list()
+    l_mid = np.mean([l[1:],l[:-1]], axis=0)
+
+    return l, l_mid, dG_f, dG_b
+    
+def get_BAR(bar):
+    '''
+    Extract key information from an alchemlyb.BAR object. Useful for plotting.
+    Arguments: a fitted BAR object
+    Returns: l[ambdas], l_mid [lambda window midpoints], f [the cumulative free energy], df [the per-window free energy], ddf [the per-window errors], errors [the cumulative error]
+    '''
+    
+    states = bar.states_
+
+    f = bar.delta_f_.iloc[0,:] # dataframe
+    l = np.array([float(s) for s in states])
+    # lambda midpoints for each window
+    l_mid = 0.5*(l[1:] + l[:-1])
+
+    # FE differences are off diagonal
+    df = np.diag(bar.delta_f_, k=1)
+
+    # error estimates are off diagonal
+    ddf = np.array([bar.d_delta_f_.iloc[i, i+1] for i in range(len(states)-1)])
+
+    # Accumulate errors as sum of squares
+    errors = np.array([np.sqrt((ddf[:i]**2).sum()) for i in range(len(states))])
+    
+    
+    return l, l_mid, f, df, ddf, errors
+    
+def do_estimation(u_nk, method='both'):
+    '''
+    Run both exponential and BAR estimators and return the results in tidy dataframes.
+    Arguments: u_nk in the alchemlyb format, method of fitting (String: BAR, EXP, or both)
+    Returns: perWindow estimates (including errors), cumulative estimates (including errors)
+    '''
+    u_nk = u_nk.sort_index(level=1)
+    cumulative = pd.DataFrame()
+    perWindow = pd.DataFrame()
+    if method=='both' or method=='BAR':
+        bar = BAR()
+        bar.fit(u_nk)
+        ls, l_mids, fs, dfs, ddfs, errors = get_BAR(bar)
+        
+        cumulative[('BAR', 'f')] = fs
+        cumulative[('BAR', 'errors')] = errors
+        cumulative.index = ls
+
+        perWindow[('BAR','df')] = dfs
+        perWindow[('BAR', 'ddf')] = ddfs
+        perWindow.index = l_mids
+        
+    if method=='both' or method=='EXP':
+        expl, expmid, dG_fs, dG_bs = get_exponential(u_nk)
+
+        cumulative[('EXP', 'ff')] = np.insert(np.cumsum(dG_fs),0,0)
+        cumulative[('EXP', 'fb')] = np.insert(-np.cumsum(dG_bs),0,0)
+        cumulative.index = expl 
+        
+        perWindow[('EXP','dG_f')] = dG_fs
+        perWindow[('EXP','dG_b')] = dG_bs
+        perWindow[('EXP', 'difference')] = np.array(dG_fs)+np.array(dG_bs)        
+        perWindow.index = expmid
+        
+    
+    perWindow.columns = pd.MultiIndex.from_tuples(perWindow.columns)
+    perWindow = perWindow.fillna(0)
+    cumulative.columns = pd.MultiIndex.from_tuples(cumulative.columns)
+    cumulative = cumulative.fillna(0)
+    
+    return perWindow.copy(), cumulative.copy()
+   
+    
+#Light-weight exponential estimator. Requires alternative parser.
+def get_dG_from_data(data, temperature):
+    from scipy.constants import R, calorie
+    beta = 1/(R/(1000*calorie) * temperature) #So that the final result is in kcal/mol
+    
+    groups = data.groupby(level=0)
+    dG=[]
+    for name, group in groups:
+        isUp = group.up
+        dE = group.dE
+        toAppend = [name, -1*np.log(np.mean(np.exp(-beta*dE[isUp]))), 1]
+        dG.append(toAppend)
+        toAppend=[name, -1*np.log(np.mean(np.exp(-beta*dE[~isUp]))), 0]
+        dG.append(toAppend)
+    
+    dG = pd.DataFrame(dG, columns=["window", "dG", "up"])
+    dG = dG.set_index('window')
+    
+    dG_f = dG.loc[dG.up==1] 
+    dG_b = dG.loc[dG.up==0]
+
+    dG_f = dG_f.dG.dropna()
+    dG_b = dG_b.dG.dropna()
+
+    return dG_f, dG_b
