08e57018933fb794c809c3fd5b74d14c341a87c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:06:39 +0000

    Add package exccpkg/3.0.0/exccpkg-3.0.0.tar.gz

---
+++ packages/exccpkg/exccpkg-3.0.0.tar.gz/exccpkg-3.0.0/setup.py
@@ -0,0 +1,15 @@
+# build: python3 -m build
+# upload: twine upload --repository pypi dist/*
+from setuptools import setup
+
+setup(
+    name = 'exccpkg',
+    version = '3.0.0',
+    # shutil.rmtree onexc https://docs.python.org/3/library/shutil.html#shutil.rmtree
+    python_requires='>=3.12',
+    description = 'An explicit C++ package builder.',
+    author = 'AdjWang',
+    author_email = 'wwang230513@gmail.com',
+    packages = ['exccpkg'],
+    install_requires = ['requests'],
+)
