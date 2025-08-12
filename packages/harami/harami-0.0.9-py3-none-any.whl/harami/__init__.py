6098e4a3df654643618913b8c0221a986b140ec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:06:38 +0000

    Add package harami/0.0.9/harami-0.0.9-py3-none-any.whl

---
+++ packages/harami/harami-0.0.9-py3-none-any.whl/harami/__init__.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: Copyright (C) Shaun Wilson
+# SPDX-License-Identifier: MIT
+
+from .EventArgs import EventArgs
+from .EventHandler import EventHandler
+from .EventSource import EventSource, event
+from .Observable import Observable
+from .Observer import Observer
+
+__version__ = '0.0.9'
+__commit__ = '170ce78'
+
+__all__ = [
+    '__version__', '__commit__',
+    'EventArgs',
+    'EventHandler',
+    'EventSource',
+    'event',
+    'Observable',
+    'Observer'
+]
