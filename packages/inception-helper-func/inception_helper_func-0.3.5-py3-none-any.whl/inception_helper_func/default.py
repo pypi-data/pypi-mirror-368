8aff5461236577ce01e63c300c65031891348982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 07:05:57 +0000

    Add package inception-helper-func/0.3.5/inception_helper_func-0.3.5-py3-none-any.whl

---
+++ packages/inception-helper-func/inception_helper_func-0.3.5-py3-none-any.whl/inception_helper_func/default.py
@@ -0,0 +1,11 @@
+def get_default_profile_photo() -> dict:
+    return {
+        "id": "default",
+        "key": "default",
+        "filename": "default.png",
+        "unique_filename": "default.png",
+        "url": "https://cdn.inceptionforce.com/png/df/default.png",
+        "content_type": "image/png",
+        "extension": "png",
+        "size": "100KB",
+    }
