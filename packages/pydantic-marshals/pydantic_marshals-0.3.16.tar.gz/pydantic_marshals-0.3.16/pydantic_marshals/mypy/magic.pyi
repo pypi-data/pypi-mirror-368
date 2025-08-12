daf9eefe90f67a0dc3db3b83e9bb8743371274b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 06:05:16 +0000

    Add package pydantic-marshals/0.3.16/pydantic_marshals-0.3.16.tar.gz

---
+++ packages/pydantic-marshals/pydantic_marshals-0.3.16.tar.gz/pydantic_marshals-0.3.16/pydantic_marshals/mypy/magic.pyi
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from collections.abc import Sequence
+from typing import Self
+
+from pydantic import BaseModel
+
+from pydantic_marshals.base.fields.properties import PropertyType
+from pydantic_marshals.base.models import MarshalModel
+from pydantic_marshals.sqlalchemy.fields.columns import ColumnType
+from pydantic_marshals.sqlalchemy.fields.relationships import RelationshipType
+
+class MappedModelStub:
+    @classmethod
+    def extend(
+        cls,
+        columns: Sequence[ColumnType] = (),
+        relationships: Sequence[RelationshipType] = (),
+        properties: Sequence[PropertyType] = (),
+        bases: Sequence[type[BaseModel]] = (),
+        includes: Sequence[MarshalModel | MappedModelStub] = (),
+    ) -> Self: ...
+    @classmethod
+    def as_patch(cls) -> Self: ...
