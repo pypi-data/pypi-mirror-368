2ea2ae0e6a00c88bc1e2b071549744ef4a2a3eac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 07:05:49 +0000

    Add package mcp-airflow-api/0.0.5/mcp_airflow_api-0.0.5-py3-none-any.whl

---
+++ packages/mcp-airflow-api/mcp_airflow_api-0.0.5-py3-none-any.whl/mcp_airflow_api/functions.py
@@ -0,0 +1,12 @@
+"""
+Airflow MCP auxiliary utility functions definition file
+"""
+import os
+import requests
+
+def airflow_request(method, url, **kwargs):
+    user = os.getenv("AIRFLOW_API_USERNAME")
+    password = os.getenv("AIRFLOW_API_PASSWORD")
+    headers = kwargs.pop("headers", {})
+    auth = (user, password) if user and password else None
+    return requests.request(method, url, headers=headers, auth=auth, **kwargs)
