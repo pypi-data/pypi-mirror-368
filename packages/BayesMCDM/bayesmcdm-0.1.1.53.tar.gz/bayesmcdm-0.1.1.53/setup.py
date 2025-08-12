8bafcc74e3eafae6cf5c6a380822cc7274e745c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 15:07:12 +0000

    Add package BayesMCDM/0.1.1.53/bayesmcdm-0.1.1.53.tar.gz

---
+++ packages/BayesMCDM/bayesmcdm-0.1.1.53.tar.gz/bayesmcdm-0.1.1.53/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="BayesMCDM",
+    version="0.1.1.53",
+    description="Bayesian Multi-Criteria Decision Making Toolkit",
+    author="Majid Mohammadi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "pystan",  
+    ],
+)   
