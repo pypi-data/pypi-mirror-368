3d62e29de477e281be8d8ede47340c563b6501a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-12 04:10:41 +0000

    Add package mais/0.2.5/mais-0.2.5-py3-none-any.whl

---
+++ packages/mais/mais-0.2.5-py3-none-any.whl/mais/application/services/__init__.py
@@ -0,0 +1,5 @@
+"""Application services."""
+
+from .model_analysis_service import ModelAnalysisService
+
+__all__ = ["ModelAnalysisService"]
---
+++ packages/mais/mais-0.2.5-py3-none-any.whl/mais/domain/model_analysis/value_objects/__init__.py
@@ -0,0 +1,6 @@
+"""Value objects for model analysis domain."""
+
+from .model_id import ModelID
+from .risk_level import RiskLevel
+
+__all__ = ["ModelID", "RiskLevel"]
