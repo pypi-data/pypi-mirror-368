303b6dab4b779615a83881e5b4d4274a117d0364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:12:29 +0000

    Add package jpcorpreg/1.7.0/jpcorpreg-1.7.0-py3-none-any.whl

---
+++ packages/jpcorpreg/jpcorpreg-1.7.0-py3-none-any.whl/jpcorpreg/__init__.py
@@ -0,0 +1,6 @@
+"""
+Corporate Number Parser Library
+~~~~~~~~~~~~~~~~~~~~~
+jpcorpreg is a simple scraping library for Corporate Number Publication Site
+"""
+from jpcorpreg.load import load, read_csv
