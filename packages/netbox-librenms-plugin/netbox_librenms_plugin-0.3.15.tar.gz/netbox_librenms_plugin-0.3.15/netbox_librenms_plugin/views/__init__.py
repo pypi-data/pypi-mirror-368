4c750648a1afbe800afd08f022394a3ebb04df17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:12:30 +0000

    Add package netbox-librenms-plugin/0.3.15/netbox_librenms_plugin-0.3.15.tar.gz

---
+++ packages/netbox-librenms-plugin/netbox_librenms_plugin-0.3.15.tar.gz/netbox_librenms_plugin-0.3.15/netbox_librenms_plugin/views/__init__.py
@@ -0,0 +1,40 @@
+"""
+Module for initializing views for the NetBox LibreNMS plugin.
+"""
+
+# Import views as needed
+from .base.cables_view import BaseCableTableView, SingleCableVerifyView
+from .base.interfaces_view import BaseInterfaceTableView
+from .base.ip_addresses_view import BaseIPAddressTableView, SingleIPAddressVerifyView
+from .base.librenms_sync_view import BaseLibreNMSSyncView
+
+from .mapping_views import (
+    InterfaceTypeMappingBulkDeleteView,
+    InterfaceTypeMappingChangeLogView,
+    InterfaceTypeMappingCreateView,
+    InterfaceTypeMappingDeleteView,
+    InterfaceTypeMappingEditView,
+    InterfaceTypeMappingListView,
+    InterfaceTypeMappingView,
+)
+from .status_check import (
+    DeviceStatusListView,
+    VMStatusListView,
+)
+from .sync.cables_sync import SyncCablesView
+from .sync.cleanup_interfaces import DeleteNetBoxInterfacesView
+from .sync.devices_sync import AddDeviceToLibreNMSView, UpdateDeviceLocationView
+from .sync.interfaces_sync import SyncInterfacesView
+from .sync.locations_sync import SyncSiteLocationView
+from .sync.ipaddresses_sync import SyncIPAddressesView
+
+from .device_views import (
+    DeviceCableTableView,
+    DeviceInterfaceTableView,
+    DeviceIPAddressTableView,
+    DeviceLibreNMSSyncView,
+    SingleInterfaceVerifyView,
+)
+
+from .vm_views import VMInterfaceTableView, VMIPAddressTableView, VMLibreNMSSyncView
+from .settings_views import LibreNMSSettingsView, TestLibreNMSConnectionView
