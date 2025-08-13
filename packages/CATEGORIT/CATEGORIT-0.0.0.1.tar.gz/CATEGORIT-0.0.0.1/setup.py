3096ffe002192e38944045f39bbbcea0b05b53d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:12:17 +0000

    Add package CATEGORIT/0.0.0.1/CATEGORIT-0.0.0.1.tar.gz

---
+++ packages/CATEGORIT/CATEGORIT-0.0.0.1.tar.gz/CATEGORIT-0.0.0.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+# Configuration
+NAME = "CATEGORIT"
+VERSION = "0.0.0.1"
+
+INSTALL_REQUIRES = [
+
+]
+
+AUTHOR = ""
+AUTHOR_EMAIL = ""
+URL = ""
+DESCRIPTION = ""
+LICENSE = ""
+KEYWORDS = ["orange3 add-on",]
+
+PACKAGES = find_packages()
+PACKAGES = [pack for pack in PACKAGES if "orangecontrib" in pack and "ALGORITHM" in pack]
+PACKAGES.append("orangecontrib")
+print("####", PACKAGES)
+
+PACKAGE_DATA = {
+    "orangecontrib.ALGORITMH.widgets": ["icons/*", "designer/*"],
+}
+
+ENTRY_POINTS = {
+    "orange.widgets": (
+        "AAIT - ALGORITMH = orangecontrib.ALGORITHM.widgets",
+    )
+}
+
+NAMESPACE_PACKAGES = ["orangecontrib"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    packages=PACKAGES,
+    package_data=PACKAGE_DATA,
+    install_requires=INSTALL_REQUIRES,
+    entry_points=ENTRY_POINTS,
+    namespace_packages=NAMESPACE_PACKAGES,
+)
