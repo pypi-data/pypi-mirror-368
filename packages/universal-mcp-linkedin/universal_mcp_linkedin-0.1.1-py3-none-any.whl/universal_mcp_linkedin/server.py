2725998f1f2a6b4c6a8089682aad36b3c98a1afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:28:59 +0000

    Add package universal-mcp-linkedin/0.1.1/universal_mcp_linkedin-0.1.1-py3-none-any.whl

---
+++ packages/universal-mcp-linkedin/universal_mcp_linkedin-0.1.1-py3-none-any.whl/universal_mcp_linkedin/server.py
@@ -0,0 +1,20 @@
+
+from universal_mcp.servers import SingleMCPServer
+from universal_mcp.integrations import AgentRIntegration
+from universal_mcp.stores import EnvironmentStore
+
+from universal_mcp_linkedin.app import LinkedinApp
+
+env_store = EnvironmentStore()
+# "name" used in AgentRIntegration should match the actual name from the backend
+integration_instance = AgentRIntegration(name="linkedin", store=env_store, api_key="sk_416e4f88-3beb-4a79-a0ef-fb1d2c095aee", base_url="https://api.agentr.dev")
+app_instance = LinkedinApp(integration=integration_instance)
+
+mcp = SingleMCPServer(
+    app_instance=app_instance,
+)
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
+
+
