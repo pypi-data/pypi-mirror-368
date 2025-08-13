51832f11467c3f236aa5904cfaef8a31277a0c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:07:40 +0000

    Add package verahession/0.1.28/verahession-0.1.28.tar.gz

---
+++ packages/verahession/verahession-0.1.28.tar.gz/verahession-0.1.28/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="verahession",
+    version="0.1.28",
+    author="Jack Hession",
+    author_email="support@hessiondynamics.com",
+    description="Python client library for Vera AI chatbot by Hession Dynamics",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/hession-dynamics/verahession",
+    packages=find_packages(where=".", include=["verahession", "verahession.*"]),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        "requests>=2.20.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    license="MIT",
+)
---
+++ packages/verahession/verahession-0.1.28.tar.gz/verahession-0.1.28/verahession/api/__init__.py
@@ -0,0 +1,5 @@
+from .client import vera_interface
+from .tts import tts_interface
+
+__all__ = ['vera_interface']
+
