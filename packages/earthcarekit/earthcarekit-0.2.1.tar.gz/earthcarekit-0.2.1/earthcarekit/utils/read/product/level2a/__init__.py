24145a030913853d61c9bcf116dbcb41c1f60b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:08:04 +0000

    Add package earthcarekit/0.2.1/earthcarekit-0.2.1.tar.gz

---
+++ packages/earthcarekit/earthcarekit-0.2.1.tar.gz/earthcarekit-0.2.1/earthcarekit/utils/read/product/level2a/__init__.py
@@ -0,0 +1,6 @@
+from .atl_aer_2a import read_product_aaer
+from .atl_ald_2a import read_product_aald
+from .atl_atc_2a import read_product_atc
+from .atl_cla_2a import read_product_acla
+from .atl_cth_2a import read_product_acth
+from .atl_ebd_2a import read_product_aebd
