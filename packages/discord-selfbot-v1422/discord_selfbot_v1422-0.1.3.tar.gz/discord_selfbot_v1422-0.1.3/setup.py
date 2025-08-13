54241d936243ce1d24252fa7eefe06fd9d1bc00b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:07:05 +0000

    Add package discord-selfbot-v1422/0.1.3/discord_selfbot_v1422-0.1.3.tar.gz

---
+++ packages/discord-selfbot-v1422/discord_selfbot_v1422-0.1.3.tar.gz/discord_selfbot_v1422-0.1.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="discord-selfbot-v1422",          # Your package name on PyPI
+    version="0.1.3",                       # Incremented version number
+    author="auth",
+    author_email="bisects@proton.me",
+    description="wrd",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["package", "package.*"]),
+    package_data={
+        "package": ["__init__.py", "module.py"],  # Explicitly include package files
+    },
+    include_package_data=True,  # Ensure package_data is included
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
