9206565861f843ed2252dbee043ce0d5aafc8be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:12:30 +0000

    Add package netbox-librenms-plugin/0.3.15/netbox_librenms_plugin-0.3.15-py3-none-any.whl

---
+++ packages/netbox-librenms-plugin/netbox_librenms_plugin-0.3.15-py3-none-any.whl/netbox_librenms_plugin/navigation.py
@@ -0,0 +1,51 @@
+from netbox.choices import ButtonColorChoices
+from netbox.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
+
+menu = PluginMenu(
+    label="LibreNMS Plugin",  # This will be your main menu heading
+    icon_class="mdi mdi-network",
+    groups=(
+        (
+            "Settings",
+            (
+                PluginMenuItem(
+                    link="plugins:netbox_librenms_plugin:settings",
+                    link_text="Server Settings",
+                    permissions=["netbox_librenms_plugin.view_librenmssettings"],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_librenms_plugin:interfacetypemapping_list",
+                    link_text="Interface Mappings",
+                    buttons=(
+                        PluginMenuButton(
+                            link="plugins:netbox_librenms_plugin:interfacetypemapping_add",
+                            title="Add",
+                            icon_class="mdi mdi-plus-thick",
+                            color=ButtonColorChoices.GRAY,
+                        ),
+                    ),
+                ),
+            ),
+        ),
+        (
+            "Status Check",
+            (
+                PluginMenuItem(
+                    link="plugins:netbox_librenms_plugin:site_location_sync",
+                    link_text="Site & Location Sync",
+                    permissions=["dcim.view_site"],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_librenms_plugin:device_status_list",
+                    link_text="Device Status",
+                    permissions=["dcim.view_device"],
+                ),
+                PluginMenuItem(
+                    link="plugins:netbox_librenms_plugin:vm_status_list",
+                    link_text="VM Status",
+                    permissions=["virtualization.view_virtualmachine"],
+                ),
+            ),
+        ),
+    ),
+)
