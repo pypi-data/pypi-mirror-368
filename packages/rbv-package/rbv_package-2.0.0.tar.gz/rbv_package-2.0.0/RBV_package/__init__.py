28920454109968a50c143001324893d7c3159241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:29:32 +0000

    Add package rbv-package/2.0.0/rbv_package-2.0.0.tar.gz

---
+++ packages/rbv-package/rbv_package-2.0.0.tar.gz/rbv_package-2.0.0/RBV_package/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["rbv_environment", "archive", "config_package", "data_extraction", "dates"]
+
---
+++ packages/rbv-package/rbv_package-2.0.0.tar.gz/rbv_package-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rbv-package",
+    version="2.0.0",
+    packages=find_packages(),
+    description="Un package de fonction utiles pour la Vérification basée sur les risques",
+    author="Hubinont Jean-Philippe",
+    author_email="jphubinont@bluesquarehub.com",
+    install_requires=[],
+)
