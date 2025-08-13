ac01c5c45fe56c0b208dbde2d55dee066f4e61ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:29:14 +0000

    Add package lhcbdiracx-core/0.0.1a15/lhcbdiracx_core-0.0.1a15-py3-none-any.whl

---
+++ packages/lhcbdiracx-core/lhcbdiracx_core-0.0.1a15-py3-none-any.whl/lhcbdiracx/core/models.py
@@ -0,0 +1,16 @@
+from diracx.core.models import SearchSpec, SortSpec
+from pydantic import BaseModel
+
+
+class BKSummaryParams(BaseModel):
+    grouping: list[str]
+    search: list[SearchSpec] = []
+    # TODO: Add more validation
+
+
+class BKSearchParams(BaseModel):
+    parameters: list[str] | None = None
+    search: list[SearchSpec] = []
+    sort: list[SortSpec] = []
+    distinct: bool = False
+    # TODO: Add more validation
