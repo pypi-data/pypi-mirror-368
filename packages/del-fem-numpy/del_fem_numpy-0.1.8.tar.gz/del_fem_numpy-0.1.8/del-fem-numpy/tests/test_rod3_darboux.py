5f3728f6136524a2403d04cf718fa72f2b55677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:11:58 +0000

    Add package del-fem-numpy/0.1.8/del_fem_numpy-0.1.8.tar.gz

---
+++ packages/del-fem-numpy/del_fem_numpy-0.1.8.tar.gz/del_fem_numpy-0.1.8/del-fem-numpy/tests/test_rod3_darboux.py
@@ -0,0 +1,49 @@
+import numpy
+
+def test_01():
+    from del_msh_numpy import Polyline
+    vtx2xyz_ini = Polyline.vtx2xyz_from_helix(30, 0.2, 0.2, 0.5)
+    # print(vtx2xyz_ini.dtype)
+    from del_fem_numpy.Rod3Darboux import Simulator
+    simulator = Simulator(vtx2xyz_ini)
+    simulator.vtx2isfix[0][:] = 1
+    simulator.vtx2isfix[1][0:3] = 1
+    simulator.vtx2isfix[-1][:] = 1
+    simulator.vtx2isfix[-2][0:3] = 1
+    simulator.initialize_with_perturbation(0.3, 0.1)
+    assert (numpy.linalg.norm(simulator.vtx2framex_def, axis=1) - numpy.ones((simulator.vtx2framex_def.shape[0]),dtype=numpy.float32) ).max() < 1.0e-5
+    # print(simulator.vtx2xyz_ini, simulator.vtx2xyz_def)
+    for x in range(0,10):
+        simulator.compute_rod_deformation_energy_grad_hessian()
+        simulator.apply_fix_bc()
+        print(simulator.w)
+        # print(simulator.dw)
+        simulator.update_solution_static()
+    assert simulator.w < 1.0e-12
+
+
+def test_02():
+    from del_msh_numpy import Polyline
+    vtx2xyz_ini = Polyline.vtx2xyz_from_helix(30, 0.2, 0.2, 0.5)
+    i_vtx_pull = 15
+    pos_goal = numpy.array([1., -1.0, 0.2])
+    # print(vtx2xyz_ini.dtype)
+    from del_fem_numpy.Rod3Darboux import Simulator
+    simulator = Simulator(vtx2xyz_ini)
+    simulator.vtx2isfix[0][:] = 1
+    simulator.vtx2isfix[1][0:3] = 1
+    simulator.vtx2isfix[-1][:] = 1
+    simulator.vtx2isfix[-2][0:3] = 1
+    assert (numpy.linalg.norm(simulator.vtx2framex_def, axis=1) - numpy.ones((simulator.vtx2framex_def.shape[0]),dtype=numpy.float32) ).max() < 1.0e-5
+    # print(simulator.vtx2xyz_ini, simulator.vtx2xyz_def)
+    for x in range(0,10):
+        simulator.compute_rod_deformation_energy_grad_hessian()
+        simulator.pull_vertex(i_vtx_pull, pos_goal)
+        simulator.apply_fix_bc()
+        print(simulator.w)
+        # print(simulator.dw)
+        simulator.update_solution_static()
+    Polyline.save_wavefront_obj(simulator.vtx2xyz_def, "../target/darboux_rod.obj")
+
+
+
