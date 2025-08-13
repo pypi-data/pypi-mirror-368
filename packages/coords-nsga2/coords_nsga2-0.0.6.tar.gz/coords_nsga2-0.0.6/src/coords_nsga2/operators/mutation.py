f51d4435385249551523b34b4af997ad11aa8f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 05:07:29 +0000

    Add package coords-nsga2/0.0.6/coords_nsga2-0.0.6.tar.gz

---
+++ packages/coords-nsga2/coords_nsga2-0.0.6.tar.gz/coords_nsga2-0.0.6/src/coords_nsga2/operators/mutation.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from ..spatial import create_point_in_polygon
+
+
+def coords_mutation(population, prob_mut, polygons):
+    """Coordinate mutation operator that mutates individual coordinates within polygons.
+    
+    Args:
+        population: numpy array of shape (n_individuals, n_points, 2)
+        prob_mut: mutation probability for each coordinate
+        polygons: list of polygons defining valid regions
+    
+    Returns:
+        Mutated population array
+    """
+    # Generate mutation mask
+    mutation_mask = np.random.random(population.shape[:-1]) < prob_mut
+    
+    # Count mutations needed
+    n_mutations = np.sum(mutation_mask)
+    
+    if n_mutations > 0:
+        # Generate all new points at once
+        new_points = np.array([create_point_in_polygon(polygons) 
+                             for _ in range(n_mutations)])
+        
+        # Apply mutations using mask
+        population[mutation_mask] = new_points
+
+    return population
