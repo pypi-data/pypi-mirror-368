07bf9efb91d9ff57317731f2f604c8f3f6b7d3da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 03:29:30 +0000

    Add package aligned/0.0.125/aligned-0.0.125-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.125-py3-none-any.whl/aligned/sources/tests/test_random.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+import pytest
+
+from aligned import feature_view, String, Int16, List
+from aligned.sources.random_source import RandomDataSource
+from conftest import DataTest
+
+
+@pytest.mark.asyncio
+async def test_random_source(point_in_time_data_test: DataTest) -> None:
+    source = RandomDataSource()
+    view = point_in_time_data_test.sources[0]
+
+    request = view.view.compile().request_all.needed_requests[0]
+
+    limit = 100
+    df = await source.all_data(request, limit=limit).to_polars()
+
+    assert df.height == limit
+
+    column = view.data.columns[0]
+    await source.write_polars(view.data.select(column).lazy())
+    df = await source.all_data(request).to_polars()
+    assert df.select(column).equals(view.data.select(column))
+    assert len(df.columns) == len(request.all_returned_columns)
+
+
+@pytest.mark.asyncio
+async def test_random_derived_feature() -> None:
+    @feature_view(source=RandomDataSource())
+    class Features:
+        some_id = Int16().as_entity()
+        values = List(String())
+        contains_a = values.contains("a")
+
+        integer = Int16()
+
+        is_above_10 = integer > 10
+
+    query = Features.query()
+
+    random_values = await query.all().limit(10).to_polars()
+    assert random_values.height == 10
+
+    with_inject = await query.features_for(
+        {"values": [["A", "b"], ["a", "B"]], "some_id": [1, 2]}
+    ).to_polars()
+    assert with_inject[Features().contains_a.name].to_list() == [False, True]
