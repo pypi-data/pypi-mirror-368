c03356db8da5b62fa2b2ad7543bd1929c63b87a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-08-13 04:12:30 +0000

    Add package polars-hist-db/0.8.0/polars_hist_db-0.8.0.tar.gz

---
+++ packages/polars-hist-db/polars_hist_db-0.8.0.tar.gz/polars_hist_db-0.8.0/polars_hist_db/loaders/ingest_payload.py
@@ -0,0 +1,19 @@
+import json
+from nats.aio.msg import Msg
+
+import polars as pl
+
+from ..config.input.ingest_fn_registry import IngestFnRegistry
+from ..config.input.jetstream_config import JetstreamIngestConfig
+
+
+def load_df_from_msg(msg: Msg, ingest_config: JetstreamIngestConfig) -> pl.DataFrame:
+    data = json.loads(msg.data.decode())
+
+    fn_name = data.get("fn_loader_name", ingest_config.fn_name)
+    fn_args = data.get("fn_loader_args", ingest_config.fn_args)
+
+    fn_reg = IngestFnRegistry()
+    df = fn_reg.call_function(data, fn_name, fn_args)
+
+    return df
---
+++ packages/polars-hist-db/polars_hist_db-0.8.0.tar.gz/polars_hist_db-0.8.0/polars_hist_db/loaders/jetstream_input_source.py
@@ -0,0 +1,147 @@
+from datetime import datetime
+import logging
+from pathlib import Path
+from typing import (
+    Any,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    List,
+    Mapping,
+    Tuple,
+    Optional,
+)
+
+import nats
+
+import polars as pl
+from sqlalchemy import Connection, Engine
+
+from .ingest_payload import load_df_from_msg
+from .jetstream.nats_client import make_nats_client
+
+from ..config.dataset import DatasetConfig
+from ..config.input.jetstream_config import JetStreamInputConfig
+from ..config.table import TableConfigs
+from .input_source import InputSource
+from .transform import apply_transformations
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class JetStreamInputSource(InputSource[JetStreamInputConfig]):
+    def __init__(
+        self,
+        tables: TableConfigs,
+        dataset: DatasetConfig,
+        config: JetStreamInputConfig,
+    ):
+        super().__init__(tables, dataset, config)
+        self._nats_client: Optional[nats.NATS] = None
+
+    async def _get_nats_client(self) -> nats.NATS:
+        if self._nats_client is None:
+            nats_servers = self.config.nats.servers
+            options = self.config.nats.options or dict()
+            self._nats_client = await make_nats_client(nats_servers, options)
+        return self._nats_client
+
+    @staticmethod
+    def _validate_auth(auth_config: Mapping[str, Any]):
+        creds_file = auth_config.get("user_credentials", None)
+        if not creds_file:
+            LOGGER.info("No auth provided")
+        else:
+            creds_path = Path(creds_file).resolve(strict=True)
+            LOGGER.info(f"Validating auth from {creds_path}")
+            if not creds_path.exists():
+                raise FileNotFoundError(f"Missing creds file: {creds_file}")
+
+            file_size = creds_path.stat().st_size
+            if file_size < 512:
+                raise ValueError(f"Invalid credentials file: {creds_file}. Check file.")
+
+    async def cleanup(self) -> None:
+        if self._nats_client is not None:
+            await self._nats_client.close()
+            self._nats_client = None
+
+    async def next_df(
+        self, _engine: Engine
+    ) -> AsyncGenerator[
+        Tuple[
+            List[Tuple[datetime, pl.DataFrame]], Callable[[Connection], Awaitable[bool]]
+        ],
+        None,
+    ]:
+        async def _generator() -> AsyncGenerator[
+            Tuple[
+                List[Tuple[datetime, pl.DataFrame]],
+                Callable[[Connection], Awaitable[bool]],
+            ],
+            None,
+        ]:
+            nc = await self._get_nats_client()
+            js = nc.jetstream(**self.config.jetstream.context)
+            remaining_msgs = self.dataset.scrape_limit
+
+            js_sub_cfg = self.config.jetstream.subscription
+
+            sub = await js.pull_subscribe(
+                subject=js_sub_cfg.subject,
+                durable=js_sub_cfg.durable,
+                stream=js_sub_cfg.stream,
+                config=nats.js.api.ConsumerConfig(
+                    **js_sub_cfg.consumer_args,
+                ),
+                **js_sub_cfg.options,
+            )
+
+            total_msgs = 0
+
+            run_until = self.config.run_until
+            while (run_until == "empty" and remaining_msgs != 0) or (
+                run_until == "forever"
+            ):
+                try:
+                    msgs = await sub.fetch(
+                        self.config.jetstream.fetch.batch_size,
+                        self.config.jetstream.fetch.batch_timeout,
+                    )
+
+                    if len(msgs) == 0:
+                        continue
+
+                    total_msgs += len(msgs)
+
+                    all_dfs = [
+                        load_df_from_msg(msg, self.config.payload_ingest)
+                        for msg in msgs
+                    ]
+                    df = pl.concat(all_dfs)
+
+                    ts: datetime = msgs[-1].metadata.timestamp
+
+                    df = apply_transformations(df, self.column_definitions)
+                    partitions = self._apply_time_partitioning(df, ts)
+
+                    async def commit_fn(_: Connection) -> bool:
+                        for msg in msgs:
+                            await msg.ack()
+                        return True
+
+                    yield partitions, commit_fn
+
+                except TimeoutError:
+                    if run_until == "empty":
+                        LOGGER.info("No more messages, exiting...")
+                        break
+                    else:
+                        LOGGER.info(
+                            f"{js_sub_cfg.stream}: polling {self.config.jetstream.fetch.batch_timeout}s..."
+                        )
+
+            LOGGER.info("Processed %d msgs", total_msgs)
+
+        return _generator()
